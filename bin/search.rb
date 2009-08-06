944939a7eec2e31787efc551dc9b9a8ca53f39c4
Pete Fritchman <petef@databits.net>
2009-08-06 05:37:59 +0000

    - search by @DATE, by default

--- bin/search.rb
+++ bin/search.rb
@@ -13,6 +13,6 @@ qp = QueryParser.new(:fields => reader.fields,
                      :tokenized_fields => reader.tokenized_fields,
                      :or_default => false)
 query = qp.parse(ARGV[1])
-search.search_each(query, :limit => :all) do |id, score|
+search.search_each(query, :limit => :all, :sort => "@DATE") do |id, score|
   puts "#{reader[id][:@SOURCE_HOST]} | #{reader[id][:@LOG_NAME]} | #{reader[id][:@LINE]}"
 end
