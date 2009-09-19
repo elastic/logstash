0c87bf740ffc83eee0f01266f14005e85fe2be8f
Jordan Sissel <jls@semicomplete.com>
2009-09-19 08:40:00 +0000

    - set / route to /search

--- web/config/router.rb
+++ web/config/router.rb
@@ -30,6 +30,8 @@ Merb::Router.prepare do
   # RESTful routes
   # resources :posts
   
+  match('/').to(:controller => 'search', :action => 'index')
+
   # Adds the required routes for merb-auth using the password slice
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
 
