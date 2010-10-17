9f3114c8836ccf80af4495c656ef219c2573b71c
Jordan Sissel <jls@semicomplete.com>
2010-10-17 10:31:26 +0000

    - Purge old web ui

--- web/app/helpers/global_helpers.rb
+++
@@ -1,5 +0,0 @@
-module Merb
-  module GlobalHelpers
-    # helpers defined here available to all views.  
-  end
-end
--- web/spec/requests/search_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
-
-describe "/search" do
-  before(:each) do
-    @response = request("/search")
-  end
-end
