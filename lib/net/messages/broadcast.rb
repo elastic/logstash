4684f6599544ba8b5b7593ef8c141eb9ff855406
Jordan Sissel <jls@semicomplete.com>
2010-10-17 10:30:22 +0000

    - Purge post-refactor. The older stuff can be found in /tags/pre-agent-refactor

--- lib/net/messages/broadcast.rb
+++
@@ -1,7 +0,0 @@
-require "mqrpc"
-
-module LogStash; module Net; module Messages
-  class BroadcastMessage < MQRPC::Message
-    argument :queue
-  end # class BroadcastRequest
-end; end; end # module LogStash::Net::Messages
