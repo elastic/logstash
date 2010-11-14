4d9d91335c305f5f1aaebd9ed1ddb61e11276025
Jordan Sissel <jls@semicomplete.com>
2010-11-14 11:48:04 +0000

    - Set result counts/etc for the template

--- web/server.rb
+++ web/server.rb
@@ -43,11 +43,23 @@ class LogStashWeb < Sinatra::Base
 
   apost '/search/ajax' do
     headers({"Content-Type" => "text/html" })
+    params[:count] = (params[:count] or 50).to_i
+    params[:offset] = (params[:offset] or 0).to_i
     elasticsearch.search(params) do |@results|
       @hits = (@results["hits"]["hits"] rescue [])
+      @total = (@results["hits"]["total"] rescue 0)
       @graphpoints = []
       @results["facets"]["by_hour"]["entries"].each do |entry|
         @graphpoints << [entry["key"], entry["count"]]
+      end
+
+      if params[:count] and params[:offset]
+        if @total > (params[:count] + params[:offset])
+          @result_end = params[:count] + params[:offset]
+        else 
+          @result_end = @total
+        end
+        @result_start = params[:offset]
       end
       body haml :"search/ajax", :layout => !request.xhr?
     end
