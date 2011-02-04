0dac4260f5394fd4269eca449fc0e48844d896f1
Jordan Sissel <jls@semicomplete.com>
2011-02-04 02:30:22 -0800

    - Got working elasticsearch tests going - Allow multiple versions of elasticsearch to be tested with the same   tests.

--- test/logstash/outputs/test_elasticsearch.rb
+++ test/logstash/outputs/test_elasticsearch.rb
@@ -5,58 +5,59 @@ $:.unshift File.dirname(__FILE__) + "/../../"
 require "logstash/testcase"
 require "logstash/agent"
 require "logstash/logging"
+require "logstash/web/lib/elasticsearch"
+
+# For checking elasticsearch health
+require "net/http"
+require "uri"
+require "json"
 
 class TestOutputElasticSearch < LogStash::TestCase
-  def em_setup_try
-    version = "0.14.4"
+  ELASTICSEARCH_VERSION = "0.14.4"
 
-    # Block for initial setup
-    system("make -C ../../setup/elasticsearch/ init-elasticsearch-#{version} wipe-elasticsearch-#{version}")
-    EventMachine::popen("make -C ../../setup/elasticsearch/ run-elasticsearch-#{version}")
-    #do |output, status|
-      #puts output.split("\n").map { |a| "elasticsearch> #{a}" }.join("\n")
-      #puts "ElasticSearch exited with code '#{status}'"
-    #end
+  def start_elasticsearch
+    # install
+    version = self.class::ELASTICSEARCH_VERSION
+    system("make -C #{File.dirname(__FILE__)}/../../setup/elasticsearch/ init-elasticsearch-#{version} wipe-elasticsearch-#{version} #{$DEBUG ? "" : "> /dev/null 2>&1"}")
 
-    tries = 10
-    es_setup = proc do
-      tries -= 1
+    # TODO(sissel): Make sure port 9200 is unused?
+    teardown if @es_pid
+    @es_pid = Process.fork do
+      Process.setsid
+      puts "Starting ElasticSearch #{version}"
+      if !$DEBUG
+        $stdout.reopen("/dev/null", "w")
+        $stderr.reopen("/dev/null", "w")
+        $stdin.reopen("/dev/null", "r")
+      end
+      exec("make", "-C", "#{File.dirname(__FILE__)}/../../setup/elasticsearch/", "run-elasticsearch-#{version}")
+      $stderr.puts "Something went wrong starting up elasticsearch?"
+      exit 1
+    end
+
+    # Wait for elasticsearch to be ready.
+    1.upto(30) do
       begin
-        # TODO(sissel): This will never actually fire an exception if ES isn't up.
-        # because output/elasticsearch does things async, the exception
-        # is tossed up much later.
-        #
-        # We'll need a way to attach exception handlers to input/output/filter/agent
-        # in order to make them better testable?
-        # Also, an agent "ready" callback would make this kind of setup easier
-        # to manage and could also be used in non-testing for sending a "Go" signal
-        # if we wanted such a thing.
-        #
-        em_setup
-        puts "ElasticSearch is ready"
+        Net::HTTP.get(URI.parse("http://localhost:9200/_status"))
+        puts "ElasticSearch is ready..."
         return
       rescue => e
-        # Abort if we are out of tries.
-        if tries <= 0
-          puts "NO MORE TRIES LEFT. ABORTING"
-          raise e 
-        end
-
-        # Try again in a few seconds otherwise
-        puts "Waiting for elasticsearch to be ready... (#{e.inspect})"
-        EventMachine.add_timer(3) do
-          es_setup.call
-        end
+        puts "ElasticSearch not yet ready... sleeping."
+        sleep 2
       end
-
-    end # proc es_setup
-
-    EventMachine.next_tick do
-      es_setup.call
     end
-  end # def em_setup_try
+
+    raise "ElasticSearch failed to start or was otherwise not running properly?"
+  end
+
+  def teardown
+    # Kill the whole process group for elasticsearch
+    Process.kill("KILL", -1 * @es_pid) if !@es_pid.nil?
+  end
 
   def em_setup
+    start_elasticsearch
+
     config = {
       "inputs" => {
         @type => [
@@ -73,17 +74,55 @@ class TestOutputElasticSearch < LogStash::TestCase
 
   def test_elasticsearch_basic
     EventMachine::run do
-      em_setup_try
+      em_setup
 
-      EventMachine.add_timer(20) do
-        EventMachine.stop_event_loop
+      events = []
+      1.upto(5).each do |i|
+        events << LogStash::Event.new("@message" => "just another log rollin' #{i}",
+                                      "@source" => "logstash tests in #{__FILE__}")
       end
-    end
-  end
 
-  def teardown
-    if @stomp_pid
-      Process.kill("KILL", @stomp_pid)
-    end
-  end # def teardown
+      # TODO(sissel): Need a way to hook when the agent is ready?
+      EventMachine.next_tick do
+        events.each do |e|
+          @input.push e
+        end
+      end # next_tick, push our events
+
+      tries = 30 
+      EventMachine.add_periodic_timer(0.2) do
+        es = LogStash::Web::ElasticSearch.new
+        es.search(:q => "*", :count => 5, :offset => 0) do |results|
+          hits = (results["hits"]["hits"] rescue [])
+          if events.size == hits.size
+            puts "Found #{hits.size} events, ready to verify!"
+            expected = events.clone
+            assert_equal(events.size, hits.size)
+            hits.each do |hit|
+              event = LogStash::Event.new(hit["_source"])
+              events.each { |e| p :expect => e }
+              #p :got => event
+              assert(expected.include?(event), "Found event in results that was not expected: #{event.inspect}")
+            end
+            EventMachine.stop_event_loop
+            next # break out
+          else
+            tries -= 1
+            if tries <= 0
+              assert(false, "Gave up trying to query elasticsearch. Maybe we aren't indexing properly?")
+              EventMachine.stop_event_loop
+            end
+          end # if events.size == hits.size
+        end # es.search
+      end # add_periodic_timer(0.2) / query elasticsearch
+    end # EventMachine::run
+  end # def test_elasticsearch_basic
 end # class TestOutputElasticSearch
+
+class TestOutputElasticSearch0_13_1 < TestOutputElasticSearch
+  ELASTICSEARCH_VERSION = self.name[/[0-9_]+/].gsub("_", ".")
+end # class TestOutputElasticSearch0_13_1
+
+class TestOutputElasticSearch0_12_0 < TestOutputElasticSearch
+  ELASTICSEARCH_VERSION = self.name[/[0-9_]+/].gsub("_", ".")
+end # class TestOutputElasticSearch0_12_0
