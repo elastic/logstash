08abcbea9dd8bd8764db0b0ebbd1880af188ee46
Jordan Sissel <jls@semicomplete.com>
2011-02-21 19:12:23 -0800

    - style cleanup - Add MultiQueue#<< alias to #push

--- lib/logstash/multiqueue.rb
+++ lib/logstash/multiqueue.rb
@@ -1,13 +1,19 @@
 class MultiQueue
+  public
   def initialize(*queues)
     @mutex = Mutex.new
     @queues = queues
   end
 
+  # Push an object to all queues.
+  public
   def push(object)
     @queues.each { |q| q.push(object) }
   end
 
+  alias_method :<<, :push
+
+  # Add a new Queue to this queue.
   public
   def add_queue(queue)
     @mutex.synchronize do
