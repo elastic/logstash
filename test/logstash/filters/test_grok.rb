03a8a37bdb4365ff286f486f8ccf4f9f7dfac1b0
Jordan Sissel <jls@semicomplete.com>
2011-02-27 03:10:42 -0800

    - Add speed test for grok filter

--- test/logstash/filters/test_grok.rb
+++ test/logstash/filters/test_grok.rb
@@ -55,4 +55,35 @@ class TestFilterGrok < Test::Unit::TestCase
 
     assert_equal(event.fields["pid"], [pid], "Expected field 'pid' to be ['#{pid.inspect}'], is #{event.fields["pid"].inspect}")
   end # def test_grok_normal
+
+  def test_speed
+    test_name "groknormal"
+    config "pattern" => [ "%{SYSLOGLINE}" ]
+
+    iterations = 5000
+
+    start = Time.now
+
+    event = LogStash::Event.new
+    event.type = @typename
+
+    logsource = "evita"
+    timestamp = "Mar 16 00:01:25"
+    message = "connect from camomile.cloud9.net[168.100.1.3]"
+    program = "postfix/smtpd"
+    pid = "1713"
+
+    event.message = "#{timestamp} #{logsource} #{program}[#{pid}]: #{message}"
+
+    check_interval = 1500
+    1.upto(iterations).each do |i|
+      event.fields.clear
+      @filter.filter(event)
+    end
+
+    duration = Time.now - start
+    max_duration = 10
+    puts "filters/grok speed test; #{iterations} iterations: #{duration} seconds (#{iterations / duration} per sec)"
+    assert(duration < max_duration, "Should be able to do #{iterations} grok parses in less than #{max_duration} seconds, got #{duration} seconds")
+  end # test_formats
 end
