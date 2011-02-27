6f1d4796763ef4f4fc2601a0bfdbd0f8c87df01e
Jordan Sissel <jls@semicomplete.com>
2011-02-26 20:32:07 -0800

    - filters/date unit tests pass again. LOGSTASH-13

--- test/logstash/filters/test_date.rb
+++ test/logstash/filters/test_date.rb
@@ -4,20 +4,30 @@ $:.unshift File.dirname(__FILE__) + "/../../"
 
 require "test/unit"
 require "logstash"
+require "logstash/loadlibs"
 require "logstash/filters"
+require "logstash/filters/date"
 require "logstash/event"
 
+ENV["TZ"] = "PST8PDT"
+$tz = Time.now.strftime("%z")
+$TZ = $tz[0..2] + ":" + $tz[3..-1]
+
 class TestFilterDate < Test::Unit::TestCase
-  def setup
-    @filter = LogStash::Filters.from_name("date", {})
-  end
 
   def test_name(name)
-    @typename = name
+    @typename = name.gsub(/[ ]/, "_")
   end
 
   def config(cfg)
-    @filter.add_config(@typename, cfg)
+    cfg["type"] = @typename
+    cfg.each_key do |key|
+      if cfg[key].is_a?(String)
+        cfg[key] = cfg[key].to_a
+      end
+    end
+
+    @filter = LogStash::Filters::Date.new(cfg)
     @filter.register
   end
 
@@ -26,20 +36,22 @@ class TestFilterDate < Test::Unit::TestCase
     config "field1" => "ISO8601"
 
     times = {
-      "2001-01-01T00:00:00"               => "2001-01-01T00:00:00.000000Z",
-      "1974-03-02T04:09:09"               => "1974-03-02T04:09:09.000000Z",
-      "2010-05-03T08:18:18+00:00"         => "2010-05-03T08:18:18.000000Z",
-      "2004-07-04T12:27:27-00:00"         => "2004-07-04T12:27:27.000000Z",
-      "2001-09-05T16:36:36+0000"          => "2001-09-05T16:36:36.000000Z",
-      "2001-11-06T20:45:45-0000"          => "2001-11-06T20:45:45.000000Z",
-      "2001-12-07T23:54:54Z"              => "2001-12-07T23:54:54.000000Z",
-      "2001-01-01T00:00:00.123456"        => "2001-01-01T00:00:00.123456Z",
-      "1974-03-02T04:09:09.123456"        => "1974-03-02T04:09:09.123456Z",
-      "2010-05-03T08:18:18.123456+00:00"  => "2010-05-03T08:18:18.123456Z",
-      "2004-07-04T12:27:27.123456-04:00"  => "2004-07-04T12:27:27.123456-0400",
-      "2001-09-05T16:36:36.123456+0700"   => "2001-09-05T16:36:36.123456+0700",
-      "2001-11-06T20:45:45.123456-0000"   => "2001-11-06T20:45:45.123456Z",
-      "2001-12-07T23:54:54.123456Z"       => "2001-12-07T23:54:54.123456Z",
+      "2001-01-01T00:00:00#{$TZ}"        => "2001-01-01T08:00:00.000Z",
+      "1974-03-02T04:09:09#{$TZ}"        => "1974-03-02T12:09:09.000Z",
+      "2010-05-03T08:18:18+00:00"        => "2010-05-03T08:18:18.000Z",
+      "2004-07-04T12:27:27-00:00"        => "2004-07-04T12:27:27.000Z",
+      "2001-09-05T16:36:36+0000"         => "2001-09-05T16:36:36.000Z",
+      "2001-11-06T20:45:45-0000"         => "2001-11-06T20:45:45.000Z",
+      "2001-12-07T23:54:54Z"             => "2001-12-07T23:54:54.000Z",
+      "2001-01-01T00:00:00.123"          => "2001-01-01T08:00:00.123Z",
+
+      # older daylights savings?
+      "1974-03-02T04:09:09.123"          => "1974-03-02T11:09:09.123Z",
+      "2010-05-03T08:18:18.123+00:00"    => "2010-05-03T08:18:18.123Z",
+      "2004-07-04T12:27:27.123-04:00"    => "2004-07-04T16:27:27.123Z",
+      "2001-09-05T16:36:36.123+0700"     => "2001-09-05T09:36:36.123Z",
+      "2001-11-06T20:45:45.123-0000"     => "2001-11-06T20:45:45.123Z",
+      "2001-12-07T23:54:54.123Z"         => "2001-12-07T23:54:54.123Z",
     }
     
     event = LogStash::Event.new
@@ -47,19 +59,21 @@ class TestFilterDate < Test::Unit::TestCase
     times.each do |input, output|
       event.fields["field1"] = input
       @filter.filter(event)
-      assert_equal(output, event.timestamp)
+      assert_equal(output, event.timestamp,
+                   "Time '#{input}' should parse to '#{output}' but got '#{event.timestamp}'")
     end
   end # def test_iso8601
 
   def test_formats
     test_name "format test"
-    config "field1" => "%b %e %H:%M:%S"
+    #config "field1" => "%b %e %H:%M:%S"
+    config "field1" => "MMM dd HH:mm:ss"
 
     now = Time.now
     now += now.gmt_offset
     year = now.year
     times = {
-      "Nov 24 01:29:01" => "#{year}-11-24T01:29:01.000000Z",
+      "Nov 24 01:29:01" => "#{year}-11-24T09:29:01.000Z",
     }
 
     event = LogStash::Event.new
