b3e19211699236bfac51d3fbcec806614685b523
Jordan Sissel <jls@semicomplete.com>
2011-04-04 01:02:04 -0700

    - set ruby thread name in addition to java thread name

--- lib/logstash/util.rb
+++ lib/logstash/util.rb
@@ -2,6 +2,8 @@ require "logstash/namespace"
 
 module LogStash::Util
   def self.set_thread_name(name)
+    # Keep java and ruby thread names in sync.
     java.lang.Thread.currentThread.setName(name)
+    Thread.current[:name] = name
   end # def set_thread_name
 end # module LogStash::Util
