b7ea986cedf3a9f4978c6c0320c05433dd4829dd
Jordan Sissel <jls@semicomplete.com>
2011-04-06 01:07:05 -0700

    - start converting the twitter input to use tweetstream gem

--- lib/logstash/inputs/twitter.rb
+++ lib/logstash/inputs/twitter.rb
@@ -1,85 +1,59 @@
-require "cgi"
-require "em-http-request"
 require "logstash/inputs/base"
 require "logstash/namespace"
+require "tweetstream" # rubygem 'tweetstream'
 
 # TODO(sissel): This could use some refactoring.
 class LogStash::Inputs::Twitter < LogStash::Inputs::Base
 
   config_name "twitter"
-  config :query, :validate => :string, :required => true
+  config :user, :validate => :string, :required => true
+  config :password, :validate => :password, :required => true
+  config :debug, :validate => :boolean
+
+  config :keywords, :validate => :array, :required => true
 
   public
+  def initialize(params)
+    super
+    @debug ||= false
+  end # def initialize
+
   def register
-    api_url = "https://stream.twitter.com/1/statuses/filter.json"
-    @http = EventMachine::HttpRequest.new(api_url)
-    @logger.info(["Registering input", { :url => @url, :api_url => api_url, :params => @urlopts }])
-    source = "twitter://...#{@url.path}?#{@url.query}"
+    # nothing to do
+  end
 
-    if @url.user.nil? or @user.password.nil?
-      message = "User and password missing for twitter input #{@url.to_s}"
-      @logger.error(message)
-      raise message
-    end
+  public
+  def run(queue)
+    loop do
+      stream = TweetStream::Client.new(@user, @password.value)
+      stream.track(@keywords) do |status|
+        @logger.debug("Got twitter status from @#{status["user"]["source"]}")
+        event = LogStash::Event.new(
+          "@message" => status["text"],
+          "@type" => @type,
+          "@tags" => @tags.clone
+        )
 
-    req = nil
-    connect = proc do
-      @logger.info(["Connecting", { :url => @url, :api_url => api_url, :params => @urlopts}])
-      req = @http.post :body => @urlopts,
-                       :head => { "Authorization" => [ @url.user, @url.password ] }
-      buffer = BufferedTokenizer.new
+        event.fields.merge!(
+          "user" => (status["user"]["screen_name"] rescue nil), 
+          "client" => (status["user"]["source"] rescue nil),
+          "retweeted" => (status["retweeted"] rescue nil)
+        )
 
-      req.stream do |chunk|
-        buffer.extract(chunk).each do |line|
-          begin
-            tweet = JSON.parse(line)
-          rescue JSON::ParserError => e
-            @logger.warn("Invalid JSON, aborting connection: #{line}")
-            req.errback
-            next
-          end
-          next if !tweet
+        event.fields["in-reply-to"] = status["in_reply_to_status_id"] if status["in_reply_to_status_id"]
 
-          event = LogStash::Event.new({
-            "@message" => tweet["text"],
-            "@type" => @type,
-            "@tags" => @tags.clone,
-          })
-
-          event.fields.merge!(
-            "user" => (tweet["user"]["screen_name"] rescue nil),
-            "client" => (tweet["user"]["source"] rescue nil),
-            "retweeted" => (tweet["retweeted"] rescue nil)
-          )
-
-          event.fields["in-reply-to"] = tweet["in_reply_to_status_id"] if tweet["in_reply_to_status_id"]
-
-          urls = tweet["entities"]["urls"] rescue []
-          if urls.size > 0
-            event.fields["urls"] = urls.collect { |u| u["url"] }
-          end
-
-          event.source = source
-          @logger.debug(["Got event", event])
-          @callback.call(event)
-        end # buffer.extract
-      end # req.stream
-
-      req.errback do
-        @logger.warn(["Error occurred, not sure what, seriously. Reconnecting!", { :url => @url }])
-
-        req.close_connection() rescue nil
-
-        EventMachine::Timer.new(60) do
-          connect.call
+        urls = status["entities"]["urls"] rescue []
+        if urls.size > 0
+          event.fields["urls"] = urls.collect { |u| u["url"] }
         end
-      end # req.errback
 
-      req.callback do
-        @logger.warn(["Request completed. Unexpected!", { :url => @url }])
-      end
-    end # connect = proc do
+        event.source = source
+        @logger.debug(["Got event", event])
+        queue << event
+      end # stream.track
 
-    connect.call
-  end # def register
+      # Some closure or error occured, sleep and try again.
+      sleep 30
+    end
+  end
 end # class LogStash::Inputs::Twitter
