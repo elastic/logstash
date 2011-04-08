c48fe7991fd26426b6cc7c12e60cb75d42792463
Jordan Sissel <jls@semicomplete.com>
2011-04-07 18:42:01 -0700

    - make the twitter input work again

--- lib/logstash/inputs/twitter.rb
+++ lib/logstash/inputs/twitter.rb
@@ -5,9 +5,14 @@ require "tweetstream" # rubygem 'tweetstream'
 class LogStash::Inputs::Twitter < LogStash::Inputs::Base
 
   config_name "twitter"
+  
+  # Your twitter username
   config :user, :validate => :string, :required => true
+
+  # Your twitter password
   config :password, :validate => :password, :required => true
 
+  # Any keywords to track in the twitter stream
   config :keywords, :validate => :array, :required => true
 
   def register
@@ -18,23 +23,24 @@ class LogStash::Inputs::Twitter < LogStash::Inputs::Base
   def run(queue)
     loop do
       stream = TweetStream::Client.new(@user, @password.value)
-      stream.track(@keywords) do |status|
-        @logger.debug("Got twitter status from @#{status["user"]["source"]}")
+      stream.track(*@keywords) do |status|
+        ap :status => status
+        @logger.debug("Got twitter status from @#{status[:user][:screen_name]}")
         event = LogStash::Event.new(
-          "@message" => status["text"],
+          "@message" => status[:text],
           "@type" => @type,
           "@tags" => @tags.clone
         )
 
         event.fields.merge!(
-          "user" => (status["user"]["screen_name"] rescue nil), 
-          "client" => (status["user"]["source"] rescue nil),
-          "retweeted" => (status["retweeted"] rescue nil)
+          "user" => (status[:user][:screen_name] rescue nil), 
+          "client" => (status[:source] rescue nil),
+          "retweeted" => (status[:retweeted] rescue nil)
         )
 
-        event.fields["in-reply-to"] = status["in_reply_to_status_id"] if status["in_reply_to_status_id"]
+        event.fields["in-reply-to"] = status[:in_reply_to_status_id] if status[:in_reply_to_status_id]
 
-        urls = status["entities"]["urls"] rescue []
+        urls = status[:entities][:urls] rescue []
         if urls.size > 0
           event.fields["urls"] = urls.collect { |u| u["url"] }
         end
