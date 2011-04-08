db46c2dd50ee7dcdbce0e6f4c62086e75f0637cf
Jordan Sissel <jls@semicomplete.com>
2011-04-07 19:08:26 -0700

    - add runner

---
+++ lib/logstash/runner.rb
@@ -0,0 +1,14 @@
+
+command = ARGV.shift
+
+case command
+when "agent"
+  require "logstash/agent"
+  agent = LogStash::Agent.new
+  agent.argv = ARGV
+  agent.run
+when "web"
+  puts "not supported yet"
+when "test"
+  puts "not supported yet"
+end
