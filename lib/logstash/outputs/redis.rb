2ca236b4d7c606ff4595a0a0e5f87c4ae23bbdb3
Bob Corsaro <bob@embed.ly>
2011-05-09 10:03:20 -0400

    Error handling and cleanup

--- lib/logstash/outputs/redis.rb
+++ lib/logstash/outputs/redis.rb
@@ -1,46 +1,59 @@
 require "logstash/outputs/base"
 require "logstash/namespace"
-require 'redis'
+require 'eventmachine'
 
 class LogStash::Outputs::Redis < LogStash::Outputs::Base
 
   config_name "redis"
+  
+  # name is used for logging in case there are multiple instances
+  config :name, :validate => :string, :default => 'default'
 
-  config :host, :validate => :string, :default => 'localhost'
+  config :host, :validate => :string
 
   config :port, :validate => :number
 
   config :db, :validate => :number
 
-  config :password, :validate => :string
+  config :timeout, :validate => :number
+
+  config :password, :validate => :password
 
   config :queue, :validate => :string, :required => true
 
   def register
-    require 'socket'
-    @hostname = Socket.gethostname
-
-    opts = {}
-    %w{host port db password}.each do |name|
-      val = instance_variable_get("@#{name}")
-      opts[name.to_sym] = val if val
-    end
-    @logger.info opts
-    @redis = Redis.new(opts)
+    @redis = nil
   end
 
-  def receive(event)
-    o = {
-      :source_host => @hostname,
-      :source => event.source,
-      :message => event.message,
-      :fields => event.fields,
-      :timestamp => event.timestamp,
-      :tags => event.tags,
-      :type => event.type
-    }
-    @logger.debug o
+  def connect
+    require 'redis'
+    Redis.new(
+      :host => @host,
+      :port => @port,
+      :timeout => @timeout,
+      :db => @db,
+      :password => @password
+    )
+  end
 
-    @redis.rpush(event.sprintf(@queue), o.to_json)
+  def receive event, tries=5
+    if tries > 0
+      begin
+        @redis ||= connect
+        @redis.rpush event.sprintf(@queue), event.to_json
+      rescue
+        @redis = nil
+        @logger.warn "Failed to log #{event.to_s} to redis #{@name}. "+
+                     "Will retry #{tries} times."
+        @logger.warn $!
+        Thread.new do
+          sleep 1
+          receive event, tries - 1
+        end
+      end
+    else
+      @logger.error "Fatal error, failed to log #{event.to_s} to redis #{@name}"
+      raise RuntimeError.new "Failed to log to redis #{@name}"
+    end
   end
 end
