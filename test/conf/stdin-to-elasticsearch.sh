0753c65331f076a46e79599f245b0df7ea2b6691
Jordan Sissel <jls@semicomplete.com>
2011-07-01 17:14:30 -0700

    - Add simpler runner scripts for testing configs. Uses new agent '-e'   flag

---
+++ test/conf/stdin-to-elasticsearch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 <command>"
+
+  echo "Example: $0 java -jar logstash-1.0.12.jar"
+  exit 1
+fi
+
+output='output { elasticsearch { embedded => true } }'
+"$@" agent -e "$output"
