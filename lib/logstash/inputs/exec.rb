08d4b03b31950c93b9a3a9e89628d25909d17f4c
Jordan Sissel <jls@semicomplete.com>
2011-08-16 23:25:42 -0700

    - clean up some style/naming - change period to interval

--- lib/logstash/inputs/exec.rb
+++ lib/logstash/inputs/exec.rb
@@ -1,9 +1,16 @@
 require "logstash/inputs/base"
 require "logstash/namespace"
 
-# Sample STDOUT from commands (ex. vmstat)
+# Run command line tools and cature output as an event.
 #
-
+# Notes:
+#
+# * The '@source' of this event will be the command run.
+# * The '@message' of this event will be the entire stdout of the command
+#   as one event.
+#
+# TODO(sissel): Implement a 'split' filter so you can split output of this
+# and other messages by newline, etc.
 class LogStash::Inputs::Exec < LogStash::Inputs::Base
 
   config_name "exec"
@@ -11,11 +18,11 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
   # Set this to true to enable debugging on an input.
   config :debug, :validate => :boolean, :default => false
   
-  # Command to run 
-  config :exec, :validate => :string, :required => true
+  # Command to run. For example, "uptime"
+  config :command, :validate => :string, :required => true
   
-  # Period to run the command
-  config :period, :validate => :number, :required => true
+  # Interval to run the command. Value is in seconds.
+  config :interval, :validate => :number, :required => true
   
   public
   def register
@@ -24,11 +31,26 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
 
   public
   def run(queue)
-    while(1)
-      out = IO.popen(@exec)
-      e = to_event(out.read, @exec)
+    loop do
+      start = Time.now
+      @logger.info("Running: #{@command}") if @debug
+      out = IO.popen(@command)
+      # out.read will block until the process finishes.
+      e = to_event(out.read, @command)
       queue << e
-      sleep @period
-    end
-  end
-end
+      duration = Time.now - start
+      @logger.info("Command '#{@command}' took #{duration} seconds") if @debug
+
+      # Sleep for the remainder of the interval, or 0 if the duration ran
+      # longer than the interval.
+      sleeptime = [0, @interval - duration].max
+      if sleeptime == 0
+        @logger.warn("Execution of '#{@command}' ran longer than the interval."
+                     " Took #{duration} seconds, but interval is #{@interval}."
+                     " Not sleeping...")
+      else
+        sleep(sleeptime)
+      end
+    end # loop
+  end # def run
+end # class LogStash::Inputs::Exec
