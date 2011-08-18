0a18b460039e792dd6a4dc5fce050a19f8606ef0
Jordan Sissel <jls@semicomplete.com>
2011-08-17 19:32:50 -0700

    - every other plugin calls it a user, so just say user.

--- lib/logstash/outputs/xmpp.rb
+++ lib/logstash/outputs/xmpp.rb
@@ -7,7 +7,7 @@ class LogStash::Outputs::Xmpp < LogStash::Outputs::Base
   config_name "xmpp"
 
   # The user or resource ID, like foo@example.com.
-  config :identity, :validate => :string, :required => :true
+  config :user, :validate => :string, :required => :true
 
   # The xmpp password for the JID.
   config :password, :validate => :password, :required => :true
@@ -31,7 +31,7 @@ class LogStash::Outputs::Xmpp < LogStash::Outputs::Base
   public
   def connect
     Jabber::debug = true
-    client = Jabber::Client.new(Jabber::JID.new(@identity))
+    client = Jabber::Client.new(Jabber::JID.new(@user))
     client.connect(@host)
     client.auth(@password.value)
     return client
