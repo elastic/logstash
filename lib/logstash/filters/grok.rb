elastic/logstash

commit 6a69dcf231a7ec53b47786890952df1e758e2c4b

- undo bad decision

lib/logstash/filters/grok.rb
@@ -234,6 +234,7 @@ class LogStash::Filters::Grok < LogStash::Filters::Base
     end
 
     @logger.debug(["Event now: ", event.to_hash])
+    filter_matched(event) if !event.cancelled?
   end # def filter
 
   private

lib/logstash/filters/base.rb
@@ -54,7 +54,7 @@ class LogStash::Filters::Base < LogStash::Plugin
   end # def register
 
   public
-  def filter(event)
+  def filter(event, input_queue)
     raise "#{self.class}#filter must be overidden"
   end # def filter
 

lib/logstash/filters/date.rb
@@ -176,8 +176,7 @@ class LogStash::Filters::Date < LogStash::Filters::Base
       end # fieldvalue.each 
     end # @parsers.each
 
-    if !event.cancelled?
-      filter_matched(event)
-    end
+    filter_matched(event) if !event.cancelled?
+    return event
   end # def filter
 end # class LogStash::Filters::Date

lib/logstash/filters/grep.rb
@@ -100,8 +100,6 @@ class LogStash::Filters::Grep < LogStash::Filters::Base
 
     @logger.debug(["Event after grep filter", event.to_hash])
 
-    if !event.cancelled?
-      filter_matched(event)
-    end
+    filter_matched(event) if !event.cancelled?
   end # def filter
 end # class LogStash::Filters::Grep

lib/logstash/filters/grokdiscovery.rb
@@ -65,5 +65,7 @@ class LogStash::Filters::Grokdiscovery < LogStash::Filters::Base
       @logger.debug(event.to_hash)
     end
     @logger.debug(["Event now: ", event.to_hash])
+
+    filter_matched(event) if !event.cancelled?
   end # def filter
 end # class LogStash::Filters::Grokdiscovery

lib/logstash/filters/multiline.rb
@@ -158,6 +158,7 @@ class LogStash::Filters::Multiline < LogStash::Filters::Base
     if !event.cancelled?
       filter_matched(event)
     end
+    filter_matched(event) if !event.cancelled?
   end # def filter
 
   # flush any pending messages

commit 59c7983623ebc030c995ffcc18157c9d03e54c39

- Fix grok add_field bug introduced in commit 6a69dcf2 (Fixes LOGSTASH-147)

lib/logstash/filters/grok.rb
@@ -235,7 +235,6 @@ class LogStash::Filters::Grok < LogStash::Filters::Base
     end
 
     @logger.debug(["Event now: ", event.to_hash])
-    filter_matched(event) if !event.cancelled?
   end # def filter
 
   private