715de0642462f24c80063eaad7a409a1f884285b
Jordan Sissel <jls@semicomplete.com>
2011-11-09 14:57:28 -0800

    - Add null output. Not sure if it'll get used, but it could be useful   for helping benchmark or debug a filter or input when output isn't   necessary.

---
+++ lib/logstash/outputs/null.rb
@@ -0,0 +1,13 @@
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+# A null output. This is useful for testing logstash inputs and filters for
+# performance.
+class LogStash::Outputs::Null < LogStash::Outputs::Base
+  config_name "null"
+
+  public
+  def receive(event)
+    # Do nothing
+  end # def event
+end # class LogStash::Outputs::Null
