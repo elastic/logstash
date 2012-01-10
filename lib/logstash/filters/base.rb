59caf137bd31e55e5466f42dd3306f0886c0eb6f
John E. Vincent <lusis.org+github.com@gmail.com>
2012-01-10 00:32:41 -0500

    working plugin_status support

--- lib/logstash/filters/base.rb
+++ lib/logstash/filters/base.rb
@@ -8,7 +8,7 @@ class LogStash::Filters::Base < LogStash::Plugin
 
   config_name "filter"
 
-  plugin_status "unstable"
+  plugin_status nil
 
   # The type to act on. If a type is given, then this filter will only
   # act on messages with the same type. See any input plugin's "type"
--- lib/logstash/inputs/stdin.rb
+++ lib/logstash/inputs/stdin.rb
@@ -10,6 +10,8 @@ class LogStash::Inputs::Stdin < LogStash::Inputs::Base
 
   config_name "stdin"
 
+  plugin_status "experimental"
+
   public
   def register
     @host = Socket.gethostname
