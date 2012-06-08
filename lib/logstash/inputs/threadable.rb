6f41ffd9676027cdf8b208b931366ba0c68a42c0
Nick Ethier <ncethier@gmail.com>
2012-06-08 11:43:19 -0600

    cleanup and comment

--- lib/logstash/inputs/threadable.rb
+++ lib/logstash/inputs/threadable.rb
@@ -1,19 +1,14 @@
 require "logstash/namespace"
-require "logstash/event"
-require "logstash/plugin"
-require "logstash/logging"
-require "logstash/config/mixin"
 require "logstash/inputs/base"
 
-# This is the base class for logstash inputs.
+# This is the threadable class for logstash inputs. 
+# Use this class in your inputs if it can support multiple threads
 class LogStash::Inputs::Threadable < LogStash::Inputs::Base
 
   # Set this to the number of threads you want this input to spawn.
   # This is the same as declaring the input multiple times
   config :threads, :validate => :number, :default => 1
  
-  attr_accessor :threadable
-
   def initialize(params)
     super
     @threadable = true
