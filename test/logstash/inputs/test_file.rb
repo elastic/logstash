e98cc455c76d9783eabce4cd79b7cd5866574be7
Marc Huffnagle <marc@element84.com>
2012-06-22 18:58:52 -0400

    Refactored file input to use the addressable gem.
    Ruby's std-lib URI parsing leaves something to be desired, so using
    addressable.  Also added a test to make sure that the file input creates
    a correct source and refactored the file input tests to timeout after 5
    seconds.

--- test/logstash/inputs/test_file.rb
+++ test/logstash/inputs/test_file.rb
@@ -9,7 +9,51 @@ require "logstash/inputs/file"
 
 require "tempfile"
 
+require "mocha"
+
 describe LogStash::Inputs::File do
+
+  test "file input sets source properly for events" do
+    stubbed_hostname = "mystubbedhostname"
+    Socket.stubs(:gethostname).returns(stubbed_hostname)
+
+    logfile = Tempfile.new("logstash")
+    begin
+      @input = LogStash::Inputs::File.new("type" => ["testing"], "path" => [logfile.path])
+      @input.register
+
+      queue = Queue.new
+
+      Thread.new { @input.run(queue) }
+      
+      event = nil
+      start = Time.now
+      while event.nil? and (Time.now - start) <= 5.0
+        logfile.write("This is my log message.\n")
+        logfile.flush
+
+        begin
+          event = queue.pop(true)
+        rescue ThreadError => error
+          raise error unless error.to_s == "queue empty"
+          sleep(0.05)
+        end
+      end
+
+      @input.teardown
+
+      refute_nil event
+
+      uri = Addressable::URI.parse(event.source)
+      assert_equal "file", uri.scheme
+      assert_equal stubbed_hostname, uri.host
+      assert_equal logfile.path, uri.path
+    ensure
+      logfile.close
+      logfile.unlink
+    end
+  end
+
   test "file input sets source_path properly for events" do
     logfile = Tempfile.new("logstash")
     begin
@@ -21,7 +65,8 @@ describe LogStash::Inputs::File do
       Thread.new { @input.run(queue) }
       
       event = nil
-      while event.nil?
+      start = Time.now
+      while event.nil? and (Time.now - start) <= 5.0
         logfile.write("This is my log message.\n")
         logfile.flush
 
@@ -41,4 +86,5 @@ describe LogStash::Inputs::File do
       logfile.unlink
     end
   end
+
 end # testing for LogStash::Inputs::File
