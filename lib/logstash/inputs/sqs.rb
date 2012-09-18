8eba33cea8cae1e8b6ab671c442b7935ef16d1ca
Sean Laurent <organicveggie@gmail.com>
2012-09-18 12:32:19 -0500

    AWS SQS input plugin.

---
+++ lib/logstash/inputs/sqs.rb
@@ -0,0 +1,36 @@
+require "logstash/inputs/base"
+require "logstash/namespace"
+
+class Logstash::Inputs::SQS < LogStash::Inputs::Base
+  config_name "sqs"
+  plugin_status "beta"
+
+  # SQS Queue name
+  config :queue, :validate => :string, :required => true
+
+  # IAM key/secret
+  config :access_key, :validate => :string, :required => true
+  config :secret_key, :validate => :string, :required => true
+
+  def initialize(*args)
+    super(*args)
+  end
+
+  public
+  def register
+    require "aws-sdk"
+    @sqs = AWS::SQS.new(
+      :access_key_id => @access_key,
+      :secret_access_key => @secret_key
+    )
+    @sqs_queue = @sqs.named(@queue)
+    @logger.info("Connected to AWS SQS queue #{@queue}")
+  end
+
+  public
+  def run(output_queue)
+    @sqs_queue.poll(:initial_timeout => false, :idle_timeout => 10) do |message|
+      output_queue << message if message
+    end
+  end
+end
