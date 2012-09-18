fdd825a2519c94bd83b84b4f1be961ac7dbae17b
Sean Laurent <organicveggie@gmail.com>
2012-09-18 12:04:42 -0500

    AWS SQS output plugin.

---
+++ lib/logstash/outputs/sqs.rb
@@ -0,0 +1,29 @@
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+class LogStash::Outputs::SQS < LogStash::Outputs::Base
+  config_name "sqs"
+  plugin_status "beta"
+
+  # SQS Queue name
+  config :queue, :validate => :string, :required => true
+
+  # IAM key/secret
+  config :access_key, :validate => :string, :required => true
+  config :secret_key, :validate => :string, :required => true
+
+  public 
+  def register
+    require "aws-sdk"
+    @sqs = AWS::SQS.new(
+      :access_key_id => @access_key,
+      :secret_access_key => @secret_key
+    )
+    @sqs_queue = @sqs.named(@queue)
+  end
+
+  public
+  def receive(event)
+    @sqs_queue.send_message(event.to_hash)
+  end
+end
