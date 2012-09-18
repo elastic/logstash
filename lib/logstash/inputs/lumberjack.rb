1136f3e1017863549ece1fc0325f7dfa548dee51
Jordan Sissel <jls@semicomplete.com>
2012-09-17 17:19:12 -0700

    - use jls-lumberjack lib

--- lib/logstash/inputs/lumberjack.rb
+++ lib/logstash/inputs/lumberjack.rb
@@ -1,8 +1,5 @@
 require "logstash/inputs/base"
 require "logstash/namespace"
-require "logstash/util/socket_peer"
-require "socket"
-require "timeout"
 
 # Receive events using the lumberjack protocol.
 #
@@ -31,177 +28,26 @@ class LogStash::Inputs::Lumberjack < LogStash::Inputs::Base
 
   public
   def register
-    require "openssl"
-    require "zlib"
+    require "lumberjack/server"
 
     @logger.info("Starting lumberjack input listener", :address => "#{@host}:#{@port}")
-    @tcp_server = TCPServer.new(@host, @port)
-
-    ssl_context = OpenSSL::SSL::SSLContext.new
-    begin
-      cert_data = File.read(@ssl_certificate)
-    rescue NameError, NoMethodError
-      raise
-    rescue => e
-      @logger.error("Failed reading ssl certificate", :path => @ssl_certificate, :exception => e)
-    end
-
-    begin
-      ssl_context.cert = OpenSSL::X509::Certificate.new(cert_data)
-    rescue NameError, NoMethodError; raise
-    rescue => e
-      @logger.error("Failed parsing ssl certificate", :path => @ssl_certificate, :exception => e)
-      raise
-    end
-
-    begin
-      key_data = File.read(@ssl_key)
-    rescue NameError, NoMethodError; raise
-    rescue => e
-      @logger.error("Failed reading ssl key file", :path => @ssl_key, :exception => e)
-      raise
-    end
-
-    begin
-      if @ssl_key_passphrase.nil?
-        ssl_context.key = OpenSSL::PKey::RSA.new(key_data)
-      else
-        ssl_context.key = OpenSSL::PKey::RSA.new(key_data, @ssl_key_passphrase.value)
-      end
-    rescue NameError, NoMethodError; raise
-    rescue => e
-      @logger.error("Failed parsing ssl key", :path => @ssl_key,
-                    :passphrase? => !@ssl_key_passphrase.nil?, :exception => e)
-      raise
-    end
-
-    @ssl_server = OpenSSL::SSL::SSLServer.new(@tcp_server, ssl_context)
+    @lumberjack = Lumberjack::Server.new(:address => @host, :port => @port
+      :ssl_certificate => @ssl_certificate, :ssl_key => @ssl_key,
+      :ssl_key_passphrase => @ssl_key_passphrase)
   end # def register
-
-  private
-  def handle_socket(socket, output_queue, event_source)
-    begin
-      last_ack = 0
-      window_size = 1 # assume window size of 1 until told otherwise
-
-      # TODO(sissel): Update the protocol to announce window size from the
-      # publisher.
-      while true
-        vf = socket.read(2)
-
-        if vf == "1W"
-          window_size = socket.read(4).unpack("N").first
-          next
-        end
-
-        if vf == "1C" # compressed frame envelope
-          length = socket.read(4).unpack("N").first
-          compressed = socket.read(length)
-          original = Zlib::Inflate.inflate(compressed)
-          # push the decompressed data back into the socket buffer
-          # to be processed as like normal
-          socket.pushback(original)
-          next
-        end
-
-        if vf != "1D" 
-          logger.warn("Unexpected version/frame type", :vf => vf);
-          socket.close
-          return
-        end
-
-        # We got a data frame, read the sequence, count of elements, then each
-        # element.
-        sequence, count = socket.read(8).unpack("NN")
-        map = {}
-        count.times do 
-          key_len = socket.read(4).unpack("N").first
-          key = socket.read(key_len);
-          value_len = socket.read(4).unpack("N").first
-          value = socket.read(value_len);
-          map[key] = value
-        end
-
-        event = LogStash::Event.new(
-          "@type" => @type,
-          "@tags" => (@tags.clone rescue []),
-          "@source_path" => map["file"],
-          "@source_host" => map["host"],
-          "@source" => "lumberjack://#{map["host"]}#{map["file"]}",
-          "@message" => map["line"])
-
-        # Add any other arbitrary fields given.
-        map.each do |k,v|
-          next if ["file", "host", "line"].include?(k)
-          event[k] = v
-        end
-
-        output_queue << event
-
-        # bulk ack if we hit the window size
-        if sequence - last_ack == window_size
-          socket.write(["1", "A", sequence].pack("AAN"))
-          last_ack = sequence
-        end
-      end # while true
-    rescue StandardError => e
-      @logger.warn("Exception caught, closing connection", :client => socket.peer,
-                    :exception => e, :backtrace => e.backtrace)
-    end # begin
-  ensure
-    begin
-      socket.close
-    rescue IOError
-      # ignore
-    end # begin
-  end # def handle_socket
 
   public
   def run(output_queue)
-    loop do
-      # Start a new thread for each connection.
-      Thread.start(@ssl_server.accept) do |client|
-        # TODO(sissel): put this block in its own method.
+    @lumberjack.run do |l|
+      event = LogStash::Event.new(
+        "@message" => l["line"],
+        "@source_path" => l["file"],
+        "@source_host" => l["host"],
+        "@type" => @type
+        "@tags" => @tags.clone
+      )
 
-        # monkeypatch a 'peer' method onto the socket.
-        client.instance_eval { class << self; include ::LogStash::Util::SocketPeer end }
-        @logger.debug("Accepted connection", :client => client.peer,
-                      :server => "#{@host}:#{@port}", :plugin => self)
-        handle_socket(IOWrap.new(client), output_queue,
-                      "lumberjack://#{@host}:#{@port}/#{client.peer}")
-      end # Thread.start
-    end # loop
+      output_queue << event
+    end
   end # def run
-
-  class IOWrap
-    def initialize(io)
-      @io = io
-      @buffer = ""
-    end
-
-    def read(bytes)
-      if @buffer.empty?
-        #puts "reading direct from @io"
-        return @io.read(bytes)
-      elsif @buffer.length > bytes
-        #puts "reading buffered"
-        data = @buffer[0...bytes]
-        @buffer[0...bytes] = ""
-        return data
-      else
-        data = @buffer.clone
-        @buffer.clear
-        return data + @io.read(bytes - data.length)
-      end
-    end
-
-    def pushback(data)
-      #puts "Pushback: #{data[0..30].inspect}..."
-      @buffer += data
-    end
-
-    def method_missing(method, *args)
-      @io.send(method, *args)
-    end
-  end
-end # class LogStash::Inputs::Tcp
+end # class LogStash::Inputs::Lumberjack
