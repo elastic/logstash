1d944cbe23bf6c50da6b4a02f68f2c6ab9efc156
Jordan Sissel <jls@semicomplete.com>
2012-09-19 13:45:18 -0700

    try again getting the source right

--- lib/logstash/inputs/lumberjack.rb
+++ lib/logstash/inputs/lumberjack.rb
@@ -39,9 +39,9 @@ class LogStash::Inputs::Lumberjack < LogStash::Inputs::Base
   public
   def run(output_queue)
     @lumberjack.run do |l|
-      event = to_event(l.delete("line"), "lumberjack")
-      event.source_host = l.delete("host")
-      event.source_path = l.delete("host")
+      event = to_event(l.delete("line"), "lumberjack://#{l.delete("host")}/#{l.delete("file")}")
+      # take any remaining fields in the lumberjack event and merge it as a
+      # field in the logstash event.
       event.fields.merge(l)
       output_queue << event
     end
