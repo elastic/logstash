0feaea2ea34d08702fe679e958c7ada2f60a52b6
Sean Laurent <organicveggie@gmail.com>
2012-09-24 16:53:37 -0500

    Fixed comments. Updated documentation. Replaced erroneous 'puts' with logger calls.

--- lib/logstash/inputs/sqs.rb
+++ lib/logstash/inputs/sqs.rb
@@ -1,43 +1,98 @@
 require "logstash/inputs/threadable"
 require "logstash/namespace"
 
+# Pull events from an Amazon Web Services Simple Queue Service (SQS) queue.
+#
+# SQS is a simple, scalable queue system that is part of the 
+# Amazon Web Services suite of tools.
+#
+# Although SQS is similar to other queuing systems like AMQP, it
+# uses a custom API and requires that you have an AWS account.
+# See http://aws.amazon.com/sqs/ for more details on how SQS works,
+# what the pricing schedule looks like and how to setup a queue.
+#
+# To use this plugin, you *must*:
+#  * Have an AWS account
+#  * Setup an SQS queue
+#  * Create an identify that has access to consume messages from the queue.
+#
+# The "consumer" identity must have the following permissions on the queue:
+#  * sqs:ChangeMessageVisibility
+#  * sqs:ChangeMessageVisibilityBatch
+#  * sqs:DeleteMessage
+#  * sqs:DeleteMessageBatch
+#  * sqs:GetQueueAttributes
+#  * sqs:GetQueueUrl
+#  * sqs:ListQueues
+#  * sqs:ReceiveMessage
+#
+# Typically, you should setup an IAM policy, create a user and apply the IAM policy to the user.
+# A sample policy is as follows:
+#
+#     {
+#       "Statement": [
+#         {
+#           "Action": [
+#             "sqs:ChangeMessageVisibility",
+#             "sqs:ChangeMessageVisibilityBatch",
+#             "sqs:GetQueueAttributes",
+#             "sqs:GetQueueUrl",
+#             "sqs:ListQueues",
+#             "sqs:SendMessage",
+#             "sqs:SendMessageBatch"
+#           ],
+#           "Effect": "Allow",
+#           "Resource": [
+#             "arn:aws:sqs:us-east-1:123456789012:Logstash"
+#           ]
+#         }
+#       ]
+#     } 
+#
+# See http://aws.amazon.com/iam/ for more details on setting up AWS identities.
+#
 class LogStash::Inputs::SQS < LogStash::Inputs::Threadable
   config_name "sqs"
   plugin_status "experimental"
 
-  # SQS Queue name
+  # Name of the SQS Queue name to pull messages from. Note that this is just the name of the queue, not the URL or ARN.
   config :queue, :validate => :string, :required => true
 
-  # IAM key/secret
+  # AWS access key. Must have the appropriate permissions.
   config :access_key, :validate => :string, :required => true
+
+  # AWS secret key. Must have the appropriate permissions.
   config :secret_key, :validate => :string, :required => true
 
   def initialize(params)
     super
     @format ||= "json_event"
-  end
+  end # def initialize
 
   public
   def register
-    @logger.info("Registering SQS input for queue '#{@queue}'")
+    @logger.info("Registering SQS input", :queue => @queue)
     require "aws-sdk"
+
+    # Connec to SQS
     @sqs = AWS::SQS.new(
       :access_key_id => @access_key,
       :secret_access_key => @secret_key
     )
+
     begin
-      @logger.debug("Connecting to AWS SQS queue '#{@queue}'...")
+      @logger.debug("Connecting to AWS SQS queue", :queue => @queue)
       @sqs_queue = @sqs.queues.named(@queue)
-      @logger.info("Connected to AWS SQS queue '#{@queue}' successfully.")
+      @logger.info("Connected to AWS SQS queue successfully.", :queue => @queue)
     rescue Exception => e
-      @logger.error("Unable to access SQS queue '#{@queue}': #{e.to_s}")
+      @logger.error("Unable to access SQS queue.", :error => e.to_s, :queue => @queue)
       throw e
-    end
-  end
+    end # begin/rescue
+  end # def register
 
   public
   def run(output_queue)
-    @logger.debug("Polling SQS queue '#{@queue}'...")
+    @logger.debug("Polling SQS queue", :queue => @queue)
     
     receive_opts = {
       :limit => 10,
@@ -46,25 +101,25 @@ class LogStash::Inputs::SQS < LogStash::Inputs::Threadable
 
     continue_polling = true
     while running? && continue_polling
-      continue_polling = run_with_backoff(60, 1, "retrieving messages from SQS queue '#{@queue}'") do
+      continue_polling = run_with_backoff(60, 1) do
         @sqs_queue.receive_message(receive_opts) do |message|
           if message
             e = to_event(message.body, @sqs_queue)
             if e
-              @logger.debug("Processed SQS message #{message.id} [#{message.md5}] from queue '#{@queue}'")
+              @logger.debug("Processed SQS message", :message_id => message.id, :message_md5 => message.md5, :queue => @queue)
               output_queue << e
               message.delete
-            end
-          end
-        end
-      end
-    end
-  end
+            end # valid event
+          end # valid SQS message
+        end # receive_message
+      end # run_with_backoff
+    end # polling loop
+  end # def run
 
   def teardown
     @sqs_queue = nil
     finished
-  end
+  end # def teardown
 
   private
   # Runs an AWS request inside a Ruby block with an exponential backoff in case
@@ -72,27 +127,26 @@ class LogStash::Inputs::SQS < LogStash::Inputs::Threadable
   #
   # @param [Integer] max_time maximum amount of time to sleep before giving up.
   # @param [Integer] sleep_time the initial amount of time to sleep before retrying.
-  # @param [message] message message to display if we get an exception.
   # @param [Block] block Ruby code block to execute.
-  def run_with_backoff(max_time, sleep_time, message, &block)
+  def run_with_backoff(max_time, sleep_time, &block)
     if sleep_time > max_time
-      puts "AWS::EC2::Errors::RequestLimitExceeded ... failed #{message}"
+      @logger.error("AWS::EC2::Errors::RequestLimitExceeded ... failed.", :queue => @queue)
       return false
-    end
+    end # retry limit exceeded
     
     begin
-      yield
+      block.call
     rescue AWS::EC2::Errors::RequestLimitExceeded
-      puts "AWS::EC2::Errors::RequestLimitExceeded ... retrying #{message} in #{sleep_time} seconds"
+      @logger.info("AWS::EC2::Errors::RequestLimitExceeded ... retrying SQS request", :queue => @queue, :sleep_time => sleep_time)
       sleep sleep_time
-      run_with_backoff(max_time, sleep_time * 2, message, &block)
+      run_with_backoff(max_time, sleep_time * 2, &block)
     rescue AWS::EC2::Errors::InstanceLimitExceeded
-      puts "AWS::EC2::Errors::InstanceLimitExceeded ... aborting launch."
+      @logger.warn("AWS::EC2::Errors::InstanceLimitExceeded ... aborting SQS message retreival.")
       return false
     rescue Error => bang
-      print "Error for #{message}: #{bang}"
+      @logger.error("Error reading SQS queue.", :errir => bang, :queue => @queue)
       return false
-    end
-    true
-  end
-end
+    end # begin/rescue
+    return true
+  end # def run_with_backoff
+end # class LogStash::Inputs::SQS
--- lib/logstash/outputs/sqs.rb
+++ lib/logstash/outputs/sqs.rb
@@ -1,15 +1,68 @@
 require "logstash/outputs/base"
 require "logstash/namespace"
 
+# Push events to an Amazon Web Services Simple Queue Service (SQS) queue.
+#
+# SQS is a simple, scalable queue system that is part of the 
+# Amazon Web Services suite of tools.
+#
+# Although SQS is similar to other queuing systems like AMQP, it
+# uses a custom API and requires that you have an AWS account.
+# See http://aws.amazon.com/sqs/ for more details on how SQS works,
+# what the pricing schedule looks like and how to setup a queue.
+#
+# To use this plugin, you *must*:
+#  * Have an AWS account
+#  * Setup an SQS queue
+#  * Create an identify that has access to publish messages to the queue.
+#
+# The "consumer" identity must have the following permissions on the queue:
+#  * sqs:ChangeMessageVisibility
+#  * sqs:ChangeMessageVisibilityBatch
+#  * sqs:GetQueueAttributes
+#  * sqs:GetQueueUrl
+#  * sqs:ListQueues
+#  * sqs:SendMessage
+#  * sqs:SendMessageBatch
+#
+# Typically, you should setup an IAM policy, create a user and apply the IAM policy to the user.
+# A sample policy is as follows:
+#
+#      {
+#        "Statement": [
+#          {
+#            "Sid": "Stmt1347986764948",
+#            "Action": [
+#              "sqs:ChangeMessageVisibility",
+#              "sqs:ChangeMessageVisibilityBatch",
+#              "sqs:DeleteMessage",
+#              "sqs:DeleteMessageBatch",
+#              "sqs:GetQueueAttributes",
+#              "sqs:GetQueueUrl",
+#              "sqs:ListQueues",
+#              "sqs:ReceiveMessage"
+#            ],
+#            "Effect": "Allow",
+#            "Resource": [
+#              "arn:aws:sqs:us-east-1:200850199751:Logstash"
+#            ]
+#          }
+#        ]
+#      }
+#
+# See http://aws.amazon.com/iam/ for more details on setting up AWS identities.
+#
 class LogStash::Outputs::SQS < LogStash::Outputs::Base
   config_name "sqs"
   plugin_status "experimental"
 
-  # SQS Queue name
+  # Name of SQS queue to push messages into. Note that this is just the name of the queue, not the URL or ARN.
   config :queue, :validate => :string, :required => true
 
-  # IAM key/secret
+  # AWS access key. Must have the appropriate permissions.
   config :access_key, :validate => :string, :required => true
+
+  # AWS secret key. Must have the appropriate permissions.
   config :secret_key, :validate => :string, :required => true
 
   public 
@@ -24,18 +77,19 @@ class LogStash::Outputs::SQS < LogStash::Outputs::Base
       @sqs_queue = @sqs.queues.named(@queue)
     rescue Exception => e
       @logger.error("Unable to access SQS queue '#{@queue}': #{e.to_s}")
-    end
+    end # begin/rescue
+
     @logger.info("Connected to AWS SQS queue '#{@queue}' successfully.")
-  end
+  end # def register
 
   public
   def receive(event)
     @sqs_queue.send_message(event.to_json)
-  end
+  end # def receive
 
   public
   def teardown
     @sqs_queue = nil
     finished
-  end
+  end # def teardown
 end
