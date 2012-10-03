417c529d36e561f87937aa87e66ce3b60239bc4e
Jordan Sissel <jls@semicomplete.com>
2012-10-03 14:26:45 -0700

    - Make multiline use grok (LOGSTASH-445) - Add tests to cover grok patterns in multiline.

--- spec/filters/multiline.rb
+++ spec/filters/multiline.rb
@@ -22,4 +22,23 @@ describe LogStash::Filters::Multiline do
       insist { subject[1].message } == "another first line"
     end
   end
+
+  describe "multiline using grok patterns" do
+    # The logstash config goes here.
+    # At this time, only filters are supported.
+    config <<-CONFIG
+    filter {
+      multiline {
+        pattern => "^%{NUMBER} %{TIME}"
+        negate => true
+        what => previous
+      }
+    }
+    CONFIG
+
+    sample [ "120913 12:04:33 first line", "second line", "third line" ] do
+      insist { subject.length } == 1
+      insist { subject[0].message } ==  "120913 12:04:33 first line\nsecond line\nthird line"
+    end
+  end
 end
