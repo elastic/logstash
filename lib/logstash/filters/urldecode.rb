17eb648464c31d807d0440337f961f3e39a471ce
Joey Imbasciano <joey.imbasciano@gmail.com>
2012-10-06 16:27:50 -0400

    LOGSTASH-612 - Enchance urldecode filter to deal with arrays and add option to urldecode all fields.

--- lib/logstash/filters/urldecode.rb
+++ lib/logstash/filters/urldecode.rb
@@ -10,6 +10,9 @@ class LogStash::Filters::Urldecode < LogStash::Filters::Base
   # The field which value is urldecoded
   config :field, :validate => :string, :default => "@message"
 
+  # Urldecode all fields
+  config :all_fields, :validate => :boolean, :default => false
+
   public
   def register
     # Nothing to do
@@ -18,8 +21,35 @@ class LogStash::Filters::Urldecode < LogStash::Filters::Base
   public
   def filter(event)
     return unless filter?(event)
-    decode_field = event[@field]
-    event[@field] = URI.unescape(decode_field)
+
+    # If all_fields is true then try to decode them all
+    if @all_fields
+      event.fields.each do |name, value|
+        # deal with arrays
+        if value.is_a?(Array)
+          all_vs = []
+          value.each do |v|
+            all_vs << URI.unescape(v)
+          end
+          event.fields[name] = all_vs
+        else 
+          event.fields[name] = URI.unescape(value)
+        end #end if array
+      end
+    # Else decode the specified field
+    else
+      decode_field = event[@field]
+      # deal with arrays
+      if decode_field.is_a?(Array)
+        all_vs = []
+        decode_field.each do |v|
+          all_vs << URI.unescape(v)
+        end
+        event[@field] = all_vs
+      else
+        event[@field] = URI.unescape(decode_field)
+      end #end if array
+    end
     filter_matched(event)
   end # def filter
 end # class LogStash::Filters::Urldecode
