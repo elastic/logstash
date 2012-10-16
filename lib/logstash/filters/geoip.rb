1dc17d813498a974ab576cad9c523319e6082f2c
Jordan Sissel <jls@semicomplete.com>
2012-10-16 13:20:56 -0700

    Use GeoLiteCity by default if we can

--- lib/logstash/filters/geoip.rb
+++ lib/logstash/filters/geoip.rb
@@ -3,22 +3,43 @@ require "logstash/namespace"
 require "geoip"
 
 # Add GeoIP fields from Maxmind database
+#
+# GeoIP filter, adds information about geographical location of IP addresses.
+# This filter uses Maxmind GeoIP databases, have a look at
+# https://www.maxmind.com/app/geolite
+#
+# Logstash releases ship with the GeoLiteCity database made available from
+# Maxmind with a CCA-ShareAlike 3.0 license. For more details on geolite, see
+# <http://www.maxmind.com/en/geolite>.
 class LogStash::Filters::GeoIP < LogStash::Filters::Base
   config_name "geoip"
   plugin_status "experimental"
 
-  # GeoIP filter, adds information about geographical location of IP addresses.
-  # This filter uses Maxmind GeoIP databases, have a look at https://www.maxmind.com/app/geolite
+  # GeoIP database file to use, Country, City, ASN, ISP and organization
+  # databases are supported
+  #
+  # If not specified, this will default to the GeoLiteCity database that ships
+  # with logstash.
+  config :database, :validate => :string
 
-  # GeoIP database file to use, Country, City, ASN, ISP and organization databases are supported
-  config :database, :validate => :string, :required => true
-
-  # The field containing IP address, hostname is also OK. If this filed is an array, the first value will be used.
-  config :ip_field, :validate => :string, :default => "IP"
-
+  # The field containing IP address, hostname is also OK. If this field is an
+  # array, only the first value will be used.
+  config :field, :validate => :string, :required => true
 
   public
   def register
+    if @database.nil?
+      if __FILE__ =~ /^file:\/.+!.+/
+        # Running from a jar, assume GeoLiteCity.dat is at the root.
+        @database = [__FILE__.split("!").first, "GeoLiteCity.dat"].join("!")
+      else
+        if File.exists?("GeoLiteCity.dat")
+          @database = "GeoLiteCity.dat"
+        else
+          raise "You must specify 'database => ...' in your geoip filter"
+        end
+      end
+    end
     @geoip = ::GeoIP.new(@database)
     @geoip_type = case @geoip.database_type
     when GeoIP::GEOIP_CITY_EDITION_REV0, GeoIP::GEOIP_CITY_EDITION_REV1
@@ -39,13 +60,13 @@ class LogStash::Filters::GeoIP < LogStash::Filters::Base
     return unless filter?(event)
     geo_data = nil
     begin
-      ip = event[@ip_field]
+      ip = event[@field]
       ip = ip.first if ip.is_a? Array
       geo_data = @geoip.send(@geoip_type, ip)
     rescue SocketError => e
-      @logger.error("IP Field contained invalid IP address or hostname", :ip_field => @ip_field, :event => event)
+      @logger.error("IP Field contained invalid IP address or hostname", :field => @field, :event => event)
     rescue Exception => e
-      @logger.error("Uknown error while looking up GeoIP data", :exception => e, :ip_field => @ip_field, :event => event)
+      @logger.error("Uknown error while looking up GeoIP data", :exception => e, :field => @field, :event => event)
     end
     unless geo_data.nil?
       geo_data_hash = geo_data.to_hash
