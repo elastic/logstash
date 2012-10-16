d05a8680df4101995fd1bc4e3e9a3bafba579baa
Jordan Sissel <jls@semicomplete.com>
2012-10-15 23:09:42 -0700

    Fix job generation

--- test/jenkins/create-jobs.rb
+++ test/jenkins/create-jobs.rb
@@ -11,10 +11,10 @@ plugindir = File.join(File.dirname(__FILE__), "..", "..", "lib", "logstash")
 
 plugins = %w(inputs filters outputs).collect { |t| Dir.glob(File.join(plugindir, t, "*.rb")) }.flatten
 
-template = ERB.new("config.xml.erb")
+template = ERB.new(File.read(File.join(File.dirname(__FILE__), "config.xml.erb")))
 plugins.each do |path|
   job = path.gsub(/.*\/([^\/]+)\/([^\/]+)\.rb$/, '\1-\2')
-  plugin_path = path.gsub(/.*\/([^\/]+)\/([^\/]+)\.rb$/, '\1/\2')
+  plugin_path = path.gsub(/.*\/([^\/]+)\/([^\/]+)$/, '\1/\2')
 
   jobdir = File.join(ENV["JENKINS_HOME"], job)
   puts "Writing #{jobdir}/config.xml"
