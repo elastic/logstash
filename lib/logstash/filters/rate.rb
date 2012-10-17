97be9df599d415bc1d602f8cf899cbb4d526a2ad
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-10-17 21:26:07 +0200

    Add count method to the rate plugin

--- lib/logstash/filters/rate.rb
+++ lib/logstash/filters/rate.rb
@@ -8,9 +8,7 @@ require "logstash/namespace"
 require "time/unit"
 
 # The rate filter is for marking events based on the rate of similiar events.
-# Rate is defined in terms of some aggregated metric for which the filter offers a few options:
-# * Exponential running average
-# * Linear decay running average
+# Rate is defined in terms of some aggregated metric for which the filter offers a few options (see below).
 class LogStash::Filters::Rate < LogStash::Filters::Base
 
   config_name "rate"
@@ -26,13 +24,17 @@ class LogStash::Filters::Rate < LogStash::Filters::Base
   # coming from a tcp input.
   #
   # The default value here is usually what you want, but there are some cases
-  # where you want to change it. One such example is if you are using a tcp
-  # input with only one client connecting at any time. If that client
-  # reconnects (due to error or client restart), then logstash will identify
-  # the new connection as a new stream and break any rate goodness that
-  # may have occurred between the old and new connection. To solve this use
-  # case, you can use "%{@source_host}.%{@type}" instead.
+  # where you want to change it. For example, suppose you want to send alerts
+  # if some IP connected too frequently to your server - you could set the 
+  # stream_identity to something like "%{@type}.%{remote_address}"
   config :stream_identity , :validate => :string, :default => "%{@source}.%{@type}"
+
+  # There are multiple methods of calculating/estimating rate. The rate filters supports the following:
+  # * EWMA - Exponential weighted moving average: For every new matching event,
+  #   calculate the current "rate" (1/ time since last event) then apply EWMA to it.
+  #   See https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average
+  # * COUNT: Keep a record of every matching event in a rolling time interval window then compute (count / time_interval)
+  config :mode, :validate => ["EWMA", "COUNT"], :default => "EWMA"
   
   public
   def initialize(config = {})
@@ -40,31 +42,59 @@ class LogStash::Filters::Rate < LogStash::Filters::Base
 
     @threadsafe = false
 
-    # This filter needs to keep state.
-    @counters = Hash.new { |h,k| h[k] = Hash.new } 
   end # def initialize
 
   public
   def register
     @interval_seconds = Time::Unit.parse(@interval.tr(" ", "")).seconds
-    @alpha = 2.0/(@interval_seconds + 1)
     @logger.debug("Registered rate plugin", :type => @type, :config => @config)
+    @rate_method = method("rate_#{mode}")
+    @rate_threshold = threshold.to_f / @interval_seconds
+    send("prepare_#{mode}")
   end # def register
 
-  public
   def filter(event)
     return unless filter?(event)
-    event_stream_id = event.sprintf(@stream_identity)
-    event_time = Time.now
-    if @counters.has_key? event_stream_id
-      time_delta = event_time - @counters[event_stream_id][:timestamp]
-      time_delta = 0.001 if time_delta == 0 # should never be zero, but just to make sure...
-      @counters[event_stream_id][:timestamp] = event_time
-      @counters[event_stream_id][:value] = @alpha/time_delta + (1-@alpha) * @counters[event_stream_id][:value]
-    else
-      @counters[event_stream_id] = {:timestamp => event_time, :value => 0}
-    end
-    filter_matched(event) if @counters[event_stream_id][:value] * @interval_seconds > threshold
+    filter_matched(event) if rate(event) > @rate_threshold
   end # def filter
 
-end # class LogStash::Filters::Date
+  private
+
+  def rate(event)
+    event_stream_id = event.sprintf(stream_identity)
+    event_time = event.unix_timestamp
+    @rate_method.call(event, event_stream_id, event_time)
+  end
+
+  def prepare_EWMA
+    @counters = Hash.new { |h,k| h[k] = Hash.new } 
+  end
+
+  def rate_EWMA(event, stream_id, event_time)
+    if @counters.has_key? stream_id
+      time_delta = event_time - @counters[stream_id][:timestamp]
+      time_delta = 0.001 if time_delta == 0 # should never be zero, but just to make sure...
+      @counters[stream_id][:timestamp] = event_time
+      alpha = (1 - Math.exp(- time_delta / @interval_seconds))
+      @counters[stream_id][:value] = alpha/time_delta + (1-alpha) * @counters[stream_id][:value]
+    else
+      @counters[stream_id] = {:timestamp => event_time, :value => 0}
+    end
+    logger.debug("Current EWMA: #{@counters[stream_id][:value]}", :event => event, :stream_id => stream_id)
+    @counters[stream_id][:value]
+  end
+
+  def prepare_COUNT
+    @counters = Hash.new { |h,k| h[k] = Array.new } 
+  end
+
+  def rate_COUNT(event, stream_id, event_time)
+    now = ::LogStash::Time.now_f
+    @counters[stream_id].delete_if { |e| e < now - @interval_seconds } if @counters[stream_id].any?
+    @counters[stream_id].push event_time
+    value = @counters[stream_id].count.to_f / @interval_seconds
+    logger.debug("Current rate: #{value}", :event => event, :stream_id => stream_id)
+    value
+  end
+
+end # class LogStash::Filters::Rate
