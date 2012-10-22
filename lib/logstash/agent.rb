dd96fc9bb36ed8c4475fca70e5cc72108e06850f
Jordan Sissel <jls@semicomplete.com>
2012-10-22 13:09:51 -0700

    Make --watchdog-timeout a flag. Default value is 10 seconds (previously 2 seconds)

--- lib/logstash/agent.rb
+++ lib/logstash/agent.rb
@@ -39,6 +39,7 @@ class LogStash::Agent
     # flag/config defaults
     @verbose = 0
     @filterworker_count = 1
+    @watchdog_timeout = 10
 
     @plugins = {}
     @plugins_mutex = Mutex.new
@@ -89,6 +90,10 @@ class LogStash::Agent
         raise ArgumentError, "filter worker count must be > 0"
       end
     end # -w
+
+    opts.on("--watchdog-timeout TIMEOUT", "Set watchdog timeout value") do |arg|
+      @watchdog_timeout = arg.to_f
+    end # --watchdog-timeout
 
     opts.on("-l", "--log FILE", "Log to a given path. Default is stdout.") do |path|
       @logfile = path
@@ -438,7 +443,8 @@ class LogStash::Agent
       end # if @filters.length > 0
 
       # A thread to supervise filter workers
-      watchdog = LogStash::ThreadWatchdog.new(@filterworkers.values)
+      watchdog = LogStash::ThreadWatchdog.new(@filterworkers.values,
+                                              @watchdog_timeout)
       watchdog.logger = logger
       Thread.new do
         watchdog.watch
