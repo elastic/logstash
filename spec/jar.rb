e0e49ba5f02ed3602cea8b2fc5fde572f821c8ce
Jordan Sissel <jls@semicomplete.com>
2012-11-02 00:17:43 -0700

    Fix path

--- spec/jar.rb
+++ spec/jar.rb
@@ -2,7 +2,7 @@ require "insist"
 
 describe "logstash jar features" do
   before :each do 
-    @jar_root = __FILE__.split("!").first
+    @jar_root = __FILE__.split("!").first + "!"
   end
 
   it "must contain GeoCityLite.dat" do
