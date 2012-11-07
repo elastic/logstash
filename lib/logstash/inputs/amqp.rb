f7bcb96db4f7b7b74f647e69675f5de8b17981c5
Jordan Sissel <jls@semicomplete.com>
2012-11-07 12:14:50 -0800

    - Add sad note to users that rabbitmq is probably the only thing that   works. Hopefully this will dissuade users from trying amqp until   things improve. If nothing else, it'll be easy to link to this note   when folks ask why their AMQP-standard-following broker doesn't work   with this plugin.

--- lib/logstash/inputs/amqp.rb
+++ lib/logstash/inputs/amqp.rb
@@ -3,6 +3,11 @@ require "logstash/namespace"
 
 # Pull events from an AMQP exchange.
 #
+# <b> NOTE: THIS IS ONLY KNOWN TO WORK WITH RECENT RELEASES OF RABBITMQ. Any
+# other amqp broker will not work with this plugin. I do not know why. If you
+# need support for brokers other than rabbitmq, please file bugs here:
+# <https://github.com/ruby-amqp/bunny> </b>
+#
 # AMQP is a messaging system. It requires you to run an AMQP server or 'broker'
 # Examples of AMQP servers are [RabbitMQ](http://www.rabbitmq.com/) and
 # [QPid](http://qpid.apache.org/)
