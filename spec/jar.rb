e4219a450898e5e9efa15b0cce0e93aab298450c
Jordan Sissel <jls@semicomplete.com>
2012-11-10 01:06:11 -0500

    only run from a jar

--- spec/jar.rb
+++ spec/jar.rb
@@ -5,6 +5,10 @@ describe "logstash jar features" do
     @jar_root = __FILE__.split("!").first + "!"
   end
 
+  it "must be only run from a jar" do
+    insist { __FILE__ } =~ /file:.*!/
+  end
+
   it "must contain GeoLiteCity.dat" do
     path = File.join(@jar_root, "GeoLiteCity.dat")
     insist { File }.exists?(path)
