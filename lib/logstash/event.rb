d91cd81d6d4fb5f72d8b6af57810e5fa1fb49a47
Jordan Sissel <jls@semicomplete.com>
2012-11-21 23:57:57 -0800

    - 'Time.parse' claims no such method even after 'require "time"'.   I give up. Use DateTime.parse.

--- lib/logstash/event.rb
+++ lib/logstash/event.rb
@@ -1,8 +1,9 @@
 require "json"
+require "time"
+require "date"
 require "logstash/time"
 require "logstash/namespace"
 require "uri"
-require "time"
 
 # General event type. 
 # Basically a light wrapper on top of a hash.
@@ -74,7 +75,9 @@ class LogStash::Event
   def unix_timestamp
     if RUBY_ENGINE != "jruby"
       # This is really slow. See LOGSTASH-217
-      return Time.parse(timestamp).to_f
+      # For some reason, ::Time.parse isn't present even after 'require "time"'
+      # so use DateTime.parse
+      return ::DateTime.parse(timestamp).to_time.to_f
     else
       time = @@date_parser.parseDateTime(timestamp)
       return time.getMillis.to_f / 1000
@@ -82,7 +85,7 @@ class LogStash::Event
   end
 
   def ruby_timestamp
-    return Time.parse(timestamp)
+    return ::DateTime.parse(timestamp).to_time
   end  
   
   
