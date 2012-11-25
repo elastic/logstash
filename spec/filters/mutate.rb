553a7b1000e8a5898ac4ec196a7a94fb3bdc9e40
Laust Rud Jacobsen <laust@object.io>
2012-11-25 16:02:29 +0100

    Spec mutate filter: using explicit result expectations
    Should make the tests easier to reason about

--- spec/filters/mutate.rb
+++ spec/filters/mutate.rb
@@ -5,8 +5,6 @@ describe LogStash::Filters::Mutate do
   extend LogStash::RSpec
 
   describe "basics" do
-    # The logstash config goes here.
-    # At this time, only filters are supported.
     config <<-CONFIG
       filter {
         mutate {
@@ -15,25 +13,27 @@ describe LogStash::Filters::Mutate do
           convert => [ "intme", "integer", "floatme", "float" ]
           rename => [ "rename1", "rename2" ]
           replace => [ "replaceme", "hello world" ]
-          remove => [ "removeme" ] 
+          remove => [ "removeme" ]
         }
       }
     CONFIG
 
-    event = { "@fields" => { 
-      "lowerme" => [ "ExAmPlE" ], "upperme" => [ "ExAmPlE" ],
-      "intme" => [ "1234" ], "floatme" => [ "1234.455" ],
+    event = { "@fields" => {
+      "lowerme" => [ "ExAmPlE" ],
+      "upperme" => [ "ExAmPlE" ],
+      "intme" => [ "1234", "7890.4", "7.9" ],
+      "floatme" => [ "1234.455" ],
       "rename1" => [ "hello world" ],
       "replaceme" => [ "who cares" ],
-      "removeme" => [ "something" ] 
-      } 
-    } 
-    
+      "removeme" => [ "something" ]
+      }
+    }
+
     sample event do
-      insist { subject["lowerme"] } == subject["lowerme"].collect(&:downcase)
-      insist { subject["upperme"] } == subject["lowerme"].collect(&:upcase)
-      insist { subject["intme"] } == subject["intme"].collect(&:to_i)
-      insist { subject["floatme"] } == subject["floatme"].collect(&:to_f)
+      insist { subject["lowerme"] } == ['example']
+      insist { subject["upperme"] } == ['EXAMPLE']
+      insist { subject["intme"] }   == [1234, 7890, 7]
+      insist { subject["floatme"] } == [1234.455]
       reject { subject }.include?("rename1")
       insist { subject["rename2"] } == [ "hello world" ]
       reject { subject }.include?("removeme")
@@ -53,7 +53,7 @@ describe LogStash::Filters::Mutate do
     CONFIG
 
     sample "HELLO WORLD" do
-      insist { subject["foo"] } == subject["foo"].collect(&:downcase)
+      insist { subject["foo"] } == ['hello']
     end
   end
 end
