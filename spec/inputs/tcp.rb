f3907ede13c007e68d59eb04ccff0928229b01ce
Jordan Sissel <jls@semicomplete.com>
2012-12-31 15:46:39 -0800

    Switch to using one socket

--- spec/inputs/tcp.rb
+++ spec/inputs/tcp.rb
@@ -76,7 +76,7 @@ describe "inputs/tcp" do
       output = Shiftback.new do |event|
         sequence += 1
         begin
-          insist { event.message } == "Hello ü Û"
+          insist { event.message } == "Hello ü Û\n"
           insist { event.message.encoding } == Encoding.find("UTF-8")
         rescue Exception => failure
           # Get out of the threads nets
@@ -93,13 +93,11 @@ describe "inputs/tcp" do
         tcp.run(output)
       end
       #Send events from clients sockets
+      client_socket = TCPSocket.new("0.0.0.0", port)
       event_count.times do |value|
-        client_socket = TCPSocket.new("0.0.0.0", port)
-        client_socket.write "Hello ü Û"
-        client_socket.close
-        # micro sleep to ensure sequencing
-        sleep(0.1)
+        client_socket.write "Hello ü Û\n"
       end
+      client_socket.close
       #wait for input termination
       puts "Waiting for tcp input thread to finish"
       thread.join
