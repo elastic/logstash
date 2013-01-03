255306a32e18625f33a91f26556ee5fbfb980e2f
Jordan Sissel <jls@semicomplete.com>
2013-01-03 12:06:00 -0800

    - version bump

--- lib/logstash/version.rb
+++ lib/logstash/version.rb
@@ -1,5 +1,5 @@
 # The version of logstash.
-LOGSTASH_VERSION = "1.1.6"
+LOGSTASH_VERSION = "1.1.7"
 
 # Note to authors: this should not include dashes because 'gem' barfs if
 # you include a dash in the version string.
