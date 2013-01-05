13c25baccd332ce129fa3cf3cfa41dc0db75dc68
Louis Zuckerman <me@louiszuckerman.com>
2013-01-05 14:47:04 -0500

    Support deep fields (foo.bar) in date filter (LOGSTASH-724) allow literal dot with \.

--- lib/logstash/event.rb
+++ lib/logstash/event.rb
@@ -131,19 +131,22 @@ class LogStash::Event
   public
   def [](key)
     # If the key isn't in fields and it starts with an "@" sign, get it out of data instead of fields
-    if ! @data["@fields"].has_key?(key) and key.slice(0,1) == "@"
+    if ! @data["@fields"].has_key?(key.gsub(/\\\./, ".")) and key.slice(0,1) == "@"
       return @data[key]
-    elsif key.include?(".")
+    elsif key.index(/(?<!\\)\./)
       value = nil
-      obj = self
+      obj = @data["@fields"]
       # "." is what ES uses to access structured data, so adopt that
       # idea here, too.  "foo.bar" will access key "bar" under hash "foo".
-      key.split('.').each do |segment|
-        # try to safely cast segment to integer for the 0 in foo.0.bar
-        begin
-          segment = Integer(segment)
-        rescue Exception
-          #not an int, do nothing, segment remains a string
+      key.split(/(?<!\\)\./).each do |segment|
+        segment.gsub!(/\\\./, ".")
+        if (obj.is_a?(Array) || (obj.is_a?(Hash) && !obj.member?(segment)) )
+          # try to safely cast segment to integer for the 0 in foo.0.bar
+          begin
+            segment = Integer(segment)
+          rescue Exception
+            #not an int, do nothing, segment remains a string
+          end
         end
         if obj
           value = obj[segment] rescue nil
@@ -155,7 +158,7 @@ class LogStash::Event
       end # key.split.each
       return value
     else
-      return @data["@fields"][key]
+      return @data["@fields"][key.gsub(/\\\./, ".")]
     end
   end # def []
   
