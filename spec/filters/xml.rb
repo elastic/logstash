3e252b4c782525e85495ea28895db76ccf21c14b
Richard Pijnenburg <richard@ispavailability.com>
2013-01-12 13:42:21 +0100

    Deprecating regexp fields
    - Replaced regexp fields with source and target variable
    - Added tests for new variables

--- spec/filters/xml.rb
+++ spec/filters/xml.rb
@@ -4,7 +4,7 @@ require "logstash/filters/xml"
 describe LogStash::Filters::Xml do
   extend LogStash::RSpec
 
-  describe "parse standard xml" do
+  describe "parse standard xml (Deprecated checks)" do
     config <<-CONFIG
     filter {
       xml {
@@ -36,7 +36,7 @@ describe LogStash::Filters::Xml do
     end
   end
 
-  describe "parse standard xml but do not store" do
+  describe "parse standard xml but do not store (Deprecated checks)" do
     config <<-CONFIG
     filter {
       xml {
@@ -52,7 +52,7 @@ describe LogStash::Filters::Xml do
     end
   end
 
-  describe "parse xml and store values with xpath" do
+  describe "parse xml and store values with xpath (Deprecated checks)" do
     config <<-CONFIG
     filter {
       xml {
@@ -74,4 +74,81 @@ describe LogStash::Filters::Xml do
       insist { subject["xpath_field"]} == ["value1","value2"]
     end
   end
+
+  ## New tests
+
+  describe "parse standard xml" do
+    config <<-CONFIG
+    filter {
+      xml {
+        source => "xmldata"
+        target => "data"
+      }
+    }
+    CONFIG
+
+    sample({"@fields" => {"xmldata" => '<foo key="value"/>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["data"]} == {"key" => "value"}
+    end
+
+    #From parse xml with array as a value
+    sample({"@fields" => {"xmldata" => '<foo><key>value1</key><key>value2</key></foo>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["data"]} == {"key" => ["value1", "value2"]}
+    end
+
+    #From parse xml with hash as a value
+    sample({"@fields" => {"xmldata" => '<foo><key1><key2>value</key2></key1></foo>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["data"]} == {"key1" => [{"key2" => ["value"]}]}
+    end
+
+    #From bad xml
+    sample({"@fields" => {"xmldata" => '<foo /'}}) do
+      insist { subject.tags}.include?("_xmlparsefailure")
+    end
+  end
+
+  describe "parse standard xml but do not store" do
+    config <<-CONFIG
+    filter {
+      xml {
+        source => "xmldata"
+        target => "data"
+        store_xml => false
+      }
+    }
+    CONFIG
+
+    sample({"@fields" => {"xmldata" => '<foo key="value"/>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["data"]} == nil
+    end
+  end
+
+  describe "parse xml and store values with xpath" do
+    config <<-CONFIG
+    filter {
+      xml {
+        source => "xmldata"
+        target => "data"
+        xpath => [ "/foo/key/text()", "xpath_field" ]
+      }
+    }
+    CONFIG
+
+    # Single value
+    sample({"@fields" => {"xmldata" => '<foo><key>value</key></foo>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["xpath_field"]} == ["value"]
+    end
+
+    #Multiple values
+    sample({"@fields" => {"xmldata" => '<foo><key>value1</key><key>value2</key></foo>'}}) do
+      reject { subject.tags}.include?("_xmlparsefailure")
+      insist { subject["xpath_field"]} == ["value1","value2"]
+    end
+  end
+
 end
