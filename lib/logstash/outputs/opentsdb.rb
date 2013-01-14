97c577f2508a8d833176a025f24c18e141299f2a
Emmet Murphy <emmet@onekingslane.com>
2013-01-14 15:28:25 -0800

    LOGSTASH-689: prefix OpenTSDB message with 'put '

--- lib/logstash/outputs/opentsdb.rb
+++ lib/logstash/outputs/opentsdb.rb
@@ -55,7 +55,7 @@ class LogStash::Outputs::Opentsdb < LogStash::Outputs::Base
   def receive(event)
     return unless output?(event)
 
-    # Opentsdb message format: metric timestamp value tagname=tagvalue tag2=value2\n
+    # Opentsdb message format: put metric timestamp value tagname=tagvalue tag2=value2\n
 
     # Catch exceptions like ECONNRESET and friends, reconnect on failure.
     begin
@@ -64,7 +64,8 @@ class LogStash::Outputs::Opentsdb < LogStash::Outputs::Base
       tags = metrics[2..-1]
 
       # The first part of the message
-      message = [event.sprintf(name),
+      message = ['put',
+                 event.sprintf(name),
                  event.sprintf("%{+%s}"),
                  event.sprintf(value),
       ].join(" ")
