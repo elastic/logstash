e9c79f27116f36071aaa4c3ffd266373ffd25cbb
Jordan Sissel <jls@semicomplete.com>
2013-01-15 00:47:10 +0000

    - add index.html - add more data to the runs

--- test/speed/run.sh
+++ test/speed/run.sh
@@ -2,6 +2,7 @@
 
 root=$(dirname $0)/../../
 workdir="$1"
+index="$workdir/_index.tsv"
 
 if [ -z "$workdir" ] ; then
   echo "Usage: $0 <output dir>"
@@ -11,17 +12,43 @@ fi
 run() {
   caller="${FUNCNAME[1]}"
   out="$workdir/${caller}.csv"
+
+  # record this run's name and arguments
+  echo "$caller	$@" >> $index
+
+  # csv header
   echo "timestamp,count,rate_1m" > $out
-  $root/bin/logstash agent -f <(m4 -DPATH="$out" bench.conf.erb)
+
+  # run logstash
+  env "$@" $root/bin/logstash agent -f <(m4 -DPATH="$out" bench.conf.erb)
 }
 
 default() {
-  JRUBY_OPTS= JAVA_OPTS= run
+  run JRUBY_OPTS= JAVA_OPTS=
 }
 
 indy() {
-  JRUBY_OPTS="-Xjit.max=100000 -Xcompile.invokedynamic=true -Xcompile.fastest=true" run
+  run JRUBY_OPTS="-Xcompile.invokedynamic=true"
 }
 
+gctune() {
+  run JAVA_OPTS="-XX:ReservedCodeCacheSize=128m -XX:+UseBiasedLocking -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError"
+}
+
+cp index.html $workdir/index.html
+
+(
+  echo "# logstash perf tests"
+
+  echo "## ruby"
+  ruby -v |& sed -e 's/^/    /'
+
+  echo "## java"
+  java -version |& sed -e 's/^/    /'
+) > $workdir/README.md
+
+
+echo "name	env" > $index
 default
 indy
+gctune
