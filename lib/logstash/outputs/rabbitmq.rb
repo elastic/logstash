a00829a9ec306f2fe5547d723d944bd0d80c1ed8
Michael Zaccari <michael.zaccari@gmail.com>
2013-01-25 15:57:54 -0500

    Change AMQP to RabbitMQ names

--- lib/logstash/outputs/rabbitmq.rb
+++ lib/logstash/outputs/rabbitmq.rb
@@ -1,30 +1,31 @@
 require "logstash/outputs/base"
 require "logstash/namespace"
 
-# Push events to an AMQP exchange.
+# Push events to a RabbitMQ exchange.
 #
-# <b> NOTE: THIS IS ONLY KNOWN TO WORK WITH RECENT RELEASES OF RABBITMQ. Any
-# other amqp broker will not work with this plugin. I do not know why. If you
-# need support for brokers other than rabbitmq, please file bugs here:
-# <https://github.com/ruby-amqp/bunny> </b>
-class LogStash::Outputs::Amqp < LogStash::Outputs::Base
+#   RabbitMQ - <http://www.rabbitmq.com/>
+#   Bunny - <https://github.com/ruby-amqp/bunny>
+class LogStash::Outputs::RabbitMQ < LogStash::Outputs::Base
   MQTYPES = [ "fanout", "direct", "topic" ]
 
-  config_name "amqp"
-  plugin_status "unsupported"
+  config_name "rabbitmq"
+  plugin_status "beta"
 
-  # Your amqp server address
+  # Your rabbitmq server address
   config :host, :validate => :string, :required => true
 
-  # The AMQP port to connect on
+  # The rabbitmq port to connect on
   config :port, :validate => :number, :default => 5672
 
-  # Your amqp username
+  # Your rabbitmq username
   config :user, :validate => :string, :default => "guest"
 
-  # Your amqp password
+  # Your rabbitmq password
   config :password, :validate => :password, :default => "guest"
 
+  # The name of the exchange
+  config :exchange, :validate => :string, :required => true
+  
   # The exchange type (fanout, topic, direct)
   config :exchange_type, :validate => [ "fanout", "direct", "topic"], :required => true
 
@@ -46,7 +47,7 @@ class LogStash::Outputs::Amqp < LogStash::Outputs::Base
   # Is this exchange durable? (aka; Should it survive a broker restart?)
   config :durable, :validate => :boolean, :default => true
 
-  # Should messages persist to disk on the AMQP broker until they are read by a
+  # Should messages persist to disk on the rabbitmq broker until they are read by a
   # consumer?
   config :persistent, :validate => :boolean, :default => true
 
@@ -76,27 +77,28 @@ class LogStash::Outputs::Amqp < LogStash::Outputs::Base
 
   public
   def connect
-    amqpsettings = {
+    
+    rabbitmq_settings = {
       :vhost => @vhost,
       :host => @host,
       :port => @port,
       :logging => @debug,
     }
-    amqpsettings[:user] = @user if @user
-    amqpsettings[:pass] = @password.value if @password
-    amqpsettings[:ssl] = @ssl if @ssl
-    amqpsettings[:verify_ssl] = @verify_ssl if @verify_ssl
+    rabbitmq_settings[:user] = @user if @user
+    rabbitmq_settings[:pass] = @password.value if @password
+    rabbitmq_settings[:ssl] = @ssl if @ssl
+    rabbitmq_settings[:verify_ssl] = @verify_ssl if @verify_ssl
 
     begin
-      @logger.debug("Connecting to AMQP", :settings => amqpsettings,
+      @logger.debug("Connecting to RabbitMQ", :settings => rabbitmq_settings,
                     :exchange_type => @exchange_type, :name => @exchange)
-      @bunny = Bunny.new(amqpsettings)
+      @bunny = Bunny.new(rabbitmq_settings)
       @bunny.start
     rescue => e
       if terminating?
         return
       else
-        @logger.error("AMQP connection error (during connect), will reconnect",
+        @logger.error("RabbitMQ connection error (during connect), will reconnect",
                       :exception => e, :backtrace => e.backtrace)
         sleep(1)
         retry
@@ -133,10 +135,10 @@ class LogStash::Outputs::Amqp < LogStash::Outputs::Base
         @logger.debug(["Publishing message", { :destination => to_s, :message => message, :key => key }])
         @bunnyexchange.publish(message, :persistent => @persistent, :key => key)
       else
-        @logger.warn("Tried to send message, but not connected to amqp yet.")
+        @logger.warn("Tried to send message, but not connected to rabbitmq yet.")
       end
     rescue *[Bunny::ServerDownError, Errno::ECONNRESET] => e
-      @logger.error("AMQP connection error (during publish), will reconnect: #{e}")
+      @logger.error("RabbitMQ connection error (during publish), will reconnect: #{e}")
       connect
       retry
     end
@@ -154,4 +156,4 @@ class LogStash::Outputs::Amqp < LogStash::Outputs::Base
     @bunnyexchange = nil
     finished
   end # def teardown
-end # class LogStash::Outputs::Amqp
+end # class LogStash::Outputs::RabbitMQ
