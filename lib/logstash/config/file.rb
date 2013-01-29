61f183844cd56a50a4ec1143dda6b7653f16fa3a
Michael Zaccari <michael.zaccari@gmail.com>
2013-01-28 22:36:42 -0500

    Minor rename amqp -> rabbitmq for file config doc

--- lib/logstash/config/file.rb
+++ lib/logstash/config/file.rb
@@ -43,7 +43,7 @@ class LogStash::Config::File
       tryload o[:type], :base
       type = registry[o[:type]]
 
-      # Load the plugin itself (inputs/file, outputs/amqp, etc)
+      # Load the plugin itself (inputs/file, outputs/rabbitmq, etc)
       # TODO(sissel): Error handling
       tryload o[:type], o[:plugin]
       plugin = registry[o[:plugin]]
@@ -84,7 +84,7 @@ class LogStash::Config::File
     @config.each do |type, plugin_config_array|
       # plugin_config_array has arrays of each component config:
       # input {
-      #   amqp { ... }
+      #   rabbitmq { ... }
       #   file { ... }
       #   file { ... }
       # }
