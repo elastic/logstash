f3d3482433bff1a82b0f1ee75473393d8676fe9e
Darren Patterson <darrenp1@stanford.edu>
2013-02-01 10:41:45 -0800

    Revert "Add agent configtest option for use when verifying logstash config files prior to restarting the logstash service"
    This reverts commit 2c2b076129c30c9c17f8d6d5507e778c6169e407.

--- lib/logstash/agent.rb
+++ lib/logstash/agent.rb
@@ -29,7 +29,6 @@ class LogStash::Agent
   attr_reader :config_path
   attr_reader :logfile
   attr_reader :verbose
-  attr_reader :configtest
 
   public
   def initialize
@@ -43,7 +42,6 @@ class LogStash::Agent
     @verbose = 0
     @filterworker_count = 1
     @watchdog_timeout = 10
-    @configtest = false
 
     @plugins = {}
     @plugins_mutex = Mutex.new
@@ -101,10 +99,6 @@ class LogStash::Agent
 
     opts.on("-l", "--log FILE", "Log to a given path. Default is stdout.") do |path|
       @logfile = path
-    end
-
-    opts.on("-t", "--configtest", "Test configuration and exit.") do |arg|
-        @configtest = true
     end
 
     opts.on("-v", "Increase verbosity") do
@@ -334,10 +328,6 @@ class LogStash::Agent
     # Load the config file
     @logger.info("Read config")
     config = read_config
-    if @configtest
-      puts "Config test passed.  Exiting..."
-      exit (0)
-    end
 
     @logger.info("Start thread")
     @thread = Thread.new do
