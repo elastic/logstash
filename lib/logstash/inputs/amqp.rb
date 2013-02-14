90af41a8158d73f644ca7bf30605643f8e6e1e48
Jordan Sissel <jls@semicomplete.com>
2013-02-14 12:12:43 -0800

    - correctly implement 'amqp' backwards-compat as an alias for 'rabbitmq'

--- lib/logstash/inputs/amqp.rb
+++ lib/logstash/inputs/amqp.rb
@@ -1,6 +1,8 @@
 require "logstash/inputs/rabbitmq"
 
 class LogStash::Inputs::AMQP < LogStash::Inputs::RabbitMQ
+  config_name "amqp"
+  plugin_status "beta"
   def register
     @logger.warn("The 'amqp' input plugin has been renamed to 'rabbitmq'. " \
                  "Please update your configuration appropriately.")
--- lib/logstash/outputs/amqp.rb
+++ lib/logstash/outputs/amqp.rb
@@ -1,6 +1,8 @@
 require "logstash/outputs/rabbitmq"
 
 class LogStash::Outputs::AMQP < LogStash::Outputs::RabbitMQ
+  config_name "amqp"
+  plugin_status "beta"
   def register
     @logger.warn("The 'amqp' output plugin has been renamed to 'rabbitmq'. " \
                  "Please update your configuration appropriately.")
