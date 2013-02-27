4d8df5667df97f017ef16c7052075085afa01e95
Jordan Sissel <jls@semicomplete.com>
2013-02-27 08:24:03 -0800

    - Add coverage for the problem reported in pull #375

---
+++ spec/support/pull375.rb
@@ -0,0 +1,21 @@
+# This spec covers the question here:
+# https://github.com/logstash/logstash/pull/375
+
+require "test_utils"
+
+describe "pull #375" do
+  extend LogStash::RSpec
+  describe  "kv after grok" do
+    config <<-CONFIG
+      filter {
+        grok { pattern => "%{URIPATH:mypath}%{URIPARAM:myparams}" }
+        kv { fields => [ "myparams" ] field_split => "&?" }
+      }
+    CONFIG
+
+    sample "/some/path?foo=bar&baz=fizz" do
+      insist { subject["foo"] } == "bar"
+      insist { subject["baz"] } == "fizz"
+    end
+  end
+end
