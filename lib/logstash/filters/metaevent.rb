cf9e13df07ee814dbc76821566d4c626ccca30f4
Bright Fulton <bright.fulton@gmail.com>
2013-03-29 15:33:38 -0400

    working proof of concept

--- lib/logstash/filters/metaevent.rb
+++ lib/logstash/filters/metaevent.rb
@@ -5,22 +5,28 @@ class LogStash::Filters::Metaevent < LogStash::Filters::Base
   config_name "metaevent"
   plugin_status "experimental"
 
+  # syntax: `followed_by_tags => [ "tag", "tag" ]`
   config :followed_by_tags, :validate => :array, :required => true
 
-  config :period, :validate => :number, :required => true
+  # syntax: `period => 60`
+  config :period, :validate => :number, :default => 30
 
   def register
     @logger.debug("registering")
-    reset_state
+    @metaevents = []
   end
 
   def filter(event)
     if filter?(event)
       start_period(event)
-    elsif within_period && followed_by_tags_match(event)
-      trigger(event)
+    elsif within_period(event)
+      if followed_by_tags_match(event)
+        trigger(event)
+      else
+        @logger.debug(["metaevent", @add_tag, "ignoring (tags don't match)", event])
+      end
     else
-      @logger.debug([@add_tag, "ignoring", event])
+      @logger.debug(["metaevent", @add_tag, "ignoring (not in period)", event])
     end
   end
 
@@ -35,25 +41,27 @@ class LogStash::Filters::Metaevent < LogStash::Filters::Base
   private
 
   def start_period(event)
-    @logger.debug([@add_tag, "start_period", event])
+    @logger.debug(["metaevent", @add_tag, "start_period", event])
     @start_event = event
   end
 
   def trigger(event)
-    @logger.debug([@add_tag, "trigger", event])
-    reset_state
+    @logger.debug(["metaevent", @add_tag, "trigger", event])
+
+    event = LogStash::Event.new
+    event.source_host = Socket.gethostname
+    event["tags"] = @add_tag
+
+    @metaevents << event
+    @start_event = nil
   end
 
   def followed_by_tags_match(event)
-    !event.empty? && (event.tags & @followed_by_tags).size == @followed_by_tags.size
+    (event.tags & @followed_by_tags).size == @followed_by_tags.size
   end
 
-  def within_period
-    @start_event.ruby_timestamp + @period < Time.now
-  end
-
-  def reset_state
-    @metaevents = []
-    @start_event = nil
+  def within_period(event)
+    time_delta = event.ruby_timestamp - @start_event.ruby_timestamp
+    time_delta >= 0 && time_delta <= @period
   end
 end
