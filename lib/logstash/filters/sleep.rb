40d70c6f0ddbf6a054ee5defdf7264e8cc6432d4
Jordan Sissel <jls@semicomplete.com>
2013-03-29 19:33:40 -0700

    - add 'replay' mode to the sleep filter. This enables replaying of events on a scale of the original event timeline. You can change the speed by setting 'time' to a multiplier (2 == 2x faster, 0.5 == half as fast)

--- lib/logstash/filters/sleep.rb
+++ lib/logstash/filters/sleep.rb
@@ -4,6 +4,7 @@ require "logstash/namespace"
 # Sleep a given amount of time. This will cause logstash
 # to stall for the given amount of time. This is useful
 # for rate limiting, etc.
+#
 class LogStash::Filters::Sleep < LogStash::Filters::Base
   config_name "sleep"
   plugin_status "experimental"
@@ -20,23 +21,64 @@ class LogStash::Filters::Sleep < LogStash::Filters::Base
   #     filter {
   #       sleep {
   #         # Sleep 1 second for every event.
-  #         duration => "1"
+  #         time => "1"
   #       }
   #     }
-  config :duration, :validate => :string, :required => true
+  config :time, :validate => :string
+
+  # Enable replay mode.
+  #
+  # Replay mode tries to sleep based on timestamps in each event.
+  #
+  # The amount of time to sleep is computed by subtracting the
+  # previous event's timestamp from the current event's timestamp.
+  # This helps you replay events in the same timeline as original.
+  #
+  # If you specify a `time` setting as well, this filter will
+  # use the `time` value as a speed modifier. For example,
+  # a `time` value of 2 will replay at double speed, while a
+  # value of 0.25 will replay at 1/4th speed.
+  #
+  # For example:
+  #
+  #     filter {
+  #       sleep {
+  #         time => 2
+  #         replay => true
+  #       }
+  #     }
+  #
+  # The above will sleep in such a way that it will perform
+  # replay 2-times faster than the original time speed.
+  config :replay, :validate => :boolean, :default => false
 
   public
   def register
-    # nothing to do
+    if @replay && @time.nil?
+      # Default time multiplier is 1 when replay is set.
+      @time = 1
+    end
   end # def register
 
   public
   def filter(event)
     return unless filter?(event)
 
-    case @duration
-      when Fixnum, Float; sleep(@duration)
-      else; sleep(event.sprintf(@duration).to_f)
+    case @time
+      when Fixnum, Float; time = @time
+      when nil; # nothing
+      else; time = event.sprintf(@time).to_f
+    end
+
+    if @replay
+      clock = event.ruby_timestamp.to_f
+      if @last_clock
+        delay = clock - @last_clock
+        sleep(delay / time)
+      end
+      @last_clock = clock
+    else
+      sleep(time)
     end
     filter_matched(event)
   end # def filter
