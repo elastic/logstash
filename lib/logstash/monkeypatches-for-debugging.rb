20233eacccf90e72fca2da88b1ceae9c3c688b9a
Jordan Sissel <jls@semicomplete.com>
2013-04-17 12:28:29 -0700

    - debug load() calls with DEBUG=require, too

--- lib/logstash/monkeypatches-for-debugging.rb
+++ lib/logstash/monkeypatches-for-debugging.rb
@@ -6,5 +6,12 @@ if $DEBUGLIST.include?("require")
       puts "require(\"#{path}\")"
       return require_debug(path)
     end
+
+    alias_method :load_debug, :load
+
+    def load(path)
+      puts "load(\"#{path}\")"
+      return load_debug(path)
+    end
   end
 end
