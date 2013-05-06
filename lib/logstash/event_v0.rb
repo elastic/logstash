7ce90b2696f46b29dd0e38a32e869055d8bd7a20
Piavlo <lolitushka@gmail.com>
2013-05-06 16:48:11 +0300

    avoid inserting duplicate tags

--- lib/logstash/event_v0.rb
+++ lib/logstash/event_v0.rb
@@ -4,6 +4,7 @@ require "date"
 require "logstash/time_addon"
 require "logstash/namespace"
 require "uri"
+require "set"
 
 # General event type. 
 # Basically a light wrapper on top of a hash.
@@ -17,7 +18,7 @@ module LogStash::EventV0
 
     @data = {
       "@source" => "unknown",
-      "@tags" => [],
+      "@tags" => Set.new,
       "@fields" => {},
     }
     @data.merge!(data) unless data.nil?
