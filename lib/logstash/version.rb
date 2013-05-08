de0019e7c8ca959f37b3a9fe6d631766f7ae3ebe
Jordan Sissel <jls@semicomplete.com>
2013-05-07 17:00:25 -0700

    - dev build

--- lib/logstash/version.rb
+++ lib/logstash/version.rb
@@ -1,5 +1,5 @@
 # The version of logstash.
-LOGSTASH_VERSION = "1.1.12"
+LOGSTASH_VERSION = "1.1.13.dev"
 
 # Note to authors: this should not include dashes because 'gem' barfs if
 # you include a dash in the version string.
