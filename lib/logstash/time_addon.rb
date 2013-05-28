b3c5cc246cc6f9c99821348983f16b60359b5bd5
Jordan Sissel <jls@semicomplete.com>
2013-05-28 10:52:27 -0700

    - make @timestamp a Time object. This removes a bunch of old string date   parsing code (hurray!)
      This code continues to be a work in progress and may not work yet.

--- lib/logstash/time_addon.rb
+++ lib/logstash/time_addon.rb
@@ -8,25 +8,8 @@ require "logstash/namespace"
 #   >> LogStash::Time.now.utc.to_iso8601
 #   => "2010-10-17 07:25:26.788704Z"
 module LogStash::Time
-  if defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
-    require "java"
-    DateTime = org.joda.time.DateTime
-    DateTimeZone = org.joda.time.DateTimeZone
-    def self.now
-      # org.joda.time.DateTime#to_s returns the time in ISO8601 form :)
-      # Could call DateTime.new(DateTimeZone::UTC) but JRuby calls the
-      # DateTime#new(Object) constructor instead of the
-      # DateTime#new(DateTimeZone) constructor. I was unable to get java_send to invoke this constructor,
-      # so instead I have to do DateTime#new#withZone(UTC)
-      return DateTime.new.withZone(DateTimeZone::UTC).to_s
-    end # def initialize
-  else
-    # Otherwise, use ruby stdlib Time, which is much slower than Joda.
-    ISO8601_STRFTIME = "%04d-%02d-%02dT%02d:%02d:%02d.%06d%+03d:00".freeze
-    def self.now
-      now = Time.new.utc
-      return sprintf(ISO8601_STRFTIME, now.year, now.month, now.day, now.hour,
-                     now.min, now.sec, now.tv_usec, now.utc_offset / 3600)
-    end
+  ISO8601_STRFTIME = "%04d-%02d-%02dT%02d:%02d:%02d.%06d%+03d:00".freeze
+  def self.now
+    return Time.new.utc
   end
 end # module LogStash::Time
