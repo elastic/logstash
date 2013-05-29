209546d3133e37039e21a74e43359a47f91dabf9
Jordan Sissel <jls@semicomplete.com>
2013-05-28 23:20:18 -0700

    - add documentation for sqlite input w/ example - style cleanup - exponential backoff when no new rows are found - store columns as fields

--- lib/logstash/inputs/sqlite.rb
+++ lib/logstash/inputs/sqlite.rb
@@ -1,119 +1,186 @@
 require "logstash/inputs/base"
 require "logstash/namespace"
-require "sequel"
-require "jdbc/sqlite3" 
+require "socket"
 
+# Read rows from an sqlite database.
+#
+# This is most useful in cases where you are logging directly to a table.
+# Any tables being watched must have an 'id' column that is monotonically
+# increasing.
+#
+# All tables are read by default except:
+# * ones matching 'sqlite_%' - these are internal/adminstrative tables for sqlite
+# * 'since_table' - this is used by this plugin to track state.
+#
+# ## Example
+# 
+#     % sqlite /tmp/example.db
+#     sqlite> CREATE TABLE weblogs (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         ip STRING,
+#         request STRING,
+#         response INTEGER);
+#     sqlite> INSERT INTO weblogs (ip, request, response) 
+#         VALUES ("1.2.3.4", "/index.html", 200);
+#
+# Then with this logstash config:
+#
+#     input {
+#       sqlite {
+#         path => "/tmp/example.db"
+#         type => weblogs
+#       }
+#     }
+#     output {
+#       stdout {
+#         debug => true
+#       }
+#     }
+#
+# Sample output:
+#
+#     {
+#       "@source"      => "sqlite://sadness/tmp/x.db",
+#       "@tags"        => [],
+#       "@fields"      => {
+#         "ip"       => "1.2.3.4",
+#         "request"  => "/index.html",
+#         "response" => 200
+#       },
+#       "@timestamp"   => "2013-05-29T06:16:30.850Z",
+#       "@source_host" => "sadness",
+#       "@source_path" => "/tmp/x.db",
+#       "@message"     => "",
+#       "@type"        => "foo"
+#     }
+#
 class LogStash::Inputs::Sqlite < LogStash::Inputs::Base
-    config_name "sqlite"
-    plugin_status "experimental"
+  config_name "sqlite"
+  plugin_status "experimental"
 
-    config :dbfile, :validate => :string, :required => true
-    config :exclude, :validate => :array
-    config :stat_interval, :validate => :number, :default => 15
+  # The path to the sqlite database file.
+  config :path, :validate => :string, :required => true
 
-    public
-    def init_placeholder_table(db)
-      begin
-        db.create_table :since_table do 
-          String :table
-          Int    :place
+  # Any tables to exclude by name.
+  # By default all tables are followed.
+  config :exclude_tables, :validate => :array, :default => []
+
+  # How many rows to fetch at a time from each SELECT call.
+  config :batch, :validate => :number, :default => 5
+
+  SINCE_TABLE = :since_table
+
+  public
+  def init_placeholder_table(db)
+    begin
+      db.create_table SINCE_TABLE do 
+        String :table
+        Int    :place
+      end
+    rescue
+      @logger.debug("since tables already exists")
+    end
+  end
+
+  public
+  def get_placeholder(db, table)
+    since = db[SINCE_TABLE]
+    x = since.where(:table => "#{table}")
+    p x
+    if x[:place].nil?
+      init_placeholder(db, table) 
+      return 0
+    else
+      @logger.debug("placeholder already exists, it is #{x[:place]}")
+      return x[:place][:place]
+    end
+  end
+
+  public 
+  def init_placeholder(db, table)
+    @logger.debug("init placeholder for #{table}")
+    since = db[SINCE_TABLE]
+    since.insert(:table => table, :place => 1)
+  end
+
+  public
+  def update_placeholder(db, table, place)
+    @logger.debug("set placeholder to #{place}")
+    since = db[SINCE_TABLE]
+    since.where(:table => table).update(:place => place)
+  end
+
+  public 
+  def get_all_tables(db)
+    return db["SELECT * FROM sqlite_master WHERE type = 'table' AND tbl_name != '#{SINCE_TABLE}' AND tbl_name NOT LIKE 'sqlite_%'"].map { |t| t[:name] }.select { |n| !@exclude_tables.include?(n) }
+  end
+  
+  public
+  def get_n_rows_from_table(db, table, offset, limit)
+    dataset = db["SELECT * FROM #{table}"]
+    return db["SELECT * FROM #{table} WHERE (id > #{offset}) ORDER BY 'id' LIMIT #{limit}"].map { |row| row }
+  end
+  
+  public
+  def register
+    require "sequel"
+    require "jdbc/sqlite3" 
+    @host = Socket.gethostname
+    LogStash::Util::set_thread_name("input|sqlite|#{@path}")
+    @logger.info("Registering sqlite input", :database => @path)
+    @db = Sequel.connect("jdbc:sqlite:#{@path}") 
+    @tables = get_all_tables(@db)
+    @table_data = {}
+    @tables.each do |table|
+      init_placeholder_table(@db)
+      last_place = get_placeholder(@db, table)
+      @table_data[table] = { :name => table, :place => last_place }
+    end
+  end # def register
+
+  public
+  def run(queue)
+    sleep_min = 0.01
+    sleep_max = 5
+    sleeptime = sleep_min
+
+    begin
+      @logger.debug("Tailing sqlite db", :path => @path)
+      loop do
+        count = 0
+        @table_data.each do |k, table|
+          table_name = table[:name]
+          offset = table[:place]
+          @logger.debug("offset is #{offset}", :k => k, :table => table_name)
+          rows = get_n_rows_from_table(@db, table_name, offset, @batch)
+          count += rows.count
+          rows.each do |row| 
+            e = to_event("", "sqlite://#{@host}/#{@path}")
+
+            # store each column as a field in the event.
+            row.each do |column, element|
+              next if column == :id
+              e[column.to_s] = element
+            end
+            queue << e
+            @table_data[k][:place] = row[:id]
+          end
+          # Store the last-seen row in the database
+          update_placeholder(@db, table_name, @table_data[k][:place])
         end
-      rescue
-        @logger.debug('since tables already exists')
-      end
-    end
 
-    public
-    def get_placeholder(db, table)
-      since = db[:since_table]
-      x = since.where(:table => "#{table}")
-      p x
-      if x[:place].nil?
-        p 'place is 0'
-        init_placeholder(db, table) 
-        return 0
-      else
-        @logger.debug("placeholder already exists, it is #{x[:place]}")
-        return x[:place][:place]
-      end
-    end
-
-    public 
-    def init_placeholder(db, table)
-      @logger.debug("init placeholder for #{table}")
-      since = db[:since_table]
-      since.insert(:table => table, :place => 1)
-    end
-
-    public
-    def update_placeholder(db, table, place)
-      @logger.debug("set placeholder to #{place}")
-      since = db[:since_table]
-      since.where(:table => table).update(:place => place)
-    end
-
-    public 
-    def get_all_tables(db)
-      tables = db["SELECT * FROM sqlite_master WHERE type = 'table'"].map {|table| table[:name]}
-      tables.delete_if { |table| table == 'since_table' }
-      return tables
-    end
-    
-    public
-    def get_n_rows_from_table(db, table, offset, limit)
-      dataset = db["SELECT * FROM #{table}"]
-      return db["SELECT * FROM #{table} WHERE (id >= #{offset}) ORDER BY 'id' LIMIT #{limit}"].map { |row| row }
-    end
-    
-    public
-    def register
-      require "digest/md5"
-      LogStash::Util::set_thread_name("input|sqlite|#{dbfile}")
-      @logger.info("Registering sqlite input", :database => @dbfile)
-      @format = "json_event"
-      @DB = Sequel.connect("jdbc:sqlite:#{dbfile}") 
-      @tables = get_all_tables(@DB)
-      @table_data = Hash.new
-      @tables.each{ |table|
-        init_placeholder_table(@DB)
-        last_place = get_placeholder(@DB, table)
-        @table_data[table] = { :name => table, :place => last_place }
-      }
-
-    end # def register
-
-    public
-    def run(queue)
-      begin
-        @logger.debug("Tailing sqlite db'#{@dbfile}'")
-        loop do
-          @table_data.each{ |k, table|
-            table_name = table[:name]
-            offset = table[:place]
-            limit = 5
-            @logger.debug("offset is #{offset}")
-            lines = get_n_rows_from_table(@DB, table_name, offset, limit)
-            lines.each{ |line| 
-                line.delete(:id)
-                entry = {}
-                line.each { |name,element|
-                  entry["@#{name}"] = element
-                }
-
-                begin
-                  e = to_event(JSON.dump(entry), "sqlite://#{@dbfile}")
-                rescue EOFError => ex
-                  # stdin closed, finish
-                  break
-                end
-                queue << e if e
-            }
-            update_placeholder(@DB, table_name, offset+limit)
-            @table_data[k][:place] = offset+limit
-          }
-        end # loop
-      end # begin/rescue
-    end #run
+        if count == 0
+          # nothing found in that iteration
+          # sleep a bit
+          @logger.debug("No new rows. Sleeping.", :time => sleeptime)
+          sleeptime = [sleeptime * 2, sleep_max].min
+          sleep(sleeptime)
+        else
+          sleeptime = sleep_min
+        end
+      end # loop
+    end # begin/rescue
+  end #run
 
 end # class Logtstash::Inputs::EventLog
 
