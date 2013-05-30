6d64774c755125a40dbaf7055966ca80fd378b2f
Jordan Sissel <jls@semicomplete.com>
2013-05-29 20:09:00 -0700

    - make anonymize specs pass under event v1

--- spec/filters/anonymize.rb
+++ spec/filters/anonymize.rb
@@ -17,7 +17,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "233.255.13.44"} do
+    sample("clientip" => "233.255.13.44") do
       insist { subject["clientip"] } == "233.255.13.0"
     end
   end
@@ -33,7 +33,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.52.122.33"} do
+    sample("clientip" => "123.52.122.33") do
       insist { subject["clientip"] } == 1541804874
     end
   end
@@ -51,7 +51,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.123.123.123"} do
+    sample("clientip" => "123.123.123.123") do
       insist { subject["clientip"] } == "fdc60acc4773dc5ac569ffb78fcb93c9630797f4"
     end
   end
@@ -70,7 +70,7 @@ describe LogStash::Filters::Anonymize do
       #}
     #CONFIG
 
-    #sample "@fields" => {"clientip" => "123.123.123.123"} do
+    #sample("clientip" => "123.123.123.123") do
       #insist { subject["clientip"] } == "5744bbcc4f64acb6a805b7fee3013a8958cc8782d3fb0fb318cec915"
     #end
   #end
@@ -88,7 +88,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.123.123.123"} do
+    sample("clientip" => "123.123.123.123") do
       insist { subject["clientip"] } == "345bec3eff242d53b568916c2610b3e393d885d6b96d643f38494fd74bf4a9ca"
     end
   end
@@ -106,7 +106,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.123.123.123"} do
+    sample("clientip" => "123.123.123.123") do
       insist { subject["clientip"] } == "22d4c0e8c4fbcdc4887d2038fca7650f0e2e0e2457ff41c06eb2a980dded6749561c814fe182aff93e2538d18593947a"
     end
   end
@@ -124,7 +124,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.123.123.123"} do
+    sample("clientip" => "123.123.123.123") do
       insist { subject["clientip"] } == "11c19b326936c08d6c50a3c847d883e5a1362e6a64dd55201a25f2c1ac1b673f7d8bf15b8f112a4978276d573275e3b14166e17246f670c2a539401c5bfdace8"
     end
   end
@@ -143,7 +143,7 @@ describe LogStash::Filters::Anonymize do
       #}
     #CONFIG
 #
-    #sample "@fields" => {"clientip" => "123.123.123.123"} do
+    #sample("clientip" => "123.123.123.123") do
       #insist { subject["clientip"] } == "0845cb571ab3646e51a07bcabf05e33d"
     #end
   #end
@@ -161,7 +161,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => "123.123.123.123"} do
+    sample("clientip" => "123.123.123.123") do
       insist { subject["clientip"] } == "9336c879e305c9604a3843fc3e75948f"
     end
   end
@@ -179,7 +179,7 @@ describe LogStash::Filters::Anonymize do
       }
     CONFIG
 
-    sample "@fields" => {"clientip" => [ "123.123.123.123", "223.223.223.223" ]} do
+    sample("clientip" => [ "123.123.123.123", "223.223.223.223" ]) do
       insist { subject["clientip"]} == [ "9336c879e305c9604a3843fc3e75948f", "7a6c66b8d3f42a7d650e3354af508df3" ]
     end
   end
