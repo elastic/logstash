ff87e2ce4228bdcc1a71bdf1763cbc0129a322d7
Jordan Sissel <jls@semicomplete.com>
2013-06-23 22:54:08 -0700

    - thin out sized_queue in prep for future.

--- lib/logstash/sized_queue.rb
+++ lib/logstash/sized_queue.rb
@@ -1,17 +1,7 @@
 require "logstash/namespace"
 require "logstash/logging"
+
 require "thread" # for SizedQueue
-
 class LogStash::SizedQueue < SizedQueue
-  attr_accessor :logger
-
-  # Set the logger for this queue
-  #
-  # This will also configure any metrics for this queue.
-  public
-  def logger=(_logger)
-    @logger = _logger
-  end # def logger=
-
-  alias_method :<<, :push
+  # TODO(sissel): Soon will implement push/pop stats, etc
 end
