50079f1bba017b7d15bf2054466cf3c1388eabfd
Jordan Sissel <jls@semicomplete.com>
2013-06-26 18:12:36 -0700

    - move ShutdownSignal to errors.rb

--- lib/logstash/errors.rb
+++ lib/logstash/errors.rb
@@ -2,4 +2,5 @@ module LogStash
   class Error < ::StandardError; end
   class ConfigurationError < Error; end
   class PluginLoadingError < Error; end
+  class ShutdownSignal < StandardError; end
 end
