af538da7826cbfa5c2b682af4337cd544f4acfb5
Jordan Sissel <jls@semicomplete.com>
2013-07-22 13:45:39 -0700

    - log the compiled code more readably - clarify rbx issues indicating that I haven't really done any debugging to figure out where the problem is. - update changelog with todos - remove unused require - try to fix up require() debugger

--- lib/logstash/monkeypatches-for-debugging.rb
+++ lib/logstash/monkeypatches-for-debugging.rb
@@ -4,7 +4,13 @@ if $DEBUGLIST.include?("require")
 
     def require(path)
       result = require_debug(path)
-      puts "require(\"#{path}\") => #{result} (from: #{caller[1]})"
+      origin = caller[1]
+      if origin =~ /rubygems\/custom_require/
+        origin = caller[3]
+      end
+      puts "require(\"#{path}\")" if result
+      #puts "require(\"#{path}\") => #{result} (from: #{origin})"
+      #puts caller.map { |c| " => #{c}" }.join("\n")
     end
 
     alias_method :load_debug, :load
