c21025c8849bf07db8997d3a2dda85de5a9b76d5
James Scott <jscott@medaptus.com>
2013-08-13 17:08:16 -0400

    LOGSTASH-1066 - fix debug puts statement and whitespace

--- lib/logstash/JRUBY-6970.rb
+++ lib/logstash/JRUBY-6970.rb
@@ -50,22 +50,22 @@ class File
           return File.join("#{jar}!", path) 
         end
         dir = expand_path_JRUBY_6970(path, dir)
-		return fix_jar_path(jar, dir)
+        return fix_jar_path(jar, dir)
       else
         return expand_path_JRUBY_6970(path, dir)
       end
     end
   end
-  
+
   protected
-  
+
   def self.fix_jar_path(jar, resource)
     # TODO(sissel): use LogStash::Util::UNAME
     if RbConfig::CONFIG["host_os"] == "mswin32"
       # 'expand_path' on "/" will return "C:/" on windows.
       # So like.. we don't want that because technically this
       # is the root of the jar, not of a disk.
-      puts :expand_path => [path, "#{jar}!#{resource.gsub(/^[A-Za-z]:/, "")}"]
+      puts :fix_jar_path => ["#{jar}!#{resource.gsub(/^[A-Za-z]:/, "")}"]
       return "#{jar}!#{resource.gsub(/^[A-Za-z]:/, "")}"
     else
       return "#{jar}!#{resource}"
