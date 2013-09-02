11366b143f085a8975dc1a2b5ed91cac90b63ee5
Jordan Sissel <jls@semicomplete.com>
2013-09-02 21:16:44 +0000

    - remove another debug print

--- lib/logstash/inputs/base.rb
+++ lib/logstash/inputs/base.rb
@@ -69,7 +69,6 @@ class LogStash::Inputs::Base < LogStash::Plugin
   def initialize(params={})
     super
     @threadable = false
-    puts caller.join("\n")
     config_init(params)
     @tags ||= []
 
