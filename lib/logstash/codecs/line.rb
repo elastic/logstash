e9740efa968c4e0434e173809de7eac52b1786d0
Jordan Sissel <jls@semicomplete.com>
2013-09-04 15:47:13 -0700

    - on flushing, don't send an event if there's no data. (LOGSTASH-1289)

--- lib/logstash/codecs/line.rb
+++ lib/logstash/codecs/line.rb
@@ -47,7 +47,9 @@ class LogStash::Codecs::Line < LogStash::Codecs::Base
   public
   def flush(&block)
     remainder = @buffer.flush
-    block.call(LogStash::Event.new({"message" => remainder})) if remainder
+    if !remainder.empty?
+      block.call(LogStash::Event.new({"message" => remainder}))
+    end
   end
 
   public
