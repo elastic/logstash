4e420dd2918d84505c7b408a0439034503c66e2b
Jordan Sissel <jls@semicomplete.com>
2013-09-04 08:28:12 -0700

    - handle cases where tags are not present in an event (LOGSTASH-1286)

--- lib/logstash/outputs/base.rb
+++ lib/logstash/outputs/base.rb
@@ -65,13 +65,14 @@ class LogStash::Outputs::Base < LogStash::Plugin
     end
 
     if !@tags.empty?
+      return false if !event["tags"]
       if !@tags.send(@include_method) {|tag| event["tags"].include?(tag)}
         @logger.debug? and @logger.debug("Dropping event because tags don't match #{@tags.inspect}", event)
         return false
       end
     end
 
-    if !@exclude_tags.empty?
+    if !@exclude_tags.empty? && event["tags"]
       if @exclude_tags.send(@exclude_method) {|tag| event["tags"].include?(tag)}
         @logger.debug? and @logger.debug("Dropping event because tags contains excluded tags: #{exclude_tags.inspect}", event)
         return false
