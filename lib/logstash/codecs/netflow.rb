b80d1ee080460d36fd591437c121d1140b3c932a
Jordan Sissel <jls@semicomplete.com>
2013-09-03 17:04:16 -0700

    - make netflow a codec (LOGSTASH-225)

--- lib/logstash/codecs/netflow.rb
+++ lib/logstash/codecs/netflow.rb
@@ -1,218 +1,7 @@
 require "logstash/filters/base"
 require "logstash/namespace"
-require "bindata"
-require "ipaddr"
 
-class IP4Addr < BinData::Primitive
-  endian :big
-  uint32 :storage
-
-  def set(val)
-    ip = IPAddr.new(val)
-    if ! ip.ipv4?
-      raise ArgumentError, "invalid IPv4 address '#{val}'"
-    end
-    self.storage = ip.to_i
-  end
-
-  def get
-    IPAddr.new_ntoh([self.storage].pack('N')).to_s
-  end
-end
-
-class IP6Addr < BinData::Primitive
-  endian  :big
-  uint128 :storage
-
-  def set(val)
-    ip = IPAddr.new(val)
-    if ! ip.ipv6?
-      raise ArgumentError, "invalid IPv6 address `#{val}'"
-    end
-    self.storage = ip.to_i
-  end
-
-  def get
-    IPAddr.new_ntoh((0..7).map { |i|
-      (self.storage >> (112 - 16 * i)) & 0xffff
-    }.pack('n8')).to_s
-  end
-end
-
-class MacAddr < BinData::Primitive
-  array :bytes, :type => :uint8, :initial_length => 6
-
-  def set(val)
-    ints = val.split(/:/).collect { |int| int.to_i(16) }
-    self.bytes = ints
-  end
-
-  def get
-    self.bytes.collect { |byte| byte.to_s(16) }.join(":")
-  end
-end
-
-class Header < BinData::Record
-  endian :big
-  uint16 :version
-end
-
-class Netflow5PDU < BinData::Record
-  endian :big
-  uint16 :version
-  uint16 :flow_records
-  uint32 :uptime
-  uint32 :unix_sec
-  uint32 :unix_nsec
-  uint32 :flow_seq_num
-  uint8  :engine_type
-  uint8  :engine_id
-  bit2   :sampling_algorithm
-  bit14  :sampling_interval
-  array  :records, :initial_length => :flow_records do
-    ip4_addr :ipv4_src_addr
-    ip4_addr :ipv4_dst_addr
-    ip4_addr :ipv4_next_hop
-    uint16   :input_snmp
-    uint16   :output_snmp
-    uint32   :in_pkts
-    uint32   :in_bytes
-    uint32   :first_switched
-    uint32   :last_switched
-    uint16   :l4_src_port
-    uint16   :l4_dst_port
-    skip     :length => 1
-    uint8    :tcp_flags # Split up the TCP flags maybe?
-    uint8    :protocol
-    uint8    :src_tos
-    uint16   :src_as
-    uint16   :dst_as
-    uint8    :src_mask
-    uint8    :dst_mask
-    skip     :length => 2
-  end
-end
-
-class TemplateFlowset < BinData::Record
-  endian :big
-  array  :templates, :read_until => lambda { array.num_bytes == flowset_length - 4 } do
-    uint16 :template_id
-    uint16 :field_count
-    array  :fields, :initial_length => :field_count do
-      uint16 :field_type
-      uint16 :field_length
-    end
-  end
-end
-
-class OptionFlowset < BinData::Record
-  endian :big
-  array  :templates, :read_until => lambda { flowset_length - 4 - array.num_bytes <= 2 } do
-    uint16 :template_id
-    uint16 :scope_length
-    uint16 :option_length
-    array  :scope_fields, :initial_length => lambda { scope_length / 4 } do
-      uint16 :field_type
-      uint16 :field_length
-    end
-    array  :option_fields, :initial_length => lambda { option_length / 4 } do
-      uint16 :field_type
-      uint16 :field_length
-    end
-  end
-  skip   :length => lambda { templates.length.odd? ? 2 : 0 }
-end
-
-class Netflow9PDU < BinData::Record
-  endian :big
-  uint16 :version
-  uint16 :flow_records
-  uint32 :uptime
-  uint32 :unix_sec
-  uint32 :flow_seq_num
-  uint32 :source_id
-  array  :records, :read_until => :eof do
-    uint16 :flowset_id
-    uint16 :flowset_length
-    choice :flowset_data, :selection => :flowset_id do
-      template_flowset 0
-      option_flowset   1
-      string           :default, :read_length => lambda { flowset_length - 4 }
-    end
-  end
-end
-
-# https://gist.github.com/joshaven/184837
-class Vash < Hash
-  def initialize(constructor = {})
-    @register ||= {}
-    if constructor.is_a?(Hash)
-      super()
-      merge(constructor)
-    else
-      super(constructor)
-    end
-  end
-
-  alias_method :regular_writer, :[]= unless method_defined?(:regular_writer)
-  alias_method :regular_reader, :[] unless method_defined?(:regular_reader)
-
-  def [](key)
-    sterilize(key)
-    clear(key) if expired?(key)
-    regular_reader(key)
-  end
-
-  def []=(key, *args)
-    if args.length == 2
-      value, ttl = args[1], args[0]
-    elsif args.length == 1
-      value, ttl = args[0], 60
-    else
-      raise ArgumentError, "Wrong number of arguments, expected 2 or 3, received: #{args.length+1}\n"+
-                           "Example Usage:  volatile_hash[:key]=value OR volatile_hash[:key, ttl]=value"
-    end
-    sterilize(key)
-    ttl(key, ttl)
-    regular_writer(key, value)
-  end
-
-  def merge(hsh)
-    hsh.map {|key,value| self[sterile(key)] = hsh[key]}
-    self
-  end
-
-  def cleanup!
-    now = Time.now.to_i
-    @register.map {|k,v| clear(k) if v < now}
-  end
-
-  def clear(key)
-    sterilize(key)
-    @register.delete key
-    self.delete key
-  end
-
-  private
-  def expired?(key)
-    Time.now.to_i > @register[key].to_i
-  end
-
-  def ttl(key, secs=60)
-    @register[key] = Time.now.to_i + secs.to_i
-  end
-
-  def sterile(key)
-    String === key ? key.chomp('!').chomp('=') : key.to_s.chomp('!').chomp('=').to_sym
-  end
-
-  def sterilize(key)
-    key = sterile(key)
-  end
-end
-
-class LogStash::Filters::Netflow < LogStash::Filters::Base
-
+class LogStash::Codecs::Netflow < LogStash::Codecs::Base
   config_name "netflow"
   milestone 1
 
@@ -220,25 +9,25 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
   config :cache_ttl, :validate => :number, :default => 4000
 
   public
-  def initialize(params)
+  def initialize(params={})
     super(params)
-
     @threadsafe = false
   end
 
   public
   def register
+    require "logstash/codecs/netflow/util"
     @templates = Vash.new()
   end # def register
 
   public
-  def filter(event)
-    header = Header.read(event["message"])
+  def decode(payload, &block)
+    header = Header.read(payload)
 
     if header.version == 5
-      flowset = Netflow5PDU.read(event["message"])
+      flowset = Netflow5PDU.read(payload)
     elsif header.version == 9
-      flowset = Netflow9PDU.read(event["message"])
+      flowset = Netflow9PDU.read(payload)
     else
       @logger.warn("Unsupported Netflow version v#{header.version}")
       return
@@ -246,19 +35,18 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
 
     flowset.records.each do |record|
       if flowset.version == 5
-        clone = event.clone
-        clone.message = ""
+        event = LogStash::Event.new
 
         # FIXME Probably not doing this right WRT JRuby?
         #
         # The flowset header gives us the UTC epoch seconds along with
         # residual nanoseconds so we can set @timestamp to that easily
-        clone.timestamp = Time.at(flowset.unix_sec, flowset.unix_nsec / 1000).utc
-        clone['netflow'] = {} if clone['netflow'].nil?
+        event["@timestamp"] = Time.at(flowset.unix_sec, flowset.unix_nsec / 1000).utc
+        event['netflow'] = {}
 
         # Copy some of the pertinent fields in the header to the event
         ['version', 'flow_seq_num', 'engine_type', 'engine_id', 'sampling_algorithm', 'sampling_interval', 'flow_records'].each do |f|
-          clone['netflow'][f] = flowset[f]
+          event['netflow'][f] = flowset[f]
         end
 
         # Create fields in the event from each field in the flow record
@@ -277,14 +65,13 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
               micros += 1000000
             end
             # FIXME Again, probably doing this wrong WRT JRuby?
-            clone['netflow'][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
+            event['netflow'][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
           else
-            clone['netflow'][k.to_s] = v
+            event['netflow'][k.to_s] = v
           end
         end
 
-        filter_matched(clone)
-        yield clone
+        yield event
       elsif flowset.version == 9
         case record.flowset_id
         when 0
@@ -300,7 +87,8 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
                 fields += entry
               end
               # We get this far, we have a list of fields
-              key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+              #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+              key = "#{flowset.source_id}|#{template.template_id}"
               @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
               # Purge any expired templates
               @templates.cleanup!
@@ -319,7 +107,8 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
                 fields += entry
               end
               # We get this far, we have a list of fields
-              key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+              #key = "#{flowset.source_id}|#{event["source"]}|#{template.template_id}"
+              key = "#{flowset.source_id}|#{template.template_id}"
               @templates[key, @cache_ttl] = BinData::Struct.new(:endian => :big, :fields => fields)
               # Purge any expired templates
               @templates.cleanup!
@@ -327,11 +116,13 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
           end 
         when 256..65535
           # Data flowset
-          key = "#{flowset.source_id}|#{event["source"]}|#{record.flowset_id}"
+          #key = "#{flowset.source_id}|#{event["source"]}|#{record.flowset_id}"
+          key = "#{flowset.source_id}|#{record.flowset_id}"
           template = @templates[key]
 
           if ! template
-            @logger.warn("No matching template for flow id #{record.flowset_id} from #{event["source"]}")
+            #@logger.warn("No matching template for flow id #{record.flowset_id} from #{event["source"]}")
+            @logger.warn("No matching template for flow id #{record.flowset_id}")
             next
           end
 
@@ -349,19 +140,17 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
           records = array.read(record.flowset_data)
 
           records.each do |r|
-            clone = event.clone
-            clone.message = ""
-
-            clone.timestamp = Time.at(flowset.unix_sec).utc
-
-            clone['netflow'] = {} if clone['netflow'].nil?
+            event = LogStash::Event.new(
+              "@timestamp" => Time.at(flowset.unix_sec).utc,
+              "netflow" => {}
+            )
 
             # Fewer fields in the v9 header
             ['version', 'flow_seq_num'].each do |f|
-              clone['netflow'][f] = flowset[f]
+              event['netflow'][f] = flowset[f]
             end
 
-            clone['netflow']['flowset_id'] = record.flowset_id
+            event['netflow']['flowset_id'] = record.flowset_id
 
             r.each_pair do |k,v|
               case k.to_s
@@ -370,23 +159,19 @@ class LogStash::Filters::Netflow < LogStash::Filters::Base
                 seconds = flowset.unix_sec - (millis / 1000)
                 # v9 did away with the nanosecs field
                 micros = 1000000 - (millis % 1000)
-                clone['netflow'][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
+                event['netflow'][k.to_s] = Time.at(seconds, micros).utc.strftime("%Y-%m-%dT%H:%M:%S.%3NZ")
               else
-                clone['netflow'][k.to_s] = v
+                event['netflow'][k.to_s] = v
               end
             end
 
-            filter_matched(clone)
-            yield clone
+            yield event
           end
         else
           @logger.warn("Unsupported flowset id #{record.flowset_id}")
         end
       end
     end
-
-    # Drop the original event
-    event.cancel
   end # def filter
 
   private
