068dd0fe50d2b01617ee6308e41083fa1d928c9f
Mathias Nakatsui <mnakatsui@atlassoftwaregroup.com>
2013-09-10 22:01:05 -0700

    Fix incorrect default aws region URI

--- lib/logstash/outputs/s3.rb
+++ lib/logstash/outputs/s3.rb
@@ -143,12 +143,18 @@ class LogStash::Outputs::S3 < LogStash::Outputs::Base
  def aws_s3_config
   
   @logger.debug "S3: waiting for establishing connection..."
+
+  endpoint = 's3-'+@endpoint_region+'.amazonaws.com'
+  if @endpoint_region <=> "us_east_1"
+    endpoint = 's3.amazonaws.com'
+  end
+
   AWS.config(
     :access_key_id => @access_key_id,
     :secret_access_key => @secret_access_key,
-    :s3_endpoint => 's3-'+@endpoint_region+'.amazonaws.com'
+    :s3_endpoint => endpoint
   )
-  @s3 = AWS::S3.new 
+  @s3 = AWS::S3.new
 
  end
 
