670d14b0afc753fca4e1c7c5837d5f6d587ae275
Jordan Sissel <jls@semicomplete.com>
2013-09-19 16:20:45 -0700

    - Check for EOF condition (remote has closed) before writing.
    If we don't do this, then the write will still succeed even after the
    remote has closed (half-closed in CLOSE_WAIT on our side), and the 2nd
    write after will fail causing the previous event to be lost.

--- lib/logstash/outputs/tcp.rb
+++ lib/logstash/outputs/tcp.rb
@@ -91,16 +91,24 @@ class LogStash::Outputs::Tcp < LogStash::Outputs::Base
         @client_threads.reject! {|t| !t.alive? }
       end
     else
-      @client_socket = nil
+      client_socket = nil
       @codec.on_event do |payload|
         begin
-          connect unless @client_socket
-          @client_socket.write(payload)
+          client_socket = connect unless client_socket
+          r,w,e = IO.select([client_socket], [client_socket], [client_socket], nil)
+          # don't expect any reads, but a readable socket might
+          # mean the remote end closed, so read it and throw it away.
+          # we'll get an EOFError if it happens.
+          client_socket.sysread(16384) if r.any?
+
+          # Now send the payload
+          client_socket.syswrite(payload) if w.any?
         rescue => e
           @logger.warn("tcp output exception", :host => @host, :port => @port,
                        :exception => e, :backtrace => e.backtrace)
+          client_socket.close rescue nil
+          client_socket = nil
           sleep @reconnect_interval
-          @client_socket = nil
           retry
         end
       end
