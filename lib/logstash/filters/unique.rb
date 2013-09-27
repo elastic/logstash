90d3c195fff601dabda9599f911efaf675194691
Adam Tucker <adam.j.tucker@gmail.com>
2013-09-26 20:06:12 -0400

    Unique filter and spec.

---
+++ lib/logstash/filters/unique.rb
@@ -0,0 +1,28 @@
+require "logstash/filters/base"
+require "logstash/namespace"
+
+class LogStash::Filters::Unique < LogStash::Filters::Base
+
+  config_name "unique"
+  milestone 1
+
+  # The fields on which to run the unique filter.
+  config :fields, :validate => :array, :required => true
+
+  public
+  def register
+    # Nothing to do
+  end # def register
+
+  public
+  def filter(event)
+    return unless filter?(event)
+
+    @fields.each do |field|
+      return unless event[field].class == Array
+
+      event[field] = event[field].uniq
+    end
+  end # def filter
+
+end # class Logstash::Filters::Unique
