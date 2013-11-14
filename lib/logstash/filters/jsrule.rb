512e55dfce7429a58e6f11535c541ebfb49c36d5
admin <admin@NCE00412.nce.amadeus.net>
2013-11-14 16:55:49 +0100

    jsrule filter - fixed timestamp + we will clone the event for each condition + do not touch timestamp!

--- lib/logstash/filters/jsrule.rb
+++ lib/logstash/filters/jsrule.rb
@@ -7,15 +7,25 @@ class LogStash::Filters::Ruby < LogStash::Filters::Base
 
   # The code to execute for every event.
   # You will have an 'event' variable available that is the event itself.
+
   config :rule, :validate => :string, :required => true
 
   public
   def register
     require 'rhino'
 
-    @jsrule = JsRule.new
-    # Load jsrule engine
-    jsrule_source = <<EOS
+    @jsrule_source = <<EOS
+function CloneObject(obj)
+{
+	var target = {}
+	for (var i in obj) {
+		if (obj.hasOwnProperty(i)) {
+			target[i] = obj[i]
+		}
+	}
+	return target
+}
+
 function jsrule()
 {
         _events = []
@@ -27,15 +37,16 @@ function jsrule()
 
         _result = []
         for(ev in _events)
-          for(cd in rule.conditions)
-                _result.push(cd(ev))
-        
+          for(cd in rule.conditions) {
+	    _result.push(rule.conditions[cd](CloneObject(_events[ev])))
+	  }
+	
+	// Bug in Date JS to Java - Remove timestamp from output
+	for(r in _result)
+		delete _result[r]['@timestamp']
         return _result
 }
 EOS
-    @jsrule.eval_js(jsrule_source)
-    # Load specific rule
-    @jsrule.eval_js(@rule)
 
   end # def register
 
@@ -43,13 +54,19 @@ EOS
   def filter(event)
     return unless filter?(event)
 
-    @jsrule['event'] = event
-    @jsrule.eval_js("result = jsrule()")
-
-    @jsrule['result'].each do |emitted_event|
-      filter_matched(emitted_event)
-      yield emitted_event
+    Rhino::Context.open do |context|
+      context['event'] = event.to_hash
+      context.eval(@jsrule_source)
+      context.eval(@rule)
+      context.eval("jsrule()").each do |emitted_event_data|
+        #@logger.debug(emitted_event_data)
+        emitted_event = LogStash::Event.new(emitted_event_data)
+	# Bug in Date JS to Java - Remove timestamp from output
+	emitted_event["@timestamp"] = event.timestamp
+        filter_matched(emitted_event)
+        yield emitted_event
+      end
     end
-      
+
   end # def filter
 end # class LogStash::Filters::Ruby
