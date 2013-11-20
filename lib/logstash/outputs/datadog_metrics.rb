c3cb78367350285a6e6464d39bbedb8f945770d7
Ian Paredes <ian@nitrous.io>
2013-11-20 14:49:24 -0800

    Add metric_type as part of the validation strings included in the plugin.

--- lib/logstash/outputs/datadog_metrics.rb
+++ lib/logstash/outputs/datadog_metrics.rb
@@ -25,7 +25,7 @@ class LogStash::Outputs::DatadogMetrics < LogStash::Outputs::Base
   config :metric_value, :default => "%{metric_value}"
 
   # The type of the metric.
-  config :metric_type, :validate => ["gauge", "counter"], :default => "%{metric_type}"
+  config :metric_type, :validate => ["gauge", "counter", "%{metric_type}"], :default => "%{metric_type}"
 
   # The name of the host that produced the metric.
   config :host, :validate => :string, :default => "%{host}"
