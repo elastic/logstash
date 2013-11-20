8880d6ed2b8d17f7ce0957fbde15b8b82aac21c0
Ben Chadwick <bchadwick@mdsol.com>
2013-11-20 18:31:48 -0500

    Moved JSON split feature into split plugin

--- lib/logstash/filters/json.rb
+++ lib/logstash/filters/json.rb
@@ -1,6 +1,6 @@
+# encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
-require "logstash/event"
 
 # JSON filter. Takes a field that contains JSON and expands it into
 # an actual datastructure.
@@ -41,9 +41,6 @@ class LogStash::Filters::Json < LogStash::Filters::Base
   # Note: if the "target" field already exists, it will be overwritten.
   config :target, :validate => :string
 
-  # Define a key containing an array of events to be split up and fed into LogStash as separate individual events.
-  config :array_split, :validate => :string
-
   public
   def register
     # Nothing to do here
@@ -65,34 +62,20 @@ class LogStash::Filters::Json < LogStash::Filters::Base
     end
 
     begin
-      source = JSON.parse(event[@source])
+      # TODO(sissel): Note, this will not successfully handle json lists
+      # like your text is '[ 1,2,3 ]' JSON.parse gives you an array (correctly)
+      # which won't merge into a hash. If someone needs this, we can fix it
+      # later.
+      dest.merge!(JSON.parse(event[@source]))
 
-      if !!@array_split
-
-        source[@array_split].each do |item|
-          next if item.empty?
-
-          event_split = LogStash::Event.new(item.clone)
-          @logger.debug("JSON Array Split item", :value => event_split)
-          filter_matched(event_split)
-
-          yield event_split # yield each item to be handled individually
-        end
-        event.cancel
-
-      else
-        dest.merge!(JSON.parse(event[@source]))
-
-        # This is a hack to help folks who are mucking with @timestamp during
-        # their json filter. You aren't supposed to do anything with "@timestamp"
-        # outside of the date filter, but nobody listens... ;)
-        if event["@timestamp"].is_a?(String)
-          event["@timestamp"] = Time.parse(event["@timestamp"]).gmtime
-        end
-
-        filter_matched(event)
+      # This is a hack to help folks who are mucking with @timestamp during
+      # their json filter. You aren't supposed to do anything with "@timestamp"
+      # outside of the date filter, but nobody listens... ;)
+      if event["@timestamp"].is_a?(String)
+        event["@timestamp"] = Time.parse(event["@timestamp"]).gmtime
       end
 
+      filter_matched(event)
     rescue => e
       event.tag("_jsonparsefailure")
       @logger.warn("Trouble parsing json", :source => @source,
--- lib/logstash/filters/split.rb
+++ lib/logstash/filters/split.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
+require "logstash/event"
 
 # The split filter is for splitting multiline messages into separate events.
 #
@@ -22,6 +22,10 @@ class LogStash::Filters::Split < LogStash::Filters::Base
   # The field which value is split by the terminator
   config :field, :validate => :string, :default => "message"
 
+  # If true, the array data will be passed forward as a single hash element with @field as the key.
+  # If false, treat the entire array element as a new event for further processing.
+  config :reuse_element, :validate => :boolean, :default => true
+
   public
   def register
     # Nothing to do
@@ -31,27 +35,32 @@ class LogStash::Filters::Split < LogStash::Filters::Base
   def filter(event)
     return unless filter?(event)
 
-    events = []
+    splits = []
 
     original_value = event[@field]
 
-    # If for some reason the field is an array of values, take the first only.
-    original_value = original_value.first if original_value.is_a?(Array)
+    if original_value.is_a?(Array)
+      splits = original_value
+    else
+      # Using -1 for 'limit' on String#split makes ruby not drop trailing empty
+      # splits.
+      splits = original_value.split(@terminator, -1)
+    end
 
-    # Using -1 for 'limit' on String#split makes ruby not drop trailing empty
-    # splits.
-    splits = original_value.split(@terminator, -1)
-
-    # Skip filtering if splitting this event resulted in only one thing found.
-    return if splits.length == 1
-    #or splits[1].empty?
+    # Skip filtering if splitting this event resulted in only one thing found
+    return event if splits.length <= 1
 
     splits.each do |value|
       next if value.empty?
 
-      event_split = event.clone
-      @logger.debug("Split event", :value => value, :field => @field)
-      event_split[@field] = value
+      event_split = nil
+      if @reuse_element
+        event_split = event.clone
+        @logger.debug("Split event", :value => value, :field => @field)
+        event_split[@field] = value
+      else
+        event_split = LogStash::Event.new(value)
+      end
       filter_matched(event_split)
 
       # Push this new event onto the stack at the LogStash::FilterWorker
