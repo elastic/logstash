17044b6ac163c315998e27def24cf10369197a03
Brad Fritz <brad@fewerhassles.com>
2013-11-20 00:09:55 -0500

    [oldlogstashjson] simplify encoding from v1 to v0

--- spec/codecs/oldlogstashjson.rb
+++ spec/codecs/oldlogstashjson.rb
@@ -32,7 +32,11 @@ describe LogStash::Codecs::OldLogStashJSON do
       got_event = false
       subject.on_event do |d|
         insist { JSON.parse(d)["@timestamp"] } != nil
+        insist { JSON.parse(d)["@type"] } == data["type"]
         insist { JSON.parse(d)["@message"] } == data["message"]
+        insist { JSON.parse(d)["@source_host"] } == data["host"]
+        insist { JSON.parse(d)["@source_path"] } == data["path"]
+        insist { JSON.parse(d)["@tags"] } == data["tags"]
         got_event = true
       end
       subject.encode(event)
