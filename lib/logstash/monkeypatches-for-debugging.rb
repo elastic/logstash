dc634a1fd24dba3956c3f5e49255413eff8cac24
Jordan Sissel <jls@semicomplete.com>
2013-11-26 15:46:12 -0800

    hack for hiding the polyglot gem's require monkeypatch; emit the duration/path/source

--- lib/logstash/monkeypatches-for-debugging.rb
+++ lib/logstash/monkeypatches-for-debugging.rb
@@ -22,12 +22,17 @@ if $DEBUGLIST.include?("require")
       # Only print require() calls that did actual work.
       # require() returns true on load, false if already loaded.
       if result
-        trace = caller.collect { |c| c.split(":").first }.join(",")
-        #puts "#{duration},#{path},#{trace}"
-        source = caller[0].split(":").first
-        target = $LOADED_FEATURES.grep(/#{path}/).first
-        fontsize = [10, duration * 48].max
-        puts "#{source.inspect} -> #{target.inspect} [label=\"#{duration}\" labelfontsize=#{fontsize}]"
+        source = caller[0]
+        #p source.include?("/lib/polyglot.rb:63:in `require'") => source
+        if source.include?("/lib/polyglot.rb:63:in `require'")
+          source = caller[1]
+        end
+
+        #target = $LOADED_FEATURES.grep(/#{path}/).first
+        #puts path
+        #puts caller.map { |c| "  #{c}" }.join("\n")
+        #fontsize = [10, duration * 48].max
+        puts "#{duration},#{path},#{source}"
       end
       #puts caller.map { |c| " => #{c}" }.join("\n")
     end
