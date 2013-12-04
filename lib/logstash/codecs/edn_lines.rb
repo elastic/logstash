88211d385227b7a0dcc877ebaed0c0b4efef2653
Jordan Sissel <jls@semicomplete.com>
2013-12-04 14:32:46 -0800

    - make the edn_lines codec tests pass

--- lib/logstash/codecs/edn_lines.rb
+++ lib/logstash/codecs/edn_lines.rb
@@ -18,11 +18,10 @@ class LogStash::Codecs::EDNLines < LogStash::Codecs::Base
 
   public
   def decode(data)
-
     @lines.decode(data) do |event|
       begin
-        yield LogStash::Event.new(EDN.read(data))
-      rescue e
+        yield LogStash::Event.new(EDN.read(event["message"]))
+      rescue => e
         @logger.info("EDN parse failure. Falling back to plain-text", :error => e, :data => data)
         yield LogStash::Event.new("message" => data)
       end
