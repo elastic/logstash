b02a5d74deceef81348ce31a08569d8dbe5c0c41
Matt Gray <matt.gray@7digital.com>
2013-12-06 15:12:16 +0000

    output : csv - add documentation comments

--- lib/logstash/outputs/csv.rb
+++ lib/logstash/outputs/csv.rb
@@ -2,10 +2,25 @@ require "csv"
 require "logstash/namespace"
 require "logstash/outputs/file"
 
+# CSV output.
+#
+# Write events to disk in CSV or other delimited format
+# Based on the file output, many config values are shared
+# Uses the Ruby csv library internally
 class LogStash::Outputs::CSV < LogStash::Outputs::File
+
   config_name "csv"
   milestone 1
+
+  # The field names from the event that should be written to the CSV file.
+  # Fields are written to the CSV in the same order as the array.
+  # If a field does not exist on the event, an empty string will be written.
+  # Supports field reference syntax eg: `fields => ["field1", "[nested][field]"]`.
   config :fields, :validate => :array, :required => true
+  
+  # Options for CSV output. This is passed directly to the Ruby stdlib to\_csv function. 
+  # Full documentation is available here: [http://ruby-doc.org/stdlib-2.0.0/libdoc/csv/rdoc/index.html].
+  # A typical use case would be to use alternative column or row seperators eg: `csv_options => {"col_sep" => "\t" "row_sep" => "\r\n"}` gives tab seperated data with windows line endings
   config :csv_options, :validate => :hash, :required => false, :default => Hash.new
 
   public
