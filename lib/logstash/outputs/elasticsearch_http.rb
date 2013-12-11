7876deb2b8167606ac1469104f7b73d4cbc7d90d
Jordan Sissel <jls@semicomplete.com>
2013-12-11 12:37:36 -0800

    - Move the elasticsearch index template to a separate directory like   other helper things

--- lib/logstash/outputs/elasticsearch_http.rb
+++ lib/logstash/outputs/elasticsearch_http.rb
@@ -176,7 +176,7 @@ class LogStash::Outputs::ElasticSearchHTTP < LogStash::Outputs::Base
       if __FILE__ =~ /^(jar:)?file:\/.+!.+/
         begin
           # Running from a jar, assume types.db is at the root.
-          jar_path = [__FILE__.split("!").first, "/elasticsearch-template.json"].join("!")
+          jar_path = [__FILE__.split("!").first, "elasticsearch/elasticsearch-template.json"].join("!")
           @template = jar_path
         rescue => ex
           raise "Failed to cache, due to: #{ex}\n#{ex.backtrace}"
@@ -184,7 +184,7 @@ class LogStash::Outputs::ElasticSearchHTTP < LogStash::Outputs::Base
       else
         if File.exists?("elasticsearch-template.json")
           @template = "elasticsearch-template.json"
-        elsif File.exists?("lib/logstash/outputs/elasticsearch-template.json")
+        elsif File.exists?("lib/logstash/outputs/elasticsearch/elasticsearch-template.json")
           @template = "lib/logstash/outputs/elasticsearch-template.json"
         else
           raise "You must specify 'template => ...' in your elasticsearch_http output"
