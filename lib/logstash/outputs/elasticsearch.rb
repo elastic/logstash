c7de32db29223c75b655bc0f3a7a348c5c8a1f42
Aaron Mildenstein <aaron@mildensteins.com>
2013-12-11 17:59:37 -0600

    Documentation updates to keep things readable (and not invisible or missing).

--- lib/logstash/outputs/elasticsearch.rb
+++ lib/logstash/outputs/elasticsearch.rb
@@ -57,8 +57,8 @@ class LogStash::Outputs::ElasticSearch < LogStash::Outputs::Base
   # This configuration option defines how the template is named inside Elasticsearch
   # Note that if you have used the template management features and subsequently
   # change this you will need to prune the old template manually, e.g.
-  # curl -XDELETE http://localhost:9200/_template/OLD_template_name?pretty
-  # where OLD_template_name is whatever the former setting was.
+  # curl -XDELETE <http://localhost:9200/_template/OLDtemplatename?pretty>
+  # where OLDtemplatename is whatever the former setting was.
   config :template_name, :validate => :string, :default => "logstash"
 
   # You can set the path to your own template here, if you so desire.  
