6abce0e0a15aab3e4e9494f2f4df22b444d2b2ec
Joshua Bussdieker <jbussdieker@gmail.com>
2014-01-23 16:25:19 -0500

    Fix priority detection issue with syslog input

--- lib/logstash/inputs/syslog.rb
+++ lib/logstash/inputs/syslog.rb
@@ -200,7 +200,7 @@ class LogStash::Inputs::Syslog < LogStash::Inputs::Base
     if event["tags"].nil? || !event["tags"].include?("_grokparsefailure")
       # Per RFC3164, priority = (facility * 8) + severity
       #                       = (facility << 3) & (severity)
-      priority = event["priority"].first.to_i rescue 13
+      priority = event["priority"].to_i rescue 13
       severity = priority & 7   # 7 is 111 (3 bits)
       facility = priority >> 3
       event["priority"] = priority
