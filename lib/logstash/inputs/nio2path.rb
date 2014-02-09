ba16320ba9dfda13f3d245c259d54f3953576f90
Louis Zuckerman <me@louiszuckerman.com>
2014-02-09 01:42:53 -0500

    add path property & minor cleanup

--- lib/logstash/inputs/nio2path.rb
+++ lib/logstash/inputs/nio2path.rb
@@ -18,21 +18,26 @@ class LogStash::Inputs::Nio2Path < LogStash::Inputs::Base
   
   # Prefix on filenames in the watched directory.
   # "access" for example
-  config :prefix, :validate => :string
+  config :prefix, :validate => :string, :default => ""
 
   # Suffix on filenames in the watched directory.
   # ".log" for example
-  config :suffix, :validate => :string
+  config :suffix, :validate => :string, :default => ""
 
-  #config :start_position, :validate => [ "beginning", "end"], :default => "end"
+  config :start_position, :validate => [ "beginning", "end"], :default => "end"
 
   public
   def register
     @host = Socket.gethostname
+    @charset = java.nio.charset.Charset.forName("UTF-8")
+    @readers = {}
     begin
       @javapath = java.nio.file.Paths.get(@path)
-      #@file = java.nio.file.Files.readAllLines(@path)
-      @charset = java.nio.charset.Charset.forName("UTF-8")
+      java.nio.file.Files.newDirectoryStream(@javapath, @prefix + "*" + @suffix).each do |file|
+        if (!java.nio.file.Files.isDirectory(file))
+          @readers[file.getFileName.toString] = java.nio.file.Files.newBufferedReader(file, @charset)
+        end
+      end
       @watcher = @javapath.getFileSystem.newWatchService
       @javapath.register(@watcher,
                          java.nio.file.StandardWatchEventKinds::ENTRY_MODIFY,
@@ -43,7 +48,21 @@ class LogStash::Inputs::Nio2Path < LogStash::Inputs::Base
     end
   end # def register
 
-  def run(queue) 
+  def run(queue)
+    @readers.each do |file, rdr|
+      if (@start_position == "end")
+        rdr.skip(java.nio.file.Files.size(@javapath.resolve(file)))
+      else
+        while (rdr.ready)
+          @codec.decode(rdr.readLine) do |event|
+            decorate(event)
+            event["host"] = @host
+            event["path"] = @javapath.resolve(file).toAbsolutePath.toString
+            queue << event
+          end
+        end
+      end
+    end
     while true
       begin
         watchkey = @watcher.take
@@ -55,7 +74,7 @@ class LogStash::Inputs::Nio2Path < LogStash::Inputs::Base
                 p = watchevent.context
                 if ((@prefix.nil? || p.getFileName.toString.start_with?(@prefix)) && (@suffix.nil? || p.getFileName.toString.end_with?(@suffix)))
 
-                  reader = java.nio.file.Files.newBufferedReader(@javapath.resolve(p), @charset)
+                  reader = @readers[p.getFileName.toString]
                   while (reader.ready)
                     @codec.decode(reader.readLine) do |event|
                       decorate(event)
@@ -64,7 +83,6 @@ class LogStash::Inputs::Nio2Path < LogStash::Inputs::Base
                       queue << event
                     end
                   end
-                  reader.close
                 end
               end
             end
