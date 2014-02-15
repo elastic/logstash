54d5d21e6bf2f487a8bd09198b29e6f90909dbda
Kurt Hurtado <github@kurtado.com>
2014-02-14 20:26:37 -0800

    redis input

--- lib/logstash/inputs/redis.rb
+++ lib/logstash/inputs/redis.rb
@@ -3,36 +3,35 @@ require "logstash/inputs/base"
 require "logstash/inputs/threadable"
 require "logstash/namespace"
 
-# Read events from a redis instance. Supports both redis channels and lists.
-# The list command (BLPOP) used by Logstash is supported in redis v1.3.1+ 
-# The channel commands used by Logstash are found in redis v1.3.8+
-# While you may be able to make these redis versions work the best performance
+# This input will read events from a Redis instance, supporting both Redis channels and lists.
+# The list command (BLPOP) used by Logstash is supported in Redis v1.3.1+, and
+# the channel commands used by Logstash are found in Redis v1.3.8+. 
+# While you may be able to make these Redis versions work, the best performance
 # and stability will be found in more recent stable versions.  Versions 2.6.0+
 # are recommended.
 #
-# For more information about redis, see <http://redis.io/>
+# For more information about Redis, see <http://redis.io/>
 #
-# ## `batch_count` note
-#
-# If you use the 'batch_count' setting, you *must* use a redis version 2.6.0 or
+# `batch_count` note: If you use the `batch_count` setting, you *must* use a Redis version 2.6.0 or
 # newer. Anything older does not support the operations used by batching.
+#
 class LogStash::Inputs::Redis < LogStash::Inputs::Threadable
   config_name "redis"
   milestone 2
 
   default :codec, "json"
 
-  # Name is used for logging in case there are multiple instances.
+  # The `name` configuration is used for logging in case there are multiple instances.
   # This feature has no real function and will be removed in future versions.
   config :name, :validate => :string, :default => "default", :deprecated => true
 
-  # The hostname of your redis server.
+  # The hostname of your Redis server.
   config :host, :validate => :string, :default => "127.0.0.1"
 
   # The port to connect on.
   config :port, :validate => :number, :default => 6379
 
-  # The redis database number.
+  # The Redis database number.
   config :db, :validate => :number, :default => 0
 
   # Initial connection timeout in seconds.
@@ -41,21 +40,21 @@ class LogStash::Inputs::Redis < LogStash::Inputs::Threadable
   # Password to authenticate with. There is no authentication by default.
   config :password, :validate => :password
 
-  # The name of the redis queue (we'll use BLPOP against this).
+  # The name of the Redis queue (we'll use BLPOP against this).
   # TODO: remove soon.
   config :queue, :validate => :string, :deprecated => true
 
-  # The name of a redis list or channel.
+  # The name of a Redis list or channel.
   # TODO: change required to true
   config :key, :validate => :string, :required => false
 
-  # Either list or channel.  If redis\_type is list, then we will BLPOP the
+  # Specify either list or channel.  If redis\_type is list, then we will BLPOP the
   # key.  If redis\_type is channel, then we will SUBSCRIBE to the key.
   # If redis\_type is pattern_channel, then we will PSUBSCRIBE to the key.
   # TODO: change required to true
   config :data_type, :validate => [ "list", "channel", "pattern_channel" ], :required => false
 
-  # How many events to return from redis using EVAL
+  # The number of events to return from Redis using EVAL.
   config :batch_count, :validate => :number, :default => 1
 
   public
