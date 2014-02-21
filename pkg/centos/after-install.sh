fa544caafb14a7181cafbff8aa5e7e78ed8252dc
Faye Salwin <faye@zippysoft.com>
2014-02-21 04:32:52 +0000

    Tarball packages. Homogenized init. Common defaults.

--- pkg/centos/after-install.sh
+++ pkg/centos/after-install.sh
@@ -1,1 +1,7 @@
 /sbin/chkconfig --add logstash
+
+chown -R logstash:logstash /opt/logstash
+chown logstash /var/log/logstash
+chown logstash:logstash /var/lib/logstash
+chmod +x /etc/rc.d/init.d/logstash
+chmod +x /opt/logstash/bin/logstash
