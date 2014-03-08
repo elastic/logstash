46853d8f9cc138d4f90650f44cb93fbc0f851436
Henrik <henrik@haf.se>
2014-03-08 16:31:20 +0100

    more explicit tests

--- spec/codecs/json.rb
+++ spec/codecs/json.rb
@@ -18,16 +18,39 @@ describe LogStash::Codecs::JSON do
       end
     end
 
-    it 'should return an event from data with v1-specific fields' do
-      data = { 'foo' => 'bar', '@timestamp' => '2014-03-08T12:34:56+0100', '@version' => 1, 'baz' => 'bax' }
-      subject.decode(data.to_json) do |event|
-        insist { event.is_s? LogStash::Event }
-        insist { event['foo'] } == data['foo']
-        insist { event.timestamp.is_a? LogStash::Time }
-        insist { event.timestamp } == LogStash::Time.parse_iso8601(data['@timestamp'])
-        insist { event.timestamp.gmtime.hour } == 11
-        insist { event['@version'] } == 1
-        insist { event['baz'] } == 'bax'
+    context 'time-specific' do
+      let :data do
+        { 'foo' => 'bar', '@timestamp' => '2014-03-08T12:34:56+0100', '@version' => 1, 'baz' => 'bax' }
+      end
+
+      it 'should decode' do
+        decoded = false
+        subject.decode(data.to_json) do |event|
+          decoded = true
+        end
+        decoded.should be_true
+      end
+
+      it 'should decode an event' do
+        subject.decode(data.to_json) do |event|
+          expect(event).to be_a LogStash::Event
+        end
+      end
+
+      it 'should return an event from data with v1-specific fields' do
+        subject.decode(data.to_json) do |event|
+          event['foo'].should eq(data['foo'])
+          event['baz'].should eq('bax')
+          event['@version'].should eq(1)
+        end
+      end
+
+      it 'should decode time to ::Time to UTC' do
+        subject.decode(data.to_json) do |event|
+          expect(event.timestamp).to be_a(::Time)
+          event.timestamp.should eq(LogStash::Time.parse_iso8601(data['@timestamp']))
+          event.timestamp.gmtime.hour.should eq(11) # alias #utc
+        end
       end
     end
 
