1e507bc196e190bd7ea417d41b3aeac41b01564d
Colin Surprenant <colin.surprenant@gmail.com>
2014-04-09 13:41:40 -0400

    removed assess_jruby! & renamed spec/environment.rb

--- spec/environment.rb
+++ spec/environment.rb
@@ -13,28 +13,4 @@ describe LogStash::Environment do
       expect{LogStash::Environment.load_elasticsearch_jars!}.to raise_error(LogStash::EnvironmentError)
     end
   end
-
-  describe "assess_jruby!" do
-
-    it "should not raise when jruby" do
-      expect(LogStash::Environment).to receive(:jruby?).twice.and_return(true)
-      expect{LogStash::Environment.assess_jruby!}.to_not raise_error
-      expect{LogStash::Environment.assess_jruby!{StandardError.new}}.to_not raise_error
-    end
-
-    it "should raise default exception" do
-      expect(LogStash::Environment).to receive(:jruby?).once.and_return(false)
-      expect{LogStash::Environment.assess_jruby!}.to raise_error(LogStash::EnvironmentError)
-    end
-
-    it "should yield to block and raise returned exception" do
-      expect(LogStash::Environment).to receive(:jruby?).once.and_return(false)
-      expect{LogStash::Environment.assess_jruby!{StandardError.new}}.to raise_error(StandardError)
-    end
-
-    it "should yield to block and raise default exception if exception not returned" do
-      expect(LogStash::Environment).to receive(:jruby?).once.and_return(false)
-      expect{LogStash::Environment.assess_jruby!{nil}}.to raise_error(LogStash::EnvironmentError)
-    end
-  end
 end
