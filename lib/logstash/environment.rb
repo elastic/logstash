022e70f349636a64975496aae7890abd4d4592ac
Colin Surprenant <colin.surprenant@gmail.com>
2014-04-21 17:33:34 -0400

    refactor --pluginpath discovery, also search for gems

--- lib/logstash/environment.rb
+++ lib/logstash/environment.rb
@@ -39,5 +39,35 @@ module LogStash
     def pattern_path(path)
       return ::File.join(LOGSTASH_HOME, "patterns", path)
     end
+
+    # @return [String] major.minor ruby version, ex 1.9
+    def ruby_minor_version
+      RUBY_VERSION[/(\d+\.\d+)(\.\d+)*/, 1]
+    end
+
+    # @return [String] jruby, ruby, rbx, ...
+    def ruby_engine
+      RUBY_ENGINE
+    end
+
+    # given a base path, typically what we'd get from --pluginpath search for logstash plugins source dirs.
+    # @param base_path [String] an existing dir path which will include logstash/... or lib/logstash/...
+    # @return [Array<String>] list of found dirs, [] if none
+    def discover_load_paths(base_path)
+      [".", "lib"].map{|search_path| ::File.expand_path(::File.join(base_path, search_path))}.select do |path|
+        !Dir.glob(::File.join(path, "logstash", "{inputs,codecs,filters,outputs}", "*.rb")).empty?
+      end
+    end
+
+    # given a base path, typically what we'd get from --pluginpath search for logstash plugins gems dirs
+    # in the given path or in the parent path.
+    # @param base_path [String] an existing dir path which will include ./vendor/bundle/... or ../vendor/bundle/...
+    # @return [Array<String>] list of found dirs, [] if none
+    def discover_gem_paths(base_path)
+      ["..", "."].map{|search_path| Dir.glob(::File.expand_path(::File.join(base_path, search_path, "vendor", "bundle", ruby_engine, ruby_minor_version)))}.select do |path|
+        !path.empty?
+      end
+    end
+
   end
 end
