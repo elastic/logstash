2ebc0fc48f2550f0ad85702e432fb5db4d282647
Thomas De Smedt <plankthom@gmail.com>
2014-04-24 13:38:18 +0200

    use json codec in gelf input, to have utf-8 fix of LOGSTASH-1595

--- lib/logstash/inputs/gelf.rb
+++ lib/logstash/inputs/gelf.rb
@@ -14,7 +14,8 @@ class LogStash::Inputs::Gelf < LogStash::Inputs::Base
   config_name "gelf"
   milestone 2
 
-  default :codec, "plain"
+  # The gelf payload is utf-8-encoded json by specification, so this should normally not be changed
+  default :codec, "json"
 
   # The IP address or hostname to listen on.
   config :host, :validate => :string, :default => "0.0.0.0"
@@ -89,16 +90,17 @@ class LogStash::Inputs::Gelf < LogStash::Inputs::Base
       # Gelfd parser outputs null if it received and cached a non-final chunk
       next if data.nil?
 
-      event = LogStash::Event.new(JSON.parse(data))
-      event["source_host"] = client[3]
-      if event["timestamp"].is_a?(Numeric)
-        event["@timestamp"] = Time.at(event["timestamp"]).gmtime
-        event.remove("timestamp")
+      @codec.decode(data) do |event| 
+        event["source_host"] = client[3]
+        if event["timestamp"].is_a?(Numeric)
+          event["@timestamp"] = Time.at(event["timestamp"]).gmtime
+          event.remove("timestamp")
+        end
+        remap_gelf(event) if @remap
+        strip_leading_underscore(event) if @strip_leading_underscore
+        decorate(event)
+        output_queue << event
       end
-      remap_gelf(event) if @remap
-      strip_leading_underscore(event) if @strip_leading_underscore
-      decorate(event)
-      output_queue << event
     end
   rescue LogStash::ShutdownSignal
     # Do nothing, shutdown.
@@ -131,5 +133,5 @@ class LogStash::Inputs::Gelf < LogStash::Inputs::Base
        event[key[1..-1]] = event[key]
        event.remove(key)
      end
-  end # deef removing_leading_underscores
+  end # def strip_leading_underscore
 end # class LogStash::Inputs::Gelf
