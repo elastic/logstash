a2373502c3814bdb0ca49b0f19e48a472ba453f7
Colin Surprenant <colin.surprenant@gmail.com>
2014-05-02 21:46:50 -0400

    removed dependency on bash

--- bin/logstash.lib.sh
+++ bin/logstash.lib.sh
@@ -12,8 +12,8 @@ setup_ruby() {
   # set $RUBY and $RUBYVER
   eval $(ruby -rrbconfig -e 'puts "RUBYVER=#{RbConfig::CONFIG["ruby_version"]}"; puts "RUBY=#{RUBY_ENGINE}"')
 
-  LAUNCHCMD="ruby"
-  LAUNCHARGS=()
+  RUBYCMD="ruby"
+  VENDORED_JRUBY=
 
   export GEM_HOME="${basedir}/vendor/bundle/${RUBY}/${RUBYVER}"
   export GEM_PATH=
@@ -43,38 +43,34 @@ setup_java() {
     export DRIP_INIT=
   fi
 
-  JAVA_OPTS_ARGS=()
-  JAVA_OPTS_ARGS+=("-Xmx${LS_HEAP_SIZE}")
+  JAVA_OPTS="$JAVA_OPTS -Xmx${LS_HEAP_SIZE}"
+  JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC"
+  JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC"
+  JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
 
-  JAVA_OPTS_ARGS+=("-XX:+UseParNewGC")
-  JAVA_OPTS_ARGS+=("-XX:+UseConcMarkSweepGC")
-  JAVA_OPTS_ARGS+=("-Djava.awt.headless=true")
-
-  JAVA_OPTS_ARGS+=("-XX:CMSInitiatingOccupancyFraction=75")
-  JAVA_OPTS_ARGS+=("-XX:+UseCMSInitiatingOccupancyOnly")
+  JAVA_OPTS="$JAVA_OPTS -XX:CMSInitiatingOccupancyFraction=75"
+  JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
 
   if [ ! -z "$LS_USE_GC_LOGGING" ] ; then
-    JAVA_OPTS_ARGS+=("-XX:+PrintGCDetails")
-    JAVA_OPTS_ARGS+=("-XX:+PrintGCTimeStamps")
-    JAVA_OPTS_ARGS+=("-XX:+PrintClassHistogram")
-    JAVA_OPTS_ARGS+=("-XX:+PrintTenuringDistribution")
-    JAVA_OPTS_ARGS+=("-XX:+PrintGCApplicationStoppedTime")
-    JAVA_OPTS_ARGS+=("-Xloggc:./logstash-gc.log")
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails"
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCTimeStamps"
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintClassHistogram"
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintTenuringDistribution"
+    JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCApplicationStoppedTime"
+    JAVA_OPTS="$JAVA_OPTS -Xloggc:./logstash-gc.log"
     echo "Writing garbage collection logs to ./logstash-gc.log"
   fi
 
   export JAVACMD
-  export JAVA_OPTS="${JAVA_OPTS_ARGS[@]}"
+  export JAVA_OPTS
 }
 
 setup_vendored_jruby() {
   RUBYVER=1.9
   RUBY=jruby
 
-  LAUNCHCMD="${JAVACMD}"
-  LAUNCHARGS=("${JAVA_OPTS_ARGS[@]}")
-  LAUNCHARGS+=("-jar")
-  LAUNCHARGS+=("${basedir}"/vendor/jar/jruby-complete-*.jar)
+  JRUBY_JAR=$(ls "${basedir}"/vendor/jar/jruby-complete-*.jar)
+  VENDORED_JRUBY=1
 
   export RUBYLIB="${basedir}/lib"
   export GEM_HOME="${basedir}/vendor/bundle/${RUBY}/${RUBYVER}"
@@ -92,9 +88,11 @@ setup() {
 
 install_deps() {
   if [ -f "$basedir/logstash.gemspec" ] ; then
-    LAUNCHARGS+=("${basedir}/gembag.rb")
-    LAUNCHARGS+=("${basedir}/logstash.gemspec")
-    exec "$LAUNCHCMD" "${LAUNCHARGS[@]}"
+    if [ -z "$VENDORED_JRUBY" ] ; then
+      exec "${RUBYCMD}" "${basedir}/gembag.rb" "${basedir}/logstash.gemspec" "$@"
+    else
+      exec "${JAVACMD}" $JAVA_OPTS "-jar" "$JRUBY_JAR" "${basedir}/gembag.rb" "${basedir}/logstash.gemspec" "$@"
+    fi
   else
     echo "Cannot install dependencies; missing logstash.gemspec. This 'deps' command only works from a logstash git clone."
   fi
