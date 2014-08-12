cf3d39fdaea281e80ff4d02c107a6992c4b134b7
Michele Orsi <morsi@map2app.com>
2014-08-12 17:57:45 +0200

    fixed pattern with resource file type

--- spec/filters/google-app-engine.rb
+++ spec/filters/google-app-engine.rb
@@ -59,7 +59,8 @@ describe "google app engine grok pattern" do
             "0.1.0.3 - - [11/Aug/2014:11:56:38 -0700] \"GET /_ah/warmup HTTP/1.1\" 200 0 - - \"mydomain.com\" ms=15192 cpu_ms=19147 loading_request=1 instance=00c61b117cca2457cdf71e7f6187ac76aa90590c app_engine_release=1.9.8 trace_id=0cb6fce856b4451a28e06194a42d59fe",
             "205.251.208.42 - - [11/Aug/2014:12:52:03 -0700] \"GET /image.jpg HTTP/1.1\" 200 62795 - \"Referrer String\" \"mydomain.com\" ms=24877 cpu_ms=17999 cpm_usd=0.007018 loading_request=1 pending_ms=10572 instance=00c61b117c58c56f7d2d90ee67d4a01e43e262 app_engine_release=1.9.8 trace_id=cd59e954c8a3d2142bcfef9f923839bf",
             "205.251.208.18 - - [11/Aug/2014:12:51:55 -0700] \"GET /public/files/ahJzfm1hcDJhcHAtcGxhdGZvcm1yLAsSC1VzZXJBY2NvdW50GK2iDAwLEgVBbGJ1bRi6FwwLEgVNZWRpYRirgQoM_ORIGINAL=s640.jpg HTTP/1.1\" 200 77961 - \"Referrer String\" \"mydomain.com\" ms=16993 cpu_ms=18395 cpm_usd=0.008713 loading_request=1 instance=00c61b117c895aad5344960bc5673b2f8652d7 app_engine_release=1.9.8 trace_id=160462e198259582cd987016057a05ce",
-            "216.137.62.18 - - [11/Aug/2014:12:37:50 -0700] \"GET /animage.jpg HTTP/1.1\" 200 22029 - \"Referrer String\" \"mydomain.com\" ms=16749 cpu_ms=17187 cpm_usd=0.002462 loading_request=1 instance=00c61b117c74a08ddd0eb033a807fdbff928ad app_engine_release=1.9.8 trace_id=3b9e760a3255e7500f348275276e4206"
+            "216.137.62.18 - - [11/Aug/2014:12:37:50 -0700] \"GET /animage.jpg HTTP/1.1\" 200 22029 - \"Referrer String\" \"mydomain.com\" ms=16749 cpu_ms=17187 cpm_usd=0.002462 loading_request=1 instance=00c61b117c74a08ddd0eb033a807fdbff928ad app_engine_release=1.9.8 trace_id=3b9e760a3255e7500f348275276e4206",
+            "79.38.174.111 - - [12/Aug/2014:00:00:48 -0700] \"GET /vthumb_grip.png HTTP/1.1\" 200 0 \"http://mydomain.com/file.jsp\" \"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36\" \"mydomain.com\" ms=57 cpu_ms=0 app_engine_release=1.9.9 trace_id=85b1c3e86fc703ecdad61b7f85456d40"
             ].each do |message|
                 sample message do
                     insist {subject["result"]} == message
@@ -76,6 +77,18 @@ describe "google app engine grok pattern" do
             insist { subject["app_engine_release"] } == "1.9.8"
             insist { subject["trace_id"] } == "649a71703f21791d12e87f46f8c83dcd"
         end
+        
+        #sample "205.251.208.42 - - [11/Aug/2014:12:52:03 -0700] \"GET /image.jpg HTTP/1.1\" 200 62795 - \"Referrer String\" \"mydomain.com\" ms=24877 cpu_ms=17999 cpm_usd=0.007018 loading_request=1 pending_ms=10572 instance=00c61b117c58c56f7d2d90ee67d4a01e43e262 app_engine_release=1.9.8 trace_id=cd59e954c8a3d2142bcfef9f923839bf" do
+        #    insist { subject["hostname"] } == "\"mydomain.com\""
+        #    insist { subject["ms"] } == "24877"
+        #    insist { subject["cpu_ms"] } == "17999"
+        #    insist { subject["cpm_usd"] } == "0.007018"
+        #    insist { subject["loading_request"] } == "1"
+        #    insist { subject["pending_ms"] } == "10572"
+        #    insist { subject["instance"] } == "00c61b117c58c56f7d2d90ee67d4a01e43e262"
+        #    insist { subject["app_engine_release"] } == "1.9.8"
+        #    insist { subject["trace_id"] } == "cd59e954c8a3d2142bcfef9f923839bf"
+        #end
     end
     
 end
