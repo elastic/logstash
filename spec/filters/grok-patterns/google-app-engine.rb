da01e3f2ad51a582efb3f627ba8b66c41ba19760
Michele Orsi <morsi@map2app.com>
2014-09-09 15:46:54 +0200

    fixed problem with last change in Google App Engine format

--- spec/filters/grok-patterns/google-app-engine.rb
+++ spec/filters/grok-patterns/google-app-engine.rb
@@ -9,41 +9,6 @@ require "test_utils"
 describe "google app engine grok pattern" do
     extend LogStash::RSpec
     
-    describe "GAELOG" do
-        config <<-CONFIG
-        filter {
-            grok {
-                match => { "message" => "%{GAELOG:result}" }
-            }
-        }
-        CONFIG
-        
-        context "should match" do
-            [
-            "0.1.0.1 - - [11/Aug/2014:05:00:00 -0700] \"GET /first/path HTTP/1.1\" 200 44 - \"AppEngine-Google; (+http://code.google.com/appengine)\"",
-            "0.1.0.2 - - [11/Aug/2014:05:00:00 -0700] \"POST /another/path HTTP/1.1\" 204 0 \"http://mydomain.com/referrer\" \"AppEngine-Google; (+http://code.google.com/appengine)\""
-            ].each do |message|
-                sample message do
-                    insist {subject["result"]} == message
-                end
-            end
-        end
-        context "should NOT match" do
-            [
-            "205.251.208.18 - - [11/Aug/2014:00:00:07 -0700] \"GET /main/path HTTP/1.1\" 200 19896 - \"Referrer String\" \"mydomain.com\" ms=59 cpu_ms=62 cpm_usd=0.002224 instance=00c61b117c8717096ff64563cf71f30641cf995d app_engine_release=1.9.8 trace_id=649a71703f21791d12e87f46f8c83dcd",
-            ].each do |message|
-                sample message do
-                    insist {subject["result"]} != message
-                end
-            end
-        end
-        
-        sample "0.1.0.2 - - [11/Aug/2014:05:00:00 -0700] \"POST /another/path HTTP/1.1\" 204 0 \"http://mydomain.com/referrer\" \"AppEngine-Google; (+http://code.google.com/appengine)\"" do
-            insist { subject["referrer"] } == "\"http://mydomain.com/referrer\""
-            insist { subject["agent"] } == "\"AppEngine-Google; (+http://code.google.com/appengine)\""
-        end
-    end
-    
     describe "GAEFULLLOG" do
         config <<-CONFIG
         filter {
@@ -55,15 +20,16 @@ describe "google app engine grok pattern" do
         
         context "should match" do
             [
-            "205.251.208.18 - - [11/Aug/2014:00:00:07 -0700] \"GET /main/path HTTP/1.1\" 200 19896 - \"Referrer String\" \"mydomain.com\" ms=59 cpu_ms=62 cpm_usd=0.002224 instance=00c61b117c8717096ff64563cf71f30641cf995d app_engine_release=1.9.8 trace_id=649a71703f21791d12e87f46f8c83dcd",
-            "0.1.0.3 - - [11/Aug/2014:11:56:38 -0700] \"GET /_ah/warmup HTTP/1.1\" 200 0 - - \"mydomain.com\" ms=15192 cpu_ms=19147 loading_request=1 instance=00c61b117cca2457cdf71e7f6187ac76aa90590c app_engine_release=1.9.8 trace_id=0cb6fce856b4451a28e06194a42d59fe",
-            "205.251.208.42 - - [11/Aug/2014:12:52:03 -0700] \"GET /image.jpg HTTP/1.1\" 200 62795 - \"Referrer String\" \"mydomain.com\" ms=24877 cpu_ms=17999 cpm_usd=0.007018 loading_request=1 pending_ms=10572 instance=00c61b117c58c56f7d2d90ee67d4a01e43e262 app_engine_release=1.9.8 trace_id=cd59e954c8a3d2142bcfef9f923839bf",
-            "205.251.208.18 - - [11/Aug/2014:12:51:55 -0700] \"GET /public/files/ahJzfm1hcDJhcHAtcGxhdGZvcm1yLAsSC1VzZXJBY2NvdW50GK2iDAwLEgVBbGJ1bRi6FwwLEgVNZWRpYRirgQoM_ORIGINAL=s640.jpg HTTP/1.1\" 200 77961 - \"Referrer String\" \"mydomain.com\" ms=16993 cpu_ms=18395 cpm_usd=0.008713 loading_request=1 instance=00c61b117c895aad5344960bc5673b2f8652d7 app_engine_release=1.9.8 trace_id=160462e198259582cd987016057a05ce",
-            "216.137.62.18 - - [11/Aug/2014:12:37:50 -0700] \"GET /animage.jpg HTTP/1.1\" 200 22029 - \"Referrer String\" \"mydomain.com\" ms=16749 cpu_ms=17187 cpm_usd=0.002462 loading_request=1 instance=00c61b117c74a08ddd0eb033a807fdbff928ad app_engine_release=1.9.8 trace_id=3b9e760a3255e7500f348275276e4206",
-            "79.38.174.111 - - [12/Aug/2014:00:00:48 -0700] \"GET /vthumb_grip.png HTTP/1.1\" 200 0 \"http://mydomain.com/file.jsp\" \"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36\" \"mydomain.com\" ms=57 cpu_ms=0 app_engine_release=1.9.9 trace_id=85b1c3e86fc703ecdad61b7f85456d40",
-            "0.1.0.2 - - [13/Aug/2014:08:29:16 -0700] \"POST /testpath HTTP/1.1\" 202 67 \"http://mydomain/endpoint\"; \"AppEngine-Google; (+http://code.google.com/appengine)\" \"mydomain.com\" ms=45787 cpu_ms=27188 cpm_usd=0.071525 queue_name=name-queue task_name=13646048306045697056 instance=00c61b117c6b9e63d18bfa55a4be5369794b2b64 app_engine_release=1.9.9 trace_id=a1f770a9c5a2e6ad23de2c0222473c81",
-            "205.251.208.42 - - [10/Aug/2014:09:50:05 -0700] \"GET /test.jpg HTTP/1.1\" 500 0 - \"Referrer String\" \"mydomain.com\" ms=1722 cpu_ms=0 exit_code=203 instance=00c61b117c47f541c61562bb9a466d2647c3db app_engine_release=1.9.8 trace_id=cb208a3496eb90039b3342682de8fa40",
-            "0.1.0.2 - - [13/Aug/2014:08:04:28 -0700] \"POST /long/path/request HTTP/1.1\" 201 57 \"http://mydomain.com/publishRequests\"; \"AppEngine-Google; (+http://code.google.com/appengine)\" \"cms.map2app.com\" ms=25105 cpu_ms=11144 cpm_usd=0.002562 queue_name=email-queue task_name=2044304020980273760 loading_request=1 pending_ms=4159 instance=00c61b117c1ed91b38fb304b463b0269134b58 app_engine_release=1.9.9 trace_id=029e3cb8e904f69e0ea1e34f14ddc113"
+            "54.240.145.210 - - [09/Sep/2014:00:00:00 -0700] \"GET /image.jpeg HTTP/1.1\" 200 14209 - \"Amazon CloudFront\" \"mydomain.com\" ms=202 cpu_ms=103 cpm_usd=0.001588 instance=00c61b117cd582cbba5e6d6c204dd63bb2e930f0 app_engine_release=1.9.11",
+            "54.240.145.210 - - [09/Sep/2014:00:00:00 -0700] \"GET /image.jpeg HTTP/1.1\" 200 14209 - \"Amazon CloudFront\" \"mydomain.com\" ms=202 cpu_ms=103 cpm_usd=0.001588 instance=00c61b117cd582cbba5e6d6c204dd63bb2e930f0 app_engine_release=1.9.9",
+            "0.1.0.3 - - [09/Sep/2014:00:06:38 -0700] \"GET /_ah/warmup HTTP/1.1\" 200 0 - - \"fulldomain.appspot.com\" ms=7425 cpu_ms=9187 loading_request=1 instance=00c61b117cd98158a19907ee622632a98276e80d app_engine_release=1.9.11",
+            "205.251.208.42 - - [09/Sep/2014:00:15:00 -0700] \"GET /public/image.jpg HTTP/1.1\" 200 51043 - \"Amazon CloudFront\" \"mydomain.com\" ms=23785 cpu_ms=19390 cpm_usd=0.005704 loading_request=1 pending_ms=8619 instance=00c61b117c0f2baf906ea8bf09e7ea9a6b03ef app_engine_release=1.9.11",
+            "0.1.0.30 - - [09/Sep/2014:01:54:34 -0700] \"POST /path/Cgw HTTP/1.1\" 500 51 - \"AppEngine-Google; (+http://code.google.com/appengine; appid: s~appengine-project)\" \"mydomain.com\" ms=59515 cpu_ms=436 cpm_usd=0.000006 exit_code=104 instance=00c61b117c4a26cfc49cbc354e9d6a9d67ebc1 app_engine_release=1.9.11",
+            "0.1.0.2 - - [09/Sep/2014:01:54:40 -0700] \"POST /path/4KDA/attach HTTP/1.1\" 400 75 \"http://mydomain.com/path/GCgw\" \"AppEngine-Google; (+http://code.google.com/appengine)\" \"mydomain.com\" ms=213 cpu_ms=28 cpm_usd=0.000008 queue_name=attach-queue task_name=8839398415480343563 instance=00c61b117c3aa499cd827085818bfc840e3727 app_engine_release=1.9.11",
+            "78.41.129.246 - - [09/Sep/2014:01:55:19 -0700] \"GET /images/logo.jpg HTTP/1.1\" 304 0 \"http://mydomain.com/File.jsp\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36\" \"mydomain.com\" ms=57 cpu_ms=0 app_engine_release=1.9.11",
+            "0.1.0.2 - - [09/Sep/2014:02:22:45 -0700] \"DELETE /longpath/Eg HTTP/1.1\" 200 28 \"http://mydomain.com/fullpath/goM\" \"AppEngine-Google; (+http://code.google.com/appengine)\" \"sub.domain.com\" ms=1707 cpu_ms=15 cpm_usd=0.000003 queue_name=generic-queue task_name=3249496514792712544 pending_ms=1560 instance=00c61b117cbb772ed6f9b6e7f202aebc350f5223 app_engine_release=1.9.11",
+            "0.1.0.30 - - [09/Sep/2014:02:25:25 -0700] \"POST /longPath HTTP/1.1\" 500 51 - \"AppEngine-Google; (+http://code.google.com/appengine; appid: s~google-project)\" \"sub.domain.com\" ms=68486 cpu_ms=20929 cpm_usd=0.000006 loading_request=1 pending_ms=8074 exit_code=104 instance=00c61b117c759fc96edabe2144566bda4cdcd3 app_engine_release=1.9.11",
+            "0.1.0.2 - - [09/Sep/2014:02:24:31 -0700] \"DELETE /path/xI HTTP/1.1\" 200 28 \"http://sub.domain.com/path/oM\" \"AppEngine-Google; (+http://code.google.com/appengine)\" \"sub.domain.com\" ms=14625 cpu_ms=14645 cpm_usd=0.000003 queue_name=generic-queue task_name=3249496514792710480 loading_request=1 pending_ms=2438 instance=00c61b117ca64be5bbdec5b392346094c983b1c8 app_engine_release=1.9.11"
             ].each do |message|
                 sample message do
                     insist {subject["result"]} == message
@@ -71,27 +37,78 @@ describe "google app engine grok pattern" do
             end
         end
         
-        sample "205.251.208.18 - - [11/Aug/2014:00:00:07 -0700] \"GET /main/path HTTP/1.1\" 200 19896 - \"Referrer String\" \"mydomain.com\" ms=59 cpu_ms=62 cpm_usd=0.002224 instance=00c61b117c8717096ff64563cf71f30641cf995d app_engine_release=1.9.8 trace_id=649a71703f21791d12e87f46f8c83dcd" do
+        # normal request
+        sample "54.240.145.210 - - [09/Sep/2014:00:00:00 -0700] \"GET /image.jpeg HTTP/1.1\" 200 14209 - \"Amazon CloudFront\" \"mydomain.com\" ms=202 cpu_ms=103 cpm_usd=0.001588 instance=00c61b117cd582cbba5e6d6c204dd63bb2e930f0 app_engine_release=1.9.11" do
             insist { subject["hostname"] } == "\"mydomain.com\""
-            insist { subject["ms"] } == 59
-            insist { subject["cpu_ms"] } == 62
-            insist { subject["cpm_usd"] } == 0.002224
-            insist { subject["instance"] } == "00c61b117c8717096ff64563cf71f30641cf995d"
-            insist { subject["app_engine_release"] } == "1.9.8"
-            insist { subject["trace_id"] } == "649a71703f21791d12e87f46f8c83dcd"
+            insist { subject["ms"] } == 202
+            insist { subject["cpu_ms"] } == 103
+            insist { subject["cpm_usd"] } == 0.001588
+            insist { subject["instance"] } == "00c61b117cd582cbba5e6d6c204dd63bb2e930f0"
+            insist { subject["app_engine_release"] } == "1.9.11"
         end
         
-        sample "205.251.208.42 - - [11/Aug/2014:12:52:03 -0700] \"GET /image.jpg HTTP/1.1\" 200 62795 - \"Referrer String\" \"mydomain.com\" ms=24877 cpu_ms=17999 cpm_usd=0.007018 loading_request=1 pending_ms=10572 instance=00c61b117c58c56f7d2d90ee67d4a01e43e262 app_engine_release=1.9.8 trace_id=cd59e954c8a3d2142bcfef9f923839bf" do
+        # request with app engine release 1.9.9
+        sample "54.240.145.210 - - [09/Sep/2014:00:00:00 -0700] \"GET /image.jpeg HTTP/1.1\" 200 14209 - \"Amazon CloudFront\" \"mydomain.com\" ms=202 cpu_ms=103 cpm_usd=0.001588 instance=00c61b117cd582cbba5e6d6c204dd63bb2e930f0 app_engine_release=1.9.11" do
             insist { subject["hostname"] } == "\"mydomain.com\""
-            insist { subject["ms"] } == 24877
-            insist { subject["cpu_ms"] } == 17999
-            insist { subject["cpm_usd"] } == 0.007018
-            insist { subject["loading_request"] } == 1
-            insist { subject["pending_ms"] } == 10572
-            insist { subject["instance"] } == "00c61b117c58c56f7d2d90ee67d4a01e43e262"
-            insist { subject["app_engine_release"] } == "1.9.8"
-            insist { subject["trace_id"] } == "cd59e954c8a3d2142bcfef9f923839bf"
+            insist { subject["ms"] } == 202
+            insist { subject["cpu_ms"] } == 103
+            insist { subject["cpm_usd"] } == 0.001588
+            insist { subject["instance"] } == "00c61b117cd582cbba5e6d6c204dd63bb2e930f0"
+            insist { subject["app_engine_release"] } == "1.9.11"
         end
+        
+        # request with loading_request param
+        sample "0.1.0.3 - - [09/Sep/2014:00:06:38 -0700] \"GET /_ah/warmup HTTP/1.1\" 200 0 - - \"fulldomain.appspot.com\" ms=7425 cpu_ms=9187 loading_request=1 instance=00c61b117cd98158a19907ee622632a98276e80d app_engine_release=1.9.11" do
+            insist { subject["ms"] } == 7425
+            insist { subject["cpu_ms"] } == 9187
+            insist { subject["loading_request"] } == 1
+            insist { subject["instance"] } == "00c61b117cd98158a19907ee622632a98276e80d"
+            insist { subject["app_engine_release"] } == "1.9.11"
+        end
+        
+        # request with pending_ms param
+        sample "205.251.208.42 - - [09/Sep/2014:00:15:00 -0700] \"GET /public/image.jpg HTTP/1.1\" 200 51043 - \"Amazon CloudFront\" \"mydomain.com\" ms=23785 cpu_ms=19390 cpm_usd=0.005704 loading_request=1 pending_ms=8619 instance=00c61b117c0f2baf906ea8bf09e7ea9a6b03ef app_engine_release=1.9.11" do
+        	insist { subject["hostname"] } == "\"mydomain.com\""
+        	insist { subject["ms"] } == 23785
+            insist { subject["cpu_ms"] } == 19390
+            insist { subject["cpm_usd"] } == 0.005704
+            insist { subject["loading_request"] } == 1
+            insist { subject["pending_ms"] } == 8619
+            insist { subject["instance"] } == "00c61b117c0f2baf906ea8bf09e7ea9a6b03ef"
+            insist { subject["app_engine_release"] } == "1.9.11"
+        end
+        
+        # request with exit_code param
+        sample "0.1.0.30 - - [09/Sep/2014:01:54:34 -0700] \"POST /path/Cgw HTTP/1.1\" 500 51 - \"AppEngine-Google; (+http://code.google.com/appengine; appid: s~appengine-project)\" \"mydomain.com\" ms=59515 cpu_ms=436 cpm_usd=0.000006 exit_code=104 instance=00c61b117c4a26cfc49cbc354e9d6a9d67ebc1 app_engine_release=1.9.11" do
+        	insist { subject["hostname"] } == "\"mydomain.com\""
+        	insist { subject["ms"] } == 59515
+            insist { subject["cpu_ms"] } == 436
+            insist { subject["cpm_usd"] } == 0.000006
+            insist { subject["exit_code"] } == 104
+            insist { subject["instance"] } == "00c61b117c4a26cfc49cbc354e9d6a9d67ebc1"
+            insist { subject["app_engine_release"] } == "1.9.11"
+        end
+        
+        # request with queue_name and task_name params
+        sample "0.1.0.2 - - [09/Sep/2014:01:54:40 -0700] \"POST /path/4KDA/attach HTTP/1.1\" 400 75 \"http://mydomain.com/path/GCgw\" \"AppEngine-Google; (+http://code.google.com/appengine)\" \"mydomain.com\" ms=213 cpu_ms=28 cpm_usd=0.000008 queue_name=attach-queue task_name=8839398415480343563 instance=00c61b117c3aa499cd827085818bfc840e3727 app_engine_release=1.9.11" do
+        	insist { subject["hostname"] } == "\"mydomain.com\""
+        	insist { subject["ms"] } == 213
+            insist { subject["cpu_ms"] } == 28
+            insist { subject["cpm_usd"] } == 0.000008
+            insist { subject["queue_name"] } == "attach-queue"
+            insist { subject["task_name"] } == "8839398415480343563"
+            insist { subject["instance"] } == "00c61b117c3aa499cd827085818bfc840e3727"
+            insist { subject["app_engine_release"] } == "1.9.11"
+        end
+        
+        # request without instance and cpm_usd
+        sample "78.41.129.246 - - [09/Sep/2014:01:55:19 -0700] \"GET /images/logo.jpg HTTP/1.1\" 304 0 \"http://mydomain.com/File.jsp\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36\" \"mydomain.com\" ms=57 cpu_ms=0 app_engine_release=1.9.11" do
+        	insist { subject["hostname"] } == "\"mydomain.com\""
+        	insist { subject["ms"] } == 57
+            insist { subject["cpu_ms"] } == 0
+            insist { subject["app_engine_release"] } == "1.9.11"
+        end
+        
     end
     
 end
