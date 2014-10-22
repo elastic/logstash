ac3f625ca44d063e3e7d0da0b9d2860575c3ad93
Colin Surprenant <colin.surprenant@gmail.com>
2014-10-22 17:33:49 -0400

    added messagepack & marshal serializers

--- lib/logstash/queue_serializer.rb
+++ lib/logstash/queue_serializer.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require "logstash/json"
+require "msgpack"
 
 # pluggable serializer for the persistent queue
 
@@ -16,5 +17,37 @@ class LogStash::JsonSerializer
   # @return [LogStash::Event] the deserialzed event
   def deserialize(data)
     LogStash::Json.load(data)
+  end
+end
+
+class LogStash::MessagePackSerializer
+
+  # @param event [LogStash::Event] the event to serialize
+  # @return [String] the serialized event or nil to skip persistence
+  def serialize(event)
+    # exclude non LogStash::Event like the LogStash::ShutdownEvent and LogStash::FlushEvent
+    event.is_a?(LogStash::Event) ? MessagePack.pack(event.to_json_with_metadata) : nil
+  end
+
+  # @param data [String] searialized string data to deserialize
+  # @return [LogStash::Event] the deserialzed event
+  def deserialize(data)
+    MessagePack.unpack(data)
+  end
+end
+
+class LogStash::MarshalSerializer
+
+  # @param event [LogStash::Event] the event to serialize
+  # @return [String] the serialized event or nil to skip persistence
+  def serialize(event)
+    # exclude non LogStash::Event like the LogStash::ShutdownEvent and LogStash::FlushEvent
+    event.is_a?(LogStash::Event) ? Marshal.dump(event.to_json_with_metadata) : nil
+  end
+
+  # @param data [String] searialized string data to deserialize
+  # @return [LogStash::Event] the deserialzed event
+  def deserialize(data)
+    Marshal.load(data)
   end
 end
