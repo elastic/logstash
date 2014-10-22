06a85513f70c72f333ab001f04e0260af001738e
Colin Surprenant <colin.surprenant@gmail.com>
2014-10-22 17:52:48 -0400

    added apache logs, changed description and now computes top 20%

--- test/integration/suite/basic_performance_long.rb
+++ test/integration/suite/basic_performance_long.rb
@@ -8,9 +8,10 @@
 #]
 #
 [
-  {:name => "simple json out", :config => "config/simple_json_out.conf", :input => "input/simple_10.txt", :time => 120},
-  {:name => "simple line out", :config => "config/simple.conf", :input => "input/simple_10.txt", :time => 120},
-  {:name => "json codec", :config => "config/json_inout_codec.conf", :input => "input/json_medium.txt", :time => 120},
-  {:name => "json filter", :config => "config/json_inout_filter.conf", :input => "input/json_medium.txt", :time => 120},
-  {:name => "complex syslog", :config => "config/complex_syslog.conf", :input => "input/syslog_acl_10.txt", :time => 120},
+  {:name => "simple line in/out", :config => "config/simple.conf", :input => "input/simple_10.txt", :time => 120},
+  {:name => "simple line in/json out", :config => "config/simple_json_out.conf", :input => "input/simple_10.txt", :time => 120},
+  {:name => "json codec in/out", :config => "config/json_inout_codec.conf", :input => "input/json_medium.txt", :time => 120},
+  {:name => "line in/json filter/json out", :config => "config/json_inout_filter.conf", :input => "input/json_medium.txt", :time => 120},
+  {:name => "apache in/json out", :config => "config/standard_apache.conf", :input => "input/apache_log.txt", :time => 120},
+  {:name => "syslog in/json out", :config => "config/complex_syslog.conf", :input => "input/syslog_acl_10.txt", :time => 120},
 ]
