905adc0aac17532a7c6084f45e75f0176f8a996c
Colin Surprenant <colin.surprenant@gmail.com>
2014-10-23 19:51:00 -0400

    removed Marshal and MessagePack and fixed Json

--- lib/logstash/queue_serializer.rb
+++ lib/logstash/queue_serializer.rb
@@ -5,49 +5,21 @@ require "msgpack"
 # pluggable serializer for the persistent queue
 
 class LogStash::JsonSerializer
+  NL = "\n".freeze
 
   # @param event [LogStash::Event] the event to serialize
   # @return [String] the serialized event or nil to skip persistence
   def serialize(event)
     # exclude non LogStash::Event like the LogStash::ShutdownEvent and LogStash::FlushEvent
-    event.is_a?(LogStash::Event) ? LogStash::Json.dump(event.to_json_with_metadata) : nil
+    return nil unless event.is_a?(LogStash::Event)
+
+    event.to_json + NL + LogStash::Json.dump(event.metadata)
   end
 
   # @param data [String] searialized string data to deserialize
   # @return [LogStash::Event] the deserialzed event
   def deserialize(data)
-    LogStash::Json.load(data)
-  end
-end
-
-class LogStash::MessagePackSerializer
-
-  # @param event [LogStash::Event] the event to serialize
-  # @return [String] the serialized event or nil to skip persistence
-  def serialize(event)
-    # exclude non LogStash::Event like the LogStash::ShutdownEvent and LogStash::FlushEvent
-    event.is_a?(LogStash::Event) ? MessagePack.pack(event.to_json_with_metadata) : nil
-  end
-
-  # @param data [String] searialized string data to deserialize
-  # @return [LogStash::Event] the deserialzed event
-  def deserialize(data)
-    MessagePack.unpack(data)
-  end
-end
-
-class LogStash::MarshalSerializer
-
-  # @param event [LogStash::Event] the event to serialize
-  # @return [String] the serialized event or nil to skip persistence
-  def serialize(event)
-    # exclude non LogStash::Event like the LogStash::ShutdownEvent and LogStash::FlushEvent
-    event.is_a?(LogStash::Event) ? Marshal.dump(event.to_json_with_metadata) : nil
-  end
-
-  # @param data [String] searialized string data to deserialize
-  # @return [LogStash::Event] the deserialzed event
-  def deserialize(data)
-    Marshal.load(data)
+    event, metadata = data.split(NL).map{|o| LogStash::Json.load(o)}
+    LogStash::Event.new(event.merge({"@metadata" => metadata}))
   end
 end
