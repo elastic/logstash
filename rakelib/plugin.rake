fec0be423a1f194eb907d1c070fad8a88c2ba62b
Richard Pijnenburg <richard@ispavailability.com>
2014-10-30 09:57:11 +0000

    Cleanup plugins to move to plugin system
    Plugins have moved to https://github.com/logstash-plugins/... into
    individual repositories. One plugin per repo.
    
    * Add plugin install task and add required plugins for testing
    * Adding a plugin task to install plugins
    * Added required plugins to a prep task for the test part.
    
    Fixes #1984
    
    Conflicts:
    	lib/logstash/filters/multiline.rb
    	lib/logstash/outputs/hipchat.rb
    	logstash.gemspec

---
+++ rakelib/plugin.rake
@@ -0,0 +1,12 @@
+namespace "plugin" do
+  task "install",  :name do |task, args|
+    name = args[:name]
+    puts "[plugin] Installing plugin: #{name}"
+
+    cmd = ['bin/logstash', 'plugin', 'install', name ]
+    system(*cmd)
+    raise RuntimeError, $!.to_s unless $?.success?
+
+    task.reenable # Allow this task to be run again
+  end # task "install"
+end # namespace "plugin"
--- spec/util/environment_spec.rb
+++ spec/util/environment_spec.rb
@@ -2,15 +2,4 @@ require "logstash/environment"
 
 describe LogStash::Environment do
 
-  describe "load_elasticsearch_jars!" do
-
-    it "should load elasticsarch jars" do
-      expect{LogStash::Environment.load_elasticsearch_jars!}.to_not raise_error
-    end
-
-    it "should raise when cannot find elasticsarch jars" do
-      stub_const("LogStash::Environment::ELASTICSEARCH_DIR", "/some/invalid/path")
-      expect{LogStash::Environment.load_elasticsearch_jars!}.to raise_error(LogStash::EnvironmentError)
-    end
-  end
 end
