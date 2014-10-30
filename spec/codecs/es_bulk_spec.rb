1310483f6df40fe78aa20c4a4773b556a67c1db7
Aaron Mildenstein <aaron@mildensteins.com>
2014-10-30 13:51:25 -0500

    Updated tests to test for accurate data

--- spec/codecs/es_bulk_spec.rb
+++ spec/codecs/es_bulk_spec.rb
@@ -1,6 +1,5 @@
 require "logstash/codecs/es_bulk"
 require "logstash/event"
-#require "logstash/json"
 require "insist"
 
 describe LogStash::Codecs::ESBulk do
@@ -9,8 +8,7 @@ describe LogStash::Codecs::ESBulk do
   end
 
   context "#decode" do
-    it "should return an event from json data" do
-      count = 0
+    it "should return 4 events from json data" do
       data = <<-HERE
       { "index" : { "_index" : "test", "_type" : "type1", "_id" : "1" } }
       { "field1" : "value1" }
@@ -21,22 +19,39 @@ describe LogStash::Codecs::ESBulk do
       { "doc" : {"field2" : "value2"} }
       HERE
 
+      count = 0
       subject.decode(data) do |event|
+        case count
+        when 0
+          insist { event['@metadata']['_id'] } == "1"
+          insist { event['@metadata']['action'] } == "index"
+          insist { event['field1'] } == "value1"
+        when 1
+          insist { event['@metadata']['_id'] } == "2"
+          insist { event['@metadata']['action'] } == "delete"
+        when 2
+          insist { event['@metadata']['_id'] } == "3"
+          insist { event['@metadata']['action'] } == "create"
+          insist { event['field1'] } == "value3"
+        when 3
+          insist { event['@metadata']['_id'] } == "1"
+          insist { event['@metadata']['action'] } == "update"
+          insist { event['doc']['field2'] } == "value2"
+        end
         count += 1
       end
       insist { count } == 4
     end
-
-#    context "processing plain text" do
-#      it "falls back to plain text" do
-#        decoded = false
-#        subject.decode("something that isn't json\n") do |event|
-#          decoded = true
-#          insist { event.is_a?(LogStash::Event) }
-#          insist { event["message"] } == "something that isn't json"
-#        end
-#        insist { decoded } == true
-#      end
-#    end
   end
+
+  context "fail to process non-bulk event then continue" do
+    it "continues after a fail" do
+      decoded = false
+      subject.decode("something that isn't a bulk event\n") do |event|
+        decoded = true
+      end
+      insist { decoded } == false
+    end
+  end
+
 end
