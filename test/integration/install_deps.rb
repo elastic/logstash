83a13c0b24c0c5e5cd85c3c4867dc2c2be19360f
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-11-26 12:53:15 +0100

    add an script to warm up the deps for a raw integrationt test run

---
+++ test/integration/install_deps.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+inputs  = ['stdin']
+outputs = ['stdout']
+filters = ['clone', 'json', 'grok', 'syslog_pri', 'date', 'mutate']
+
+`rake bootstrap`
+
+inputs.each do |input|
+  `./bin/logstash plugin install logstash-input-#{input}`
+end
+
+outputs.each do |output|
+  `./bin/logstash plugin install logstash-output-#{output}`
+end
+
+filters.each do |filter|
+  `./bin/logstash plugin install logstash-filter-#{filter}`
+end
+
