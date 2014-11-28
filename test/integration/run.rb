3aa35ab777719a12c11b052535fb313c92ab1249
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-11-28 13:18:08 +0100

    format the output to be more csv alike

--- test/integration/run.rb
+++ test/integration/run.rb
@@ -111,7 +111,7 @@ end
 
 # standalone quick & dirty options parsing
 args = ARGV.dup
-if args.size != 6
+if args.size != 8
   $stderr.puts("usage: ruby run.rb --events [events count] --config [config file] --input [input file]")
   $stderr.puts("       ruby run.rb --time [seconds] --config [config file] --input [input file]")
   exit(1)
@@ -131,6 +131,8 @@ while !args.empty?
     options[:config] = option
   when "--input"
     options[:input] = option
+  when "--headers"
+    options[:headers] = option
   else
     raise(IllegalArgumentException, "invalid config #{config}")
   end
@@ -140,9 +142,12 @@ end
 
 required_events_count = options[:events].to_i # total number of events to feed, independant of input file size
 required_run_time = options[:time].to_i
+add_headers       = options[:headers]
 
 puts("using config file=#{options[:config]}, input file=#{options[:input]}") if @debug
 
 runner = Runner.new(options[:config], @debug)
 p, elaspsed, real_events_count = runner.run(required_events_count, required_run_time, runner.read_input_file(options[:input]))
-puts("elaspsed=#{"%.2f" % elaspsed}s, events=#{real_events_count}, avg tps=#{"%.0f" % (real_events_count / elaspsed)}, best tps=#{p.last}, avg top 20% tps=#{"%.0f" % (p.reduce(:+) / p.size)}")
+puts("elaspsed, events, avg tps, best tps, avg top 20% tps") if add_headers
+puts("#{"%.2f" % elaspsed}, #{real_events_count}, #{"%.0f" % (real_events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}")
+
