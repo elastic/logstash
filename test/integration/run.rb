09b936f1d2fca734ca85dc2e63b2e147918e01ee
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-11-28 15:52:33 +0100

    more refactoring and output clean up to match csv format
    Conflicts:
    	test/integration/run.rb

--- test/integration/run.rb
+++ test/integration/run.rb
@@ -16,16 +16,17 @@ class Runner
   LOGSTASH_BIN  = File.join(File.expand_path("../../../bin/", __FILE__), "logstash").freeze
   REFRESH_COUNT = 100
 
-  attr_reader :command
+  attr_reader :command, :headers
 
   def initialize(config, debug=false)
     @debug = debug
     @command = [LOGSTASH_BIN, "-f", config]
+    @headers = ["elaspsed", "events", "avg tps", "best tps", "avg top 20% tps"]
   end
 
 
   def run(required_events_count, required_run_time, input_lines)
-    puts("launching #{command.join(" ")}") if @debug
+    puts("launching #{command.join(" ")} #{required_events_count} #{required_run_time}") if @debug
     stats = Stats.new
     real_events_count = 0
     Open3.popen3(*@command) do |i, o, e|
@@ -58,7 +59,6 @@ class Runner
   def read_input_file(file_path)
     IO.readlines(file_path).map(&:chomp)
   end
-
 
   private
 
@@ -106,48 +106,55 @@ class Runner
 
 end
 
+def extract_options(args)
+  options = {}
+  while !args.empty?
+    config = args.shift.to_s.strip
+    option = args.shift.to_s.strip
+    raise(IllegalArgumentException, "invalid option for #{config}") if option.empty?
+    case config
+    when "--events"
+      options[:events] = option
+    when "--time"
+      options[:time] = option
+    when "--config"
+      options[:config] = option
+    when "--input"
+      options[:input] = option
+    when "--headers"
+      options[:headers] = option
+    else
+      raise(IllegalArgumentException, "invalid config #{config}")
+    end
+  end
+
+  options
+
+end
+
 #
 ## script main
-
-# standalone quick & dirty options parsing
-args = ARGV.dup
-if args.size != 8
-  $stderr.puts("usage: ruby run.rb --events [events count] --config [config file] --input [input file]")
-  $stderr.puts("       ruby run.rb --time [seconds] --config [config file] --input [input file]")
-  exit(1)
-end
-
-options = {}
-while !args.empty?
-  config = args.shift.to_s.strip
-  option = args.shift.to_s.strip
-  raise(IllegalArgumentException, "invalid option for #{config}") if option.empty?
-  case config
-  when "--events"
-    options[:events] = option
-  when "--time"
-    options[:time] = option
-  when "--config"
-    options[:config] = option
-  when "--input"
-    options[:input] = option
-  when "--headers"
-    options[:headers] = option
-  else
-    raise(IllegalArgumentException, "invalid config #{config}")
+if __FILE__ == $0
+  # standalone quick & dirty options parsing
+  args = ARGV.dup
+  if args.size != 8
+    $stderr.puts("usage: ruby run.rb --events [events count] --config [config file] --input [input file]")
+    $stderr.puts("       ruby run.rb --time [seconds] --config [config file] --input [input file]")
+    exit(1)
   end
+
+  @debug = !!ENV["DEBUG"]
+
+  options      = extract_options args
+  events_count = options[:events].to_i # total number of events to feed, independant of input file size
+  run_time     = options[:time].to_i
+
+  puts("using config file=#{options[:config]}, input file=#{options[:input]}") if @debug
+
+  runner = Runner.new(options[:config], @debug)
+  p, elaspsed, real_events_count = runner.run(events_count, run_time, runner.read_input_file(options[:input]))
+
+  puts runner.headers if options[:headers]
+  puts("#{"%.2f" % elaspsed}, #{real_events_count}, #{"%.0f" % (real_events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}")
+
 end
-
-@debug = !!ENV["DEBUG"]
-
-required_events_count = options[:events].to_i # total number of events to feed, independant of input file size
-required_run_time = options[:time].to_i
-add_headers       = options[:headers]
-
-puts("using config file=#{options[:config]}, input file=#{options[:input]}") if @debug
-
-runner = Runner.new(options[:config], @debug)
-p, elaspsed, real_events_count = runner.run(required_events_count, required_run_time, runner.read_input_file(options[:input]))
-puts("elaspsed, events, avg tps, best tps, avg top 20% tps") if add_headers
-puts("#{"%.2f" % elaspsed}, #{real_events_count}, #{"%.0f" % (real_events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}")
-
