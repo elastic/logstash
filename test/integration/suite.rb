796e5cb65754bcc490450c1120b9763108894ae6
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-11-28 15:52:33 +0100

    more refactoring and output clean up to match csv format

--- test/integration/suite.rb
+++ test/integration/suite.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'test/integration/run'
+
 RUNNER = File.join(File.expand_path(File.dirname(__FILE__)), "run.rb")
 BASE_DIR = File.expand_path(File.dirname(__FILE__))
 
@@ -13,12 +15,30 @@ end
 @debug = !!ENV["DEBUG"]
 
 tests = eval(IO.read(ARGV[0]))
+lines = []
+first = true
 
-tests.each do |test|
-  duration = test[:events] ? ["--events", test[:events]] : ["--time", test[:time]]
-  command = ["ruby", RUNNER, *duration, "--config", File.join(BASE_DIR, test[:config]), "--input", File.join(BASE_DIR, test[:input]), "--headers", true]
-  IO.popen(command.join(" "), "r") do |io|
-    puts("#{test[:name]}")
-    io.each_line{|line| puts(line)}
+reporter = Thread.new do
+  loop do
+    $stderr.print "."
+    sleep 1
   end
 end
+
+tests.each do |test|
+
+  events = test[:events].to_i # total number of events to feed, independant of input file size
+  time   = test[:time].to_i
+  config = File.join(BASE_DIR, test[:config])
+  input  = File.join(BASE_DIR, test[:input])
+
+  runner = Runner.new(config, @debug)
+  p, elaspsed, events_count = runner.run(events, time, runner.read_input_file(input))
+
+  lines << "name, #{runner.headers.join(' ')}" if first
+  lines << "#{test[:name]}, #{"%.2f" % elaspsed}, #{events_count}, #{"%.0f" % (events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}"
+  first = false
+end
+
+reporter.kill
+puts lines.join("\n")
