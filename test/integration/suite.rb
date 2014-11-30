02ed94c48d31d7135860adafa7e08d9ed4f53f53
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2014-11-30 17:19:32 +0100

    fix separator in the csv format

--- test/integration/suite.rb
+++ test/integration/suite.rb
@@ -35,7 +35,7 @@ tests.each do |test|
   runner = Runner.new(config, @debug)
   p, elaspsed, events_count = runner.run(events, time, runner.read_input_file(input))
 
-  lines << "name, #{runner.headers.join(' ')}" if first
+  lines << "name, #{runner.headers.join(',')}" if first
   lines << "#{test[:name]}, #{"%.2f" % elaspsed}, #{events_count}, #{"%.0f" % (events_count / elaspsed)},#{p.last}, #{"%.0f" % (p.reduce(:+) / p.size)}"
   first = false
 end
