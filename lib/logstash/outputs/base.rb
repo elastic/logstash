elastic/logstash

commit e0eeba4a70d91027139e16b07f45bc7885bde6f0

Include filter/output class name in skip/drop debug log messages
Bit distressing to see log messages like 'Skipping event' without seeing
*what* is skipping it!

lib/logstash/outputs/base.rb
@@ -59,7 +59,7 @@ class LogStash::Outputs::Base < LogStash::Plugin
   def output?(event)
     if !@type.empty?
       if event.type != @type
-        @logger.debug? and @logger.debug(["Dropping event because type doesn't match #{@type}", event])
+        @logger.debug? and @logger.debug(["outputs/#{self.class.name}: Dropping event because type doesn't match #{@type}", event])
         return false
       end
     end
@@ -67,14 +67,14 @@ class LogStash::Outputs::Base < LogStash::Plugin
     if !@tags.empty?
       return false if !event["tags"]
       if !@tags.send(@include_method) {|tag| event["tags"].include?(tag)}
-        @logger.debug? and @logger.debug("Dropping event because tags don't match #{@tags.inspect}", event)
+        @logger.debug? and @logger.debug("outputs/#{self.class.name}: Dropping event because tags don't match #{@tags.inspect}", event)
         return false
       end
     end
 
     if !@exclude_tags.empty? && event["tags"]
       if @exclude_tags.send(@exclude_method) {|tag| event["tags"].include?(tag)}
-        @logger.debug? and @logger.debug("Dropping event because tags contains excluded tags: #{exclude_tags.inspect}", event)
+        @logger.debug? and @logger.debug("outputs/#{self.class.name}: Dropping event because tags contains excluded tags: #{exclude_tags.inspect}", event)
         return false
       end
     end

lib/logstash/filters/base.rb
@@ -159,7 +159,7 @@ class LogStash::Filters::Base < LogStash::Plugin
   def filter?(event)
     if !@type.empty?
       if event.type != @type
-        @logger.debug? and @logger.debug(["Skipping event because type doesn't match #{@type}", event])
+        @logger.debug? and @logger.debug(["filters/#{self.class.name}: Skipping event because type doesn't match #{@type}", event])
         return false
       end
     end
@@ -171,14 +171,14 @@ class LogStash::Filters::Base < LogStash::Plugin
 
       # Is @tags a subset of the event's tags? If not, skip it.
       if (event["tags"] & @tags).size != @tags.size
-        @logger.debug(["Skipping event because tags don't match #{@tags.inspect}", event])
+        @logger.debug(["filters/#{self.class.name}: Skipping event because tags don't match #{@tags.inspect}", event])
         return false
       end
     end
 
     if !@exclude_tags.empty? && event["tags"]
       if (diff_tags = (event["tags"] & @exclude_tags)).size != 0
-        @logger.debug(["Skipping event because tags contains excluded tags: #{diff_tags.inspect}", event])
+        @logger.debug(["filters/#{self.class.name}: Skipping event because tags contains excluded tags: #{diff_tags.inspect}", event])
         return false
       end
     end

commit d24a80f7cc935af3182f5ffc03d30481962caefb

Missing brackets
https://github.com/elasticsearch/logstash/commit/e0eeba4a70d91027139e16b07f45bc7885bde6f0 introduced this bug long ago

lib/logstash/outputs/base.rb
@@ -103,14 +103,14 @@ class LogStash::Outputs::Base < LogStash::Plugin
       return false if !event["tags"]
       @include_method = :any?
       if !@tags.send(@include_method) {|tag| event["tags"].include?(tag)}
-        @logger.debug? and @logger.debug("outputs/#{self.class.name}: Dropping event because tags don't match #{@tags.inspect}", event)
+        @logger.debug? and @logger.debug(["outputs/#{self.class.name}: Dropping event because tags don't match #{@tags.inspect}", event])
         return false
       end
     end
 
     if !@exclude_tags.empty? && event["tags"]
       if @exclude_tags.send(@exclude_method) {|tag| event["tags"].include?(tag)}
-        @logger.debug? and @logger.debug("outputs/#{self.class.name}: Dropping event because tags contains excluded tags: #{exclude_tags.inspect}", event)
+        @logger.debug? and @logger.debug(["outputs/#{self.class.name}: Dropping event because tags contains excluded tags: #{exclude_tags.inspect}", event])
         return false
       end
     end