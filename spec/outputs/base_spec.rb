232cf92178889892aebf9edbd0877db92e977bcf
wiibaa <philippeweber57@yahoo.fr>
2015-01-12 06:08:31 +0100

    harmonized event filtering  by type/tags in filter and output + correct logging
    Fixes #2339

---
+++ spec/outputs/base_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+# use a dummy NOOP output to test Outputs::Base
+class LogStash::Outputs::NOOP < LogStash::Outputs::Base
+  config_name "noop"
+  milestone 2
+
+  def register; end
+
+  def receive(event)
+    return output?(event)
+  end
+end
+
+describe "LogStash::Outputs::Base#output?" do
+  it "should filter by type" do
+    output = LogStash::Outputs::NOOP.new("type" => "noop")
+    expect(output.receive(LogStash::Event.new({"type" => "noop"}))).to eq(true)
+    expect(output.receive(LogStash::Event.new({"type" => "not_noop"}))).to eq(false)
+  end
+  
+  it "should filter by tags" do
+    output = LogStash::Outputs::NOOP.new("tags" => ["value", "value2"])
+    expect(output.receive(LogStash::Event.new({"tags" => ["value","value2"]}))).to eq(true)
+    expect(output.receive(LogStash::Event.new({"tags" => ["notvalue"]}))).to eq(false)
+    expect(output.receive(LogStash::Event.new({"tags" => ["value"]}))).to eq(false)
+  end
+
+  it "should exclude by tags" do
+    output = LogStash::Outputs::NOOP.new("exclude_tags" => ["value"])
+    expect(output.receive(LogStash::Event.new({"tags" => ["value"]}))).to eq(false)
+    expect(output.receive(LogStash::Event.new({"tags" => ["notvalue"]}))).to eq(true)
+  end
+end
