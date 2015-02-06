1cab10dc35118ee129602e1c99aba7a76f5f94a7
Colin Surprenant <colin.surprenant@gmail.com>
2015-02-06 14:58:05 -0500

    reenable list, some cleanups

--- lib/logstash/pluginmanager/list.rb
+++ lib/logstash/pluginmanager/list.rb
@@ -7,35 +7,26 @@ class LogStash::PluginManager::List < Clamp::Command
 
   parameter "[PLUGIN]", "Plugin name to search for, leave empty for all plugins"
 
-  option "--group", "NAME", "Show all plugins from a certain group. Can be one of 'output', 'input', 'codec', 'filter'"
-
-  def execute
-    $stdout.puts("the plugin list command is not yet supported")
-    99
+  option "--group", "NAME", "Filter plugins per group: input, output, filter or codec" do |arg|
+    raise(ArgumentError, "should be one of: input, output, filter or codec") unless ['input', 'output', 'filter', 'codec'].include?(arg)
+    arg
   end
 
-  # def execute
-  #   if group
-  #     unless ['input', 'output', 'filter', 'codec'].include?(group)
-  #       signal_usage_error "Group name not valid"
-  #     end
-  #     plugin_name = nil
-  #   else
-  #     plugin_name = plugin
-  #   end
+  def execute
+    plugin_name = group ? nil : plugin
 
-  #   Gem.configuration.verbose = false
+    Gem.configuration.verbose = false
 
-  #   # If we are listing a group make sure we check all gems
-  #   specs = LogStash::PluginManager::Util.matching_specs(plugin_name) \
-  #           .select{|spec| LogStash::PluginManager::Util.logstash_plugin?(spec) } \
-  #           .select{|spec| group ? group == spec.metadata['logstash_group'] : true}
-  #   if specs.empty?
-  #     $stderr.puts ("No plugins found.")
-  #     return 0
-  #   end
-  #   specs.each {|spec| puts ("#{spec.name} (#{spec.version})") }
-  #   return 0
-  # end
+    # If we are listing a group make sure we check all gems
+    specs = LogStash::PluginManager::Util.matching_specs(plugin_name) \
+            .select{|spec| LogStash::PluginManager::Util.logstash_plugin?(spec) } \
+            .select{|spec| group ? group == spec.metadata['logstash_group'] : true}
+    if specs.empty?
+      $stderr.puts ("No plugins found.")
+      return 0
+    end
+    specs.each {|spec| puts ("#{spec.name} (#{spec.version})") }
+    return 0
+  end
 
 end # class Logstash::PluginManager
