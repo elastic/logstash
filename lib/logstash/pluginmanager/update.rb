55bfefd3721c0c306cbb3bacf64c2dc60a56594c
Colin Surprenant <colin.surprenant@gmail.com>
2015-02-06 20:17:59 -0500

    verify valid plugin and update all now only update plugins not all gems

--- lib/logstash/pluginmanager/update.rb
+++ lib/logstash/pluginmanager/update.rb
@@ -13,71 +13,41 @@ class LogStash::PluginManager::Update < Clamp::Command
   parameter "[PLUGIN]", "Plugin name to upgrade to latest version"
 
   def execute
-    puts("Updating " + (plugin ? plugin : "all plugins"))
+    gemfile = LogStash::Gemfile.new(File.new(LogStash::Environment::GEMFILE_PATH, "r+")).load
+    # keep a copy of the gemset to revert on error
+    original_gemset = gemfile.gemset.copy
+
+    # create list of plugins to update
+    plugins = if plugin
+      unless LogStash::PluginManager.is_installed_plugin?(plugin, gemfile)
+        $stderr.puts("Plugin #{plugin} has not been previously installed, aborting")
+        return 99
+      end
+      [plugin]
+    else
+      LogStash::PluginManager.all_installed_plugins(gemfile)
+    end
+
+    # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
+    # calling update without requiremend will remove any previous requirements
+    plugins.each{|plugin| gemfile.update(plugin)}
+    gemfile.save
+
+    puts("Updating " + plugins.join(", "))
 
     # any errors will be logged to $stderr by invoke_bundler!
     output, exception = LogStash::PluginManager.invoke_bundler!(:update => (plugin || true))
+    if exception
+      # revert to original Gemfile content
+      gemfile.gemset = original_gemset
+      gemfile.save
+      return 99
+    end
+
     if ENV["DEBUG"]
       $stderr.puts(output)
       $stderr.puts("Error: #{exception.class}, #{exception.message}") if exception
     end
     return exception ? 99 : 0
   end
-
-  # def execute
-  #   LogStash::Environment.load_logstash_gemspec!
-  #   ::Gem.configuration.verbose = false
-  #   ::Gem.configuration[:http_proxy] = proxy
-
-  #   if plugin.nil?
-  #     puts ("Updating all plugins")
-  #   else
-  #     puts ("Updating #{plugin} plugin")
-  #   end
-
-  #   specs = LogStash::PluginManager::Util.matching_specs(plugin).select{|spec| LogStash::PluginManager::Util.logstash_plugin?(spec) }
-  #   if specs.empty?
-  #     $stderr.puts ("No plugins found to update or trying to update a non logstash plugin.")
-  #     return 99
-  #   end
-  #   specs.each { |spec| update_gem(spec, version) }
-  #   return 0
-  # end
-
-
-  # def update_gem(spec, version)
-
-  #   unless gem_path = LogStash::PluginManager::Util.download_gem(spec.name, version)
-  #     $stderr.puts ("Plugin '#{spec.name}' does not exist remotely. Skipping.")
-  #     return 0
-  #   end
-
-  #   unless gem_meta = LogStash::PluginManager::Util.logstash_plugin?(gem_path)
-  #     $stderr.puts ("Invalid logstash plugin gem. skipping.")
-  #     return 99
-  #   end
-
-  #   unless Gem::Version.new(gem_meta.version) > Gem::Version.new(spec.version)
-  #     puts ("No newer version available for #{spec.name}. skipping.")
-  #     return 0
-  #   end
-
-  #   puts ("Updating #{spec.name} from version #{spec.version} to #{gem_meta.version}")
-
-  #   if LogStash::PluginManager::Util.installed?(spec.name)
-  #     ::Gem.done_installing_hooks.clear
-  #     ::Gem::Uninstaller.new(gem_meta.name, {:force => true}).uninstall
-  #   end
-
-  #   ::Gem.configuration.verbose = false
-  #   FileDependencies::Gem.hook
-  #   options = {}
-  #   options[:document] = []
-  #   inst = Gem::DependencyInstaller.new(options)
-  #   inst.install spec.name, gem_meta.version
-  #   specs, _ = inst.installed_gems
-  #   puts ("Update successful")
-  #   return 0
-  # end
-
-end # class Logstash::PluginManager
+end
