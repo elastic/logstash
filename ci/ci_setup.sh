5cf8a45eeb88ec72790579854e418ed9a317e43a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-02-18 13:24:24 +0100

    Add a set of scripts that simplify:   * The necessary setup for the CI test to be executed.   * The actuall CI test execution, having a different scripts for each     different workflow. This makes the test execution simpler and standard, so changes in internal task don't need big changes in the CI config. It also documents and makes easy for developers to run the tests.

---
+++ ci/ci_setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+##
+# Note this setup needs a system ruby to be available, this can not
+# be done here as is higly system dependant.
+##
+
+#squid proxy work, so if there is a proxy it can be cached.
+sed -i.bak 's/https:/http:/' Gemfile
+
+# Clean up some  possible stale directories
+rm -rf vendor       # make sure there are no vendorized dependencies
+rm -rf spec/reports # no stale spec reports from previous executions
+
+# Setup the environment
+rake bootstrap # Bootstrap your logstash instance
---
+++ ci/ci_test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+##
+# Keep in mind to run ci/ci_setup.sh if you need to setup/clean up your environment before
+# running the test suites here.
+##
+
+SELECTED_TEST_SUITE=$1
+
+if [[ $SELECTED_TEST_SUITE == $"core-fail-fast" ]]; then
+  echo "Running core-fail-fast tests"
+  rake test:install-core    # Install core dependencies for testing.
+  rake test:core-fail-fast  # Run core tests
+elif [[ $SELECTED_TEST_SUITE == $"all" ]]; then
+  echo "Running all plugins tests"
+  rake test:install-all     # Install all plugins in this logstash instance, including development dependencies
+  rake test:plugins         # Run all plugins tests
+else
+  echo "Running core tests"
+  rake test:install-core    # Install core dependencies for testing.
+  rake test:core            # Run core tests
+fi
