945e184e67b9515f2ac5d5c1dea3d7c2b72b55af
Aaron Mildenstein <aaron@mildensteins.com>
2015-02-20 13:55:13 -0700

    First pass at cleaning out logstash web
    All internal references, flags, kibana, vendor, everything must go!
    
    If it referenced kibana or logstash-web, it is deprecated, and removed!
    
    Operators are standing by...
    
    Fixes #2661

--- spec/core/runner_spec.rb
+++ spec/core/runner_spec.rb
@@ -1,6 +1,5 @@
 require "spec_helper"
 require "logstash/runner"
-require "logstash/kibana"
 require "stud/task"
 
 class NullRunner
@@ -35,18 +34,6 @@ describe LogStash::Runner do
       args = ["welp"]
       expect(subject.run(args).wait).to eq(1)
     end
-
-    it "should run agent help and not run following commands" do
-      expect(subject).to receive(:show_help).once.and_return(nil)
-      args = ["agent", "-h", "web"]
-      expect(subject.run(args).wait).to eq(0)
-    end
-
-    it "should not run agent and web" do
-      expect(Stud::Task).to receive(:new).once
-      args = ["agent", "-e", "", "web"]
-      args = subject.run(args)
-      expect(args).to eq(nil)
 
       expect(LogStash::Kibana::Runner).to_not receive(:new)
     end
