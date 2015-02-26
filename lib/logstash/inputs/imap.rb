75b90e434d0d3d5cf219fe8e41d05f247213fad8
Zeger Knops <z.knops@wogertrading.com>
2015-02-26 19:11:16 +0100

    Fixed Error: undefined method `encode' for nil:NilClass"

--- lib/logstash/inputs/imap.rb
+++ lib/logstash/inputs/imap.rb
@@ -121,7 +121,11 @@ class LogStash::Inputs::IMAP < LogStash::Inputs::Base
         # Details at:
         #   https://github.com/mikel/mail/blob/master/README.md#encodings
         #   http://tools.ietf.org/html/rfc2047#section-2
-        value = transcode_to_utf8(header.decoded)
+        if header.decoded != nil
+          value = transcode_to_utf8(header.decoded)
+        else
+          value = false
+        end	
 
         # Assume we already processed the 'date' above.
         next if name == "Date"
