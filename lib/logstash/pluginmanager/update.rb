a473fd6a6fa7278642b28cc653495dd04552d35a
Joao Duarte <jsvduarte@gmail.com>
2015-03-02 23:16:45 +0000

    handle LogStash::Gemfile file handle closing
    Fixes #2740

--- lib/logstash/pluginmanager/update.rb
+++ lib/logstash/pluginmanager/update.rb
@@ -12,7 +12,7 @@ class LogStash::PluginManager::Update < Clamp::Command
   parameter "[PLUGIN] ...", "Plugin name(s) to upgrade to latest version"
 
   def execute
-    gemfile = LogStash::Gemfile.new(File.new(LogStash::Environment::GEMFILE_PATH, "r+")).load
+    gemfile = LogStash::Gemfile.open(LogStash::Environment::GEMFILE_PATH)
     # keep a copy of the gemset to revert on error
     original_gemset = gemfile.gemset.copy
 
@@ -60,6 +60,8 @@ class LogStash::PluginManager::Update < Clamp::Command
       end
     end
     puts("No plugin updated") if update_count.zero?
+  ensure
+    gemfile.close
   end
 
   private
