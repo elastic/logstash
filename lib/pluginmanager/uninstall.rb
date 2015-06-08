0d82ae87c17a46c3e778a046f4fd7acc3cb39a13
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-06-08 17:50:40 +0200

    fix the issue caused by jruby-kafka that prevent to run the packaging
    Fixes #3393

--- lib/pluginmanager/uninstall.rb
+++ lib/pluginmanager/uninstall.rb
@@ -4,6 +4,13 @@ class LogStash::PluginManager::Uninstall < LogStash::PluginManager::Command
   def execute
     signal_error("File #{LogStash::Environment::GEMFILE_PATH} does not exist or is not writable, aborting") unless File.writable?(LogStash::Environment::GEMFILE_PATH)
 
+    ##
+    # Need to setup the bundler status to enable uninstall of plugins
+    # installed as local_gems, otherwise gem:specification is not
+    # finding the plugins
+    ##
+    LogStash::Bundler.setup!({:without => [:build, :development]})
+
     # make sure this is an installed plugin and present in Gemfile.
     # it is not possible to uninstall a dependency not listed in the Gemfile, for example a dependent codec
     signal_error("This plugin has not been previously installed, aborting") unless LogStash::PluginManager.installed_plugin?(plugin, gemfile)
