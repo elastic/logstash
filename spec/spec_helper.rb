eb64b35d390346ebdb0b0f07bd4248466010d017
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-06-11 17:16:54 +0200

    add a runtime license test
    Fixes #3426

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,5 @@
 require "logstash/devutils/rspec/spec_helper"
+
+def installed_plugins
+  Gem::Specification.find_all.select { |spec| spec.metadata["logstash_plugin"] }.map { |plugin| plugin.name }
+end
