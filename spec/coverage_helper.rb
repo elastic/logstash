40c57454a2a307a46235b1a0f6a6ec7c0b54cf43
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-06-29 11:05:45 +0200

    introduce direct loading for modules that need to be loaded in a given order becuase of missing dependencies, this fix the issue we had with early released of JDK7 and linux systems
    Fixes #3509

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -2,12 +2,32 @@
 # running coverage analysis
 module CoverageHelper
 
-  SKIP_LIST = ["lib/bootstrap/rspec.rb", "lib/logstash/util/prctl.rb"]
+  ##
+  # Skip list used to avoid loading certain patterns within
+  # the logstash directories, this patterns are excluded becuause
+  # of potential problems or because they are going to be loaded
+  # in another way.
+  ##
+  SKIP_LIST = Regexp.union([
+    /^lib\/bootstrap\/rspec.rb$/,
+    /^lib\/logstash\/util\/prctl.rb$/,
+    /^lib\/pluginmanager/
+  ])
+
+  ##
+  # List of files going to be loaded directly, this files
+  # are already loading their dependencies in the necessary
+  # order so everything is setup properly.
+  ##
+  DIRECT_LOADING_MODULES = [ "lib/pluginmanager/main.rb" ]
 
   def self.eager_load
     Dir.glob("lib/**/*.rb") do |file|
-      next if SKIP_LIST.include?(file)
+      next if file =~ SKIP_LIST
       require file
+    end
+    DIRECT_LOADING_MODULES.each do |_module|
+      require _module
     end
   end
 
