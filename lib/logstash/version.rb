3ccf84bf915f61f792029d0ccd9bb33840492082
Suyog Rao <suyog.rao@gmail.com>
2015-06-30 08:18:22 -0700

    New snapshot version for 1.5.2

--- lib/logstash/version.rb
+++ lib/logstash/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 # The version of logstash.
-LOGSTASH_VERSION = "1.5.2.snapshot1"
+LOGSTASH_VERSION = "1.5.2.snapshot2"
 
 # Note to authors: this should not include dashes because 'gem' barfs if
 # you include a dash in the version string.
