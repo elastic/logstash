3314d41d50e5d717ee58acf5de9c91770bc50d44
Jordan Sissel <jls@semicomplete.com>
2015-07-02 09:46:25 -0700

    Improve default security for SSLContext with a monkeypatch.
    New defaults:
    * Cipher suite based on Mozilla's Intermediate set from
      https://wiki.mozilla.org/Security/Server_Side_TLS (at time of writing)
    * Disable SSLv2 explicitly
    * Disable SSLv3 explicitly
    * Disable compression if possible
    
    The SSL option setting came from the ruby-ftw library's FTW::Connection
    (apache 2 licensed, I am author), and transitively through work
    published by jmhodges to improve Ruby's SSL strength.
    
    I include specs to ensure we never include export or weak ciphers by
    default.
    
    Using this patch to test the security improvements according to
    `www.howsmyssl.com` shows much improved results:
    
    ---
    
    Testing this:
    
    ```
    ruby -r ./lib/logstash/patches/stronger_openssl_defaults.rb  -ropenssl -rsocket -rjson -rawesome_print -e 'c = OpenSSL::SSL::SSLContext.new; t = TCPSocket.new("www.howsmyssl.com", 443); o = OpenSSL::SSL::SSLSocket.new(t, c); o.connect; o.puts "GET /a/check HTTP/1.1\r\nHost: www.howsmyssl.com\r\n\r\n"; headers,body = o.read.split("\r\n\r\n", 2); puts body'
    ```
    
    (I processed the JSON output w/ jq for easier reading)
    
    The purpose of the above is to test the default behavior of SSLContext.
    
    * JRuby 1.7.19 w/ this patch reports no cipher problems.
    * JRuby 1.7.19 without this patch has several weak ciphers used:
    
    ```
      "TLS_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA: [\"uses keys smaller than 128 bits in its encryption\"]",
      "TLS_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA: [\"uses keys smaller than 128 bits in its encryption\"]",
      "TLS_DHE_RSA_WITH_DES_CBC_SHA: [\"uses keys smaller than 128 bits in its encryption\"]",
      "TLS_RSA_EXPORT_WITH_DES40_CBC_SHA: [\"uses keys smaller than 128 bits in its encryption\"]",
      "TLS_RSA_EXPORT_WITH_RC4_40_MD5: [\"uses keys smaller than 128 bits in its encryption\",\"use RC4 which has insecure biases in its output\"]",
      "TLS_RSA_WITH_DES_CBC_SHA: [\"uses keys smaller than 128 bits in its encryption\"]",
      "TLS_RSA_WITH_RC4_128_MD5: [\"use RC4 which has insecure biases in its output\"]",
      "TLS_RSA_WITH_RC4_128_SHA: [\"use RC4 which has insecure biases in its output\"]"
    ```
    
    Under MRI, similar cipher selection problems are observed without this patch (weak export
    ciphers, other weak small-key ciphers, RC4 complaints). With this patch, no cipher complaints
    are reported by www.howsmyssl.com
    
    One other note: Because JRuby defaults to TLS 1.0 and only makes CBC ciphers
    available under the Mozilla Intermediate cipher set, I believe (and
    howsmyssl.com agrees) that these defaults still make the BEAST exploit a
    problem. Switching to TLS 1.1 should fix this, but we need to do more research
    to determine the what, if any, impact it will have if we force TLS 1.1 to be
    the default..
    
    Fixes #3579

--- lib/logstash/patches.rb
+++ lib/logstash/patches.rb
@@ -1,3 +1,4 @@
 require "logstash/patches/bugfix_jruby_2558"
 require "logstash/patches/cabin"
 require "logstash/patches/profile_require_calls"
+require "logstash/patches/stronger_openssl_defaults"
