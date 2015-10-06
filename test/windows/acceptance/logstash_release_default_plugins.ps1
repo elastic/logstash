7735de00944385a3714f8835ed2614a5ba161a64
Gabriel Moskovicz <gmoskovicz@gmail.com>
2015-10-06 12:33:39 -0200

    Add powershell script for windows testing
    Added three different tests:
    
    - Release acceptance test, which tests ports and run Logstash.
    - Default plugins, which runs the same plugin installation and
    environment for windows
    - Simple integration, which integrates Logstash with Elasticsearch and a
    simple configuration

---
+++ test/windows/acceptance/logstash_release_default_plugins.ps1
@@ -0,0 +1,30 @@
+# Created By: Gabriel Moskovicz
+#
+# To be run on Jenkins
+#
+# Requirements to run the test:
+# 
+# - Powershell 4
+# - Windows 7 or newer
+# - Java 7 or newer
+# - Ruby 7 or newer
+
+$ruby = $env:RUBY_HOME  + "\jruby.exe"
+
+$env:SPEC_OPTS="--format documentation"
+
+sleep 30
+
+cd rakelib
+
+$install_default = start $ruby -ArgumentList "-S rake test:install-default" -Passthru -NoNewWindow -Wait
+
+If ($install_default.exitCode -gt 0){
+     exit 1
+}
+
+$plugins = start $ruby -ArgumentList "-S rake test:plugins" -Passthru -NoNewWindow -Wait
+
+If ($plugins.exitCode -gt 0){
+     exit 1
+}
