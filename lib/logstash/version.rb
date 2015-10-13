b3571a4f40717bc5276835602b5ab09ba3cfcb6a
Suyog Rao <suyog.rao@gmail.com>
2015-10-13 16:59:56 -0700

    Bump version to 2.0.0.snapshot4 to use JrJackson changes
    Fixes #4026

--- lib/logstash/version.rb
+++ lib/logstash/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 # The version of logstash.
-LOGSTASH_VERSION = "2.0.0-snapshot3"
+LOGSTASH_VERSION = "2.0.0-snapshot4"
 
 # Note to authors: this should not include dashes because 'gem' barfs if
 # you include a dash in the version string.
