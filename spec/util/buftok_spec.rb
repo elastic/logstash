b445fc1f24d458fd011fd8ce2f2f8e6b9825070f
Colin Surprenant <colin.surprenant@gmail.com>
2015-10-14 14:51:20 -0400

    cleanups
    Fixes #4037

--- spec/util/buftok_spec.rb
+++ spec/util/buftok_spec.rb
@@ -4,35 +4,28 @@ require "logstash/util/buftok"
 
 describe  FileWatch::BufferedTokenizer  do
 
-  context "test" do
+  subject { FileWatch::BufferedTokenizer.new }
 
-    it "should tokenize a single token" do
-      t = FileWatch::BufferedTokenizer.new
-      expect(t.extract("foo\n")).to eq(["foo"])
-    end
+  it "should tokenize a single token" do
+    expect(subject.extract("foo\n")).to eq(["foo"])
+  end
 
-    it "should merge multiple token" do
-      t = FileWatch::BufferedTokenizer.new
-      expect(t.extract("foo")).to eq([])
-      expect(t.extract("bar\n")).to eq(["foobar"])
-    end
+  it "should merge multiple token" do
+    expect(subject.extract("foo")).to eq([])
+    expect(subject.extract("bar\n")).to eq(["foobar"])
+  end
 
-    it "should tokenize multiple token" do
-      t = FileWatch::BufferedTokenizer.new
-      expect(t.extract("foo\nbar\n")).to eq(["foo", "bar"])
-    end
+  it "should tokenize multiple token" do
+    expect(subject.extract("foo\nbar\n")).to eq(["foo", "bar"])
+  end
 
-    it "should ignore empty payload" do
-      t = FileWatch::BufferedTokenizer.new
-      expect(t.extract("")).to eq([])
-      expect(t.extract("foo\nbar")).to eq(["foo"])
-    end
+  it "should ignore empty payload" do
+    expect(subject.extract("")).to eq([])
+    expect(subject.extract("foo\nbar")).to eq(["foo"])
+  end
 
-    it "should tokenize empty payload with newline" do
-      t = FileWatch::BufferedTokenizer.new
-      expect(t.extract("\n")).to eq([""])
-      expect(t.extract("\n\n\n")).to eq(["", "", ""])
-    end
-
+  it "should tokenize empty payload with newline" do
+    expect(subject.extract("\n")).to eq([""])
+    expect(subject.extract("\n\n\n")).to eq(["", "", ""])
   end
 end
