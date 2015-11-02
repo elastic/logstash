ee4f87b35e3cd76047547853d698727a44302289
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-11-02 14:46:56 -0500

    Extract metric helper into `spec/support/matchers`

--- spec/logstash/instrument/metric_spec.rb
+++ spec/logstash/instrument/metric_spec.rb
@@ -2,15 +2,6 @@
 require "logstash/instrument/metric"
 require "logstash/instrument/collector"
 
-RSpec::Matchers.define :be_a_metric_event do |type, key, value|
-  match do |actual|
-    actual.first == type &&
-      actual[1].kind_of?(Time) &&
-      actual[2] == key &&
-      actual.last == value
-  end
-end
-
 describe LogStash::Instrument::Metric do
   let(:collector) { [] }
 
