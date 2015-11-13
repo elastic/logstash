25b8d151177c0104ce6d229403458689b841482b
Andrew Cholakian <andrew@andrewvc.com>
2015-11-13 17:02:09 -0600

    Pluggable agents

--- logstash-core/spec/logstash/agent_spec.rb
+++ logstash-core/spec/logstash/agent_spec.rb
@@ -3,6 +3,10 @@ require "logstash/agent"
 require "spec_helper"
 
 describe LogStash::Agent do
+  def make_agent(options={})
+    LogStash::Agent.new(Cabin::Channel.get(), options)
+  end
+
   context "#node_name" do
     let(:hostname) { "the-logstash" }
 
@@ -11,17 +15,17 @@ describe LogStash::Agent do
     end
 
     it "fallback to hostname when no name is provided" do
-      expect(LogStash::Agent.new.node_name).to be(hostname)
+      expect(make_agent.node_name).to be(hostname)
     end
 
     it "uses the user provided name" do
-      expect(LogStash::Agent.new({ :node_name => "a-name" }).node_name).to eq("a-name")
+      expect(make_agent({ :node_name => "a-name" }).node_name).to eq("a-name")
     end
   end
 
   context "#node_uuid" do
     it "create a unique uuid between agent instances" do
-      expect(LogStash::Agent.new.node_uuid).not_to be(LogStash::Agent.new.node_uuid)
+      expect(make_agent.node_uuid).not_to be(make_agent.node_uuid)
     end
   end
 end
