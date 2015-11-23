e014d78e9420eea1cc83a4b0deb062cbae6dfc81
Colin Surprenant <colin.surprenant@gmail.com>
2015-11-23 15:45:31 -0500

    use rspec own SPEC_OPTS

--- ci/ci_test.sh
+++ ci/ci_test.sh
@@ -7,7 +7,7 @@
 
 SELECTED_TEST_SUITE=$1
 
-export RSPEC_OPTIONS="--order rand --format documentation --format CI::Reporter::RSpecFormatter"
+export SPEC_OPTS="--order rand --format documentation --format CI::Reporter::RSpecFormatter"
 
 if [[ $SELECTED_TEST_SUITE == $"core-fail-fast" ]]; then
   echo "Running core-fail-fast tests"
