3fe0d91daac009e16e8d092a7e371383e4a4f74a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-11-30 15:09:13 +0100

    correct require path broken in integration specs

--- integration/plugin_manager/logstash_spec.rb
+++ integration/plugin_manager/logstash_spec.rb
@@ -1,10 +1,10 @@
 # Encoding: utf-8
 require_relative "../spec_helper"
-require_relative "../../lib/logstash/version"
+require_relative "../../logstash-core/lib/logstash/version"
 
 describe "bin/logstash" do
   it "returns the logstash version" do
-    result = command("bin/logstash version")
+    result = command("bin/logstash --version")
     expect(result.exit_status).to eq(0)
     expect(result.stdout).to match(/^logstash\s#{LOGSTASH_VERSION}/)
   end
