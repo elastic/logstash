0fbbe5562782fd6dfe390dc025fa0fcb7305eab4
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-12-08 15:28:56 -0500

    Change the API interface for the periodic poller to reduce the clutter and remove the external observer class

--- logstash-core/lib/logstash/instrument/periodic_poller/jvm.rb
+++ logstash-core/lib/logstash/instrument/periodic_poller/jvm.rb
@@ -3,8 +3,8 @@ require "logstash/instrument/periodic_poller/abstract_poller"
 
 module LogStash module Instrument module PeriodicPoller
   class JVM < AbstractPoller
-    def initialize(interval = AbstractPoller::DEFAULT_POLLING_INTERVAL)
-      super(interval)
+    def initialize(metric, options = {})
+      super(metric, options)
     end
 
     def collect
--- logstash-core/lib/logstash/instrument/periodic_poller/os.rb
+++ logstash-core/lib/logstash/instrument/periodic_poller/os.rb
@@ -3,7 +3,7 @@ require "logstash/instrument/periodic_poller/abstract_poller"
 
 module LogStash module Instrument module PeriodicPoller
   class Os < AbstractPoller
-    def initialize(interval = AbstractPoller::DEFAULT_POLLING_INTERVAL)
+    def initialize(metric, options = {})
       super(interval)
     end
 
