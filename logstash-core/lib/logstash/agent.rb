47c5ce98fd17d40aa9313b9b280524d614e166a9
Joao Duarte <jsvduarte@gmail.com>
2015-12-14 15:03:08 +0000

    add reload config flag and allow reload through sighup
    * move signal handling to runner
    * add "-r" flag to detect changes in config and reload
    * make SIGHUP reload config file
    * make agent execute either loop to detect changes or sleep forever
    * sigint/sigterm shut down the agent.execute task

--- logstash-core/lib/logstash/agent.rb
+++ logstash-core/lib/logstash/agent.rb
@@ -12,45 +12,51 @@ LogStash::Environment.load_locale!
 class LogStash::Agent
   attr_reader :logger
 
-  def initialize(logger, config_string, config_path)
-    @logger = logger
+  def initialize(params)
+    @logger = params[:logger]
     @pipelines = {}
     @pipeline_threads = {}
-    @state = ""
+    @state = clean_state
     @config_loader = LogStash::Config::Loader.new(@logger, false)
-    @config_str = config_string
-    @config_path = config_path
+    @config_str = params[:config_string]
+    @config_path = params[:config_path]
+    @auto_reload = params[:auto_reload]
   end
 
   def execute
-    sigint_id = trap_sigint()
-    sigterm_id = trap_sigterm()
-
-    @logger.info "starting agent"
-    loop do
-      new_state = fetch_state
-      if valid_state?(new_state)
-        if new_state?(@state, new_state)
-          @logger.info "new state: #{new_state}"
-          upgrade_state(new_state)
-        else
-          @logger.debug("same state, ignoring..")
-        end
-      else
-        @logger.error("invalid state #{new_state}")
-      end
-      sleep 10
+    @thread = Thread.current
+    @logger.info("starting agent", :state => @state)
+    reload_state
+    if @auto_reload
+      Stud.interval(5) { reload_state }
+    else
+      sleep 0.5 until Stud.stop?
     end
-    return 0
+  end
+
+  def shutdown
+    shutdown_pipelines
+  end
+
+  def reload_state
+    new_state = fetch_state
+    if valid_state?(new_state)
+      if new_state?(@state, new_state)
+        @logger.warn("fetched new state. upgrading..", :state => new_state)
+        upgrade_state(new_state)
+      else
+        @logger.debug("same state, ignoring..")
+      end
+    else
+      @logger.error("invalid state", :state => new_state)
+    end
   rescue => e
     @logger.fatal I18n.t("oops", :error => e)
     @logger.fatal e.backtrace if @logger.debug? || $DEBUGLIST.include?("stacktrace")
     return 1
-  ensure
-    Stud::untrap("INT", sigint_id) unless sigint_id.nil?
-    Stud::untrap("TERM", sigterm_id) unless sigterm_id.nil?
   end
 
+  private
   def add_pipeline(pipeline_id, config_str, settings = {})
     @pipelines[pipeline_id] = LogStash::Pipeline.new(config_str, settings)
   end
@@ -66,7 +72,7 @@ class LogStash::Agent
 
   def stop_pipeline(id)
     return unless @pipelines[id]
-    @logger.info("stopping pipeline", :id => id)
+    @logger.warn("stopping pipeline", :id => id)
     @pipelines[id].shutdown do
       # TODO uncomment once shutdown controller can be stopped
       #LogStash::ShutdownController.start(@pipelines[id])
@@ -74,32 +80,29 @@ class LogStash::Agent
     @pipeline_threads[id].join
   end
 
-  def shutdown_pipeline(id)
-    return unless @pipelines[id]
-    @pipelines[id].shutdown do
-      ::LogStash::ShutdownController.start(@pipelines[id])
-    end
-  end
-
   def shutdown_pipelines
-    @pipelines.each do |_, pipeline|
-      pipeline.shutdown do
-        ::LogStash::ShutdownController.start(pipeline)
-      end
+    @pipelines.each do |id, pipeline|
+      stop_pipeline(id)
+      #pipeline.shutdown do
+      #  ::LogStash::ShutdownController.start(pipeline)
+      #end
     end
   end
 
-  private
-  # Emit a warning message.
-  def warn(message)
-    # For now, all warnings are fatal.
-    raise LogStash::ConfigurationError, message
-  end # def warn
-
-  # Emit a failure message and abort.
-  def fail(message)
-    raise LogStash::ConfigurationError, message
-  end # def fail
+  # Override the methods below if you're implementing your own agent
+  def upgrade_state(new_state)
+    stop_pipeline("base")
+    add_pipeline("base", new_state)
+  rescue Exception => e
+    @logger.error("failed to update state", :new_state => new_state, :message => e.message, :backtrace => e.backtrace)
+    @logger.warn("reverting to previous state", :state => @state)
+    add_pipeline("base", @state) unless clean_state?
+    @state
+  else
+    @state = new_state
+  ensure
+    start_pipeline("base") unless clean_state?
+  end
 
   def fetch_state
     @config_loader.format_config(@config_path, @config_string)
@@ -113,38 +116,12 @@ class LogStash::Agent
     old_state != new_state
   end
 
-  def upgrade_state(new_state)
-    stop_pipeline("base")
-    add_pipeline("base", new_state)
-  rescue Exception => e
-    @logger.error("failed to update state", :new_state => new_state, :message => e.message, :backtrace => e.backtrace)
-    @logger.warn("reverting to previous state", :state => @state)
-    add_pipeline("base", @state) unless @state.empty?
-    @state
-  else
-    @state = new_state
-  ensure
-    start_pipeline("base")
+  def clean_state
+    ""
   end
 
-  def trap_sigterm
-    Stud::trap("TERM") do
-      @logger.warn(I18n.t("logstash.agent.sigterm"))
-      shutdown_pipelines
-    end
+  def clean_state?
+    @state == clean_state
   end
 
-  def trap_sigint
-    Stud::trap("INT") do
-      if @interrupted_once
-        @logger.fatal(I18n.t("logstash.agent.forced_sigint"))
-        exit
-      else
-        @logger.warn(I18n.t("logstash.agent.sigint"))
-        Thread.new(@logger) {|logger| sleep 5; logger.warn(I18n.t("logstash.agent.slow_shutdown")) }
-        @interrupted_once = true
-        shutdown_pipelines
-      end
-    end
-  end
 end # class LogStash::Agent
