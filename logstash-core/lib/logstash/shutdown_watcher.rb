feb038275e881f16c7044365df794ff7b63cf2a5
Andrew Cholakian <andrew@andrewvc.com>
2015-12-14 14:57:07 -0600

    Better shutdown logic for controller and pipeline

--- logstash-core/lib/logstash/shutdown_watcher.rb
+++ logstash-core/lib/logstash/shutdown_watcher.rb
@@ -9,8 +9,9 @@ module LogStash
 
     attr_reader :cycle_period, :report_every, :abort_threshold
 
-    def initialize(pipeline, cycle_period=CHECK_EVERY, report_every=REPORT_EVERY, abort_threshold=ABORT_AFTER)
+    def initialize(pipeline, pipeline_thread, cycle_period=CHECK_EVERY, report_every=REPORT_EVERY, abort_threshold=ABORT_AFTER)
       @pipeline = pipeline
+      @pipeline_thread = pipeline_thread
       @cycle_period = cycle_period
       @report_every = report_every
       @abort_threshold = abort_threshold
@@ -33,9 +34,9 @@ module LogStash
       @logger ||= Cabin::Channel.get(LogStash)
     end
 
-    def self.start(pipeline, cycle_period=CHECK_EVERY, report_every=REPORT_EVERY, abort_threshold=ABORT_AFTER)
-      watcher = self.new(pipeline, cycle_period, report_every, abort_threshold)
-      Thread.new(watcher) { |watcher| watcher.start }
+    def self.start(pipeline, pipeline_thread, cycle_period=CHECK_EVERY, report_every=REPORT_EVERY, abort_threshold=ABORT_AFTER)
+      controller = self.new(pipeline, pipeline_thread, cycle_period, report_every, abort_threshold)
+      Thread.new(controller) { |controller| controller.start }
     end
 
     def logger
@@ -43,11 +44,14 @@ module LogStash
     end
 
     def start
+      sleep 0.1 until @pipeline.ready?
+
       sleep(@cycle_period)
       cycle_number = 0
       stalled_count = 0
       Stud.interval(@cycle_period) do
-        @reports << pipeline_report_snapshot
+        break unless @pipeline_thread.alive?
+        @reports << Report.from_pipeline(@pipeline)
         @reports.delete_at(0) if @reports.size > @report_every # expire old report
         if cycle_number == (@report_every - 1) # it's report time!
           logger.warn(@reports.last)
