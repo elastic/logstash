7ee5038ccf8f93b54f96d12d6247a79e9405a14d
Andrew Cholakian <andrew@andrewvc.com>
2015-12-14 14:57:07 -0600

    Better shutdown logic for controller and pipeline
    Fixes #4520

--- logstash-core/lib/logstash/pipeline.rb
+++ logstash-core/lib/logstash/pipeline.rb
@@ -88,6 +88,10 @@ module LogStash; class Pipeline
     @ready.value
   end
 
+  def running?
+    @running.value
+  end
+
   def configure(setting, value)
     @settings[setting] = value
   end
@@ -127,7 +131,7 @@ module LogStash; class Pipeline
   end
 
   def run
-    LogStash::Util.set_thread_name("[#{pipeline_id}]-pipeline-manager")
+    @thread = Thread.current
     @logger.terminal(LogStash::Util::DefaultsPrinter.print(@settings))
 
     start_workers
@@ -369,15 +373,7 @@ module LogStash; class Pipeline
 
   # initiate the pipeline shutdown sequence
   # this method is intended to be called from outside the pipeline thread
-  # @param before_stop [Proc] code block called before performing stop operation on input plugins
   def shutdown(&before_stop)
-    # shutdown can only start once the pipeline has completed its startup.
-    # avoid potential race conditoon between the startup sequence and this
-    # shutdown method which can be called from another thread at any time
-    sleep(0.1) while !ready?
-
-    # TODO: should we also check against calling shutdown multiple times concurently?
-
     before_stop.call if block_given?
 
     @logger.info "Closing inputs"
