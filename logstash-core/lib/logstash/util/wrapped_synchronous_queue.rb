4597eac21d504925fd257c3536ce49626cb8df44
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-12-30 13:59:40 -0500

    Add a `WrappedSynchronousQueue#offer` method
    Expose the Java's `#offer` method of the `SynchronousQueue` class,
    This method allow the input developper to correctly apply back pressure
    to the network clients.
    
    If you use `#push` it will block forever until the pipeline free some
    space, this behavior is sufficient if the pipeline outputs are healthy.
    But if the output stale the backpressure will be applied up to the input
    (producers) when reading a file this scenario is fine since we will just
    stop reading the file until the thread unblock again.
    
    In the context of network clients, the story is a bit different the
    clients will timeout and try to reconnect to restransmit their payload
    creating multiple new connection thread block on the queue. In some case
    this will lead into a OOM issues.
    
    This PR is the first step to communicate that the queue is under
    pressure.
    
    Fixes #4408

--- logstash-core/lib/logstash/util/wrapped_synchronous_queue.rb
+++ logstash-core/lib/logstash/util/wrapped_synchronous_queue.rb
@@ -9,10 +9,24 @@ module LogStash; module Util
       @queue = java.util.concurrent.SynchronousQueue.new()
     end
 
+    # Push an object to the queue if the queue is full
+    # it will block until the object can be added to the queue.
+    #
+    # @param [Object] Object to add to the queue
     def push(obj)
       @queue.put(obj)
     end
     alias_method(:<<, :push)
+
+    # Offer an object to the queue, wait for the specified amout of time.
+    # If adding to the queue was successfull it wil return true, false otherwise.
+    #
+    # @param [Object] Object to add to the queue
+    # @param [Integer] Time in milliseconds to wait before giving up
+    # @return [Boolean] True if adding was successfull if not it return false
+    def offer(obj, timeout_ms)
+      @queue.offer(obj, timeout_ms, TimeUnit::MILLISECONDS)
+    end
 
     # Blocking
     def take
