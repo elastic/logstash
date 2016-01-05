90228385e1ad4291b1ffa159ad4a0ad0adf88792
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-01-05 16:21:20 -0500

    Remove sized_queue.rb
    Since Logstash 2.2, we now use Java SynchronousQueue instead of ruby
    sizequeue

--- logstash-core/lib/logstash/sized_queue.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-require "logstash/namespace"
-require "logstash/logging"
-
-require "thread" # for SizedQueue
-class LogStash::SizedQueue < SizedQueue
-  # TODO(sissel): Soon will implement push/pop stats, etc
-end
