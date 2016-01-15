f166ea20114217d209c396070a2ab5820e5ac079
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-01-15 16:45:40 +0100

    Add initial version of a hot threads report in plain text format
    add skipped threads for the hotthreads reporter

--- logstash-core/api/lib/app/stats/hotthreads_command.rb
+++ logstash-core/api/lib/app/stats/hotthreads_command.rb
@@ -1,17 +1,35 @@
 # encoding: utf-8
 require "app/command"
 require 'monitoring'
+require "socket"
 
 class LogStash::Api::HotThreadsCommand < LogStash::Api::Command
 
+  SKIPPED_THREADS = [ "Finalizer", "Reference Handler", "Signal Dispatcher" ]
+
   def run
-    generate
+    hash = JRMonitor.threads.generate
+    report = "::: {#{hostname}} <br/> Hot threads at #{Time.now}, busiestThreads=#{hash.count}:"
+    hash.each_pair do |thread_name, container|
+      next if SKIPPED_THREADS.include?(thread_name)
+      report << "<p> #{build_report(container)} </p>"
+    end
+    report
   end
 
   private
 
-  def generate
-    JRMonitor.threads.generate
+  def build_report(hash)
+    thread_name, thread_path = hash["thread.name"].split(": ")
+    report = <<-REPORT
+       0.1% (#{hash["cpu.time"]}micros out of 500ms) cpu usage by #{hash["thread.state"]} thread named '#{thread_name}'
+    REPORT
+    report << "<br/> #{thread_path}<br/>" if thread_path
+    report
+  end
+
+  def hostname
+    Socket.gethostname
   end
 
 end
--- logstash-core/api/lib/app_helpers.rb
+++ logstash-core/api/lib/app_helpers.rb
@@ -3,7 +3,11 @@ require "logstash/json"
 
 module LogStash::Api::AppHelpers
 
-  def respond_with(data)
-    LogStash::Json.dump(data)
+  def respond_with(data, as=:json)
+    if as == :json
+      LogStash::Json.dump(data)
+    else
+      data.to_s
+    end
   end
 end
