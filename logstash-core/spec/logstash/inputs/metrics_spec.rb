63f1031bd1ad5c7e251634ea2392f58e725af6d6
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-01-22 13:25:06 +0100

    add specs for the metrics input update call, so we've sure the update to the collector works as expected

--- logstash-core/spec/logstash/inputs/metrics_spec.rb
+++ logstash-core/spec/logstash/inputs/metrics_spec.rb
@@ -14,6 +14,23 @@ describe LogStash::Inputs::Metrics do
     end
   end
 
+  describe "#update" do
+
+    let(:namespaces)  { [:root, :base] }
+    let(:key)        { :foo }
+    let(:metric_store) { LogStash::Instrument::MetricStore.new }
+
+    it "should fill up the queue with received events" do
+      Thread.new { subject.run(queue) }
+      sleep(0.1)
+      subject.stop
+
+      metric_store.fetch_or_store(namespaces, key, LogStash::Instrument::MetricType::Counter.new(namespaces, key))
+      subject.update(LogStash::Instrument::Snapshot.new(metric_store))
+      expect(queue.count).to eq(1)
+    end
+  end
+
   describe "#stop" do
     it "should remove itself from the the collector observer" do
       expect(LogStash::Instrument::Collector.instance).to receive(:delete_observer).with(subject)
