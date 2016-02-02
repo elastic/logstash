acd9b682bfa8c5c72fdbf889e8a64b99fd3c0734
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-02-02 14:23:26 +0100

    relocate logstash-core/api/ into logstash-core/lib/logstash/api/
    Fixes #4614

--- logstash-core/spec/api/spec_helper.rb
+++ logstash-core/spec/api/spec_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
-ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "lib", "logstash", "api"))
 $LOAD_PATH.unshift File.join(ROOT, 'lib')
-Dir.glob('lib/**').each{ |d| $LOAD_PATH.unshift(File.join(ROOT, d)) }
+Dir.glob(File.join(ROOT, "lib" "**")).each{ |d| $LOAD_PATH.unshift(d) }
 
 require "logstash/devutils/rspec/spec_helper"
 
@@ -10,6 +10,8 @@ require 'rspec'
 require "json"
 
 ENV['RACK_ENV'] = 'test'
+
+Rack::Builder.parse_file(File.join(ROOT, 'init.ru'))
 
 def read_fixture(name)
   path = File.join(File.dirname(__FILE__), "fixtures", name)
