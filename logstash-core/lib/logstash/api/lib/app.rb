99d098ce38d18fcd4c4c16ba2884da7584ae9219
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-02-02 17:27:58 +0100

    introduced basic error logging
    Fixes #4635

--- logstash-core/lib/logstash/api/lib/app.rb
+++ logstash-core/lib/logstash/api/lib/app.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 require "cabin"
 require "logstash/json"
-require "app_helpers"
+require "helpers/app_helpers"
 require "app/service"
 require "app/command_factory"
+require "logstash/util/loggable"
 
 module LogStash::Api
   class BaseApp < ::Sinatra::Application
@@ -11,14 +12,21 @@ module LogStash::Api
     attr_reader :factory
 
     if settings.environment != :production
+      set :raise_errors, true
       set :show_exceptions, :after_handler
     end
+
+    include LogStash::Util::Loggable
 
     helpers AppHelpers
 
     def initialize(app=nil)
       super(app)
       @factory = CommandFactory.new(settings.service)
+    end
+
+    error do
+      logger.error(env['sinatra.error'].message, :url => request.url, :ip => request.ip, :params => request.params)
     end
 
   end
