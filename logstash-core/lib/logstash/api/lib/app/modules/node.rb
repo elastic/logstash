91385e4cf3a3ef49e31ce1013eb948e9a7ebf260
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-02-08 18:10:58 +0100

    remove thread and stacktraces count lines to have by default 10 and 10 and not being a param2

--- logstash-core/lib/logstash/api/lib/app/modules/node.rb
+++ logstash-core/lib/logstash/api/lib/app/modules/node.rb
@@ -8,16 +8,12 @@ module LogStash::Api
 
     # return hot threads information
     get "/hot_threads" do
-      top_threads_count = params["threads"] || 3
       ignore_idle_threads = params["ignore_idle_threads"] || true
 
       options = {
-        :threads => top_threads_count.to_i,
         :ignore_idle_threads => as_boolean(ignore_idle_threads),
         :human => params.has_key?("human")
       }
-      options[:stacktrace_size] = params['stacktraces'].to_i if params['stacktraces']
-
       command = factory.build(:hot_threads_command)
       type    = options[:human] ? :string : :json
       respond_with(command.run(options), type)
