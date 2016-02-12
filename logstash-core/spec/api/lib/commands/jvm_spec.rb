82dde31afee55c7462c71d19656bbb890ebf0f82
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-02-12 13:06:14 -0500

    Fix an issue with the expectation on the agent#uptime
    Fixes #4687
    
    Fixes #4653

--- logstash-core/spec/api/lib/commands/jvm_spec.rb
+++ logstash-core/spec/api/lib/commands/jvm_spec.rb
@@ -12,6 +12,7 @@ describe "JVM stats" do
     before(:each) do
       allow(agent).to receive(:node_name).and_return("foo")
       expect_any_instance_of(LogStash::Api::Service).to receive(:agent).and_return(agent)
+      allow(subject).to receive(:uptime).and_return(10)
     end
 
     context "#schema" do
