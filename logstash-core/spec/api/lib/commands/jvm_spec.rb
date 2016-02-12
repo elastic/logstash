48c32afbe6d5a46ac98804e587307328ed5844e8
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-02-12 15:39:43 +0100

    cleanup non used expectation inside the jvm specs
    Fixes #4683

--- logstash-core/spec/api/lib/commands/jvm_spec.rb
+++ logstash-core/spec/api/lib/commands/jvm_spec.rb
@@ -12,7 +12,6 @@ describe "JVM stats" do
     before(:each) do
       allow(agent).to receive(:node_name).and_return("foo")
       expect_any_instance_of(LogStash::Api::Service).to receive(:agent).and_return(agent)
-      expect(subject).to receive(:uptime).and_return(10).at_least(:once)
     end
 
     context "#schema" do
