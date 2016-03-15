af4121865093755df2d189688ada2fea5f92494e
Jordan Sissel <jls@semicomplete.com>
2016-03-15 11:36:44 -0700

    Allow Logstash to write its logs in JSON format
    This is made available by a new `--log-in-json` flag. Default is false.
    When false, the old behavior [1] is used. When true, JSON logs are
    emitted.
    
    [1] The old behavior is realy two things. First, using Object#inspect to
    serialize. Second, to color the output if the IO is a tty.
    
    Fixes #1569
    
    Fixes #4820

---
+++ logstash-core/lib/logstash/logging/json.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require "logstash/namespace"
+require "logstash/logging"
+require "logstash/json"
+
+module LogStash; class Logging; class JSON
+  def initialize(io)
+    raise ArgumentError, "Expected IO, got #{io.class.name}" unless io.is_a?(IO)
+
+    @io = io
+    @lock = Mutex.new
+  end
+
+  def <<(obj)
+    serialized = LogStash::Json.dump(obj)
+    @lock.synchronize do
+      @io.puts(serialized)
+      @io.flush
+    end
+  end
+end; end; end
