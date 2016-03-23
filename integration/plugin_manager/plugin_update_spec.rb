4b07413cd96e4cd82bbf476113375b30189cb3cb
Suyog Rao <suyog.rao@gmail.com>
2016-03-23 09:09:40 -0700

    Changes after review from @purbon
    Fixes #4874

--- integration/plugin_manager/plugin_update_spec.rb
+++ integration/plugin_manager/plugin_update_spec.rb
@@ -6,13 +6,13 @@ describe "update" do
   let(:previous_version) { "2.0.1" }
 
   before do
-    command("bin/plugin install --version #{previous_version} #{plugin_name}")
-    cmd = command("bin/plugin list --verbose #{plugin_name}")
+    command("bin/logstash-plugin install --version #{previous_version} #{plugin_name}")
+    cmd = command("bin/logstash-plugin list --verbose #{plugin_name}")
     expect(cmd.stdout).to match(/#{plugin_name} \(#{previous_version}\)/)
   end
 
   context "update a specific plugin" do
-    subject { command("bin/plugin update #{plugin_name}") }
+    subject { command("bin/logstash-plugin update #{plugin_name}") }
 
     it "has executed succesfully" do
       expect(subject.exit_status).to eq(0)
@@ -21,11 +21,11 @@ describe "update" do
   end
 
   context "update all the plugins" do
-    subject { command("bin/plugin update") }
+    subject { command("bin/logstash-plugin update") }
 
     it "has executed succesfully" do
       expect(subject.exit_status).to eq(0)
-      cmd = command("bin/plugin list --verbose #{plugin_name}").stdout
+      cmd = command("bin/logstash-plugin list --verbose #{plugin_name}").stdout
       expect(cmd).to match(/logstash-input-stdin \(#{LogStashTestHelpers.latest_version(plugin_name)}\)/)
     end
   end
