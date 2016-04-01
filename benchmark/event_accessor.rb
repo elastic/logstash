b5952c1d68a864bc30643c9b36c48f1dbc9cb783
Guy Boertje <guy@elastic.co>
2016-04-01 10:21:05 +0100

    update benchmark Deep fetch      6.015M (± 4.4%) i/s -     60.086M

--- benchmark/event_accessor.rb
+++ benchmark/event_accessor.rb
@@ -4,10 +4,12 @@ require "logstash/event"
 
 options = { :time => 10, :warmup => 10 }
 puts "Same Event instance"
+
+event = LogStash::Event.new("foo" => {"bar" => {"foobar" => "morebar"} })
+STDERR.puts " ----------> event[\"[foo][bar][foobar]\"] = #{event["[foo][bar][foobar]"]}"
+
 Benchmark.ips do |x|
   x.config(options)
-  event = LogStash::Event.new("foo" => "bar",
-                    "foobar" => "morebar")
 
-  x.report("Deep fetch") { event.sprintf("/first/%{foo}/%{foobar}/%{+YYY-mm-dd}") }
+  x.report("Deep fetch") { event["[foo][bar][foobar]"] }
 end
