136e140e47ac82f2742a21feb198f501ff720626
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-04-08 15:14:18 +0200

    add a method to list all defined methods in the API

--- logstash-core/lib/logstash/api/lib/app.rb
+++ logstash-core/lib/logstash/api/lib/app.rb
@@ -25,6 +25,19 @@ module LogStash::Api
       @factory = CommandFactory.new(settings.service)
     end
 
+    get "/_directory" do
+      data = {}
+      ROUTES.each_pair do |namespace, _routes|
+        routes = {}
+        _routes.each_pair do |verb, methods|
+          next if "HEAD" == verb
+          routes[verb] = methods.map { |m| "http://localhost:9600#{namespace}/#{m[0].source.gsub("\\A/","").gsub("\\z", "")}" }
+        end
+        data[namespace] = routes
+      end
+      respond_with data
+    end
+
     not_found do
       status 404
       as   = params.has_key?("human") ? :string : :json
