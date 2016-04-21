f4a3bc82d4b566589e2a203d9a99ae51dc371680
Colin Surprenant <colin.surprenant@gmail.com>
2016-04-21 13:56:50 -0400

    update jackson dependencies to 2.7.3

--- logstash-core-event-java/lib/logstash-core-event-java_jars.rb
+++ logstash-core-event-java/lib/logstash-core-event-java_jars.rb
@@ -1,6 +1,6 @@
 # this is a generated file, to avoid over-writing it just delete this comment
 require 'jar_dependencies'
 
-require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.7.1' )
-require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.7.0' )
-require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.7.1-1' )
+require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.7.3' )
+require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.7.3' )
+require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.7.3' )
