99b7f772cfa2f6eb0de4c18066770eb8ffb2ad14
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-04-25 11:05:54 -0400

    Introduce the use of a platforms.json file where to keep all platforms information to simplify the vagrantfile structure. Also make the vagrant execution more elegant under rake
    Fixes #5350

--- qa/vagrant-helpers.rb
+++ qa/vagrant-helpers.rb
@@ -1,15 +1,56 @@
 # encoding: utf-8
 require "open3"
+require "bundler"
 
 module LogStash
+  class CommandExecutor
+    class CommandError < StandardError; end
+
+    class CommandResponse
+      attr_reader :stdin, :stdout, :stderr, :exitstatus
+
+      def initialize(stdin, stdout, stderr, exitstatus)
+        @stdin = stdin, 
+        @stdout = stdout
+        @stderr = stderr
+        @exitstatus = exitstatus
+      end
+
+      def success?
+        exitstatus == 0
+      end
+    end
+
+    def self.run(cmd)
+      # This block is require to be able to launch a ruby subprocess
+      # that use bundler.
+      Bundler.with_clean_env do
+        Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+          CommandResponse.new(stdin, stdout.read.chomp, stderr.read.chomp, wait_thr.value.exitstatus)
+        end
+      end
+    end
+
+    # This method will raise an exception if the `CMD`
+    # was not run successfully and will display the content of STDERR
+    def self.run!(cmd)
+      response = run(cmd)
+    
+      unless response.success?
+        raise CommandError, "CMD: #{cmd} STDERR: #{response.stderr}"
+      end
+      response
+    end
+  end
+
   class VagrantHelpers
 
     def self.bootstrap
-      execute("vagrant up")
+      CommandExecutor.run!("vagrant up")
     end
 
     def self.fetch_config
-      execute("vagrant ssh-config")
+      CommandExecutor.run!("vagrant ssh-config")
     end
 
     def self.parse(lines)
@@ -29,15 +70,5 @@ module LogStash
       hosts << host
       hosts
     end
-
-    private
-
-    def self.execute(cmd)
-      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-        { :stdout => stdout.read.chomp, :stderr => stderr.read.chomp,
-          :exit_status => wait_thr.value.exitstatus }
-      end
-    end
-
   end
 end
