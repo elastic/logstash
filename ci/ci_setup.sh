8294c57b589da60f5044674d77cbdaa578243835
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-04-26 11:42:42 -0400

    Set the HEAP Size to 1gig when running rake tasks
    Logstash uses by default in the shell script 1 gig of memory for the
    JVM. but when we use rake it uses the default settings of JRUBY and
    start a vm 500megs of memory. The recent changes in JRUBY and
    JRuby-OpenSSL have increased the base memory and make the installing of
    plugins with a OOM issues. This PR make sure the CI scripts and the rake
    jruby launch uses the default of 1gig.
    
    Fixes: #5179

--- ci/ci_setup.sh
+++ ci/ci_setup.sh
@@ -13,6 +13,11 @@ rm -rf vendor       # make sure there are no vendorized dependencies
 rm -rf .bundle
 rm -rf spec/reports # no stale spec reports from previous executions
 
+# Since we are using the system jruby, we need to make sure our jvm process
+# uses at least 1g of memory, If we don't do this we can get OOM issues when
+# installing gems. See https://github.com/elastic/logstash/issues/5179
+export JRUBY_OPTS="-J-Xmx1g"
+
 # Setup the environment
 rake bootstrap # Bootstrap your logstash instance
 
