cf36f31229b413dd3e5a5ed7e48bebbf0b9f4e12
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-04-27 16:11:24 -0400

    Allow to run acceptance test with the local logstash
    This change introduce a new command ci/ci_acceptance which is
    responsable of building the packages, bootstraping the acceptance test
    environment, launching the VMs and running the tests.
    
    You can use the following command to target specific platform.
    
    ```
    ci/ci_acceptance.sh all
    ci/ci_acceptance.sh centos
    ci/ci_acceptance.sh debian
    ```
    
    This PR also add a new rake task to build all the artifacts in a single
    run called rake artifact:all it make sure we only install the plugin
    only once and make the build process faster.
    
    Fixes: #5212
    
    Fixes #5218

---
+++ ci/ci_acceptance.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+
+# Since we are using the system jruby, we need to make sure our jvm process
+# uses at least 1g of memory, If we don't do this we can get OOM issues when
+# installing gems. See https://github.com/elastic/logstash/issues/5179
+export JRUBY_OPTS="-J-Xmx1g"
+
+SELECTED_TEST_SUITE=$1
+
+if [[ $SELECTED_TEST_SUITE == $"centos" ]]; then
+  rake artifact:rpm
+  echo "Acceptance: Installing dependencies"
+  cd qa
+  bundle install
+
+  echo "Acceptance: Running the tests"
+  bundle exec rake test:setup
+  bundle exec rake test:ssh_config
+  bundle exec rake test:acceptance:centos
+elif [[ $SELECTED_TEST_SUITE == $"debian" ]]; then
+  rake artifact:deb
+  echo "Acceptance: Installing dependencies"
+  cd qa
+  bundle install
+
+  echo "Acceptance: Running the tests"
+  bundle exec rake test:setup
+  bundle exec rake test:ssh_config
+  bundle exec rake test:acceptance:debian
+elif [[ $SELECTED_TEST_SUITE == $"all" ]]; then
+  echo "Building Logstash artifacts"
+  rake artifact:all
+
+  echo "Acceptance: Installing dependencies"
+  cd qa
+  bundle install
+
+  echo "Acceptance: Running the tests"
+  bundle exec rake test:setup
+  bundle exec rake test:ssh_config
+  bundle exec rake test:acceptance:all
+  cd ..
+fi
--- qa/sys/centos/user_bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd
-wget --no-verbose https://download.elastic.co/logstash/logstash/packages/centos/logstash-2.3.1-1.noarch.rpm
