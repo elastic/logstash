e9ce0b16f8d968156deb6a4dfa835dfeac989e93
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-04-29 18:55:36 +0200

    renamed commands organization to be simplier also introduced the idea of artifact subject, used as a mather of fact to wrap the test cases in a simplier way.
    simplified the idea of shared examples so they could be wrapped in
    different behaviours we aim to cover, this might be useful when
    composing different levels of tests.
    
    Fixes #5226

--- qa/rspec/commands.rb
+++ qa/rspec/commands.rb
@@ -1,8 +1,49 @@
 # encoding: utf-8
-require_relative "./debian/commands"
-require_relative "./redhat/commands"
+require_relative "./commands/debian"
+require_relative "./commands/redhat"
 
 module ServiceTester
+
+  class Artifact
+
+    attr_reader :client, :host
+
+    def initialize(host, options={})
+      @host     = host
+      @options  = options
+      @client = CommandsFactory.fetch(options["type"])
+    end
+
+    def name
+      "logstash"
+    end
+
+    def hosts
+      [@host]
+    end
+
+    def start_service
+      client.start_service(name, host)
+    end
+
+    def stop_service
+      client.stop_service(name, host)
+    end
+
+    def install(version)
+      package = client.package_for(version)
+      client.install(package, host)
+    end
+
+    def uninstall
+      client.uninstall(name, host)
+    end
+
+    def to_s
+      "Artifact #{name}@#{host}"
+    end
+  end
+
   class CommandsFactory
 
     def self.fetch(type)
