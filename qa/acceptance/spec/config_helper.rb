ae38fd89d208b60f8a7b3fefb4c8cd76f8a5eaeb
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-04-29 11:13:16 +0200

    Introduce the concept of a single spec for the tests, we removed the platform depant test as they where mostly similar, but only used with small platform dependant metadata, this all has been moved to factory methods.
    make sure platforms uses canonicals "former" names, so no "ubuntu"
    platfrom as is debian based, and no "centos" as is redhat based.
    
    Fixes #5226

--- qa/acceptance/spec/config_helper.rb
+++ qa/acceptance/spec/config_helper.rb
@@ -13,15 +13,18 @@ module SpecsHelper
 
   def self.configure(vagrant_boxes)
     setup_config = JSON.parse(File.read(File.join(File.dirname(__FILE__), "..", "..", ".vm_ssh_config")))
-
+    boxes        = vagrant_boxes.inject({}) do |acc, v|
+      acc[v.name] = v.type
+      acc
+    end
     ServiceTester.configure do |config|
       config.servers = []
       config.lookup  = {}
       setup_config.each do |host_info|
-        next unless vagrant_boxes.include?(host_info["host"])
+        next unless boxes.keys.include?(host_info["host"])
         url = "#{host_info["hostname"]}:#{host_info["port"]}"
         config.servers << url
-        config.lookup[url] = host_info["host"]
+        config.lookup[url] = {"host" => host_info["host"], "type" => boxes[host_info["host"]] }
       end
     end
   end
