f60469ad7316839394a6d19cfa7f20016af8c276
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-05-02 10:28:15 -0400

    Added a note on the ci/ci_acceptance.sh concerning the package generation

--- ci/ci_acceptance.sh
+++ ci/ci_acceptance.sh
@@ -9,6 +9,7 @@ export JRUBY_OPTS="-J-Xmx1g"
 SELECTED_TEST_SUITE=$1
 
 if [[ $SELECTED_TEST_SUITE == $"centos" ]]; then
+  echo "Generating the RPM, make sure you start with a clean environment before generating other packages."
   rake artifact:rpm
   echo "Acceptance: Installing dependencies"
   cd qa
@@ -19,6 +20,7 @@ if [[ $SELECTED_TEST_SUITE == $"centos" ]]; then
   bundle exec rake test:ssh_config
   bundle exec rake test:acceptance:centos
 elif [[ $SELECTED_TEST_SUITE == $"debian" ]]; then
+  echo "Generating the DEB, make sure you start with a clean environment before generating other packages."
   rake artifact:deb
   echo "Acceptance: Installing dependencies"
   cd qa
