b0c99b0ae0886da961a7e4c615ebb64c716c79dc
Joao Duarte <jsvduarte@gmail.com>
2016-05-06 17:18:05 +0100

    added specs for Settings class

--- logstash-core/spec/logstash/settings_spec.rb
+++ logstash-core/spec/logstash/settings_spec.rb
@@ -2,129 +2,61 @@
 require "spec_helper"
 require "logstash/settings"
 
-describe LogStash::Setting do
-  let(:logger) { double("logger") }
-  describe "#value" do
-    context "when using a default value" do
-      context "when no value is set" do
-        subject { described_class.new("number", Numeric, 1) }
-        it "should return the default value" do
-          expect(subject.value).to eq(1)
-        end
-      end
-
-      context "when a value is set" do
-        subject { described_class.new("number", Numeric, 1) }
-        let(:new_value) { 2 }
-        before :each do
-          subject.set(new_value)
-        end
-        it "should return the set value" do
-          expect(subject.value).to eq(new_value)
-        end
-      end
-    end
-
-    context "when not using a default value" do
-      context "when no value is set" do
-        subject { described_class.new("number", Numeric, nil, false) }
-        it "should return the default value" do
-          expect(subject.value).to eq(nil)
-        end
-      end
-
-      context "when a value is set" do
-        subject { described_class.new("number", Numeric, nil, false) }
-        let(:new_value) { 2 }
-        before :each do
-          subject.set(new_value)
-        end
-        it "should return the set value" do
-          expect(subject.value).to eq(new_value)
-        end
-      end
-    end
-  end
-
-  describe "#set?" do
-    context "when there is not value set" do
-      subject { described_class.new("number", Numeric, 1) }
-      it "should return false" do
-        expect(subject.set?).to be(false)
-      end
-    end
-    context "when there is a value set" do
-      subject { described_class.new("number", Numeric, 1) }
+describe LogStash::Settings do
+  let(:numeric_setting_name) { "number" }
+  let(:numeric_setting) { LogStash::Setting.new(numeric_setting_name, Numeric, 1) }
+  describe "#register" do
+    context "if setting has already been registered" do
       before :each do
-        subject.set(2)
+        subject.register(numeric_setting)
       end
-      it "should return false" do
-        expect(subject.set?).to be(true)
-      end
-    end
-  end
-  describe "#set" do
-    subject { described_class.new("number", Numeric, 1) }
-    it "should change the value of a setting" do
-      expect(subject.value).to eq(1)
-      subject.set(4)
-      expect(subject.value).to eq(4)
-    end
-    context "when executed for the first time" do
-      it "should change the result of set?" do
-        expect(subject.set?).to eq(false)
-        subject.set(4)
-        expect(subject.set?).to eq(true)
-      end
-    end
-
-    context "when the argument's class does not match @klass" do
-      it "should throw an exception" do
-        expect { subject.set("not a number") }.to raise_error
-      end
-    end
-  end
-
-  describe "#reset" do
-    subject { described_class.new("number", Numeric, 1) }
-    context "if value is already set" do
-      before :each do
-        subject.set(2)
-      end
-      it "should reset value to default" do
-        subject.reset
-        expect(subject.value).to eq(1)
-      end
-      it "should reset set? to false" do
-        expect(subject.set?).to eq(true)
-        subject.reset
-        expect(subject.set?).to eq(false)
-      end
-    end
-  end
-
-  describe "validator_proc" do
-    let(:default_value) { "small text" }
-    subject { described_class.new("mytext", String, default_value) {|v| v.size < 20 } }
-    context "when validation fails" do
-      let(:new_value) { "very very very very very big text" }
       it "should raise an exception" do
-        expect { subject.set(new_value) }.to raise_error
-      end
-      it "should not change the value" do
-        subject.set(new_value) rescue nil
-        expect(subject.value).to eq(default_value)
+        expect { subject.register(numeric_setting) }.to raise_error
       end
     end
-    context "when validation is successful" do
-      let(:new_value) { "smaller text" }
+    context "if setting hasn't been registered" do
       it "should not raise an exception" do
-        expect { subject.set(new_value) }.to_not raise_error
+        expect { subject.register(numeric_setting) }.to_not raise_error
       end
-      it "should change the value" do
-        subject.set(new_value)
-        expect(subject.value).to eq(new_value)
+    end
+  end
+  describe "#get_setting" do
+    context "if setting has been registered" do
+      before :each do
+        subject.register(numeric_setting)
       end
+      it "should return the setting" do
+        expect(subject.get_setting(numeric_setting_name)).to eq(numeric_setting)
+      end
+    end
+    context "if setting hasn't been registered" do
+      it "should raise an exception" do
+        expect { subject.get_setting(numeric_setting_name) }.to raise_error
+      end
+    end
+  end
+  describe "#get_subset" do
+    let(:numeric_setting_1) { LogStash::Setting.new("num.1", Numeric, 1) }
+    let(:numeric_setting_2) { LogStash::Setting.new("num.2", Numeric, 2) }
+    let(:numeric_setting_3) { LogStash::Setting.new("num.3", Numeric, 3) }
+    let(:string_setting_1) { LogStash::Setting.new("string.1", String, "hello") }
+    before :each do
+      subject.register(numeric_setting_1)
+      subject.register(numeric_setting_2)
+      subject.register(numeric_setting_3)
+      subject.register(string_setting_1)
+    end
+
+    it "supports regex" do
+      expect(subject.get_subset(/num/).get_setting("num.3")).to eq(numeric_setting_3)
+      expect { subject.get_subset(/num/).get_setting("string.1") }.to raise_error
+    end
+
+    it "returns a copy of settings" do
+      subset = subject.get_subset(/num/)
+      subset.set("num.2", 1000)
+      expect(subject.get("num.2")).to eq(2)
+      expect(subset.get("num.2")).to eq(1000)
     end
   end
 end
