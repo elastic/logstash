619f1eb32ff48f78ec61c20ed69a4ceecc7732fe
Jordan Sissel <jls@semicomplete.com>
2016-05-06 10:18:53 -0700

    Include ci/reporter so bin/rpsec works for me.

--- lib/bootstrap/rspec.rb
+++ lib/bootstrap/rspec.rb
@@ -7,6 +7,7 @@ $LOAD_PATH.unshift(File.join(LogStash::Environment::LOGSTASH_CORE, "spec"))
 
 require "rspec/core"
 require "rspec"
+require 'ci/reporter/rake/rspec_loader'
 
 status = RSpec::Core::Runner.run(ARGV.empty? ? ["spec"] : ARGV).to_i
 exit status if status != 0
