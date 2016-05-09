9a1044e183ac9169fbeb434f70d72691f778ff05
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-05-09 17:08:25 +0200

    updated the list of available platforms for acceptance testing, including latest ubuntu, oel, fedora, debian and suse versions
    Fixes #5260

--- qa/rspec/commands/suse.rb
+++ qa/rspec/commands/suse.rb
@@ -2,47 +2,45 @@
 require_relative "base"
 
 module ServiceTester
-  class DebianCommands < Base
+  class SuseCommands < Base
 
     def installed?(hosts, package)
       stdout = ""
       at(hosts, {in: :serial}) do |host|
-        cmd = sudo_exec!("dpkg -s  #{package}")
+        cmd = exec!("zypper search #{package}")
         stdout = cmd.stdout
       end
-      stdout.match(/^Package: #{package}$/)
-      stdout.match(/^Status: install ok installed$/)
+      stdout.match(/^i | logstash | An extensible logging pipeline | package$/)
     end
 
     def package_for(version)
-      File.join(ServiceTester::Base::LOCATION, "logstash-#{version}_all.deb")
+      File.join(ServiceTester::Base::LOCATION, "logstash-#{version}.noarch.rpm")
     end
 
     def install(package, host=nil)
-      hosts = (host.nil? ? servers : Array(host))
-      at(hosts, {in: :serial}) do |_|
-        sudo_exec!("dpkg -i  #{package}")
+      hosts  = (host.nil? ? servers : Array(host))
+      errors = {}
+      at(hosts, {in: :serial}) do |_host|
+        cmd = sudo_exec!("zypper --no-gpg-checks --non-interactive install  #{package}")
+        errors[_host] = cmd.stderr unless cmd.stderr.empty?
       end
+      errors
     end
 
     def uninstall(package, host=nil)
       hosts = (host.nil? ? servers : Array(host))
       at(hosts, {in: :serial}) do |_|
-        sudo_exec!("dpkg -r #{package}")
-        sudo_exec!("dpkg --purge #{package}")
+        cmd = sudo_exec!("zypper --no-gpg-checks --non-interactive remove #{package}")
       end
     end
 
     def removed?(hosts, package)
       stdout = ""
       at(hosts, {in: :serial}) do |host|
-        cmd = sudo_exec!("dpkg -s #{package}")
-        stdout = cmd.stderr
+        cmd    = exec!("zypper search #{package}")
+        stdout = cmd.stdout
       end
-      (
-        stdout.match(/^Package `#{package}' is not installed and no info is available.$/) ||
-        stdout.match(/^dpkg-query: package '#{package}' is not installed and no information is available$/)
-      )
+      stdout.match(/No packages found/)
     end
 
     def running?(hosts, package)
