77cef1982b04eefdc4d4bdf8f5d94d02c9162754
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-05-10 11:35:02 +0200

    cleanup the tools folder, including tools/release.sh, tools/upload.sh and tools/Gemfile.beaker cause not used anymore
    Fixes #5268

--- tools/upload.sh
+++
@@ -1,8 +0,0 @@
-
-basedir=$(dirname $0)/../
-bucket=download.elasticsearch.org
-
-s3cmd put -P $basedir/build/release/build/*.gz s3://${bucket}/logstash/logstash/
-s3cmd put -P $basedir/build/release/build/*.rpm s3://${bucket}/logstash/logstash/packages/centos/
-s3cmd put -P $basedir/build/release/build/*.deb s3://${bucket}/logstash/logstash/packages/debian
-s3cmd put -P $basedir/build/release/build/*.deb s3://${bucket}/logstash/logstash/packages/ubuntu
