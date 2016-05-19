5fc9ced9629e558a20212940398c97be76d7d181
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-05-19 16:07:12 +0200

    make sure debian based machines use java8
    Fixes #5321

--- qa/sys/debian/bootstrap.sh
+++ qa/sys/debian/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+add-apt-repository ppa:openjdk-r/ppa
 apt-get update
-apt-get install -y openjdk-7-jdk
+apt-get install -y openjdk-8-jdk
+update-alternatives --config java
+update-alternatives --config javac
