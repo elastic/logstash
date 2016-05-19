f814c94a7d900f1d309e6f19669f7d4a5a585e52
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-05-19 17:02:06 +0200

    make sure to use hostname and not just ip:port address when reporting the names
    Fixes #5323

--- qa/acceptance/spec/shared_examples/updated.rb
+++ qa/acceptance/spec/shared_examples/updated.rb
@@ -6,14 +6,14 @@ RSpec.shared_examples "updated" do |logstash|
   before (:all) { logstash.snapshot }
   after  (:all) { logstash.restore }
 
-  it "can update on #{logstash.host}" do
+  it "can update on #{logstash.hostname}" do
     logstash.install(LOGSTASH_LATEST_VERSION, "./")
     expect(logstash).to be_installed
     logstash.install(LOGSTASH_VERSION)
     expect(logstash).to be_installed
   end
 
-  it "can run on #{logstash.host}" do
+  it "can run on #{logstash.hostname}" do
     logstash.start_service
     expect(logstash).to be_running
     logstash.stop_service
