a1032b1040d825d528575967016871441e3405d2
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-05-23 12:53:32 +0200

    make sure downloaded logstashes use the new (arch independant files name) this will help the test scripts to not have an if in the meanwhile the packages with the new naming are released
    Fixes #5346

--- qa/sys/debian/user_bootstrap.sh
+++ qa/sys/debian/user_bootstrap.sh
@@ -3,3 +3,4 @@
 VERSION=`cat /vagrant/config/platforms.json | grep  latest | cut -d":" -f2 | sed 's/["\|,| ]//g'`
 LOGSTASH_FILENAME="logstash-${VERSION}_all.deb"
 wget -q https://download.elastic.co/logstash/logstash/packages/debian/$LOGSTASH_FILENAME
+mv $LOGSTASH_FILENAME "logstash-${VERSION}.deb" # necessary patch until new version with the standard name format are released
