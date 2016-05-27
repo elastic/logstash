b6ea2b714c3d2671c79d1a9fef119a0f412dffc0
Aaron Mildenstein <aaron@mildensteins.com>
2016-05-27 14:43:40 -0600

    Fix starting logstash from command-line
    This fix is for packages only (RPM/DEB)
    
    This puts the requisite changes into /etc/logstash/logstash.yml so that starting logstash from the command-line, e.g. `/usr/share/logstash/bin/logstash --path.settings /etc/logstash` will read the pipeline from `/etc/logstash/conf.d` and log to `/var/log/logstash/logstash.log`.
    
    fixes #5379
    
    Comment out tmpdir in jvm.options
    
    The absence of the tmpdir can cause JRuby to fail

--- pkg/centos/after-install.sh
+++ pkg/centos/after-install.sh
@@ -1,7 +1,9 @@
-# /sbin/chkconfig --add logstash
-
 chown -R logstash:logstash /usr/share/logstash
 chown logstash /var/log/logstash
 chown logstash:logstash /var/lib/logstash
 chmod 0644 /etc/logrotate.d/logstash
+sed -i \
+  -e 's|# path.config:|path.config: /etc/logstash/conf.d|' \
+  -e 's|# path.log:|path.log: /var/log/logstash/logstash.log|' \
+  /etc/logstash/logstash.yml
 /usr/share/logstash/bin/system-install /etc/logstash/startup.options
--- pkg/debian/after-install.sh
+++ pkg/debian/after-install.sh
@@ -5,4 +5,8 @@ chown logstash /var/log/logstash
 chown logstash:logstash /var/lib/logstash
 chmod 755 /etc/logstash
 chmod 0644 /etc/logrotate.d/logstash
+sed -i \
+  -e 's|# path.config:|path.config: /etc/logstash/conf.d|' \
+  -e 's|# path.log:|path.log: /var/log/logstash/logstash.log|' \
+  /etc/logstash/logstash.yml
 /usr/share/logstash/bin/system-install /etc/logstash/startup.options
