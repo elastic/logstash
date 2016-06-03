ddcf88e31c53282f9fbec78afce29f50beb56a4c
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-06-03 12:06:04 +0200

    move unecessary shared examples as regular test, this will also serve as example when to use shared and when not

--- qa/spec/lib/artifact_operation_spec.rb
+++ qa/spec/lib/artifact_operation_spec.rb
@@ -6,9 +6,51 @@ require_relative '../shared_examples/updated'
 
 describe "artifacts operation" do
   config = ServiceTester.configuration
+
   config.servers.each do |address|
     logstash = ServiceTester::Artifact.new(address, config.lookup[address])
-    it_behaves_like "installable", logstash
-    it_behaves_like "updated", logstash
+
+    describe "installation" do
+      before(:each) do
+        logstash.install(LOGSTASH_VERSION)
+      end
+
+      it "is installed on #{logstash.hostname}" do
+        expect(logstash).to be_installed
+      end
+
+      it "is running on #{logstash.hostname}" do
+        logstash.start_service
+        expect(logstash).to be_running
+        logstash.stop_service
+      end
+
+      it "is removable on #{logstash.hostname}" do
+        logstash.uninstall
+        expect(logstash).to be_removed
+      end
+    end
+  end
+
+  config.servers.each do |address|
+    logstash = ServiceTester::Artifact.new(address, config.lookup[address])
+
+    describe "update" do
+      before (:all) { logstash.snapshot }
+      after  (:all) { logstash.restore }
+
+      it "can update on #{logstash.hostname}" do
+        logstash.install(LOGSTASH_LATEST_VERSION, "./")
+        expect(logstash).to be_installed
+        logstash.install(LOGSTASH_VERSION)
+        expect(logstash).to be_installed
+      end
+
+      it "can run on #{logstash.hostname}" do
+        logstash.start_service
+        expect(logstash).to be_running
+        logstash.stop_service
+      end
+    end
   end
 end
