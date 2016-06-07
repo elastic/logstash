6ae54801da0e0f4ff549e04636007bd0d30ced28
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-06-07 13:56:40 +0200

    rework the acceptance update scenario to avoid using snapshort restore, this might be a temporary solution for ssh issues in vagrant and ubuntu hosts
    Fixes #5456

--- qa/acceptance/spec/shared_examples/updated.rb
+++ qa/acceptance/spec/shared_examples/updated.rb
@@ -4,19 +4,22 @@ require          'logstash/version'
 # This test checks if the current package could used to update from the latest version released.
 RSpec.shared_examples "updated" do |logstash|
 
-  before (:all) { logstash.snapshot }
-  after  (:all) { logstash.restore }
-
-  it "can update on #{logstash.hostname}" do
-    logstash.install(LOGSTASH_LATEST_VERSION, "./")
-    expect(logstash).to be_installed
-    logstash.install(LOGSTASH_VERSION)
-    expect(logstash).to be_installed
+  before(:all) { logstash.uninstall }
+  after(:all)  do
+    logstash.stop_service # make sure the service is stopped
+    logstash.uninstall #remove the package to keep uniform state
   end
 
-  it "can run on #{logstash.hostname}" do
+  before(:each) do
+    logstash.install(LOGSTASH_LATEST_VERSION, "./") # make sure latest version is installed
+  end
+
+  it "can be updated an run on #{logstash.hostname}" do
+    # Performing the update
+    logstash.install(LOGSTASH_VERSION)
+    expect(logstash).to be_installed
+    # starts the service to be sure it runs after the upgrade
     logstash.start_service
     expect(logstash).to be_running
-    logstash.stop_service
   end
 end
