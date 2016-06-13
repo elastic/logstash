b148df70fcd75a1a36709cf9fc3b3c2f33e87363
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-06-13 15:21:35 +0200

    remove patch ot use common file name, wihtout arch, also update ls filename in bootstrap scripts
    Fixes #5486

--- qa/sys/debian/user_bootstrap.sh
+++ qa/sys/debian/user_bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 VERSION=`cat /vagrant/config/platforms.json | grep  latest | cut -d":" -f2 | sed 's/["\|,| ]//g'`
-LOGSTASH_FILENAME="logstash-${VERSION}_all.deb"
+LOGSTASH_FILENAME="logstash-${VERSION}.deb"
 wget -q https://download.elastic.co/logstash/logstash/packages/debian/$LOGSTASH_FILENAME
-mv $LOGSTASH_FILENAME "logstash-${VERSION}.deb" # necessary patch until new version with the standard name format are released
--- qa/sys/redhat/user_bootstrap.sh
+++ qa/sys/redhat/user_bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 VERSION=`cat /vagrant/config/platforms.json | grep  latest | cut -d":" -f2 | sed 's/["\|,| ]//g'`
-LOGSTASH_FILENAME="logstash-${VERSION}.noarch.rpm"
+LOGSTASH_FILENAME="logstash-${VERSION}.rpm"
 wget -q https://download.elastic.co/logstash/logstash/packages/centos/$LOGSTASH_FILENAME
-mv $LOGSTASH_FILENAME "logstash-${VERSION}.rpm" # necessary patch until new version with the standard name format are released
