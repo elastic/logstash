6268e063e322158edf416b64e652a3787bfed02d
Andrew Cholakian <andrew@andrewvc.com>
2016-06-14 14:29:29 -0500

    Move _plugin api to _node/plugin + add tests
    Fixes #5381

---
+++ logstash-core/spec/api/lib/api/node_plugins_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+require_relative "../../spec_helper"
+require "sinatra"
+require "logstash/api/modules/plugins"
+require "logstash/json"
+
+describe LogStash::Api::Modules::Plugins do
+  include_context "api setup"
+
+  extend ResourceDSLMethods
+
+  before(:all) do
+    do_request { get "/" }
+  end
+
+  let(:payload) { LogStash::Json.load(last_response.body) }
+
+  describe "retrieving plugins" do
+    it "should return OK" do
+      expect(last_response).to be_ok
+    end
+
+    it "should return a list of plugins" do      
+      expect(payload["plugins"]).to be_a(Array)
+    end
+
+    it "should return the total number of plugins" do
+      expect(payload["total"]).to be_a(Numeric)
+    end
+    
+  end
+end
