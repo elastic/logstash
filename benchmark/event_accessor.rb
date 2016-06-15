4ad2229797917d2947e0a5cb2ce8cb88c3d983ba
Guy Boertje <guy@elastic.co>
2016-06-15 12:13:01 +0100

    Benchmarks are still good

--- benchmark/event_accessor.rb
+++ benchmark/event_accessor.rb
@@ -2,14 +2,16 @@
 require "benchmark/ips"
 require "logstash/event"
 
-options = { :time => 10, :warmup => 30 }
+options = { :time => 10, :warmup => 60 }
 puts "Same Event instance"
 
 event = LogStash::Event.new("foo" => {"bar" => {"foobar" => "morebar"} })
-STDERR.puts " ----------> event[\"[foo][bar][foobar]\"] = #{event["[foo][bar][foobar]"]}"
+STDERR.puts ""
+STDERR.puts " ----------> event.get(\"[foo][bar][foobar]\") => #{event.get("[foo][bar][foobar]")}"
+STDERR.puts ""
 
 Benchmark.ips do |x|
   x.config(options)
 
-  x.report("Deep fetch") { event["[foo][bar][foobar]"] }
+  x.report("Deep fetch") { event.get("[foo][bar][foobar]") }
 end
