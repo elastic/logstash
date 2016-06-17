d48896a0b1acfd68dfb5f96001ebd65d8d949d18
Guy Boertje <guy@elastic.co>
2016-06-17 20:26:17 +0100

    Fix Javafier for MapJavaProxy, ArrayJavaProxy, ConcreteJavaProxy. (#5474)
    * Fix Javafier for MapJavaProxy, ArrayJavaProxy, ConcreteJavaProxy.
    + JUnit tests + Rspec test
    
    * fix splat imports
    * use a method instead of switch case code and throw an error if we don't
    know how to handle the wrapped type.
    * Add hand crafted map to use in rspec from Java directly and rspec test
    * changes for review comments.

--- logstash-core-event-java/src/main/java/com/logstash/Javafier.java
+++ logstash-core-event-java/src/main/java/com/logstash/Javafier.java
@@ -1,44 +1,76 @@
 package com.logstash;
 
+import com.logstash.ext.JrubyTimestampExtLibrary;
+import org.joda.time.DateTime;
 import org.jruby.RubyArray;
-import org.jruby.RubyHash;
-import org.jruby.RubyString;
-import org.jruby.RubyObject;
-import org.jruby.RubyBoolean;
-import org.jruby.RubyArray;
-import org.jruby.RubyFloat;
-import org.jruby.RubyInteger;
-import org.jruby.RubyNil;
+import org.jruby.RubyBignum;
 import org.jruby.RubyBoolean;
 import org.jruby.RubyFixnum;
-import org.jruby.RubyTime;
+import org.jruby.RubyFloat;
+import org.jruby.RubyHash;
+import org.jruby.RubyInteger;
+import org.jruby.RubyNil;
+import org.jruby.RubyString;
 import org.jruby.RubySymbol;
-import org.jruby.RubyBignum;
+import org.jruby.RubyTime;
 import org.jruby.ext.bigdecimal.RubyBigDecimal;
-import com.logstash.ext.JrubyTimestampExtLibrary;
+import org.jruby.java.proxies.JavaProxy;
+import org.jruby.java.proxies.MapJavaProxy;
+import org.jruby.javasupport.JavaUtil;
 import org.jruby.runtime.builtin.IRubyObject;
-import java.math.BigDecimal;
-import org.joda.time.DateTime;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Javafier {
+    private static final String ERR_TEMPLATE = "Missing Ruby class handling for full class name=%s, simple name=%s";
+    private static final String PROXY_ERR_TEMPLATE = "Missing Ruby class handling for full class name=%s, simple name=%s, wrapped object=%s";
 
     private Javafier(){}
 
-    public static List<Object> deep(RubyArray a) {
+    public static List<Object> deep(IRubyObject[] a) {
         final ArrayList<Object> result = new ArrayList();
 
-        // TODO: (colin) investagate why .toJavaArrayUnsafe() which should be faster by avoiding copying produces nil values spec errors in arrays
-        for (IRubyObject o : a.toJavaArray()) {
+        for (IRubyObject o : a) {
             result.add(deep(o));
         }
         return result;
     }
 
+    public static List<Object> deep(RubyArray a) {
+        return deep(a.toJavaArray());
+    }
+
+    private static HashMap<String, Object> deepMap(final Map<?, ?> map) {
+        final HashMap<String, Object> result = new HashMap();
+
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            String k;
+            if (entry.getKey() instanceof IRubyObject) {
+                k = ((IRubyObject) entry.getKey()).asJavaString();
+            } else {
+                k = String.valueOf(entry.getKey());
+            }
+            result.put(k, deepAnything(entry.getValue()));
+        }
+        return result;
+    }
+
+    private static List<Object> deepList(List<Object> a) {
+        final ArrayList<Object> result = new ArrayList();
+
+        for (Object o : a) {
+            result.add(deepAnything(o));
+        }
+        return result;
+    }
+
     public static HashMap<String, Object> deep(RubyHash h) {
-        final HashMap result = new HashMap();
+        final HashMap<String, Object> result = new HashMap();
 
         h.visitAll(new RubyHash.Visitor() {
             @Override
@@ -47,6 +79,20 @@ public class Javafier {
             }
         });
         return result;
+    }
+
+    private static Object deepAnything(Object o) {
+        // because, although we have a Java object (from a JavaProxy??), it may have IRubyObjects inside
+        if (o instanceof IRubyObject) {
+            return deep((IRubyObject) o);
+        }
+        if (o instanceof Map) {
+            return deepMap((Map) o);
+        }
+        if (o instanceof List) {
+            return deepList((List) o);
+        }
+        return o;
     }
 
     public static String deep(RubyString s) {
@@ -101,14 +147,26 @@ public class Javafier {
         return false;
     }
 
+    private static Object deepJavaProxy(JavaProxy jp) {
+        Object obj = JavaUtil.unwrapJavaObject(jp);
+        if (obj instanceof IRubyObject[]) {
+            return deep((IRubyObject[])obj);
+        }
+        if (obj instanceof List) {
+            return deepList((List<Object>) obj);
+        }
+        Class cls = jp.getClass();
+        throw new IllegalArgumentException(missingHandlerString(PROXY_ERR_TEMPLATE, cls.getName(), cls.getSimpleName(), obj.getClass().getName()));
+    }
+
     public static Object deep(IRubyObject o) {
         // TODO: (colin) this enum strategy is cleaner but I am hoping that is not slower than using a instanceof cascade
-
+        Class cls = o.getClass();
         RUBYCLASS clazz;
         try {
-            clazz = RUBYCLASS.valueOf(o.getClass().getSimpleName());
+            clazz = RUBYCLASS.valueOf(cls.getSimpleName());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Missing Ruby class handling for full class name=" + o.getClass().getName() + ", simple name=" + o.getClass().getSimpleName());
+            throw new IllegalArgumentException(missingHandlerString(ERR_TEMPLATE, cls.getName(), cls.getSimpleName()));
         }
 
         switch(clazz) {
@@ -127,6 +185,9 @@ public class Javafier {
             case RubyNil: return deep((RubyNil)o);
             case True: return deep((RubyBoolean.True)o);
             case False: return deep((RubyBoolean.False)o);
+            case MapJavaProxy: return deepMap((Map)((MapJavaProxy) o).getObject());
+            case ArrayJavaProxy:  return deepJavaProxy((JavaProxy) o);
+            case ConcreteJavaProxy: return deepJavaProxy((JavaProxy) o);
         }
 
         if (o.isNil()) {
@@ -134,7 +195,12 @@ public class Javafier {
         }
 
         // TODO: (colin) temporary trace to spot any unhandled types
-        System.out.println("***** WARN: UNHANDLED IRubyObject full class name=" + o.getMetaClass().getRealClass().getName() + ", simple name=" + o.getClass().getSimpleName() + " java class=" + o.getJavaClass().toString() + " toString=" + o.toString());
+        System.out.println(String.format(
+                "***** WARN: UNHANDLED IRubyObject full class name=%s, simple name=%s java class=%s toString=%s",
+                o.getMetaClass().getRealClass().getName(),
+                o.getClass().getSimpleName(),
+                o.getJavaClass().toString(),
+                o.toString()));
 
         return o.toJava(o.getJavaClass());
     }
@@ -150,12 +216,19 @@ public class Javafier {
         RubyBoolean,
         RubyFixnum,
         RubyBignum,
-        RubyObject,
         RubyNil,
         RubyTime,
         RubySymbol,
         True,
-        False;
+        False,
+        // these proxies may wrap a java collection of IRubyObject types
+        MapJavaProxy,
+        ArrayJavaProxy,
+        ConcreteJavaProxy
+    }
+
+    private static String missingHandlerString(String fmt, String... subs) {
+        return String.format(fmt, subs);
     }
 }
 
--- logstash-core-event-java/src/main/java/com/logstash/Util.java
+++ logstash-core-event-java/src/main/java/com/logstash/Util.java
@@ -1,12 +1,43 @@
 package com.logstash;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
+
 public class Util {
     private Util() {}
+
+    public static Object getMapFixtureJackson() throws IOException {
+        StringBuilder json = new StringBuilder();
+        json.append("{");
+        json.append("\"string\": \"foo\", ");
+        json.append("\"int\": 42, ");
+        json.append("\"float\": 42.42, ");
+        json.append("\"array\": [\"bar\",\"baz\"], ");
+        json.append("\"hash\": {\"string\":\"quux\"} }");
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json.toString(), Object.class);
+    }
+
+    public static Map<String, Object> getMapFixtureHandcrafted() {
+        HashMap<String, Object> inner = new HashMap<>();
+        inner.put("string", "quux");
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("string", "foo");
+        map.put("int", 42);
+        map.put("float", 42.42);
+        map.put("array", Arrays.asList("bar", "baz"));
+        map.put("hash", inner);
+        return map;
+    }
 
     public static void mapMerge(Map<String, Object> target, Map<String, Object> add) {
         for (Map.Entry<String, Object> e : add.entrySet()) {
