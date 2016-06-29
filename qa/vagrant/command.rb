95a387f990266fd2076e93037439c7761fe42292
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-06-29 12:21:25 +0200

    Add the idea of a debug flag to make the vagrant commands output explicit to the end user. Also does the changes necessaries to make the vagrant command runner fetch the stdout and stdin on demand and not waiting till the end of it to report.
    Fixes #5566

--- qa/vagrant/command.rb
+++ qa/vagrant/command.rb
@@ -21,25 +21,51 @@ module LogStash
       end
     end
 
-    def self.run(cmd)
+    def self.run(cmd, debug=false)
       # This block is require to be able to launch a ruby subprocess
       # that use bundler.
       Bundler.with_clean_env do
-        Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-          CommandResponse.new(stdin, stdout.read.chomp, stderr.read.chomp, wait_thr.value.exitstatus)
+        stdin, stdout, stderr, wait_thr = Open3.popen3(cmd)
+        stdout_acc, stderr_acc = "", ""
+        stdout_reporter = reporter(stdout, wait_thr) do |c|
+          stdout_acc << c
+          print c if debug
         end
+        reporter(stderr, wait_thr) do |c|
+          stderr_acc << c;
+          print c if debug
+        end
+        stdout_reporter.join
+        CommandResponse.new(stdin, stdout_acc, stderr_acc, wait_thr.value.exitstatus)
       end
     end
 
     # This method will raise an exception if the `CMD`
     # was not run successfully and will display the content of STDERR
-    def self.run!(cmd)
-      response = run(cmd)
+    def self.run!(cmd, debug=false)
+      response = run(cmd, debug)
 
       unless response.success?
         raise CommandError, "CMD: #{cmd} STDERR: #{response.stderr}"
       end
       response
     end
+
+    private
+
+    def self.reporter(io, wait_thr, &block)
+      Thread.new(io, wait_thr) do |_io, _wait_thr|
+        while (_wait_thr.status == "run")
+          begin
+            c = _io.read(1)
+            block.call(c) if c
+          rescue IO::WaitReadable
+            IO.select([_io])
+            retry
+          end
+        end
+      end
+    end
+
   end
 end
