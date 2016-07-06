d9a7e44afc016fd4ee9e30c94a758376826004a8
Guy Boertje <guy@elastic.co>
2016-07-06 07:48:12 +0100

    add build method from a ReadElementValue

--- logstash-core/src/main/java/org/logstash/ackedqueue/ElementFactory.java
+++ logstash-core/src/main/java/org/logstash/ackedqueue/ElementFactory.java
@@ -1,8 +1,16 @@
 package org.logstash.ackedqueue;
 
+import org.logstash.common.io.ReadElementValue;
+
 public class ElementFactory {
 
     public static Queueable deserialize(byte[] bytes) {
         return StringElement.deserialize(bytes);
+    }
+
+    public static Queueable build(ReadElementValue value) {
+        StringElement ele = StringElement.deserialize(value.getBinaryValue());
+        ele.setSeqNum(value.getSeqNum());
+        return ele;
     }
 }
