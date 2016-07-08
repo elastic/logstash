bcefdac6338f19433d05719eed6246e00d01f00a
Guy Boertje <guy@elastic.co>
2016-07-08 15:31:22 +0100

    add @FunctionalInterface annotations

--- logstash-core/src/main/java/org/logstash/common/io/PageIOFactory.java
+++ logstash-core/src/main/java/org/logstash/common/io/PageIOFactory.java
@@ -2,6 +2,7 @@ package org.logstash.common.io;
 
 import java.io.IOException;
 
+@FunctionalInterface
 public interface PageIOFactory {
     PageIO build(int capacity, String dirPath) throws IOException;
 }
