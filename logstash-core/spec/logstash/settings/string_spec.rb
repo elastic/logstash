1c16e14c66fa9c9092c7f86ec887b94cdd809a5b
Joao Duarte <jsvduarte@gmail.com>
2016-07-13 12:19:41 +0100

    fix string setting validation of possible values
    Fixes #5628

---
+++ logstash-core/spec/logstash/settings/string_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require "spec_helper"
+require "logstash/settings"
+
+describe LogStash::Setting::String do
+  let(:possible_values) { ["a", "b", "c"] }
+  subject { described_class.new("mytext", nil, false, possible_values) }
+  describe "#set" do
+    context "when a value is given outside of possible_values" do
+      it "should raise an ArgumentError" do
+        expect { subject.set("d") }.to raise_error(ArgumentError)
+      end
+    end
+    context "when a value is given within possible_values" do
+      it "should set the value" do
+        expect { subject.set("a") }.to_not raise_error
+        expect(subject.value).to eq("a")
+      end
+    end
+  end
+end
