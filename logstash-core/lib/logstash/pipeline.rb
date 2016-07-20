108a56d0e6854618831849875edf88bbc2f7a37b
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-07-20 16:53:28 +0200

    add namespaced metrics for inputs
    Fixes #5668

--- logstash-core/lib/logstash/pipeline.rb
+++ logstash-core/lib/logstash/pipeline.rb
@@ -438,7 +438,9 @@ module LogStash; class Pipeline
       LogStash::FilterDelegator.new(@logger, klass, pipeline_scoped_metric.namespace(:filters), *args)
     else
       new_plugin = klass.new(*args)
-      new_plugin.metric = pipeline_scoped_metric.namespace(:inputs)
+      inputs_metric = pipeline_scoped_metric.namespace(:inputs)
+      namespaced_metric = inputs_metric.namespace(new_plugin.plugin_unique_name.to_sym)
+      new_plugin.metric = namespaced_metric
       new_plugin
     end
   end
