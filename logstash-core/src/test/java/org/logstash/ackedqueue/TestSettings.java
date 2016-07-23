2e231b99deb3825e6c581e94a01cbc4f7fc48290
Guy Boertje <guy@elastic.co>
2016-07-23 15:49:44 +0100

    add a settings for file based checkpoint IO and memory based page IO

--- logstash-core/src/test/java/org/logstash/ackedqueue/TestSettings.java
+++ logstash-core/src/test/java/org/logstash/ackedqueue/TestSettings.java
@@ -2,6 +2,7 @@ package org.logstash.ackedqueue;
 
 import org.logstash.common.io.ByteBufferPageIO;
 import org.logstash.common.io.CheckpointIOFactory;
+import org.logstash.common.io.FileCheckpointIO;
 import org.logstash.common.io.MemoryCheckpointIO;
 import org.logstash.common.io.PageIOFactory;
 
@@ -12,6 +13,17 @@ public class TestSettings {
         Settings s = new MemorySettings();
         PageIOFactory pageIOFactory = (pageNum, size, path) -> new ByteBufferPageIO(pageNum, size, path);
         CheckpointIOFactory checkpointIOFactory = (source) -> new MemoryCheckpointIO(source);
+        s.setCapacity(capacity);
+        s.setElementIOFactory(pageIOFactory);
+        s.setCheckpointIOFactory(checkpointIOFactory);
+        s.setElementDeserialiser(new ElementDeserialiser(StringElement.class));
+        return s;
+    }
+
+    public static Settings getSettingsCheckpointFilePageMemory(int capacity, String folder) {
+        Settings s = new FileSettings(folder);
+        PageIOFactory pageIOFactory = (pageNum, size, path) -> new ByteBufferPageIO(pageNum, size, path);
+        CheckpointIOFactory checkpointIOFactory = (source) -> new FileCheckpointIO(source);
         s.setCapacity(capacity);
         s.setElementIOFactory(pageIOFactory);
         s.setCheckpointIOFactory(checkpointIOFactory);
