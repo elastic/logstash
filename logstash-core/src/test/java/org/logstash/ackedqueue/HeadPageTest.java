dbfed6cad9e965629da724e774ee028d6ec7f5ee
Guy Boertje <guy@elastic.co>
2016-07-23 16:07:54 +0100

    add test for a checkpoint.head without the referenced page files.

--- logstash-core/src/test/java/org/logstash/ackedqueue/HeadPageTest.java
+++ logstash-core/src/test/java/org/logstash/ackedqueue/HeadPageTest.java
@@ -1,17 +1,22 @@
 package org.logstash.ackedqueue;
 
 import org.junit.Test;
-import org.logstash.common.io.*;
+import org.logstash.common.io.ByteBufferPageIO;
+import org.logstash.common.io.FileCheckpointIOTest;
+import org.logstash.common.io.PageIO;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-
 public class HeadPageTest {
-    
+
     @Test
     public void newHeadPage() throws IOException {
         Settings s = TestSettings.getSettings(100);
@@ -88,5 +93,22 @@ public class HeadPageTest {
         assertThat(p.hasSpace(element.serialize().length), is(false));
         assertThat(p.isFullyRead(), is(true));
         assertThat(p.isFullyAcked(), is(false));
+    }
+
+    @Test
+    public void pageViaQueueOpenForHeadCheckpointWithoutSupportingPageFiles() throws Exception {
+        URL url = FileCheckpointIOTest.class.getResource("checkpoint.head");
+        String dirPath = Paths.get(url.toURI()).getParent().toString();
+        Queueable element = new StringElement("foobarbaz", 1);
+        int singleElementCapacity = ByteBufferPageIO.HEADER_SIZE + ByteBufferPageIO.persistedByteCount(element.serialize().length);
+        Settings s = TestSettings.getSettingsCheckpointFilePageMemory(singleElementCapacity, dirPath);
+        TestQueue q = new TestQueue(s);
+        try {
+            q.open();
+        } catch (NoSuchFileException e) {
+            assertThat(e.getMessage(), containsString("checkpoint.2"));
+        }
+        HeadPage p = q.getHeadPage();
+        assertThat(p, is(equalTo(null)));
     }
 }
