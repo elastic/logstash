a7130c783cc4e0c0b507a1708ab555494e490b2a
Andrew Cholakian <andrew@andrewvc.com>
2016-08-16 17:37:54 -0500

    Add new `Codec#encode_sync`, `Codec#multi_encode` and `Output#multi_receive_encoded` methods.
    These methods allow outputs to receive their events pre-encoded for them
    by the pipeline. This is mostly useful in the context of `#shared` outputs, for whom
    encoding a discrete batch in a threadsafe way is not necessarily straightforward.
    
    It would be advised for codecs to prefer `#multi_encode` as the main way of operating
    as the standard `#encode` method is not threadsafe.

--- logstash-core/lib/logstash/codecs/base.rb
+++ logstash-core/lib/logstash/codecs/base.rb
@@ -13,6 +13,7 @@ module LogStash::Codecs; class Base < LogStash::Plugin
     super
     config_init(@params)
     register if respond_to?(:register)
+    setup_multi_encode!
   end
 
   public
@@ -23,9 +24,36 @@ module LogStash::Codecs; class Base < LogStash::Plugin
   alias_method :<<, :decode
 
   public
+  # DEPRECATED: Prefer defining encode_sync or multi_encode
   def encode(event)
-    raise "#{self.class}#encode must be overidden"
+    encoded = multi_encode([event])
+    encoded.each {|event,data| @on_event.call(event,data) }
   end # def encode
+
+  public
+  # Relies on the codec being synchronous (which they all are!)
+  # We need a better long term design here, but this is an improvement
+  # over the current API for shared plugins
+  # It is best if the codec implements this directly
+  def multi_encode(events)
+    if @has_encode_sync              
+      events.map {|event| [event, self.encode_sync(event)]}
+    else
+      batch = Thread.current[:logstash_output_codec_batch] ||= []
+      batch.clear
+      
+      events.each {|event| self.encode(event) }
+      batch
+    end
+  end
+
+  def setup_multi_encode!
+    @has_encode_sync = self.methods.include?(:encode_sync)
+
+    on_event do |event, data|
+      Thread.current[:logstash_output_codec_batch] << [event, data]
+    end
+  end
 
   public
   def close; end;
