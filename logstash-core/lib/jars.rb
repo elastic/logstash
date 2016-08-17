9dadf384005bd146a1ee36bac7dba094a0d39a28
Tal Levy <jubboy333@gmail.com>
2016-08-16 23:18:20 -0700

    update jars version of log4j to 2.6.2

--- logstash-core/lib/jars.rb
+++ logstash-core/lib/jars.rb
@@ -1,6 +1,6 @@
 require 'jar_dependencies'
-require_jar('org.apache.logging.log4j', 'log4j-api', '2.6.1')
-require_jar('org.apache.logging.log4j', 'log4j-core', '2.6.1')
+require_jar('org.apache.logging.log4j', 'log4j-api', '2.6.2')
+require_jar('org.apache.logging.log4j', 'log4j-core', '2.6.2')
 require_jar('com.fasterxml.jackson.core', 'jackson-core', '2.7.4')
 require_jar('com.fasterxml.jackson.core', 'jackson-databind', '2.7.4')
 require_jar('org.logstash', 'logstash-core', '1.0.0-SNAPSHOT')
