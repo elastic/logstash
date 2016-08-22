b5e13421194833973ec5d66896df91616e0719e3
Suyog Rao <suyog.rao@gmail.com>
2016-08-22 17:55:28 +0000

    Mirror 5.0 to master's version

--- logstash-core-event-java/lib/logstash-core-event-java_jars.rb
+++ logstash-core-event-java/lib/logstash-core-event-java_jars.rb
@@ -3,12 +3,12 @@ begin
   require 'jar_dependencies'
 rescue LoadError
   require 'com/fasterxml/jackson/core/jackson-databind/2.7.3/jackson-databind-2.7.3.jar'
-  require 'com/fasterxml/jackson/core/jackson-annotations/2.7.3/jackson-annotations-2.7.3.jar'
+  require 'com/fasterxml/jackson/core/jackson-annotations/2.7.0/jackson-annotations-2.7.0.jar'
   require 'com/fasterxml/jackson/core/jackson-core/2.7.3/jackson-core-2.7.3.jar'
 end
 
 if defined? Jars
   require_jar( 'com.fasterxml.jackson.core', 'jackson-databind', '2.7.3' )
-  require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.7.3' )
+  require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.7.0' )
   require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.7.3' )
 end
