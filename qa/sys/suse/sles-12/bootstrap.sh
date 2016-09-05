f84cda02e30ba517a13dd5cb063551367745a08a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-09-05 18:28:57 +0200

    fix suse installed? command and the suse-12 openjdk8 install procedure
    Fixes #5861

--- qa/sys/suse/sles-12/bootstrap.sh
+++ qa/sys/suse/sles-12/bootstrap.sh
@@ -6,4 +6,6 @@ zypper addrepo -t yast2 http://demeter.uni-regensburg.de/SLES12-x64/DVD2/ dvd2 |
 zypper addrepo http://download.opensuse.org/repositories/Java:Factory/SLE_12/Java:Factory.repo || true
 zypper --no-gpg-checks --non-interactive refresh
 zypper --non-interactive list-updates
-zypper --non-interactive --no-gpg-checks --quiet install --no-recommends java-1_8_0-openjdk-devel
+ln -s /usr/sbin/update-alternatives /usr/sbin/alternatives
+curl -L 'https://edelivery.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.rpm' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0' -H 'Cookie: oraclelicense=accept-securebackup-cookie;' -H 'Connection: keep-alive' --compressed -o oracle_jdk_1.8.rpm
+zypper -q -n --non-interactive install oracle_jdk_1.8.rpm
