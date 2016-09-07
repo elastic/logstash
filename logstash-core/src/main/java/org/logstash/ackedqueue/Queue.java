5766c7ecd91d76d435f804e390192b01b75bc8a4
Colin Surprenant <colin.surprenant@gmail.com>
2016-09-07 17:34:07 -0400

    replace method level synchronization with Lock and Condition, groundwork for bounding queue size

--- logstash-core/src/main/java/org/logstash/ackedqueue/Queue.java
+++ logstash-core/src/main/java/org/logstash/ackedqueue/Queue.java
@@ -11,7 +11,11 @@ import java.lang.reflect.Method;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 // TODO: Notes
@@ -39,10 +43,14 @@ public class Queue implements Closeable {
 
     private final AtomicBoolean closed;
 
-
     // deserialization
     private final Class elementClass;
     private final Method deserializeMethod;
+
+    // thread safety
+    final Lock lock = new ReentrantLock();
+    final Condition notFull  = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
 
     public Queue(Settings settings) {
         this(settings.getDirPath(), settings.getCapacity(), settings.getCheckpointIOFactory().build(settings.getDirPath()), settings.getPageIOFactory(), settings.getElementClass());
@@ -146,59 +154,72 @@ public class Queue implements Closeable {
 
     // @param element the Queueable object to write to the queue
     // @return long written sequence number
-    public synchronized long write(Queueable element) throws IOException {
+    public long write(Queueable element) throws IOException {
         element.setSeqNum(nextSeqNum());
         byte[] data = element.serialize();
 
-        if (! this.headPage.hasCapacity(data.length)) {
-            throw new IOException("data to be written is bigger than page capacity");
-        }
+        lock.lock();
+        try {
+            if (! this.headPage.hasCapacity(data.length)) {
+                throw new IOException("data to be written is bigger than page capacity");
+            }
 
-        boolean wasEmpty = (firstUnreadPage() == null);
+            boolean wasEmpty = (firstUnreadPage() == null);
 
-        // create a new head page if the current does not have suffient space left for data to be written
-        if (! this.headPage.hasSpace(data.length)) {
-            // beheading includes checkpoint+fsync if required
-            BeheadedPage tailPage = this.headPage.behead();
+            // create a new head page if the current does not have suffient space left for data to be written
+            if (! this.headPage.hasSpace(data.length)) {
+                // beheading includes checkpoint+fsync if required
+                BeheadedPage tailPage = this.headPage.behead();
 
-            this.tailPages.add(tailPage);
+                this.tailPages.add(tailPage);
 
-            // create new head page
-            int headPageNum = tailPage.pageNum + 1;
-            PageIO pageIO = this.pageIOFactory.build(headPageNum, this.capacity, this.dirPath);
-            this.headPage = new HeadPage(headPageNum, this, pageIO);
-            this.headPage.checkpoint();
-        }
+                // create new head page
+                int headPageNum = tailPage.pageNum + 1;
+                PageIO pageIO = this.pageIOFactory.build(headPageNum, this.capacity, this.dirPath);
+                this.headPage = new HeadPage(headPageNum, this, pageIO);
+                this.headPage.checkpoint();
+            }
 
-        this.headPage.write(data, element);
-        this.unreadCount++;
+            this.headPage.write(data, element);
+            this.unreadCount++;
 
-        // if the queue was empty before write, notifyAll blocking reachBatch threads that queue is now non-empty
-        if (wasEmpty) {
-            notifyAll();
+            // if the queue was empty before write, signal non emptiness
+            if (wasEmpty) { notEmpty.signal(); }
+        } finally {
+            lock.unlock();
         }
 
         return element.getSeqNum();
     }
 
     // @param seqNum the element sequence number upper bound for which persistence should be garanteed (by fsync'ing)
-    public synchronized void ensurePersistedUpto(long seqNum) throws IOException{
-         this.headPage.ensurePersistedUpto(seqNum);
+    public void ensurePersistedUpto(long seqNum) throws IOException{
+        lock.lock();
+        try {
+             this.headPage.ensurePersistedUpto(seqNum);
+        } finally {
+            lock.unlock();
+        }
     }
 
     // non-blockin queue read
     // @param limit read the next bach of size up to this limit. the returned batch size can be smaller than than the requested limit if fewer elements are available
     // @return Batch the batch containing 1 or more element up to the required limit or null of no elements were available
-    public synchronized Batch nonBlockReadBatch(int limit) throws IOException {
-        Page p = firstUnreadPage();
-        if (p == null) {
-            return null;
-        }
+    public Batch nonBlockReadBatch(int limit) throws IOException {
+        lock.lock();
+        try {
+            Page p = firstUnreadPage();
+            if (p == null) {
+                return null;
+            }
 
-        Batch b = p.readBatch(limit);
-        this.unreadCount -= b.size();
-        return b;
-     }
+            Batch b = p.readBatch(limit);
+            this.unreadCount -= b.size();
+            return b;
+        } finally {
+            lock.unlock();
+        }
+    }
 
 
     // blocking readBatch notes:
@@ -208,51 +229,61 @@ public class Queue implements Closeable {
     // blocking queue read until elements are available for read
     // @param limit read the next bach of size up to this limit. the returned batch size can be smaller than than the requested limit if fewer elements are available
     // @return Batch the batch containing 1 or more element up to the required limit or null if no elements were available
-    public synchronized Batch readBatch(int limit) throws IOException {
+    public Batch readBatch(int limit) throws IOException {
         Page p;
 
-        while ((p = firstUnreadPage()) == null && !isClosed()) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                // TODO: what should we do with an InterruptedException here?
-                throw new RuntimeException("blocking readBatch InterruptedException", e);
+        lock.lock();
+        try {
+            while ((p = firstUnreadPage()) == null && !isClosed()) {
+                try {
+                    notEmpty.await();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    // TODO: what should we do with an InterruptedException here?
+                    throw new RuntimeException("blocking readBatch InterruptedException", e);
+                }
             }
+
+            // need to check for close since it is a condition for exiting the while loop
+            if (isClosed()) { return null; }
+
+            Batch b = p.readBatch(limit);
+            this.unreadCount -= b.size();
+            return b;
+        } finally {
+            lock.unlock();
         }
-
-        // need to check for close since it is a condition for exiting the while loop
-        if (isClosed()) { return null; }
-
-        Batch b = p.readBatch(limit);
-        this.unreadCount -= b.size();
-        return b;
     }
 
     // blocking queue read until elements are available for read or the given timeout is reached.
     // @param limit read the next batch of size up to this limit. the returned batch size can be smaller than than the requested limit if fewer elements are available
     // @param timeout the maximum time to wait in milliseconds
     // @return Batch the batch containing 1 or more element up to the required limit or null if no elements were available
-    public synchronized Batch readBatch(int limit, long timeout) throws IOException {
+    public Batch readBatch(int limit, long timeout) throws IOException {
         Page p;
 
-        // wait only if queue is empty
-        if ((p = firstUnreadPage()) == null) {
-            try {
-                wait(timeout);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                // TODO: what should we do with an InterruptedException here?
-                throw new RuntimeException("timeout blocking readBatch InterruptedException", e);
+        lock.lock();
+        try {
+            // wait only if queue is empty
+            if ((p = firstUnreadPage()) == null) {
+                try {
+                    notEmpty.await(timeout, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    // TODO: what should we do with an InterruptedException here?
+                    throw new RuntimeException("timeout blocking readBatch InterruptedException", e);
+                }
+
+                // if after returnining from wait queue is still empty, or the queue was closed return null
+                if ((p = firstUnreadPage()) == null || isClosed()) { return null; }
             }
 
-            // if after returnining from wait queue is still empty, or the queue was closed return null
-            if ((p = firstUnreadPage()) == null || isClosed()) { return null; }
+            Batch b = p.readBatch(limit);
+            this.unreadCount -= b.size();
+            return b;
+        } finally {
+            lock.unlock();
         }
-
-        Batch b = p.readBatch(limit);
-        this.unreadCount -= b.size();
-        return b;
     }
 
     private static class TailPageResult {
@@ -295,32 +326,37 @@ public class Queue implements Closeable {
         return null;
     }
 
-    public synchronized void ack(List<Long> seqNums) throws IOException {
+    public void ack(List<Long> seqNums) throws IOException {
         // as a first implementation we assume that all batches are created from the same page
         // so we will avoid multi pages acking here for now
 
         // find the page to ack by travesing from oldest tail page
         long firstAckSeqNum = seqNums.get(0);
 
-        // dual search strategy: if few tail pages search linearily otherwise perform binary search
-        TailPageResult result = (this.tailPages.size() > 3) ? binaryFindPageForSeqnum(firstAckSeqNum) : linearFindPageForSeqnum(firstAckSeqNum);
+        lock.lock();
+        try {
+            // dual search strategy: if few tail pages search linearily otherwise perform binary search
+            TailPageResult result = (this.tailPages.size() > 3) ? binaryFindPageForSeqnum(firstAckSeqNum) : linearFindPageForSeqnum(firstAckSeqNum);
 
-        if (result == null) {
-            // if not found then it is in head page
-            assert this.headPage.getMinSeqNum() > 0 && firstAckSeqNum >= this.headPage.getMinSeqNum() && firstAckSeqNum < this.headPage.getMinSeqNum() + this.headPage.getElementCount():
-                    String.format("seqNum=%d is not in head page with minSeqNum=%d", firstAckSeqNum, this.headPage.getMinSeqNum());
-            this.headPage.ack(seqNums);
-        } else {
-            result.page.ack(seqNums);
+            if (result == null) {
+                // if not found then it is in head page
+                assert this.headPage.getMinSeqNum() > 0 && firstAckSeqNum >= this.headPage.getMinSeqNum() && firstAckSeqNum < this.headPage.getMinSeqNum() + this.headPage.getElementCount():
+                        String.format("seqNum=%d is not in head page with minSeqNum=%d", firstAckSeqNum, this.headPage.getMinSeqNum());
+                this.headPage.ack(seqNums);
+            } else {
+                result.page.ack(seqNums);
 
-            // cleanup fully acked tail page
-            if (result.page.isFullyAcked()) {
-                this.tailPages.remove(result.index);
-                this.headPage.checkpoint();
-                result.page.purge();
+                // cleanup fully acked tail page
+                if (result.page.isFullyAcked()) {
+                    this.tailPages.remove(result.index);
+                    this.headPage.checkpoint();
+                    result.page.purge();
+                }
             }
+        } finally {
+            lock.unlock();
         }
-     }
+    }
 
     public CheckpointIO getCheckpointIO() {
         return this.checkpointIO;
@@ -337,21 +373,22 @@ public class Queue implements Closeable {
         }
     }
 
-    public synchronized void close() throws IOException {
+    public void close() throws IOException {
         // TODO: review close strategy and exception handling and resiliency of first closing tail pages if crash in the middle
 
-        // for now the AtomicBoolean close is not necessary since the close() method is synchronized but
-        // this may change and it'll be future proof
         if (closed.getAndSet(true) == false) {
-            // TODO: not sure if we need to do this here since the headpage close will also call ensurePersited
-            ensurePersistedUpto(this.seqNum);
+            lock.lock();
+            try {
+                // TODO: not sure if we need to do this here since the headpage close will also call ensurePersited
+                ensurePersistedUpto(this.seqNum);
 
-            for (BeheadedPage p : this.tailPages) {
-                p.close();
+                for (BeheadedPage p : this.tailPages) { p.close(); }
+                this.headPage.close();
+
+                notEmpty.signalAll();
+            } finally {
+                lock.unlock();
             }
-            this.headPage.close();
-
-            notifyAll();
         }
     }
 
