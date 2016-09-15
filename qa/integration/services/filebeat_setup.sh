54f8cc1bb0ba4a49b8921f1097466b160c4aa89e
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-09-15 18:42:46 -0400

    QA Integration dont ship with certificates (#5928)
    We are removing the pre generated certificate from the repository, the
    certificates needed for the test will be generated before downloading
    filebeat.
    
    Fixes #5917

--- qa/integration/services/filebeat_setup.sh
+++ qa/integration/services/filebeat_setup.sh
@@ -20,6 +20,13 @@ setup_fb() {
     rm $current_dir/filebeat.tar.gz
 }
 
+generate_certificate() {
+    target_directory=$current_dir/../fixtures/certificates
+    mkdir -p $target_directory
+    openssl req -subj '/CN=localhost/' -x509 -days $((100 * 365)) -batch -nodes -newkey rsa:2048 -keyout $target_directory/certificate.key -out $target_directory/certificate.crt
+}
+
 if [ ! -d $current_dir/filebeat ]; then
+    generate_certificate
     setup_fb $version
 fi
