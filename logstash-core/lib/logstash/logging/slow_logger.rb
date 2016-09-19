970864e326a048248be7e1faba4070ad8c877f88
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2016-09-19 08:50:39 +0200

    Few small changes,   * add a settings example for an slow log threshold definition   * add comments for slow log classes   * remove cached elements function as now is only one collector used

--- logstash-core/lib/logstash/logging/slow_logger.rb
+++ logstash-core/lib/logstash/logging/slow_logger.rb
@@ -2,41 +2,38 @@
 require "logstash/namespace"
 require "logstash/logging/slow_logger/freq_items"
 
-module LogStash
-  module Logging
+module LogStash; module Logging
 
-    class NullLogger
-      def log(threshold, time, data={})
-      end
-      alias_method :warn, :log
+  # Slow log null logger, this logger does
+  # nothing when used.
+  class NullLogger
+    def log(threshold, time, data={})
+    end
+    alias_method :warn, :log
+  end
+
+  # Generic slow logger, this class is responsible of
+  # reporting to the specific logger and holding an
+  # aggregated view of it's reported events.
+  class SlowLogger
+
+    attr_reader :logger, :freq_items
+
+    def initialize(name="loggers.slow", params={})
+      @logger     = LogStash::Logging::Logger.new(name)
+      @freq_items = LogStash::Logging::Util::FreqItems.new
     end
 
-    class SlowLogger
+    def log(threshold, time, data={})
+      message          = data.delete(:message)
+      data[:threshold] = threshold
+      data[:time]      = time
 
-      attr_reader :logger, :freq_items
-
-      def initialize(name="loggers.slow", params={})
-        @logger     = LogStash::Logging::Logger.new(name)
-        @freq_items = LogStash::Logging::Util::FreqItems.new
-      end
-
-      def log(threshold, time, data={})
-        message          = data.delete(:message)
-        data[:threshold] = threshold
-        data[:time]      = time
-
-        cache_elements(threshold, time)
-        logger.warn(message, data)
-      end
-      alias_method :warn, :log
-
-      private
-
-      def cache_elements(threshold, time)
-        freq_items.add(threshold, time)
-      end
-
+      freq_items.add(threshold, time)
+      logger.warn(message, data)
     end
+    alias_method :warn, :log
 
   end
-end
+
+end; end
