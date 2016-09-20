057302ad3a9cd9df5033965283c788755892e45b
Suyog Rao <suyog.rao@gmail.com>
2016-09-20 11:19:16 -0700

    Fixes after @ph review
    Fixes #5917

--- qa/integration/services/elasticsearch_teardown.sh
+++ qa/integration/services/elasticsearch_teardown.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 my_dir="$(dirname "$0")"
-source "$my_dir/helpers.sh"
+
+stop_es() {
+    pid=$(cat $current_dir/elasticsearch.pid)
+    [ "x$pid" != "x" ] && [ "$pid" -gt 0 ]
+    kill -SIGTERM $pid
+}
 
 stop_es
