513a38b2eb179f9b458e6ca07793b2ac90bd607a
Suyog Rao <suyog.rao@gmail.com>
2016-09-23 14:25:28 -0700

    Improvements to RATS test suite
    * Reduce sleeps and output messages
    * Cache installed services directory

--- qa/integration/services/kafka_teardown.sh
+++ qa/integration/services/kafka_teardown.sh
@@ -2,16 +2,21 @@
 set -e
 current_dir="$(dirname "$0")"
 
+source "$current_dir/helpers.sh"
+
+KAFKA_HOME=$INSTALL_DIR/kafka
+
 stop_kafka() {
     echo "Stopping Kafka broker"
-    $current_dir/kafka/bin/kafka-server-stop.sh
-    sleep 2
+    $KAFKA_HOME/bin/kafka-server-stop.sh
     echo "Stopping zookeeper"
-    $current_dir/kafka/bin/zookeeper-server-stop.sh
-    sleep 2
+    $KAFKA_HOME/bin/zookeeper-server-stop.sh
 }
 
 # delete test topic
 echo "Deleting test topic in Kafka"
-$current_dir/kafka/bin/kafka-topics.sh --delete --topic logstash_topic_plain --zookeeper localhost:2181 --if-exists
+$KAFKA_HOME/bin/kafka-topics.sh --delete --topic logstash_topic_plain --zookeeper localhost:2181 --if-exists
 stop_kafka
+rm -rf /tmp/ls_integration/kafka-logs
+rm -rf /tmp/zookeeper
+
