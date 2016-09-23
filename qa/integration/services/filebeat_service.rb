4e33f66986a9a8fe432677abb7132648275007c7
Suyog Rao <suyog.rao@gmail.com>
2016-09-23 14:25:28 -0700

    Improvements to RATS test suite
    * Reduce sleeps and output messages
    * Cache installed services directory
    
    Fixes #5968

--- qa/integration/services/filebeat_service.rb
+++ qa/integration/services/filebeat_service.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class FilebeatService < Service
-  FILEBEAT_CMD = [File.join(File.dirname(__FILE__), "filebeat", "filebeat"), "-c"]
+  FILEBEAT_CMD = [File.join(File.dirname(__FILE__), "installed", "filebeat", "filebeat"), "-c"]
 
   class BackgroundProcess
     def initialize(cmd)
--- qa/integration/services/filebeat_setup.sh
+++ qa/integration/services/filebeat_setup.sh
@@ -2,6 +2,8 @@
 set -e
 current_dir="$(dirname "$0")"
 
+source "$current_dir/helpers.sh"
+
 if [ -n "${FILEBEAT_VERSION}" ]; then
   echo "Filebeat version is $FILEBEAT_VERSION"
   version=$FILEBEAT_VERSION
@@ -9,15 +11,17 @@ else
   version=5.0.0-beta1
 fi
 
+FB_HOME=$INSTALL_DIR/filebeat
+
 setup_fb() {
     local version=$1
     platform=`uname -s | tr '[:upper:]' '[:lower:]'`
     architecture=`uname -m | tr '[:upper:]' '[:lower:]'`
     download_url=https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$version-$platform-$architecture.tar.gz
-    curl -sL $download_url > $current_dir/filebeat.tar.gz
-    mkdir $current_dir/filebeat
-    tar -xzf $current_dir/filebeat.tar.gz --strip-components=1 -C $current_dir/filebeat/.
-    rm $current_dir/filebeat.tar.gz
+    curl -sL $download_url > $INSTALL_DIR/filebeat.tar.gz
+    mkdir $FB_HOME
+    tar -xzf $INSTALL_DIR/filebeat.tar.gz --strip-components=1 -C $FB_HOME/.
+    rm $INSTALL_DIR/filebeat.tar.gz
 }
 
 generate_certificate() {
@@ -26,7 +30,9 @@ generate_certificate() {
     openssl req -subj '/CN=localhost/' -x509 -days $((100 * 365)) -batch -nodes -newkey rsa:2048 -keyout $target_directory/certificate.key -out $target_directory/certificate.crt
 }
 
-if [ ! -d $current_dir/filebeat ]; then
+setup_install_dir
+
+if [ ! -d $FB_HOME ]; then
     generate_certificate
     setup_fb $version
 fi
