cbf256cf0dbbaceb7e96f4f9a9433fc4f79f2a32
Guy Boertje <guy@elastic.co>
2016-10-13 17:59:46 +0100

    re-enforce "metric.collect" must be true and fix NullMetric spec

--- logstash-core/lib/logstash/environment.rb
+++ logstash-core/lib/logstash/environment.rb
@@ -23,7 +23,7 @@ module LogStash
            Setting::Boolean.new("config.test_and_exit", false),
            Setting::Boolean.new("config.reload.automatic", false),
            Setting::Numeric.new("config.reload.interval", 3), # in seconds
-           Setting::Boolean.new("metric.collect", true), #{|v| v == true }, # metric collection cannot be disabled
+           Setting::Boolean.new("metric.collect", true) {|v| v == true }, # metric collection cannot be disabled
             Setting::String.new("pipeline.id", "main"),
    Setting::PositiveInteger.new("pipeline.workers", LogStash::Config::CpuCoreStrategy.maximum),
    Setting::PositiveInteger.new("pipeline.output.workers", 1),
