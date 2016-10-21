0bf45a71cea34265a8923d8404b8dfea408e853c
Suyog Rao <suyog.rao@gmail.com>
2016-10-21 14:47:39 -0700

    Debug postgres setup

--- qa/integration/services/postgres_setup.sh
+++ qa/integration/services/postgres_setup.sh
@@ -3,7 +3,7 @@
 # Note: This assumes for now that Postgres DB is setup already, outside of RATS. This test is targeted
 # for now to run on Travis using their postgres services
 
-set -e
+set -ex
 current_dir="$(dirname "$0")"
 POSTGRES_USER=postgres
 
