24292dab101a05be050457cf1e61e335651e5523
Suyog Rao <suyog.rao@gmail.com>
2016-10-21 17:29:11 -0700

    try 4

--- qa/integration/services/postgres_setup.sh
+++ qa/integration/services/postgres_setup.sh
@@ -2,15 +2,7 @@
 
 # Note: This assumes for now that Postgres DB is setup already, outside of RATS. This test is targeted
 # for now to run on Travis using their postgres services
-
 set -e
-current_dir="$(dirname "$0")"
-POSTGRES_USER=postgres
-
-source "$current_dir/helpers.sh"
-
-setup_install_dir
-
 curl -s -o /tmp/postgres-driver.jar "https://jdbc.postgresql.org/download/postgresql-9.4.1211.jar"
 [[ ! -s /tmp/postgres-driver.jar ]]
 psql -c 'create database travis_logstash_db;' -U $POSTGRES_USER
