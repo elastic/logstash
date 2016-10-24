b1c62da57046007bb80c2f2a9ee85b143bed16a0
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-10-24 09:52:32 -0400

    rescope the mock classes
    Fixes #6109

--- logstash-core/spec/support/mocks_classes.rb
+++ logstash-core/spec/support/mocks_classes.rb
@@ -3,49 +3,49 @@ require "logstash/outputs/base"
 require "thread"
 
 module LogStash module Outputs
-class DummyOutput < LogStash::Outputs::Base
-  config_name "dummyoutput"
-  milestone 2
+  class DummyOutput < LogStash::Outputs::Base
+    config_name "dummyoutput"
+    milestone 2
 
-  attr_reader :num_closes, :events
+    attr_reader :num_closes, :events
 
-  def initialize(params={})
-    super
-    @num_closes = 0
-    @events = Queue.new
+    def initialize(params={})
+      super
+      @num_closes = 0
+      @events = Queue.new
+    end
+
+    def register
+    end
+
+    def receive(event)
+      @events << event
+    end
+
+    def close
+      @num_closes = 1
+    end
   end
 
-  def register
+  class DummyOutputWithEventsArray < LogStash::Outputs::Base
+    config_name "dummyoutput2"
+    milestone 2
+
+    attr_reader :events
+
+    def initialize(params={})
+      super
+      @events = []
+    end
+
+    def register
+    end
+
+    def receive(event)
+      @events << event
+    end
+
+    def close
+    end
   end
-
-  def receive(event)
-    @events << event
-  end
-
-  def close
-    @num_closes = 1
-  end
-end
-
-class DummyOutputWithEventsArray < LogStash::Outputs::Base
-  config_name "dummyoutput2"
-  milestone 2
-
-  attr_reader :events
-
-  def initialize(params={})
-    super
-    @events = []
-  end
-
-  def register
-  end
-
-  def receive(event)
-    @events << event
-  end
-
-  def close
-  end
-end
 end end
