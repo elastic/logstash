329b6a4ff195ab512e525dcf9b62c11df86be92e
Tal Levy <jubboy333@gmail.com>
2016-11-10 13:47:08 -0800

    add logstash-core resources to classpath during development
    Fixes #6235.
    
    Fixes #6237

--- logstash-core/lib/logstash-core/logstash-core.rb
+++ logstash-core/lib/logstash-core/logstash-core.rb
@@ -9,10 +9,12 @@ require "logstash-core_jars"
 
 # local dev setup
 classes_dir = File.expand_path("../../../build/classes/main", __FILE__)
+resources_dir = File.expand_path("../../../build/resources/main", __FILE__)
 
-if File.directory?(classes_dir)
+if File.directory?(classes_dir) && File.directory?(resources_dir)
   # if in local dev setup, add target to classpath
   $CLASSPATH << classes_dir unless $CLASSPATH.include?(classes_dir)
+  $CLASSPATH << resources_dir unless $CLASSPATH.include?(resources_dir)
 else
   # otherwise use included jar
   begin
