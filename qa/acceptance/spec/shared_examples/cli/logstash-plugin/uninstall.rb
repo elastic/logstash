d669540123375dce2f20cc99f090d558f687a5b1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-11-29 14:19:26 -0500

    Relax the acceptance test expectation to match only the exception message
    Fixes: #6310
    
    Fixes #6325

--- qa/acceptance/spec/shared_examples/cli/logstash-plugin/uninstall.rb
+++ qa/acceptance/spec/shared_examples/cli/logstash-plugin/uninstall.rb
@@ -16,7 +16,7 @@ shared_examples "logstash uninstall" do |logstash|
     context "when the plugin isn't installed" do
       it "fails to uninstall it" do
         result = logstash.run_command_in_path("bin/logstash-plugin uninstall logstash-filter-qatest")
-        expect(result.stderr).to match(/ERROR: Uninstall Aborted, message: This plugin has not been previously installed, aborting/)
+        expect(result.stderr).to match(/This plugin has not been previously installed/)
       end
     end
 
