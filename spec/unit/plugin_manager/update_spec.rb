4e30c6bfbdc040a5a1bc2a7be389f17a4911af6d
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-12-01 18:30:53 -0500

    remove the expect

--- spec/unit/plugin_manager/update_spec.rb
+++ spec/unit/plugin_manager/update_spec.rb
@@ -10,7 +10,6 @@ describe LogStash::PluginManager::Update do
     expect(cmd).to receive(:find_latest_gem_specs).and_return({})
     allow(cmd).to receive(:warn_local_gems).and_return(nil)
     expect(cmd).to receive(:display_updated_plugins).and_return(nil)
-    expect_any_instance_of(LogStash::Bundler).to receive(:invoke!).with(:clean => true)
   end
 
   it "pass all gem sources to the bundle update command" do
