1c47578961c3cf7c864a66c771a6e13a28a248c0
Suyog Rao <suyog.rao@gmail.com>
2017-01-26 13:12:17 -0800

    test3

--- ci/windows/appveyor_integration_run.ps1
+++ ci/windows/appveyor_integration_run.ps1
@@ -1,4 +1,4 @@
 echo "Running integration tests from qa\integration directory"
 cd qa\integration
 bundle install
-bundle exec rspec specs\01_logstash_bin_smoke_spec.rb
+rspec specs\01_logstash_bin_smoke_spec.rb
