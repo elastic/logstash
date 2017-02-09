05f2600cccbabf9ded7787d8eefaa498111d4320
Suyog Rao <suyog.rao@gmail.com>
2017-02-09 12:00:31 -0800

    Support params to specify output dir
    Fixes #6682

--- ci/ci_docs.sh
+++ ci/ci_docs.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+output_dir=$1
+
+if [[ -z $output_dir ]]; then
+  echo "Docs will be generated in default directory in LS_HOME/build/docs"
+else
+  echo "Docs will be generated in directory $output_dir"
+fi
+
 # Since we are using the system jruby, we need to make sure our jvm process
 # uses at least 1g of memory, If we don't do this we can get OOM issues when
 # installing gems. See https://github.com/elastic/logstash/issues/5179
@@ -12,4 +20,4 @@ rm -rf build/docs/*
 grep -q -F "logstash-docgen" Gemfile || echo 'gem "logstash-docgen", :path => "./tools/logstash-docgen"' >> Gemfile
 rake bootstrap
 rake test:install-core
-rake docs:generate-plugins
+rake docs:generate-plugins[$output_dir]
