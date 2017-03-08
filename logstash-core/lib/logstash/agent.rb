0b8ed01dd1c8dc7c2d8d0ef272f6b4eccadc4b9e
Josh Soref <jsoref@users.noreply.github.com>
2017-03-08 07:04:07 +0000

    spelling: implicitly
    Fixes #6969

--- logstash-core/lib/logstash/agent.rb
+++ logstash-core/lib/logstash/agent.rb
@@ -76,7 +76,7 @@ class LogStash::Agent
   end
 
   def execute
-    @thread = Thread.current # this var is implicilty used by Stud.stop?
+    @thread = Thread.current # this var is implicitly used by Stud.stop?
     logger.debug("starting agent")
 
     start_webserver
