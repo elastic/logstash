7cb078801ca8200bbbc0036f92682d0ea7030ec7
Josh Soref <jsoref@users.noreply.github.com>
2017-03-08 07:20:57 +0000

    spelling: separator

--- logstash-core/lib/logstash/instrument/periodic_poller/cgroup.rb
+++ logstash-core/lib/logstash/instrument/periodic_poller/cgroup.rb
@@ -9,7 +9,7 @@ module LogStash module Instrument module PeriodicPoller
     include LogStash::Util::Loggable
 
     CONTROL_GROUP_RE = Regexp.compile("\\d+:([^:,]+(?:,[^:,]+)?):(/.*)");
-    CONTROLLER_SEPERATOR_RE = ","
+    CONTROLLER_SEPARATOR_RE = ","
 
     PROC_SELF_CGROUP_FILE = Pathname.new("/proc/self/cgroup")
     PROC_CGROUP_CPU_DIR = Pathname.new("/sys/fs/cgroup/cpu")
@@ -37,7 +37,7 @@ module LogStash module Instrument module PeriodicPoller
         read_proc_self_cgroup_lines.each do |line|
           matches = CONTROL_GROUP_RE.match(line)
           # multiples controls, same hierarchy
-          controllers = matches[1].split(CONTROLLER_SEPERATOR_RE)
+          controllers = matches[1].split(CONTROLLER_SEPARATOR_RE)
           controllers.each_with_object(response) { |controller| response[controller] = matches[2] }
         end
 
