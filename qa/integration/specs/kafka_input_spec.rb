3af916287025e9b6702325ec8d8addeec9b3d1cf
Armin <me@obrown.io>
2017-05-12 20:08:29 +0200

    Port of #7093 to 5.x
    * Dockerize Kafka in ITs
    
    Fixes #7096

--- qa/integration/specs/kafka_input_spec.rb
+++ qa/integration/specs/kafka_input_spec.rb
@@ -25,7 +25,7 @@ describe "Test Kafka Input" do
     end
 
     try(num_retries) do
-      count = File.foreach(@fixture.actual_output).inject(0) {|c, line| c+1}
+      count = File.foreach(@fixture.actual_output).inject(0) {|c, _| c+1}
       expect(count).to eq(num_events)
     end
   end
