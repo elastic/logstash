2ac72226d373138081d30f89e8878e3eed613cef
Armin <me@obrown.io>
2017-05-12 20:08:29 +0200

    #7091 Dockerized Kafka IT fixture
    Fixes #7093

--- qa/integration/services/kafka_service.rb
+++ qa/integration/services/kafka_service.rb
@@ -1,7 +1,30 @@
 require_relative "service"
+require "docker"
 
 class KafkaService < Service
   def initialize(settings)
+    @kafka_image = Docker::Image.build_from_dir(File.expand_path("../kafka_dockerized", __FILE__))
+                     .insert_local(
+                       'localPath' => File.join(TestSettings::FIXTURES_DIR, "how_sample.input"),
+                       'outputPath' => '/')
     super("kafka", settings)
+  end
+
+  def setup
+    @kafka_container = Docker::Container.create(:Image => @kafka_image.id,
+                                                :HostConfig => {
+                                                  :PortBindings => {
+                                                    '9092/tcp' => [{ :HostPort => '9092' }],
+                                                    '2181/tcp' => [{ :HostPort => '2181' }]
+                                                  }
+                                                }, :Cmd => ["/bin/bash", "-l", "/run.sh"])
+    @kafka_container.start
+    super()
+  end
+
+  def teardown
+    @kafka_container.kill(:signal => "SIGHUP")
+    @kafka_container.delete(:force => true, :volumes => true)
+    super()
   end
 end
