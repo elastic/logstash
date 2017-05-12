3744880a85b545cbe1a71273da8ce94f2e601d06
Armin <me@obrown.io>
2017-05-12 17:38:30 +0200

    #7081 up Kafka IT version to 0.10.2.1
    Fixes #7090

--- qa/integration/services/kafka_setup.sh
+++ qa/integration/services/kafka_setup.sh
@@ -8,7 +8,7 @@ if [ -n "${KAFKA_VERSION+1}" ]; then
     echo "KAFKA_VERSION is $KAFKA_VERSION"
     version=$KAFKA_VERSION
 else
-    version=0.10.0.1
+    version=0.10.2.1
 fi
 
 KAFKA_HOME=$INSTALL_DIR/kafka
