4163af5bea623bd6fa4586a01a528eb75889fad3
Wainer dos Santos Moschetta <wainersm@gmail.com>
2017-05-15 19:37:06 -0300

    Add test case for FileLockFactory
    This change introduce a test for cross-process/jvm file locking
    using the FileLockFactory class. It uses a client program that
    will lock a file aftwards the test case proceed.
    
    Fixes #6610
    
    Fixes #7117

---
+++ logstash-core/src/test/java/org/logstash/FileLockFactoryMain.java
@@ -0,0 +1,23 @@
+package org.logstash;
+
+import java.io.IOException;
+
+/*
+ * This program is used to test the FileLockFactory in cross-process/JVM.
+ */
+public class FileLockFactoryMain {
+
+    public static void main(String[] args) {
+        try {
+            FileLockFactory.getDefault().obtainLock(args[0], args[1]);
+            System.out.println("File locked");
+            // Sleep enough time until this process is killed.
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            // This process is killed. Do nothing.
+        } catch (IOException e) {
+            // Failed to obtain the lock.
+            System.exit(1);
+        }
+    }
+}
