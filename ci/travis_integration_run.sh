036ad57d8bde9a0cb13e9e81da4902946f8625e4
Andrew Cholakian <andrew@andrewvc.com>
2017-05-22 09:36:38 -0500

    Run offline specs separately from online ones.
    The internet disabling C hack can break other tests, so, we now run
    those tests separately.
    
    Fixes https://github.com/elastic/logstash/issues/7178
    
    Fixes #7179

--- ci/travis_integration_run.sh
+++ ci/travis_integration_run.sh
@@ -7,4 +7,10 @@ fi
 
 echo "Running integration tests from qa/integration directory"
 cd qa/integration
-rspec
+
+# The offline specs can break the online ones
+# due to some sideeffects of the seccomp policy interfering with
+# the docker daemon
+# See prepare_offline_pack_spec.rb for details
+rspec --tag ~offline
+rspec --tag offline
