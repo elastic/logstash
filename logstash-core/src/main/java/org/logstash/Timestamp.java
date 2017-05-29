82a6aea2baf35517d80fb5a335d4b7df7acbc071
Armin <me@obrown.io>
2017-05-29 11:45:33 +0200

    remove question only todo

--- logstash-core/src/main/java/org/logstash/Timestamp.java
+++ logstash-core/src/main/java/org/logstash/Timestamp.java
@@ -18,7 +18,6 @@ public class Timestamp implements Cloneable, Comparable, Queueable {
     // all methods setting the time object must set it in the UTC timezone
     private DateTime time;
 
-    // TODO: is this DateTimeFormatter thread safe?
     private static DateTimeFormatter iso8601Formatter = ISODateTimeFormat.dateTime();
 
     private static final LocalDateTime JAN_1_1970 = new LocalDateTime(1970, 1, 1, 0, 0);
