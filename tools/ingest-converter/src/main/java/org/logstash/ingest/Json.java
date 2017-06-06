6a1cfcf4b929b548322576e336729a0692634458
Suyog Rao <suyog.rao@gmail.com>
2017-06-05 23:25:38 -0700

    Refactor common code to a util function
    Fixes #7331

--- tools/ingest-converter/src/main/java/org/logstash/ingest/Json.java
+++ tools/ingest-converter/src/main/java/org/logstash/ingest/Json.java
@@ -1,12 +1,6 @@
 package org.logstash.ingest;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
 import javax.script.ScriptException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * Ingest JSON processor DSL to Logstash json Transpiler.
@@ -17,16 +11,6 @@ public class Json {
     }
 
     public static void main(final String... args) throws ScriptException, NoSuchMethodException {
-        try {
-            final ScriptEngine engine = JsUtil.engine();
-            Files.write(Paths.get(args[1]), ((String) ((Invocable) engine).invokeFunction(
-                "ingest_json_to_logstash",
-                new String(
-                    Files.readAllBytes(Paths.get(args[0])), StandardCharsets.UTF_8
-                )
-            )).getBytes(StandardCharsets.UTF_8));
-        } catch (final IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+        JsUtil.convert(args, "ingest_json_to_logstash");
     }
 }
