bd4fc216d230aab59ede6c96c6ea57c80b31a62c
Guy Boertje <guy@elastic.co>
2017-06-08 17:41:32 +0100

    MODULES: fixes for bad modules and client logging (#7378)
    adjust the spec for 5.x
    
    change Gemfile.jruby-1.9.lock.release one last time

--- logstash-core/lib/logstash/config/modules_common.rb
+++ logstash-core/lib/logstash/config/modules_common.rb
@@ -13,30 +13,56 @@ module LogStash module Config
       plugin_modules = LogStash::PLUGIN_REGISTRY.plugins_with_type(:modules)
 
       modules_array = settings.get("modules.cli").empty? ? settings.get("modules") : settings.get("modules.cli")
-      logger.debug("Configured modules", :modules_array => modules_array.to_s)
-      module_names = []
+      if modules_array.empty?
+        # no specifed modules
+        return pipelines
+      end
+      logger.debug("Specified modules", :modules_array => modules_array.to_s)
+
       module_names = modules_array.collect {|module_hash| module_hash["name"]}
       if module_names.length > module_names.uniq.length
         duplicate_modules = module_names.group_by(&:to_s).select { |_,v| v.size > 1 }.keys
         raise LogStash::ConfigLoadingError, I18n.t("logstash.modules.configuration.modules-must-be-unique", :duplicate_modules => duplicate_modules)
       end
-      ### Here is where we can force the modules_array to use only [0] for 5.5, and leave
-      ### a warning/error message to that effect.
-      modules_array.each do |module_hash|
-        begin
-          import_engine = LogStash::Modules::Importer.new(LogStash::ElasticsearchClient.build(module_hash))
 
-          current_module = plugin_modules.find { |allmodules| allmodules.module_name == module_hash["name"] }
-          alt_name = "module-#{module_hash["name"]}"
+      available_module_names = plugin_modules.map(&:module_name)
+      specified_and_available_names = module_names & available_module_names
+
+      if (specified_and_available_names).empty?
+        i18n_opts = {:specified_modules => module_names, :available_modules => available_module_names}
+        raise LogStash::ConfigLoadingError, I18n.t("logstash.modules.configuration.modules-unavailable", i18n_opts)
+      end
+
+      specified_and_available_names.each do |module_name|
+        connect_fail_args = {}
+        begin
+          module_hash = modules_array.find {|m| m["name"] == module_name}
+          current_module = plugin_modules.find { |allmodules| allmodules.module_name == module_name }
+
+          alt_name = "module-#{module_name}"
           pipeline_id = alt_name
 
           current_module.with_settings(module_hash)
-          current_module.import(import_engine)
-          config_string = current_module.config_string
+          esclient = LogStash::ElasticsearchClient.build(module_hash)
+          config_test = settings.get("config.test_and_exit")
+          if esclient.can_connect? || config_test
+            if !config_test
+              current_module.import(LogStash::Modules::Importer.new(esclient))
+            end
 
-          pipelines << {"pipeline_id" => pipeline_id, "alt_name" => alt_name, "config_string" => config_string, "settings" => settings}
+            config_string = current_module.config_string
+
+            pipelines << {"pipeline_id" => pipeline_id, "alt_name" => alt_name, "config_string" => config_string, "settings" => settings}
+          else
+            connect_fail_args[:module_name] = module_name
+            connect_fail_args[:hosts] = esclient.host_settings
+          end
         rescue => e
           raise LogStash::ConfigLoadingError, I18n.t("logstash.modules.configuration.parse-failed", :error => e.message)
+        end
+
+        if !connect_fail_args.empty?
+          raise LogStash::ConfigLoadingError, I18n.t("logstash.modules.configuration.elasticsearch_connection_failed", connect_fail_args)
         end
       end
       pipelines
