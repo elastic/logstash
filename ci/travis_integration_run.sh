c2584014b8dc6766a0dfefff9f3868cab3ee363e
Suyog Rao <suyog.rao@gmail.com>
2017-06-14 18:46:40 -0700

    use bin/rspec instead of rspec

--- ci/travis_integration_run.sh
+++ ci/travis_integration_run.sh
@@ -8,5 +8,5 @@ cd qa/integration
 # due to some sideeffects of the seccomp policy interfering with
 # the docker daemon
 # See prepare_offline_pack_spec.rb for details
-rspec --tag ~offline
-rspec --tag offline
+bin/rspec --tag ~offline
+bin/rspec --tag offline
