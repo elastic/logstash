7b57dc3286dcc257c858a16ed747872cccb68a59
Andrew Cholakian <andrew@andrewvc.com>
2017-06-15 10:50:37 -0500

    Properly hash when no original source is present.
    This corrects a mistake where the digest of the string source should be
    used when present, and the hash of the graph should be used when not.

--- logstash-core/src/test/java/org/logstash/config/ir/PipelineIRTest.java
+++ logstash-core/src/test/java/org/logstash/config/ir/PipelineIRTest.java
@@ -1,9 +1,13 @@
 package org.logstash.config.ir;
 
 import org.junit.Test;
+import org.logstash.common.Util;
 import org.logstash.config.ir.graph.Graph;
 
+import java.nio.channels.Pipe;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.logstash.config.ir.DSL.*;
 import static org.logstash.config.ir.PluginDefinition.Type.*;
@@ -12,21 +16,42 @@ import static org.logstash.config.ir.PluginDefinition.Type.*;
  * Created by andrewvc on 9/20/16.
  */
 public class PipelineIRTest {
-    @Test
-    public void testPipelineCreation() throws InvalidIRException {
-        Graph inputSection = iComposeParallel(iPlugin(INPUT, "generator"), iPlugin(INPUT, "stdin")).toGraph();
-        Graph filterSection = iIf(eEq(eEventValue("[foo]"), eEventValue("[bar]")),
+    public Graph makeInputSection() throws InvalidIRException {
+        return iComposeParallel(iPlugin(INPUT, "generator"), iPlugin(INPUT, "stdin")).toGraph();
+    }
+
+    public Graph makeFilterSection() throws InvalidIRException {
+        return iIf(eEq(eEventValue("[foo]"), eEventValue("[bar]")),
                                     iPlugin(FILTER, "grok"),
                                     iPlugin(FILTER, "kv")).toGraph();
-        Graph outputSection = iIf(eGt(eEventValue("[baz]"), eValue(1000)),
+    }
+
+    public Graph makeOutputSection() throws InvalidIRException {
+        return iIf(eGt(eEventValue("[baz]"), eValue(1000)),
                                     iComposeParallel(
                                             iPlugin(OUTPUT, "s3"),
                                             iPlugin(OUTPUT, "elasticsearch")),
                                     iPlugin(OUTPUT, "stdout")).toGraph();
+    }
 
-        PipelineIR pipelineIR = new PipelineIR(inputSection, filterSection, outputSection);
+    @Test
+    public void testPipelineCreation() throws InvalidIRException {
+        PipelineIR pipelineIR = new PipelineIR(makeInputSection(), makeFilterSection(), makeOutputSection());
         assertEquals(2, pipelineIR.getInputPluginVertices().size());
         assertEquals(2, pipelineIR.getFilterPluginVertices().size());
         assertEquals(3, pipelineIR.getOutputPluginVertices().size());
+    }
+
+    @Test
+    public void hashingWithoutOriginalSource() throws InvalidIRException {
+        PipelineIR pipelineIR = new PipelineIR(makeInputSection(), makeFilterSection(), makeOutputSection());
+        assertEquals(pipelineIR.uniqueHash(), pipelineIR.getGraph().uniqueHash());
+    }
+
+    @Test
+    public void hashingWithOriginalSource() throws InvalidIRException {
+        String source = "mysource";
+        PipelineIR pipelineIR = new PipelineIR(makeInputSection(), makeFilterSection(), makeOutputSection(), source);
+        assertEquals(pipelineIR.uniqueHash(), Util.digest(source));
     }
 }
