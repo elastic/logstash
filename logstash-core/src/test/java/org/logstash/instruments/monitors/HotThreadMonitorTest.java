8c92340a9ced9b8387aa361f7ba19c876c4e4311
Armin <me@obrown.io>
2017-07-07 13:59:48 +0200

    MINOR: Clean up HotThreadsMonitor

--- logstash-core/src/test/java/org/logstash/instruments/monitors/HotThreadMonitorTest.java
+++ logstash-core/src/test/java/org/logstash/instruments/monitors/HotThreadMonitorTest.java
@@ -16,32 +16,32 @@ public class HotThreadMonitorTest {
 
     @Test
     public void testThreadsReportsGenerated(){
-        assertThat(new HotThreadsMonitor().detect().size() > 0, is(true));
+        assertThat(HotThreadsMonitor.detect().size() > 0, is(true));
     }
 
     @Test
     public void testAllThreadsHaveCpuTime(){
-        new HotThreadsMonitor().detect().forEach(x -> assertThat(x.toMap().keySet(), hasItem("cpu.time")));
+        HotThreadsMonitor.detect().forEach(x -> assertThat(x.toMap().keySet(), hasItem("cpu.time")));
     }
 
     @Test
     public void testAllThreadsHaveThreadState(){
-        new HotThreadsMonitor().detect().forEach(x -> assertThat(x.toMap().keySet(), hasItem("thread.state")));
+        HotThreadsMonitor.detect().forEach(x -> assertThat(x.toMap().keySet(), hasItem("thread.state")));
     }
 
     @Test
     public void testAllThreadsHaveBlockedInformation(){
-        new HotThreadsMonitor().detect().forEach(x -> assertThat(x.toMap().keySet(), hasItems("blocked.count", "blocked.time")));
+        HotThreadsMonitor.detect().forEach(x -> assertThat(x.toMap().keySet(), hasItems("blocked.count", "blocked.time")));
     }
 
     @Test
     public void testAllThreadsHaveWaitedInformation(){
-        new HotThreadsMonitor().detect().forEach(x -> assertThat(x.toMap().keySet(), hasItems("waited.count", "waited.time")));
+        HotThreadsMonitor.detect().forEach(x -> assertThat(x.toMap().keySet(), hasItems("waited.count", "waited.time")));
     }
 
     @Test
     public void testAllThreadsHaveStackTraces(){
-        new HotThreadsMonitor().detect().forEach(tr -> assertThat(tr.toMap().keySet(), hasItem("thread.stacktrace")));
+        HotThreadsMonitor.detect().forEach(tr -> assertThat(tr.toMap().keySet(), hasItem("thread.stacktrace")));
     }
 
     @Test
@@ -49,7 +49,7 @@ public class HotThreadMonitorTest {
         final String testStackSize = "4";
         Map<String, String> options = new HashMap<>();
         options.put("stacktrace_size", testStackSize);
-        new HotThreadsMonitor().detect(options).stream().filter(tr -> !tr.getThreadName().equals("Signal Dispatcher") &&
+        HotThreadsMonitor.detect(options).stream().filter(tr -> !tr.getThreadName().equals("Signal Dispatcher") &&
                                                                       !tr.getThreadName().equals("Reference Handler"))
                                                         .forEach(tr -> {
             List stackTrace = (List)tr.toMap().get("thread.stacktrace");
@@ -63,7 +63,7 @@ public class HotThreadMonitorTest {
         options.put("ordered_by", "cpu");
         // Using single element array to circumvent lambda expectation of 'effective final'
         final long[] lastCpuTime = {Long.MAX_VALUE};
-        new HotThreadsMonitor().detect(options).forEach(tr -> {
+        HotThreadsMonitor.detect(options).forEach(tr -> {
             Long cpuTime = (Long)tr.toMap().get("cpu.time");
             assertThat(lastCpuTime[0] >= cpuTime, is(true));
             lastCpuTime[0] = cpuTime;
@@ -76,7 +76,7 @@ public class HotThreadMonitorTest {
         options.put("ordered_by", "wait");
         // Using single element array to circumvent lambda expectation of 'effectively final'
         final long[] lastWaitTime = {Long.MAX_VALUE};
-        new HotThreadsMonitor().detect(options).forEach(tr -> {
+        HotThreadsMonitor.detect(options).forEach(tr -> {
             Long waitTime = (Long)tr.toMap().get("waited.time");
             assertThat(lastWaitTime[0] >= waitTime, is(true));
             lastWaitTime[0] = waitTime;
@@ -89,7 +89,7 @@ public class HotThreadMonitorTest {
         options.put("ordered_by", "block");
         // Using single element array to circumvent lambda expectation of 'effectively final'
         final long[] lastBlockedTime = {Long.MAX_VALUE};
-        new HotThreadsMonitor().detect(options).forEach(tr -> {
+        HotThreadsMonitor.detect(options).forEach(tr -> {
             Long blockedTime = (Long)tr.toMap().get("blocked.time");
             assertThat(lastBlockedTime[0] >= blockedTime, is(true));
             lastBlockedTime[0] = blockedTime;
