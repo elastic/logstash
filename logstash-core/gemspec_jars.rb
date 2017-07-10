47ca82f1333aec154f5cc92ea4a0995421bd9cc7
Jake Landis <jake.landis@elastic.co>
2017-07-10 18:43:59 -0500

    Fix ominous warning: jar coordinate ... already loaded
    This change to ensure that Java and JRuby agree upon the version of Jackson. This downgrades Java from 2.7.4 to 2.7.3 (which is the JRuby version).
    Fixes: #6781

--- logstash-core/gemspec_jars.rb
+++ logstash-core/gemspec_jars.rb
@@ -2,9 +2,11 @@
 # runtime dependencies to generate this gemspec dependencies file to be eval'ed by the gemspec
 # for the jar-dependencies requirements.
 
+gem.requirements << "jar org.apache.logging.log4j:log4j-slf4j-impl, 2.6.2"
 gem.requirements << "jar org.apache.logging.log4j:log4j-api, 2.6.2"
 gem.requirements << "jar org.apache.logging.log4j:log4j-core, 2.6.2"
-gem.requirements << "jar com.fasterxml.jackson.core:jackson-core, 2.7.4"
-gem.requirements << "jar com.fasterxml.jackson.core:jackson-databind, 2.7.4"
-gem.requirements << "jar com.fasterxml.jackson.module:jackson-module-afterburner, 2.7.4"
-gem.requirements << "jar com.fasterxml.jackson.dataformat:jackson-dataformat-cbor, 2.7.4"
+gem.requirements << "jar com.fasterxml.jackson.core:jackson-core, 2.7.3"
+gem.requirements << "jar com.fasterxml.jackson.core:jackson-databind, 2.7.3"
+gem.requirements << "jar com.fasterxml.jackson.core:jackson-annotations, 2.7.3"
+gem.requirements << "jar com.fasterxml.jackson.module:jackson-module-afterburner, 2.7.3"
+gem.requirements << "jar com.fasterxml.jackson.dataformat:jackson-dataformat-cbor, 2.7.3"
