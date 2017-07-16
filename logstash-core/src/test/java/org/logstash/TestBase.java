e488e15cff3a114023216b286c707e8aab2d5ae8
Armin <me@obrown.io>
2017-07-16 20:04:02 +0200

    #7707 backport to 5.x

--- logstash-core/src/test/java/org/logstash/TestBase.java
+++ logstash-core/src/test/java/org/logstash/TestBase.java
@@ -1,23 +1,19 @@
 package org.logstash;
 
-import org.logstash.ext.JrubyTimestampExtLibrary;
 import org.jruby.CompatVersion;
 import org.jruby.Ruby;
-import org.jruby.RubyInstanceConfig;
 import org.jruby.ext.bigdecimal.RubyBigDecimal;
 import org.junit.Before;
+import org.logstash.ext.JrubyTimestampExtLibrary;
 
 public abstract class TestBase {
     private static boolean setupDone = false;
-    public static Ruby ruby;
+    public static final Ruby ruby = Ruby.getGlobalRuntime();
 
     @Before
     public void setUp() throws Exception {
         if (setupDone) return;
-
-        RubyInstanceConfig config_19 = new RubyInstanceConfig();
-        config_19.setCompatVersion(CompatVersion.RUBY1_9);
-        ruby = Ruby.newInstance(config_19);
+        ruby.getInstanceConfig().setCompatVersion(CompatVersion.RUBY1_9);
         RubyBigDecimal.createBigDecimal(ruby); // we need to do 'require "bigdecimal"'
         JrubyTimestampExtLibrary.createTimestamp(ruby);
         setupDone = true;
