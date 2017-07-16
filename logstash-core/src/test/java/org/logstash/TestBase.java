6af100d2916cdfa716664319c640ab9e64a45545
Armin <me@obrown.io>
2017-07-16 20:04:02 +0200

    MINOR: Turn NullBiValue into a Singleton

--- logstash-core/src/test/java/org/logstash/TestBase.java
+++ logstash-core/src/test/java/org/logstash/TestBase.java
@@ -1,22 +1,17 @@
 package org.logstash;
 
-import org.logstash.ext.JrubyTimestampExtLibrary;
-import org.jruby.CompatVersion;
 import org.jruby.Ruby;
-import org.jruby.RubyInstanceConfig;
 import org.jruby.ext.bigdecimal.RubyBigDecimal;
 import org.junit.Before;
+import org.logstash.ext.JrubyTimestampExtLibrary;
 
 public abstract class TestBase {
     private static boolean setupDone = false;
-    public static Ruby ruby;
+    public static final Ruby ruby = Ruby.getGlobalRuntime();
 
     @Before
     public void setUp() throws Exception {
         if (setupDone) return;
-
-        RubyInstanceConfig config = new RubyInstanceConfig();
-        ruby = Ruby.newInstance(config);
         RubyBigDecimal.createBigDecimal(ruby); // we need to do 'require "bigdecimal"'
         JrubyTimestampExtLibrary.createTimestamp(ruby);
         setupDone = true;
