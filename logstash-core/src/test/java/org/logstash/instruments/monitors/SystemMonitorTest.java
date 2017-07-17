9b9d20c9dfb0c5eb143db54b80d1b0712d57dc75
Jake Landis <jake.landis@elastic.co>
2017-07-17 14:58:34 -0500

    Fix gradle tests on Windows
    Addresses #7637

--- logstash-core/src/test/java/org/logstash/instruments/monitors/SystemMonitorTest.java
+++ logstash-core/src/test/java/org/logstash/instruments/monitors/SystemMonitorTest.java
@@ -14,10 +14,12 @@ public class SystemMonitorTest {
     @Test
     public void systemMonitorTest(){
         Map<String, Object> map = SystemMonitor.detect().toMap();
-        assertThat("system.load_average is missing", (Double)map.get("system.load_average") > 0, is(true));
+        assertThat("os.name is missing", map.get("os.name"), allOf(notNullValue(), instanceOf(String.class)));
+        if (!((String) map.get("os.name")).startsWith("Windows")) {
+            assertThat("system.load_average is missing", (Double) map.get("system.load_average") > 0, is(true));
+        }
         assertThat("system.available_processors is missing ", ((Integer)map.get("system.available_processors")) > 0, is(true));
         assertThat("os.version is missing", map.get("os.version"), allOf(notNullValue(), instanceOf(String.class)));
         assertThat("os.arch is missing", map.get("os.arch"), allOf(notNullValue(), instanceOf(String.class)));
-        assertThat("os.name is missing", map.get("os.name"), allOf(notNullValue(), instanceOf(String.class)));
     }
 }
