405b5f3cbd3232fb46a75f8b694fd9712b0a9f71
Rob Bavey <rob.bavey@elastic.co>
2017-07-24 12:44:43 -0400

    Fix seek by Timestamp bugs
    Fixes 2 seekByTimestamp failure scenarios that occur when a segment is closed with entries smaller than BLOCK_SIZE
    
    If the next segment is > BLOCK_SIZE, then the seek will advance to the next segment. (Skipping past all events
     in the closed segment)
    If the next segment < BLOCK_SIZE, then the seek will advance to the start of the small segment. (Processing
     entries prior to the timestamp)
    
    Fixes #7789

--- logstash-core/src/main/java/org/logstash/common/io/RecordIOReader.java
+++ logstash-core/src/main/java/org/logstash/common/io/RecordIOReader.java
@@ -79,11 +79,7 @@ public final class RecordIOReader implements Closeable {
         int matchingBlock = UNSET;
         int lowBlock = 0;
         int highBlock = (int) (channel.size() - VERSION_SIZE) / BLOCK_SIZE;
-
-        if (highBlock == 0) {
-            return null;
-        }
-
+        
         while (lowBlock < highBlock) {
             int middle = (int) Math.ceil((highBlock + lowBlock) / 2.0);
             seekToBlock(middle);
