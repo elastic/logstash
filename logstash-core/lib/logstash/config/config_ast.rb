307d25676ececb5aabd59e296c48c302574585d4
Armin <me@obrown.io>
2017-08-08 18:12:02 +0200

    Revert "PERFORMANCE: Generate more optimal filter_func"
    This reverts commit 99a21bc5c109d9048ef4b64c0fe416767bfe2542.

--- logstash-core/lib/logstash/config/config_ast.rb
+++ logstash-core/lib/logstash/config/config_ast.rb
@@ -43,7 +43,7 @@ module LogStash; module Config; module AST
     end
 
 
-    def compile(debug_logging)
+    def compile
       LogStash::Config::AST.deferred_conditionals = []
       LogStash::Config::AST.deferred_conditionals_index = 0
       LogStash::Config::AST.plugin_instance_index = 0
@@ -60,7 +60,7 @@ module LogStash; module Config; module AST
 
       sections = recursive_select(LogStash::Config::AST::PluginSection)
       sections.each do |s|
-        code << s.compile_initializer(debug_logging)
+        code << s.compile_initializer
       end
 
       # start inputs
@@ -75,12 +75,12 @@ module LogStash; module Config; module AST
         definitions << "define_singleton_method :#{type}_func do |event|"
         definitions << "  targeted_outputs = []" if type == "output"
         definitions << "  events = [event]" if type == "filter"
-        if debug_logging
-          definitions << "  @logger.debug(\"#{type} received\", \"event\" => event.to_hash)"
+        definitions << "  @logger.debug? && @logger.debug(\"#{type} received\", \"event\" => event.to_hash)"
+
+        sections.select { |s| s.plugin_type.text_value == type }.each do |s|
+          definitions << s.compile.split("\n", -1).map { |e| "  #{e}" }
         end
-        sections.select {|s| s.plugin_type.text_value == type}.each do |s|
-          definitions << s.compile.split("\n", -1).map {|e| "  #{e}"}
-        end
+
         definitions << "  events" if type == "filter"
         definitions << "  targeted_outputs" if type == "output"
         definitions << "end"
@@ -104,7 +104,7 @@ module LogStash; module Config; module AST
     end
 
     # Generate ruby code to initialize all the plugins.
-    def compile_initializer(debug_logging)
+    def compile_initializer
       generate_variables
       code = []
       @variables.each do |plugin, name|
@@ -120,22 +120,14 @@ module LogStash; module Config; module AST
 
           code << <<-CODE
             @generated_objects[:#{name}_flush] = lambda do |options, &block|
-          CODE
-          if debug_logging
-            code << <<-CODE
-              @logger.debug(\"Flushing\", :plugin => @generated_objects[:#{name}])
-            CODE
-          end
-          code << <<-CODE
+              @logger.debug? && @logger.debug(\"Flushing\", :plugin => @generated_objects[:#{name}])
+
               events = @generated_objects[:#{name}].flush(options)
+
               return if events.nil? || events.empty?
-          CODE
-          if debug_logging
-            code << <<-CODE
-              @logger.debug(\"Flushing\", :plugin => @generated_objects[:#{name}], :events => events.map { |x| x.to_hash  })
-            CODE
-          end
-          code << <<-CODE
+
+              @logger.debug? && @logger.debug(\"Flushing\", :plugin => @generated_objects[:#{name}], :events => events.map { |x| x.to_hash  })
+
               #{plugin.compile_starting_here.gsub(/^/, "  ")}
 
               events.each{|e| block.call(e)}
