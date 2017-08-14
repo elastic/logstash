529639bef997bb73a406c99ebe8050033de30ca0
Armin <me@obrown.io>
2017-08-14 21:30:43 +0200

    bck

--- logstash-core/src/main/java/org/logstash/config/ir/compiler/Stage.java
+++ logstash-core/src/main/java/org/logstash/config/ir/compiler/Stage.java
@@ -1,5 +1,9 @@
 package org.logstash.config.ir.compiler;
 
+import java.util.Collection;
+import org.logstash.config.ir.PipelineIR;
+import org.logstash.ext.JrubyEventExtLibrary;
+
 public interface Stage {
 
     void flushAndExecute(RubyIntegration.Batch batch);
@@ -7,6 +11,17 @@ public interface Stage {
     void execute(RubyIntegration.Batch batch);
 
     final class Compiler {
+
+        public static Stage compileFull(final PipelineIR java,
+            final RubyIntegration.Pipeline ruby) {
+            return new Stage.Compiler.FullStage();
+        }
+
+        public static Stage compileFilter(final PipelineIR java,
+            final RubyIntegration.Pipeline ruby,
+            final Collection<JrubyEventExtLibrary.RubyEvent> buffer) {
+            return new Stage.Compiler.FilterStage();
+        }
 
         private static final class FullStage implements Stage {
 
