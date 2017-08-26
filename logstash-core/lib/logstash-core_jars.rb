e085f072a44ee782b8e005164e40c895991754a5
Armin <me@obrown.io>
2017-08-26 08:11:24 +0200

    PERFORMANCE: Move Memory Queue Drain Loop to Java fully

--- logstash-core/lib/logstash-core_jars.rb
+++ logstash-core/lib/logstash-core_jars.rb
@@ -8,7 +8,6 @@ rescue LoadError
   require 'org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar'
   require 'com/fasterxml/jackson/core/jackson-annotations/2.7.3/jackson-annotations-2.7.3.jar'
   require 'org/apache/logging/log4j/log4j-slf4j-impl/2.6.2/log4j-slf4j-impl-2.6.2.jar'
-  require 'com/fasterxml/jackson/module/jackson-module-afterburner/2.7.3/jackson-module-afterburner-2.7.3.jar'
   require 'com/fasterxml/jackson/dataformat/jackson-dataformat-cbor/2.7.3/jackson-dataformat-cbor-2.7.3.jar'
   require 'com/fasterxml/jackson/core/jackson-core/2.7.3/jackson-core-2.7.3.jar'
 end
@@ -20,7 +19,6 @@ if defined? Jars
   require_jar( 'org.slf4j', 'slf4j-api', '1.7.21' )
   require_jar( 'com.fasterxml.jackson.core', 'jackson-annotations', '2.7.3' )
   require_jar( 'org.apache.logging.log4j', 'log4j-slf4j-impl', '2.6.2' )
-  require_jar( 'com.fasterxml.jackson.module', 'jackson-module-afterburner', '2.7.3' )
   require_jar( 'com.fasterxml.jackson.dataformat', 'jackson-dataformat-cbor', '2.7.3' )
   require_jar( 'com.fasterxml.jackson.core', 'jackson-core', '2.7.3' )
 end
