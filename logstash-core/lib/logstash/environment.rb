c734513f6e1b5222287a4e09582c24643a83cc84
Jake Landis <jake.landis@elastic.co>
2017-08-28 15:27:18 -0500

    Remove Metrics Namespace API : Part 1 - DO NOT MERGE (#8090)
    This is part of a much much larger commit set kept small for easier review.
    
    The tests are not expected to pass, and will fixup the commit log once all parts are reviewed.
    
    Changes in this commit are:
    * Removal of Metrics namespace and related Ruby classes.
    * Changes neccessary for the agent.rb class
    * Removal of un-dcoumented optional 'turn off metrics' flag
    
    Part of #7788

--- logstash-core/lib/logstash/environment.rb
+++ logstash-core/lib/logstash/environment.rb
@@ -32,7 +32,6 @@ module LogStash
            Setting::Boolean.new("config.reload.automatic", false),
            Setting::TimeValue.new("config.reload.interval", "3s"), # in seconds
            Setting::Boolean.new("config.support_escapes", false),
-           Setting::Boolean.new("metric.collect", true),
             Setting::String.new("pipeline.id", "main"),
            Setting::Boolean.new("pipeline.system", false),
    Setting::PositiveInteger.new("pipeline.workers", LogStash::Config::CpuCoreStrategy.maximum),
