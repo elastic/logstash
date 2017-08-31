da6fba91f25def37a3f5a57792589faaaddbff66
Guy Boertje <guy@elastic.co>
2017-08-31 19:30:17 +0100

    Really fix verbose logging in tests

--- logstash-core/spec/logstash/runner_spec.rb
+++ logstash-core/spec/logstash/runner_spec.rb
@@ -35,16 +35,12 @@ describe LogStash::Runner do
     allow(LogStash::Logging::Logger).to receive(:configure_logging) do |level, path|
       allow(logger).to receive(:level).and_return(level.to_sym)
     end
-
+    allow(LogStash::Logging::Logger).to receive(:reconfigure).with(any_args)
     # Make sure we don't start a real pipeline here.
     # because we cannot easily close the pipeline
     allow(LogStash::Agent).to receive(:new).with(any_args).and_return(agent)
     allow(agent).to receive(:execute)
     allow(agent).to receive(:shutdown)
-  end
-
-  after :each do
-    LogStash::Logging::Logger::configure_logging("info")
   end
 
   describe "argument precedence" do
