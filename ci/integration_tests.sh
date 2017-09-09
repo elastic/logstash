c21f54d5e35f5b064f63782462f65f1bf802c818
Jake Landis <jake.landis@elastic.co>
2017-09-08 20:56:21 -0500

    Docker - split docker build in two phases and simplify the scripts

---
+++ ci/integration_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ie
+
+# Since we are using the system jruby, we need to make sure our jvm process
+# uses at least 1g of memory, If we don't do this we can get OOM issues when
+# installing gems. See https://github.com/elastic/logstash/issues/5179
+export JRUBY_OPTS="-J-Xmx1g"
+
+export SPEC_OPTS="--order rand --format documentation"
+
+if [[ ! -d "build" ]]; then
+  mkdir build
+fi
+rm -rf build/*
+echo "Building tar"
+rake artifact:tar
+cd build
+tar xf *.tar.gz
+
+cd ../qa/integration
+echo "Installing test dependencies"
+bundle install
+
+#exit early if only setting up
+[[ $1 = "setup" ]] && exit 0
+
+echo "Running integration tests $@"
+rspec --tag ~offline $@
+rspec --tag offline $@
--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -1,17 +1,11 @@
-#!/usr/bin/env bash
-set -e
-
-##
-# Keep in mind to run ci/ci_setup.sh if you need to setup/clean up your environment before
-# running the test suites here.
-##
+#!/bin/bash -ie
 
 # Since we are using the system jruby, we need to make sure our jvm process
 # uses at least 1g of memory, If we don't do this we can get OOM issues when
 # installing gems. See https://github.com/elastic/logstash/issues/5179
-#if [[ "$JRUBY_OPTS" != *Xmx* ]]; then
-  export JRUBY_OPTS="-J-Xmx1g"
-#fi
+export JRUBY_OPTS="-J-Xmx1g"
+
+export SPEC_OPTS="--order rand --format documentation"
 
 SELECTED_TEST_SUITE=$1
 
@@ -24,7 +18,8 @@ elif [[ $SELECTED_TEST_SUITE == $"all" ]]; then
   rake test:install-all     # Install all plugins in this logstash instance, including development dependencies
   rake test:plugins         # Run all plugins tests
 else
-  echo "Running core tests"
+  echo "Running test:install-core"
   rake test:install-core    # Install core dependencies for testing.
+  echo "Running test:core"
   rake test:core            # Run core tests
 fi
