582501bfad25d4d9fc54c4f87ae9aa9618be35a5
Jake Landis <jake.landis@elastic.co>
2017-09-09 10:44:52 -0500

    [wip] Clean up builds and add docker prune

--- ci/docker_integration_tests.sh
+++ ci/docker_integration_tests.sh
@@ -15,7 +15,8 @@ docker build  -t $IMAGE_NAME .
 exit_code=$?; [[ $exit_code != 0 ]] && exit $exit_code
 docker run -t --rm $IMAGE_NAME ci/integration_tests.sh
 exit_code=$?
-[[ $IMAGE_NAME = "logstash-integration-tests" ]] && docker rmi $IMAGE_NAME
+[[ $IMAGE_NAME != "logstash-integration-tests" ]] && docker rmi $IMAGE_NAME
+echo "exiting with code: '$exit_code'"
 exit $exit_code #preserve the exit code from the test run
 
 #Note - ensure that the -e flag is NOT set, and explicitly check the $? status to allow for clean up
