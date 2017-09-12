f4f906437389d4c54c378c97d7350bd98f7de442
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-09-12 10:58:56 -0400

    Use standard name for Gemfile's lockfile
    In previous iteration of logstash < 1.5 we were trying as much as
    possible to make the code compatible with different ruby
    implementations. This features required use to create a lock specific
    for each Ruby version, with the migration to Java code we are only
    targetting JRuby implementation.
    
    Reference: #8207

--- qa/integration/services/logstash_service.rb
+++ qa/integration/services/logstash_service.rb
@@ -205,7 +205,7 @@ class LogstashService < Service
   end
 
   def lock_file
-    File.join(@logstash_home, "Gemfile.jruby-2.3.lock")
+    File.join(@logstash_home, "Gemfile.lock")
   end
 
   class PluginCli
