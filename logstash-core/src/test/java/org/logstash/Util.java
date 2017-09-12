7275bd2111ba93994e8e4b5bea107799810afd37
Jordan Sissel <jls@semicomplete.com>
2017-09-12 15:21:42 -0700

    Javadocs

--- logstash-core/src/test/java/org/logstash/Util.java
+++ logstash-core/src/test/java/org/logstash/Util.java
@@ -3,6 +3,8 @@ package org.logstash;
 import java.util.Random;
 
 public class Util {
+    /**
+     * A random instance to share in the test suite.
+     */
     public static final Random random = new Random();
-
 }
