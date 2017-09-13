a7d094e91c7f4effd374b059b6b0c31386992ad5
Jordan Sissel <jls@semicomplete.com>
2017-09-12 22:11:54 -0700

    Clean up some unused/old code.
    Add 'greeting' optional setting.

--- logstash-core/src/test/java/org/logstash/plugin/example/ExampleInput.java
+++ logstash-core/src/test/java/org/logstash/plugin/example/ExampleInput.java
@@ -5,7 +5,6 @@ import org.logstash.plugin.ConstructingObjectParser;
 import org.logstash.plugin.Input;
 import org.logstash.plugin.Plugin;
 
-import javax.net.ssl.SSLContext;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.function.Consumer;
@@ -16,24 +15,27 @@ public class ExampleInput implements Input, Plugin {
     static {
         // Pass an integer named "port" as the first constructor argument.
         EXAMPLE.integer("port");
+        EXAMPLE.string("setting", ExampleInput::setGreeting);
     }
-
-    private SSLContext context;
 
     private int port;
 
-    void setPort(int port) {
+    // A setting with a default value.
+    private String greeting = "Hello";
+
+    private ExampleInput(int port) {
         this.port = port;
     }
 
-    public ExampleInput(int port) {
-        this.port = port;
+    private void setGreeting(String greeting) {
+        this.greeting = greeting;
     }
 
     @Override
     public void run(Consumer<Collection<Event>> consumer) {
         Event event = new Event();
         event.setField("message", "Hello from Example");
+        event.setField("port", port);
         Collection<Event> events = Collections.singleton(event);
         consumer.accept(events);
     }
