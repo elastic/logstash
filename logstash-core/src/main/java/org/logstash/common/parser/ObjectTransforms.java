6119ae73d1b5414b688c9063c4a20f27e9c86f1d
Jordan Sissel <jls@semicomplete.com>
2017-09-15 15:36:25 -0700

    Make ObjectTransforms public

--- logstash-core/src/main/java/org/logstash/common/parser/ObjectTransforms.java
+++ logstash-core/src/main/java/org/logstash/common/parser/ObjectTransforms.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-class ObjectTransforms {
+public class ObjectTransforms {
     /**
      * A function which takes an Object and returns an Integer
      *
