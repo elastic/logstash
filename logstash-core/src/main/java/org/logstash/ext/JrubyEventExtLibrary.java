9baf5b1f241535ac397f290884e48caecec9ea07
liketic <ke.like@outlook.com>
2017-09-19 21:57:45 +0800

    Allow pass a default value for Event.getField
    Supersedes: https://github.com/elastic/logstash/pull/8315

--- logstash-core/src/main/java/org/logstash/ext/JrubyEventExtLibrary.java
+++ logstash-core/src/main/java/org/logstash/ext/JrubyEventExtLibrary.java
@@ -82,6 +82,16 @@ public final class JrubyEventExtLibrary {
             );
         }
 
+        @JRubyMethod(name = "get", required = 2)
+        public IRubyObject ruby_get_field(final ThreadContext context,
+                                          final RubyString reference,
+                                          final IRubyObject defaultValue) {
+            return Rubyfier.deep(
+                    context.runtime,
+                    this.event.getField(reference.asJavaString(), defaultValue)
+            );
+        }
+
         @JRubyMethod(name = "set", required = 2)
         public IRubyObject ruby_set_field(ThreadContext context, RubyString reference, IRubyObject value)
         {
