6ef8427eb2bf3e348d100a0dc46300812db2b46b
Armin <me@obrown.io>
2017-09-20 19:00:52 +0200

    #8341 raise tolerance in timestamp_spec.rb tests
    Fixes #8342
    
    Fixes #8346

--- logstash-core/spec/logstash/timestamp_spec.rb
+++ logstash-core/spec/logstash/timestamp_spec.rb
@@ -7,10 +7,10 @@ describe LogStash::Timestamp do
   context "constructors" do
     it "should work" do
       t = LogStash::Timestamp.new
-      expect(t.time.to_i).to be_within(1).of Time.now.to_i
+      expect(t.time.to_i).to be_within(2).of Time.now.to_i
 
       t = LogStash::Timestamp.now
-      expect(t.time.to_i).to be_within(1).of Time.now.to_i
+      expect(t.time.to_i).to be_within(2).of Time.now.to_i
 
       now = Time.now.utc
       t = LogStash::Timestamp.new(now)
