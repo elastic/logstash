4e0ff4e9e9dbbbdeac5f309df8266f27464d4331
Guy Boertje <guy@elastic.co>
2017-09-22 15:58:12 +0100

    bump JrJackson/Jackson versions, source all version refs from versions.yml
    final (I hope) fixes for consolidated versioning.
    
    After testing with rake artifact:zip, need to try both files.
    Add desc to rake test:install-* tasks, tired of
      not seeing them in rake -vT
    
    changes requested via review
    
    Fixes #8373
    
    Fixes #8389
    
    Fixes #8395

--- logstash-core/lib/logstash-core/version.rb
+++ logstash-core/lib/logstash-core/version.rb
@@ -2,7 +2,11 @@
 
 # The version of logstash core gem.
 #
-# Note to authors: this should not include dashes because 'gem' barfs if
-# you include a dash in the version string.
-
-LOGSTASH_CORE_VERSION = "5.6.3"
+# sourced from a copy of the master versions.yml file, see logstash-core/logstash-core.gemspec
+if !defined?(ALL_VERSIONS)
+  require 'yaml'
+  ALL_VERSIONS = YAML.load_file(File.expand_path("../../versions-gem-copy.yml", File.dirname(__FILE__)))
+end
+if !defined?(LOGSTASH_CORE_VERSION)
+  LOGSTASH_CORE_VERSION = ALL_VERSIONS.fetch("logstash-core")
+end
