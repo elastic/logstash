3c9cd0c503fecfcc1ae8d557f7062a101fe025fe
Colin Surprenant <colin.surprenant@gmail.com>
2017-09-26 18:32:09 -0400

    dynamically assign drive letter to WORKSPACE env var

--- ci/unit_tests.bat
+++ ci/unit_tests.bat
@@ -1,6 +1,47 @@
 @echo off
+setlocal enabledelayedexpansion
 
-setlocal
+if not exist %WORKSPACE% (
+  echo Error: env var WORKSPACE must be defined
+  exit /B 1
+)
+
+:: see if %WORKSPACE% is alread mapped to a drive
+for /f "tokens=1* delims==> " %%G IN ('subst') do (
+  set sdrive=%%G
+  :: removing extra space
+  set sdrive=!sdrive:~0,2!
+  :: expanding H to a short path in order not to break the resulting command line
+  set spath=%%~sfH
+
+  rem echo trying !spath! vs "%WORKSPACE%"
+  if /I "!spath!" == "%WORKSPACE%" (
+    rem echo found drive=!sdrive!, path=!spath!
+    set use_drive=!sdrive!
+    goto :found_drive
+  )
+)
+
+:: no existing mapping
+:: try to assign "%WORKSPACE%" to the first drive letter which works
+for %%i in (A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z) do (
+    set "drive=%%i:"
+    rem echo trying subst !drive! "%WORKSPACE%"
+    subst !drive! "%WORKSPACE%" >nul
+    if not errorlevel 1 (
+        set use_drive=!drive!
+        goto :found_drive
+    )
+)
+
+echo Error: unable to subst drive to path "%WORKSPACE%"
+exit /B 1
+
+:found_drive
+echo using drive !use_drive! for "%WORKSPACE%"
+
+:: change current directory to that drive
+!use_drive!
 
 REM Since we are using the system jruby, we need to make sure our jvm process
 REM uses at least 1g of memory, If we don't do this we can get OOM issues when
