7460d813a6c231f687b04e0aeeee79bff69fc986
Andrew Cholakian <andrew@andrewvc.com>
2017-09-29 18:21:53 -0500

    Version bump to 6.0.0-rc2
    This also updates the breaking changes for the ES output since this bump updates that as well.

--- logstash-core/lib/logstash-core_jars.rb
+++ logstash-core/lib/logstash-core_jars.rb
@@ -8,7 +8,7 @@ rescue LoadError
   require 'org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar'
   require 'com/fasterxml/jackson/core/jackson-annotations/2.9.1/jackson-annotations-2.9.1.jar'
   require 'org/apache/logging/log4j/log4j-slf4j-impl/2.6.2/log4j-slf4j-impl-2.6.2.jar'
-  require 'com/fasterxml/jackson/module/jackson-module-afterburner/2.7.3/jackson-module-afterburner-2.9.1.jar'
+  require 'com/fasterxml/jackson/module/jackson-module-afterburner/2.9.1/jackson-module-afterburner-2.9.1.jar'
   require 'com/fasterxml/jackson/dataformat/jackson-dataformat-cbor/2.9.1/jackson-dataformat-cbor-2.9.1.jar'
   require 'com/fasterxml/jackson/core/jackson-core/2.9.1/jackson-core-2.9.1.jar'
 end
