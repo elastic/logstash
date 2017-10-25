84e01680c7bbd636689e8842b459bc485d92c0c5
Armin <me@obrown.io>
2017-10-25 15:34:59 +0200

    #8154 break cyclic dependency between Ruby and Java Event classes
    Fixes #8529

--- logstash-core/src/main/java/org/logstash/RubyUtil.java
+++ logstash-core/src/main/java/org/logstash/RubyUtil.java
@@ -2,8 +2,12 @@ package org.logstash;
 
 import org.jruby.NativeException;
 import org.jruby.Ruby;
+import org.jruby.RubyClass;
+import org.jruby.RubyException;
 import org.jruby.RubyModule;
+import org.jruby.anno.JRubyClass;
 import org.jruby.exceptions.RaiseException;
+import org.logstash.ext.JrubyEventExtLibrary;
 
 /**
  * Utilities around interaction with the {@link Ruby} runtime.
@@ -20,9 +24,45 @@ public final class RubyUtil {
      */
     public static final RubyModule LOGSTASH_MODULE;
 
+    public static final RubyClass RUBY_EVENT_CLASS;
+
+    public static final RubyClass PARSER_ERROR;
+
+    public static final RubyClass GENERATOR_ERROR;
+
+    public static final RubyClass LOGSTASH_ERROR;
+
     static {
         RUBY = Ruby.getGlobalRuntime();
         LOGSTASH_MODULE = RUBY.getOrCreateModule("LogStash");
+        RUBY_EVENT_CLASS = RUBY.defineClassUnder(
+            "Event", RUBY.getObject(), JrubyEventExtLibrary.RubyEvent::new, LOGSTASH_MODULE
+        );
+        final RubyModule json = LOGSTASH_MODULE.defineOrGetModuleUnder("Json");
+        LOGSTASH_ERROR = LOGSTASH_MODULE.defineClassUnder(
+            "Error", RUBY.getStandardError(), RubyUtil.LogstashRubyError::new
+        );
+        PARSER_ERROR = json.defineClassUnder(
+            "ParserError", LOGSTASH_ERROR, RubyUtil.LogstashRubyParserError::new
+        );
+        GENERATOR_ERROR = json.defineClassUnder("GeneratorError", LOGSTASH_ERROR,
+            RubyUtil.LogstashRubyGeneratorError::new
+        );
+        RUBY_EVENT_CLASS.setConstant("METADATA", RUBY.newString(Event.METADATA));
+        RUBY_EVENT_CLASS.setConstant(
+            "METADATA_BRACKETS", RUBY.newString(Event.METADATA_BRACKETS)
+        );
+        RUBY_EVENT_CLASS.setConstant("TIMESTAMP", RUBY.newString(Event.TIMESTAMP));
+        RUBY_EVENT_CLASS.setConstant(
+            "TIMESTAMP_FAILURE_TAG", RUBY.newString(Event.TIMESTAMP_FAILURE_TAG)
+        );
+        RUBY_EVENT_CLASS.setConstant(
+            "TIMESTAMP_FAILURE_FIELD", RUBY.newString(Event.TIMESTAMP_FAILURE_FIELD)
+        );
+        RUBY_EVENT_CLASS.setConstant("VERSION", RUBY.newString(Event.VERSION));
+        RUBY_EVENT_CLASS.setConstant("VERSION_ONE", RUBY.newString(Event.VERSION_ONE));
+        RUBY_EVENT_CLASS.defineAnnotatedMethods(JrubyEventExtLibrary.RubyEvent.class);
+        RUBY_EVENT_CLASS.defineAnnotatedConstants(JrubyEventExtLibrary.RubyEvent.class);
     }
 
     private RubyUtil() {
@@ -38,5 +78,29 @@ public final class RubyUtil {
     public static RaiseException newRubyIOError(Ruby runtime, Throwable e) {
         // will preserve Java stacktrace & bubble up as a Ruby IOError
         return new RaiseException(e, new NativeException(runtime, runtime.getIOError(), e));
+    }
+
+    @JRubyClass(name = "Error")
+    public static final class LogstashRubyError extends RubyException {
+
+        public LogstashRubyError(final Ruby runtime, final RubyClass metaClass) {
+            super(runtime, metaClass);
+        }
+    }
+
+    @JRubyClass(name = "ParserError")
+    public static final class LogstashRubyParserError extends RubyException {
+
+        public LogstashRubyParserError(final Ruby runtime, final RubyClass metaClass) {
+            super(runtime, metaClass);
+        }
+    }
+
+    @JRubyClass(name = "GeneratorError")
+    public static final class LogstashRubyGeneratorError extends RubyException {
+
+        public LogstashRubyGeneratorError(final Ruby runtime, final RubyClass metaClass) {
+            super(runtime, metaClass);
+        }
     }
 }
