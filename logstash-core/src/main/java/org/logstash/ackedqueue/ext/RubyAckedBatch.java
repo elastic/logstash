9037a7cf588daf35f56c2a6e1e33124be8b259c1
Armin <me@obrown.io>
2017-11-08 17:25:43 +0100

    CLEANUP: Remove unused Ruby Constructor from RubyAckedBatch
    Fixes #8617

--- logstash-core/src/main/java/org/logstash/ackedqueue/ext/RubyAckedBatch.java
+++ logstash-core/src/main/java/org/logstash/ackedqueue/ext/RubyAckedBatch.java
@@ -1,8 +1,6 @@
 package org.logstash.ackedqueue.ext;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import org.jruby.Ruby;
 import org.jruby.RubyArray;
 import org.jruby.RubyClass;
@@ -14,8 +12,6 @@ import org.jruby.runtime.builtin.IRubyObject;
 import org.logstash.Event;
 import org.logstash.RubyUtil;
 import org.logstash.ackedqueue.Batch;
-import org.logstash.ackedqueue.Queueable;
-import org.logstash.ackedqueue.io.LongVector;
 import org.logstash.ext.JrubyEventExtLibrary;
 
 @JRubyClass(name = "AckedBatch")
@@ -32,30 +28,6 @@ public final class RubyAckedBatch extends RubyObject {
             new RubyAckedBatch(runtime, RubyUtil.RUBY_ACKED_BATCH_CLASS);
         ackedBatch.batch = batch;
         return ackedBatch;
-    }
-
-    @SuppressWarnings("unchecked") // for the getList() calls
-    @JRubyMethod(name = "initialize", required = 3)
-    public IRubyObject ruby_initialize(ThreadContext context, IRubyObject events,
-        IRubyObject seqNums, IRubyObject queue) {
-        if (!(events instanceof RubyArray)) {
-            context.runtime.newArgumentError("expected events array");
-        }
-        if (!(seqNums instanceof RubyArray)) {
-            context.runtime.newArgumentError("expected seqNums array");
-        }
-        if (!(queue instanceof AbstractJRubyQueue.RubyAckedQueue)) {
-            context.runtime.newArgumentError("expected queue AckedQueue");
-        }
-        final Collection<Long> seqList = (List<Long>) seqNums;
-        final LongVector seqs = new LongVector(seqList.size());
-        for (final long seq : seqList) {
-            seqs.add(seq);
-        }
-        this.batch = new Batch((List<Queueable>) events, seqs,
-            ((AbstractJRubyQueue.RubyAckedQueue) queue).getQueue()
-        );
-        return context.nil;
     }
 
     @JRubyMethod(name = "get_elements")
