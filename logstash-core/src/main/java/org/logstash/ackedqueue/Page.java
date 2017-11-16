3c6474ba9322e1ef91dd9a6afe015f9b15ee214f
Colin Surprenant <colin.surprenant@gmail.com>
2017-11-16 16:50:36 -0500

    refactor Page inheritance with PageAccess delegation
    cleanups and fixed concurrency issue
    
    cleanups
    
    cleanups
    
    re-refactor and simplify just using a writable flag
    
    dead imports
    
    review fixes

--- logstash-core/src/main/java/org/logstash/ackedqueue/Page.java
+++ logstash-core/src/main/java/org/logstash/ackedqueue/Page.java
@@ -2,19 +2,20 @@ package org.logstash.ackedqueue;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.BitSet;
-import java.util.List;
+
+import org.logstash.ackedqueue.io.CheckpointIO;
 import org.logstash.ackedqueue.io.LongVector;
 import org.logstash.ackedqueue.io.PageIO;
 
-public abstract class Page implements Closeable {
+public final class Page implements Closeable {
     protected final int pageNum;
     protected long minSeqNum; // TODO: see if we can make it final?
     protected int elementCount;
     protected long firstUnreadSeqNum;
     protected final Queue queue;
     protected PageIO pageIO;
+    private boolean writable;
 
     // bit 0 is minSeqNum
     // TODO: go steal LocalCheckpointService in feature/seq_no from ES
@@ -22,7 +23,7 @@ public abstract class Page implements Closeable {
     protected BitSet ackedSeqNums;
     protected Checkpoint lastCheckpoint;
 
-    public Page(int pageNum, Queue queue, long minSeqNum, int elementCount, long firstUnreadSeqNum, BitSet ackedSeqNums, PageIO pageIO) {
+    public Page(int pageNum, Queue queue, long minSeqNum, int elementCount, long firstUnreadSeqNum, BitSet ackedSeqNums, PageIO pageIO, boolean writable) {
         this.pageNum = pageNum;
         this.queue = queue;
 
@@ -32,6 +33,7 @@ public abstract class Page implements Closeable {
         this.ackedSeqNums = ackedSeqNums;
         this.lastCheckpoint = new Checkpoint(0, 0, 0, 0, 0);
         this.pageIO = pageIO;
+        this.writable = writable;
     }
 
     public String toString() {
@@ -39,11 +41,11 @@ public abstract class Page implements Closeable {
     }
 
     /**
-     * @param limit the maximum number of elements to read
-     * @return {@link SequencedList} collection of elements read. the number of elements can be {@literal <=} limit
+     * @param limit the maximum number of elements to read, actual number readcan be smaller
+     * @return {@link SequencedList} collection of serialized elements read
+     * @throws IOException
      */
     public SequencedList<byte[]> read(int limit) throws IOException {
-
         // first make sure this page is activated, activating previously activated is harmless
         this.pageIO.activate();
 
@@ -54,6 +56,27 @@ public abstract class Page implements Closeable {
         this.firstUnreadSeqNum += serialized.getElements().size();
 
         return serialized;
+    }
+
+    public void write(byte[] bytes, long seqNum, int checkpointMaxWrites) throws IOException {
+        if (! this.writable) {
+            throw new IllegalStateException(String.format("page=%d is not writable", this.pageNum));
+        }
+
+        this.pageIO.write(bytes, seqNum);
+
+        if (this.minSeqNum <= 0) {
+            this.minSeqNum = seqNum;
+            this.firstUnreadSeqNum = seqNum;
+        }
+        this.elementCount++;
+
+        // force a checkpoint if we wrote checkpointMaxWrites elements since last checkpoint
+        // the initial condition of an "empty" checkpoint, maxSeqNum() will return -1
+        if (checkpointMaxWrites > 0 && (seqNum >= this.lastCheckpoint.maxSeqNum() + checkpointMaxWrites)) {
+            // did we write more than checkpointMaxWrites elements? if so checkpoint now
+            checkpoint();
+        }
     }
 
     /**
@@ -83,14 +106,17 @@ public abstract class Page implements Closeable {
         return this.elementCount <= 0 ? 0 : Math.max(0, (maxSeqNum() - this.firstUnreadSeqNum) + 1);
     }
 
-    // update the page acking bitset. trigger checkpoint on the page if it is fully acked or if we acked more than the
-    // configured threshold checkpointMaxAcks.
-    // note that if the fully acked tail page is the first unacked page, it is not really necessary to also checkpoint
-    // the head page to update firstUnackedPageNum because it will be updated in the next upcoming head page checkpoint
-    // and in a crash condition, the Queue open recovery will detect and purge fully acked pages
-    //
-    // @param seqNums the list of same-page seqNums to ack
-    // @param checkpointMaxAcks the number of acks that will trigger a page checkpoint
+    /**
+     * update the page acking bitset. trigger checkpoint on the page if it is fully acked or if we acked more than the
+     * configured threshold checkpointMaxAcks.
+     * note that if the fully acked tail page is the first unacked page, it is not really necessary to also checkpoint
+     * the head page to update firstUnackedPageNum because it will be updated in the next upcoming head page checkpoint
+     * and in a crash condition, the Queue open recovery will detect and purge fully acked pages
+     *
+     * @param seqNums the list of same-page seqNums to ack
+     * @param checkpointMaxAcks number of acks before forcing a checkpoint
+     * @throws IOException
+     */
     public void ack(LongVector seqNums, int checkpointMaxAcks) throws IOException {
         final int count = seqNums.size();
         for (int i = 0; i < count; ++i) {
@@ -100,7 +126,7 @@ public abstract class Page implements Closeable {
             assert seqNum >= this.minSeqNum :
                     String.format("seqNum=%d is smaller than minSeqnum=%d", seqNum, this.minSeqNum);
 
-            assert seqNum < this.minSeqNum + this.elementCount:
+            assert seqNum < this.minSeqNum + this.elementCount :
                     String.format("seqNum=%d is greater than minSeqnum=%d + elementCount=%d = %d", seqNum, this.minSeqNum, this.elementCount, this.minSeqNum + this.elementCount);
             int index = (int)(seqNum - this.minSeqNum);
 
@@ -123,9 +149,102 @@ public abstract class Page implements Closeable {
         }
     }
 
-    public abstract void checkpoint() throws IOException;
+    public void checkpoint() throws IOException {
+        if (this.writable) {
+            headPageCheckpoint();
+        } else {
+            tailPageCheckpoint();
+        }
+    }
 
-    public abstract void close() throws IOException;
+    private void headPageCheckpoint() throws IOException {
+        if (this.elementCount > this.lastCheckpoint.getElementCount()) {
+            // fsync & checkpoint if data written since last checkpoint
+
+            this.pageIO.ensurePersisted();
+            this.forceCheckpoint();
+        } else {
+            Checkpoint checkpoint = new Checkpoint(this.pageNum, this.queue.firstUnackedPageNum(), this.firstUnackedSeqNum(), this.minSeqNum, this.elementCount);
+            if (! checkpoint.equals(this.lastCheckpoint)) {
+                // checkpoint only if it changed since last checkpoint
+
+                // non-dry code with forceCheckpoint() to avoid unnecessary extra new Checkpoint object creation
+                CheckpointIO io = this.queue.getCheckpointIO();
+                io.write(io.headFileName(), checkpoint);
+                this.lastCheckpoint = checkpoint;
+            }
+        }
+
+    }
+
+    public void tailPageCheckpoint() throws IOException {
+        // since this is a tail page and no write can happen in this page, there is no point in performing a fsync on this page, just stamp checkpoint
+        CheckpointIO io = this.queue.getCheckpointIO();
+        this.lastCheckpoint = io.write(io.tailFileName(this.pageNum), this.pageNum, 0, this.firstUnackedSeqNum(), this.minSeqNum, this.elementCount);
+    }
+
+
+    public void ensurePersistedUpto(long seqNum) throws IOException {
+        long lastCheckpointUptoSeqNum = this.lastCheckpoint.getMinSeqNum() + this.lastCheckpoint.getElementCount();
+
+        // if the last checkpoint for this headpage already included the given seqNum, no need to fsync/checkpoint
+        if (seqNum > lastCheckpointUptoSeqNum) {
+            // head page checkpoint does a data file fsync
+            checkpoint();
+        }
+    }
+
+    public void forceCheckpoint() throws IOException {
+        Checkpoint checkpoint = new Checkpoint(this.pageNum, this.queue.firstUnackedPageNum(), this.firstUnackedSeqNum(), this.minSeqNum, this.elementCount);
+        CheckpointIO io = this.queue.getCheckpointIO();
+        io.write(io.headFileName(), checkpoint);
+        this.lastCheckpoint = checkpoint;
+    }
+
+    public void behead() throws IOException {
+        checkpoint();
+
+        this.writable = false;
+        this.lastCheckpoint = new Checkpoint(0, 0, 0, 0, 0);
+
+        // first thing that must be done after beheading is to create a new checkpoint for that new tail page
+        // tail page checkpoint does NOT includes a fsync
+        checkpoint();
+
+        // TODO: should we have a better deactivation strategy to avoid too rapid reactivation scenario?
+        Page firstUnreadPage = queue.firstUnreadPage();
+        if (firstUnreadPage == null || (this.getPageNum() > firstUnreadPage.getPageNum())) {
+            // deactivate if this new tailPage is not where the read is occurring
+            this.getPageIO().deactivate();
+        }
+    }
+
+    public boolean hasSpace(int byteSize) {
+        return this.pageIO.hasSpace((byteSize));
+    }
+
+    /**
+     * verify if data size plus overhead is not greater than the page capacity
+     *
+     * @param byteSize the date size to verify
+     * @return true if data plus overhead fit in page
+     */
+    public boolean hasCapacity(int byteSize) {
+        return this.pageIO.persistedByteCount(byteSize) <= this.pageIO.getCapacity();
+    }
+
+    public void close() throws IOException {
+        checkpoint();
+        if (this.pageIO != null) {
+            this.pageIO.close();
+        }
+    }
+
+    public void purge() throws IOException {
+        if (this.pageIO != null) {
+            this.pageIO.purge(); // page IO purge calls close
+        }
+    }
 
     public int getPageNum() {
         return pageNum;
