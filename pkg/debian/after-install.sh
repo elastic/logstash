51b2756c9bb2daa088e1210549c85df4e888686e
Jordan Sissel <jls@semicomplete.com>
2017-12-23 17:20:12 -0800

    Use multiple pipelines feature by default in RPM and Debian packages.
    This change keeps the previous behavior but implements it using multiple
    pipelines (pipelines.yml) instead of with `path.config`.
    
    * The default pipeline name is still "main"
    * Default config path is still /etc/logstash/conf.d/*.conf
    
    Other small changes included:
    * Specify fpm workdir. Otherwise rpmbuild is given an empty value for
      its temporary directory. It's unclear if this is a bug in fpm or in my
      environment, but I like the change anyway to put fpm's build activity
      in the build directory.
    * DRY's up the common files going in /etc/logstash for both rpm and deb
      packages.
    
    For #8893

--- pkg/debian/after-install.sh
+++ pkg/debian/after-install.sh
@@ -5,7 +5,6 @@ chown -R logstash /var/log/logstash
 chown logstash:logstash /var/lib/logstash
 chmod 755 /etc/logstash
 sed -i \
-  -e 's|# path.config:|path.config: /etc/logstash/conf.d/*.conf|' \
   -e 's|# path.logs:|path.logs: /var/log/logstash|' \
   -e 's|# path.data:|path.data: /var/lib/logstash|' \
   /etc/logstash/logstash.yml
