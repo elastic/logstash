4091ddfdb4ddc4fe4181299a971d48d434f1e46e
Armin <me@obrown.io>
2018-01-22 10:59:43 +0100

    Remove mock file io
    Fixes #8999

--- logstash-core/src/test/java/org/logstash/ackedqueue/QueueTestHelpers.java
+++ logstash-core/src/test/java/org/logstash/ackedqueue/QueueTestHelpers.java
@@ -1,8 +1,7 @@
 package org.logstash.ackedqueue;
 
-import org.logstash.ackedqueue.io.ByteBufferPageIO;
-
 import java.io.IOException;
+import org.logstash.ackedqueue.io.AbstractByteBufferPageIO;
 
 /**
  * Class containing common methods to help DRY up acked queue tests.
@@ -10,18 +9,12 @@ import java.io.IOException;
 public class QueueTestHelpers {
 
     /**
-     * Returns the minimum capacity required for {@link ByteBufferPageIO}
-     * @return int - minimum capacity required
-     */
-    public static final int BYTE_BUF_PAGEIO_MIN_CAPACITY = ByteBufferPageIO.WRAPPER_SIZE;
-
-    /**
-     * Returns the {@link ByteBufferPageIO} capacity required for the supplied element
+     * Returns the {@link org.logstash.ackedqueue.io.MmapPageIO} capacity required for the supplied element
      * @param element
      * @return int - capacity required for the supplied element
      * @throws IOException Throws if a serialization error occurs
      */
     public static int singleElementCapacityForByteBufferPageIO(final Queueable element) throws IOException {
-        return ByteBufferPageIO.WRAPPER_SIZE + element.serialize().length;
+        return AbstractByteBufferPageIO.WRAPPER_SIZE + element.serialize().length;
     }
 }
