7ac0789033d4450bcc0ab7f2ae46f549dac3c90c
Armin <me@obrown.io>
2018-01-24 13:39:23 +0100

    BUG: Memory Queue needs to always be drained
    Fixes #9024

--- logstash-core/lib/logstash/pipeline.rb
+++ logstash-core/lib/logstash/pipeline.rb
@@ -183,7 +183,7 @@ module LogStash; class Pipeline < BasePipeline
     @filter_queue_client.set_pipeline_metric(
         metric.namespace([:stats, :pipelines, pipeline_id.to_s.to_sym, :events])
     )
-    @drain_queue =  @settings.get_value("queue.drain")
+    @drain_queue =  @settings.get_value("queue.drain") || settings.get("queue.type") == "memory"
 
 
     @events_filtered = Concurrent::AtomicFixnum.new(0)
