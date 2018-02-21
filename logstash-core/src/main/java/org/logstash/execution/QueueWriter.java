fdc6702ab9bc7f5af63fd46bf6963791359bd1f0
Armin <me@obrown.io>
2018-02-21 08:54:53 +0100

    remove timeout and batch writes

--- logstash-core/src/main/java/org/logstash/execution/QueueWriter.java
+++ logstash-core/src/main/java/org/logstash/execution/QueueWriter.java
@@ -1,6 +1,5 @@
 package org.logstash.execution;
 
-import java.util.Collection;
 import java.util.Map;
 
 /**
@@ -15,36 +14,6 @@ public interface QueueWriter {
      * @return Sequence number of the event or -1 if push failed
      */
     long push(Map<String, Object> event);
-
-    /**
-     * Pushes a single event to the Queue, blocking for the given timeout if the Queue is not ready
-     * for a write.
-     * @param event Logstash Event Data
-     * @param millis Timeout in millis
-     * @return Sequence number of the event or -1 if push failed
-     */
-    long push(Map<String, Object> event, long millis);
-
-    /**
-     * Pushes a multiple events to the Queue, blocking for the given timeout if the Queue is not
-     * ready for a write.
-     * Guarantees that a return {@code != -1} means that all events were pushed to the Queue
-     * successfully and no partial writes of only a subset of the input events will ever occur.
-     * @param events Logstash Events Data
-     * @return Sequence number of the first event or -1 if push failed
-     */
-    long push(Collection<Map<String, Object>> events);
-
-    /**
-     * Pushes a multiple events to the Queue, blocking for the given timeout if the Queue is not
-     * ready for a write.
-     * Guarantees that a return {@code != -1} means that all events were pushed to the Queue
-     * successfully and no partial writes of only a subset of the input events will ever occur.
-     * @param events Logstash Events Data
-     * @param millis Timeout in millis
-     * @return Sequence number of the first event or -1 if push failed
-     */
-    long push(Collection<Map<String, Object>> events, long millis);
 
     /**
      * Returns the upper bound for acknowledged sequence numbers.
