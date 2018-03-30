9a3fea83bc479c7192d95957d06165611f9b90f7
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2018-03-30 21:16:39 +0000

    SecretStoreFactory: allow for running with custom environment for simplified testing
    Obviates the need for a testing helper that required platform-specific hackery to
    mutate the underlying map of `System.getEnv()`.
    
    Fixes #9305

--- logstash-core/src/main/java/org/logstash/secret/store/SecretStoreFactory.java
+++ logstash-core/src/main/java/org/logstash/secret/store/SecretStoreFactory.java
@@ -4,12 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.logstash.secret.SecretIdentifier;
 
+import java.util.Map;
+
 /**
  * <p>A factory to load the implementation of a {@link SecretStore}. Implementation may be defined via the {@link SecureConfig} via with a key of "keystore.classname" and
  * value equal to the fqn of the class that implements {@link SecretStore}
  * </p>
  */
 public class SecretStoreFactory {
+    @FunctionalInterface
+    public interface EnvironmentVariableProvider {
+        String get(String env);
+    }
+
+    private final EnvironmentVariableProvider environmentVariableProvider;
 
     public static final String KEYSTORE_ACCESS_KEY = "keystore.pass";
     //secret stores should create a secret with this as the key and value to identify a logstash secret
@@ -18,9 +26,29 @@ public class SecretStoreFactory {
     public final static String ENVIRONMENT_PASS_KEY = "LOGSTASH_KEYSTORE_PASS";
 
     /**
-     * Private constructor
+     * @return a {@link SecretStoreFactory} that is capable of pulling credentials from the process' environment
+     *         variables using the platform-specific {@link System#getenv(String)}.
      */
-    private SecretStoreFactory() {
+    public static SecretStoreFactory fromEnvironment() {
+        return new SecretStoreFactory(System::getenv);
+    }
+
+    /**
+     * Get a {@link SecretStoreFactory} that is capable of pulling credentials from the specified environment variable map.
+     *
+     * Useful for testing scenarios, where overwriting the pseudo-immutable mapping of environment variables available
+     * via official APIs would require platform-dependent hackery.
+     *
+     * @param environmentVariables a Map of environment variables that wholly-overrides the process' environment for
+     *                             purposes of credential extraction by the returned instance of {@link SecretStoreFactory}.
+     * @return a {@link SecretStoreFactory} that uses only the provided environment variables.
+     */
+    public static SecretStoreFactory withEnvironment(final Map<String,String> environmentVariables) {
+        return new SecretStoreFactory(environmentVariables::get);
+    }
+
+    private SecretStoreFactory(final EnvironmentVariableProvider environmentVariableProvider) {
+        this.environmentVariableProvider = environmentVariableProvider;
     }
 
     private static final Logger LOGGER = LogManager.getLogger(SecretStoreFactory.class);
@@ -31,7 +59,7 @@ public class SecretStoreFactory {
      * Determine if this secret store currently exists
      * @return true if the secret store exists, false otherwise
      */
-    public static boolean exists(SecureConfig secureConfig) {
+    public boolean exists(SecureConfig secureConfig) {
         return doIt(MODE.EXISTS, secureConfig).exists(secureConfig);
     }
 
@@ -41,7 +69,7 @@ public class SecretStoreFactory {
      * @param secureConfig The configuration to pass to the implementation
      * @return the newly created SecretStore, throws {@link SecretStoreException} if errors occur while loading, or if store already exists
      */
-    static public SecretStore create(SecureConfig secureConfig) {
+    public SecretStore create(SecureConfig secureConfig) {
         return doIt(MODE.CREATE, secureConfig);
     }
 
@@ -51,7 +79,7 @@ public class SecretStoreFactory {
      * @param secureConfig The configuration to pass to the implementation
      * throws {@link SecretStoreException} if errors occur
      */
-    static public void delete(SecureConfig secureConfig) {
+    public void delete(SecureConfig secureConfig) {
         doIt(MODE.DELETE, secureConfig);
     }
 
@@ -61,12 +89,12 @@ public class SecretStoreFactory {
      * @param secureConfig The configuration to pass to the implementation
      * @return the loaded SecretStore, throws {@link SecretStoreException} if errors occur while loading, or if store does not exist
      */
-    static public SecretStore load(SecureConfig secureConfig) {
+    public SecretStore load(SecureConfig secureConfig) {
         return doIt(MODE.LOAD, secureConfig);
     }
 
     @SuppressWarnings({"unchecked", "JavaReflectionMemberAccess"})
-    private static SecretStore doIt(MODE mode, SecureConfig secureConfig) {
+    private SecretStore doIt(MODE mode, SecureConfig secureConfig) {
         char[] configuredClassName = secureConfig.getPlainText("keystore.classname");
         String className = configuredClassName != null ? new String(configuredClassName) : "org.logstash.secret.store.backend.JavaKeyStore";
         try {
@@ -99,13 +127,12 @@ public class SecretStoreFactory {
      *
      * @param secureConfig The configuration to add the secret store access
      */
-    private static void addSecretStoreAccess(SecureConfig secureConfig) {
-        String environment = System.getenv(ENVIRONMENT_PASS_KEY);
+    private void addSecretStoreAccess(SecureConfig secureConfig) {
+        String keystore_pass = environmentVariableProvider.get(ENVIRONMENT_PASS_KEY);
 
-        char[] pass = null;
-        if (environment != null) {
-            secureConfig.add(KEYSTORE_ACCESS_KEY, environment.toCharArray());
-            environment = null;
+        if (keystore_pass != null) {
+            secureConfig.add(KEYSTORE_ACCESS_KEY, keystore_pass.toCharArray());
+            keystore_pass = null;
         }
 
         //futile attempt to remove the original pass from memory
