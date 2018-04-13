7c77cb8eca2ae5e323d69d42be7ed666635ac4b8
Armin <me@obrown.io>
2018-04-13 17:14:07 +0200

    merge in master

--- logstash-core/src/main/java/org/logstash/config/ir/compiler/PluginFactory.java
+++ logstash-core/src/main/java/org/logstash/config/ir/compiler/PluginFactory.java
@@ -43,7 +43,7 @@ public interface PluginFactory extends RubyIntegration.PluginFactory {
         }
 
         @Override
-        public IRubyObject buildOutput(final RubyString name, final RubyInteger line, final RubyInteger column, final IRubyObject args) {
+        public OutputDelegatorExt buildOutput(final RubyString name, final RubyInteger line, final RubyInteger column, final IRubyObject args) {
             return rubyFactory.buildOutput(name, line, column, args);
         }
 
