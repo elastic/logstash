e40db6ad9615f392af7de6972896208be4c218da
Shaunak Kashyap <ycombinator@gmail.com>
2018-04-26 05:56:55 -0700

    Adding Builder for SSL and basic auth options

--- logstash-core/src/main/java/org/logstash/common/kibana/Client.java
+++ logstash-core/src/main/java/org/logstash/common/kibana/Client.java
@@ -1,16 +1,28 @@
 package org.logstash.common.kibana;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.StatusLine;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.*;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+
+import javax.net.ssl.*;
 
 /**
  * Basic Kibana Client. Allows consumers to perform requests against Kibana's HTTP APIs.
@@ -20,54 +32,14 @@ import org.apache.http.impl.client.HttpClients;
  * TODO: Unit tests
  */
 public class Client {
-    private URL baseUrl;
     private CloseableHttpClient httpClient;
+    private URL baseUrl;
 
     private static final String STATUS_API = "api/status";
 
-    /**
-     * @param baseUrl The base URL for Kibana, e.g. http://localhost:5601/
-     */
-    public Client(URL baseUrl) {
+    private Client(CloseableHttpClient httpClient, URL baseUrl) {
+        this.httpClient = httpClient;
         this.baseUrl = baseUrl;
-        this.httpClient = HttpClients.createDefault();
-    }
-
-    /**
-     * @param baseUrl The base URL for Kibana, e.g. http://localhost:5601/
-     */
-    public Client(String baseUrl) throws MalformedURLException {
-        this(new URL(baseUrl));
-    }
-
-    /**
-     * @param protocol  The protocol part of the base URL for Kibana, e.g. http
-     * @param host      The hostname part of the base URL for Kibana, e.g. localhost
-     * @param port      The port part of the base URL for Kibana, e.g. 5601
-     * @param basePath  The base path part of the base URL for Kibana, e.g. /
-     * @throws MalformedURLException
-     */
-    public Client(String protocol, String host, int port, String basePath) throws MalformedURLException {
-        this(new URL(protocol, host, port, basePath));
-    }
-
-    /**
-     * @param protocol  The protocol part of the base URL for Kibana, e.g. http
-     * @param host      The hostname part of the base URL for Kibana, e.g. localhost
-     * @param port      The port part of the base URL for Kibana, e.g. 5601
-     * @throws MalformedURLException
-     */
-    public Client(String protocol, String host, int port) throws MalformedURLException {
-        this(protocol, host, port, "/");
-    }
-
-    /**
-     * Default constructor. Creates Kibana client with base URL = http://localhost:5601/
-     *
-     * @throws MalformedURLException
-     */
-    public Client() throws MalformedURLException {
-        this("http", "localhost", 5601);
     }
 
     /**
@@ -107,6 +79,7 @@ public class Client {
     public String get(String relativePath, Map<String, String> headers) throws RequestFailedException, IOException {
         String url = makeUrlFrom(relativePath);
 
+        // TODO: use this.httpClient instead
         HttpGet request = new HttpGet(url);
         headers.forEach(request::addHeader);
         CloseableHttpResponse response = null;
@@ -148,7 +121,7 @@ public class Client {
     public void head(String relativePath, Map<String, String> headers) throws RequestFailedException, IOException {
         String url = makeUrlFrom(relativePath);
 
-
+        // TODO: use this.httpClient instead
         HttpHead request = new HttpHead(url);
         headers.forEach(request::addHeader);
 
@@ -199,6 +172,7 @@ public class Client {
 
         String url = makeUrlFrom(relativePath);
 
+        // TODO: use this.httpClient instead
         HttpPost request = new HttpPost(url);
         headers.forEach(request::addHeader);
         request.setEntity(new StringEntity(requestBody));
@@ -243,6 +217,199 @@ public class Client {
                 message += "; reason: " + reason;
             }
             return message;
+        }
+    }
+
+    public static class Builder {
+
+        private URL baseUrl;
+        private boolean useStrictSslVerificationMode;
+        private Certificate caCertificate;
+        private Certificate clientPublicKeyCertificate;
+        private byte[] clientPrivateKey;
+        private String basicAuthUsername;
+        private String basicAuthPassword;
+
+        /**
+         * @param baseUrl The base URL for Kibana, e.g. http://localhost:5601/
+         */
+        public Builder(URL baseUrl) {
+            this.baseUrl = baseUrl;
+            this.useStrictSslVerificationMode = true;
+        }
+
+        /**
+         * @param baseUrl The base URL for Kibana, e.g. http://localhost:5601/
+         */
+        public Builder(String baseUrl) throws MalformedURLException {
+            this(new URL(baseUrl));
+        }
+
+        /**
+         * @param protocol  The protocol part of the base URL for Kibana, e.g. http
+         * @param host      The hostname part of the base URL for Kibana, e.g. localhost
+         * @param port      The port part of the base URL for Kibana, e.g. 5601
+         * @param basePath  The base path part of the base URL for Kibana, e.g. /
+         * @throws MalformedURLException
+         */
+        public Builder(String protocol, String host, int port, String basePath) throws MalformedURLException {
+            this(new URL(protocol, host, port, basePath));
+        }
+
+        /**
+         * @param protocol  The protocol part of the base URL for Kibana, e.g. http
+         * @param host      The hostname part of the base URL for Kibana, e.g. localhost
+         * @param port      The port part of the base URL for Kibana, e.g. 5601
+         * @throws MalformedURLException
+         */
+        public Builder(String protocol, String host, int port) throws MalformedURLException {
+            this(protocol, host, port, "/");
+        }
+
+        /**
+         * Default constructor. Creates Kibana client with base URL = http://localhost:5601/
+         *
+         * @throws MalformedURLException
+         */
+        public Builder() throws MalformedURLException {
+            this("http", "localhost", 5601);
+        }
+
+        // TODO: Throw custom exception wrapping lower-level exceptions
+        public Builder withSSL(boolean useStrictSslVerificationMode, String caCertificatePath, String clientPublicKeyCertificatePath, String clientPrivateKeyPath) throws CertificateException, IOException {
+            this.useStrictSslVerificationMode = useStrictSslVerificationMode;
+
+            if (caCertificatePath != null) {
+                this.caCertificate = getCertificate(caCertificatePath);
+            }
+
+            if (clientPublicKeyCertificatePath != null) {
+                this.clientPublicKeyCertificate = getCertificate(clientPublicKeyCertificatePath);
+            }
+
+            if (clientPrivateKeyPath != null) {
+                this.clientPrivateKey = getPrivateKey(clientPrivateKeyPath);
+            }
+
+            return this;
+        }
+
+        // TODO: Throw custom exception wrapping lower-level exceptions
+        public Builder withSSL(boolean useStrictSslVerificationMode, String caCertificatePath) throws CertificateException, IOException {
+            return withSSL(useStrictSslVerificationMode, caCertificatePath, null, null);
+        }
+
+        // TODO: Throw custom exception wrapping lower-level exceptions
+        public Builder withSSL(boolean useStrictSslVerificationMode) throws CertificateException, IOException {
+            return withSSL(useStrictSslVerificationMode, null, null, null);
+        }
+
+        public Builder withBasicAuth(String username, String password) {
+            this.basicAuthUsername = username;
+            this.basicAuthPassword = password;
+
+            return this;
+        }
+
+        // TODO: Throw custom exception wrapping lower-level exceptions
+        // TODO: Decide how much work to do in this build() method vs. in withSSL() method
+        public Client build() throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException, KeyManagementException, UnrecoverableKeyException {
+
+            if (!usesSsl() && !usesBasicAuth()) {
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                return new Client(httpClient, this.baseUrl);
+            }
+
+            HttpClientBuilder httpClientBuilder = HttpClients.custom();
+
+            if (usesSsl()) {
+                TrustManager[] trustManagers = null;
+                if (this.caCertificate != null) {
+                    KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+                    keystore.load(null, null);
+
+                    keystore.setCertificateEntry("caCertificate", this.caCertificate);
+
+                    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                    trustManagerFactory.init(keystore);
+
+                    trustManagers = trustManagerFactory.getTrustManagers();
+                }
+
+                KeyManager[] keyManagers = null;
+                if ((this.clientPublicKeyCertificate != null) && (this.clientPrivateKey != null)) {
+                    KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+                    keystore.load(null, null);
+
+                    keystore.setCertificateEntry("clientPublicKeyCertificate", this.clientPublicKeyCertificate);
+                    keystore.setKeyEntry("clientPrivateKey", this.clientPrivateKey, new Certificate[]{this.clientPublicKeyCertificate});
+
+                    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                    keyManagerFactory.init(keystore, null);
+
+                    keyManagers = keyManagerFactory.getKeyManagers();
+                }
+
+                SSLContext sslContext = SSLContext.getDefault();
+                sslContext.init(keyManagers, trustManagers, null);
+
+                SSLConnectionSocketFactory sslSocketFactory;
+                if (useStrictSslVerificationMode) {
+                    sslSocketFactory = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.STRICT_HOSTNAME_VERIFIER);
+                } else {
+                    sslSocketFactory = new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+                }
+
+                httpClientBuilder.setSSLSocketFactory(sslSocketFactory);
+            }
+
+            if (usesBasicAuth()) {
+                CredentialsProvider basicAuthCredentialsProvider = new BasicCredentialsProvider();
+                UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(this.basicAuthUsername, this.basicAuthPassword);
+                basicAuthCredentialsProvider.setCredentials(AuthScope.ANY, credentials);
+
+                httpClientBuilder.setDefaultCredentialsProvider(basicAuthCredentialsProvider);
+            }
+
+            return new Client(httpClientBuilder.build(), this.baseUrl);
+        }
+
+        private boolean usesSsl() {
+            return (this.baseUrl.getProtocol() == "https") && this.useStrictSslVerificationMode;
+        }
+
+        private boolean usesBasicAuth() {
+            return (this.basicAuthUsername != null) && (this.basicAuthPassword != null);
+        }
+
+        private Certificate getCertificate(String certificateFilePath) throws CertificateException, IOException {
+            FileInputStream fis = new FileInputStream(certificateFilePath);
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            Certificate certificate;
+
+            try {
+                certificate = cf.generateCertificate(fis);
+            } finally {
+                fis.close();
+            }
+
+            return certificate;
+        }
+
+        private byte[] getPrivateKey(String privateKeyPath) throws IOException {
+            FileInputStream fis = new FileInputStream(privateKeyPath);
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            int numBytesRead;
+            byte[] data = new byte[16384];
+
+            while ((numBytesRead = fis.read(data)) != -1) {
+                buffer.write(data, 0, numBytesRead);
+            }
+            buffer.flush();
+
+            return buffer.toByteArray();
         }
     }
 
