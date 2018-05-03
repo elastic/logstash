9b3971865bd476d02cb9b4206fac7d23f927aa1e
Ferdinand Holzer <fholzer@users.noreply.github.com>
2018-05-03 14:54:11 +0200

    Fix rpm post-install scriptlet
    `/var/lib/logstash` may contain files and folders from a previous installation. In case a user removes logstash, and later decides to install it again, logstash may not work due to wrong ownership.

--- pkg/centos/after-install.sh
+++ pkg/centos/after-install.sh
@@ -1,6 +1,6 @@
 chown -R logstash:logstash /usr/share/logstash
 chown -R logstash /var/log/logstash
-chown logstash:logstash /var/lib/logstash
+chown -R logstash:logstash /var/lib/logstash
 sed -i \
   -e 's|# path.logs:|path.logs: /var/log/logstash|' \
   -e 's|# path.data:|path.data: /var/lib/logstash|' \
