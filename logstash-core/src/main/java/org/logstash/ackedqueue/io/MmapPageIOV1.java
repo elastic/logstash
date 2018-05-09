09ecb3e0bfbe937d051bad76e077bf16f3e379e8
Armin <me@obrown.io>
2018-05-09 09:04:37 -0500

    bck
    Fixes #9538

--- logstash-core/src/main/java/org/logstash/ackedqueue/io/MmapPageIOV1.java
+++ logstash-core/src/main/java/org/logstash/ackedqueue/io/MmapPageIOV1.java
@@ -5,27 +5,20 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.CRC32;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.logstash.LogstashJavaCompat;
 import org.logstash.ackedqueue.SequencedList;
 
+/**
+ * {@link PageIO} implementation for V1 PQ serialization format. Only supports read operations
+ * for use in {@link org.logstash.ackedqueue.QueueUpgrade}.
+ */
 public final class MmapPageIOV1 implements PageIO {
 
     public static final byte VERSION_ONE = 1;
-    public static final int VERSION_SIZE = Byte.BYTES;
-    public static final int CHECKSUM_SIZE = Integer.BYTES;
-    public static final int LENGTH_SIZE = Integer.BYTES;
-    public static final int SEQNUM_SIZE = Long.BYTES;
-    public static final int MIN_CAPACITY = VERSION_SIZE + SEQNUM_SIZE + LENGTH_SIZE + 1 + CHECKSUM_SIZE; // header overhead plus elements overhead to hold a single 1 byte element
-    public static final boolean VERIFY_CHECKSUM = true;
-
-    private static final Logger LOGGER = LogManager.getLogger(MmapPageIOV1.class);
 
     /**
      * Cleaner function for forcing unmapping of backing {@link MmapPageIOV1#buffer}.
@@ -81,7 +74,7 @@ public final class MmapPageIOV1 implements PageIO {
 
             for (int i = 0; i < this.elementCount; i++) {
                 // verify that seqNum must be of strict + 1 increasing order
-                readNextElement(this.minSeqNum + i, !VERIFY_CHECKSUM);
+                readNextElement(this.minSeqNum + i, !MmapPageIOV2.VERIFY_CHECKSUM);
             }
         }
     }
@@ -126,40 +119,8 @@ public final class MmapPageIOV1 implements PageIO {
         return new SequencedList<>(elements, seqNums);
     }
 
-    // recover will overwrite/update/set this object minSeqNum, capacity and elementCount attributes
-    // to reflect what it recovered from the page
-    @Override
-    public void recover() throws IOException {
-        mapFile();
-        buffer.position(0);
-        this.version = buffer.get();
-        validateVersion(this.version);
-        this.head = 1;
-
-        // force minSeqNum to actual first element seqNum
-        this.minSeqNum = buffer.getLong();
-        // reset back position to first seqNum
-        buffer.position(this.head);
-
-        // reset elementCount to 0 and increment to octal number of valid elements found
-        this.elementCount = 0;
-
-        for (int i = 0; ; i++) {
-            try {
-                // verify that seqNum must be of strict + 1 increasing order
-                readNextElement(this.minSeqNum + i, VERIFY_CHECKSUM);
-                this.elementCount += 1;
-            } catch (MmapPageIOV1.PageIOInvalidElementException e) {
-                // simply stop at first invalid element
-                LOGGER.debug("PageIO recovery element index:{}, readNextElement exception: {}", i, e.getMessage());
-                break;
-            }
-        }
-
-        // if we were not able to read any element just reset minSeqNum to zero
-        if (this.elementCount <= 0) {
-            this.minSeqNum = 0;
-        }
+    public void recover() {
+        throw new UnsupportedOperationException("Recovering v1 pages is not supported anymore.");
     }
 
     @Override
@@ -188,10 +149,9 @@ public final class MmapPageIOV1 implements PageIO {
     }
 
     @Override
-    public void purge() throws IOException {
-        close();
-        Files.delete(this.file.toPath());
-        this.head = 0;
+    public void purge() {
+        throw new UnsupportedOperationException("Purging v1 pages is not supported anymore");
+
     }
 
     @Override
@@ -231,7 +191,8 @@ public final class MmapPageIOV1 implements PageIO {
 
     @Override
     public int persistedByteCount(int byteCount) {
-        return SEQNUM_SIZE + LENGTH_SIZE + byteCount + CHECKSUM_SIZE;
+        return MmapPageIOV2.SEQNUM_SIZE + MmapPageIOV2.LENGTH_SIZE
+            + byteCount + MmapPageIOV2.CHECKSUM_SIZE;
     }
 
     @Override
@@ -261,7 +222,7 @@ public final class MmapPageIOV1 implements PageIO {
             // update capacity to actual raf length. this can happen if a page size was changed on a non empty queue directory for example.
             this.capacity = pageFileCapacity;
 
-            if (this.capacity < MIN_CAPACITY) {
+            if (this.capacity < MmapPageIOV2.MIN_CAPACITY) {
                 throw new IOException(String.format("Page file size is too small to hold elements"));
             }
             this.buffer = raf.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, this.capacity);
@@ -271,11 +232,10 @@ public final class MmapPageIOV1 implements PageIO {
 
     // read and validate next element at page head
     // @param verifyChecksum if true the actual element data will be read + checksumed and compared to written checksum
-    private void readNextElement(long expectedSeqNum, boolean verifyChecksum) throws MmapPageIOV1.PageIOInvalidElementException {
+    private void readNextElement(long expectedSeqNum, boolean verifyChecksum) throws MmapPageIOV2.PageIOInvalidElementException {
         // if there is no room for the seqNum and length bytes stop here
-        // TODO: I know this isn't a great exception message but at the time of writing I couldn't come up with anything better :P
-        if (this.head + SEQNUM_SIZE + LENGTH_SIZE > capacity) {
-            throw new MmapPageIOV1.PageIOInvalidElementException(
+        if (this.head + MmapPageIOV2.SEQNUM_SIZE + MmapPageIOV2.LENGTH_SIZE > capacity) {
+            throw new MmapPageIOV2.PageIOInvalidElementException(
                 "cannot read seqNum and length bytes past buffer capacity");
         }
 
@@ -283,24 +243,24 @@ public final class MmapPageIOV1 implements PageIO {
         int newHead = this.head;
 
         long seqNum = buffer.getLong();
-        newHead += SEQNUM_SIZE;
+        newHead += MmapPageIOV2.SEQNUM_SIZE;
 
         if (seqNum != expectedSeqNum) {
-            throw new MmapPageIOV1.PageIOInvalidElementException(
+            throw new MmapPageIOV2.PageIOInvalidElementException(
                 String.format("Element seqNum %d is expected to be %d", seqNum, expectedSeqNum));
         }
 
         int length = buffer.getInt();
-        newHead += LENGTH_SIZE;
+        newHead += MmapPageIOV2.LENGTH_SIZE;
 
         // length must be > 0
         if (length <= 0) {
-            throw new MmapPageIOV1.PageIOInvalidElementException("Element invalid length");
+            throw new MmapPageIOV2.PageIOInvalidElementException("Element invalid length");
         }
 
         // if there is no room for the proposed data length and checksum just stop here
-        if (newHead + length + CHECKSUM_SIZE > capacity) {
-            throw new MmapPageIOV1.PageIOInvalidElementException(
+        if (newHead + length + MmapPageIOV2.CHECKSUM_SIZE > capacity) {
+            throw new MmapPageIOV2.PageIOInvalidElementException(
                 "cannot read element payload and checksum past buffer capacity");
         }
 
@@ -314,14 +274,14 @@ public final class MmapPageIOV1 implements PageIO {
             int checksum = buffer.getInt();
             int computedChecksum = (int) this.checkSummer.getValue();
             if (computedChecksum != checksum) {
-                throw new MmapPageIOV1.PageIOInvalidElementException(
+                throw new MmapPageIOV2.PageIOInvalidElementException(
                     "Element invalid checksum");
             }
         }
 
         // at this point we recovered a valid element
         this.offsetMap.add(elementOffset);
-        this.head = newHead + length + CHECKSUM_SIZE;
+        this.head = newHead + length + MmapPageIOV2.CHECKSUM_SIZE;
 
         buffer.position(this.head);
     }
@@ -329,28 +289,10 @@ public final class MmapPageIOV1 implements PageIO {
     // we don't have different versions yet so simply check if the version is VERSION_ONE for basic integrity check
     // and if an unexpected version byte is read throw PageIOInvalidVersionException
     private static void validateVersion(byte version)
-        throws MmapPageIOV1.PageIOInvalidVersionException {
+        throws MmapPageIOV2.PageIOInvalidVersionException {
         if (version != VERSION_ONE) {
-            throw new MmapPageIOV1.PageIOInvalidVersionException(String
+            throw new MmapPageIOV2.PageIOInvalidVersionException(String
                 .format("Expected page version=%d but found version=%d", VERSION_ONE, version));
-        }
-    }
-
-    public static final class PageIOInvalidElementException extends IOException {
-
-        private static final long serialVersionUID = 1L;
-
-        public PageIOInvalidElementException(String message) {
-            super(message);
-        }
-    }
-
-    public static final class PageIOInvalidVersionException extends IOException {
-
-        private static final long serialVersionUID = 1L;
-
-        public PageIOInvalidVersionException(String message) {
-            super(message);
         }
     }
 }
