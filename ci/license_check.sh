534eaee86867cdd61d8742caf95ebb9f543e2f19
Andrew Cholakian <andrew@andrewvc.com>
2018-05-22 09:30:31 -0500

    Revert commit that broke dep report
    This reverts commit 8f03e822499d1c8c9f6c68739b419909a2bedb56.
    
    That was part of a larger series of changes that shouldn't be in 6.3
    
    Fixes #9635

--- ci/license_check.sh
+++ ci/license_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.logging.level=info"
 
-./gradlew installDefaultGems
+rake plugin:install-default
 bin/dependencies-report --csv report.csv
 # We want this to show on the CI server
 cat report.csv
