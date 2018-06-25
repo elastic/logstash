fdf5c43ea8d635d303f8139c9ff06cf0f7af43c3
Dan Hermann <dan.hermann@elastic.co>
2018-06-25 08:50:07 -0500

    Port PQ disk space check (#9767) to 5.6

--- logstash-core/src/main/java/org/logstash/common/FsUtil.java
+++ logstash-core/src/main/java/org/logstash/common/FsUtil.java
@@ -21,28 +21,14 @@ public final class FsUtil {
     private static final Logger logger = LogManager.getLogger(FsUtil.class);
 
     /**
-     * Checks if the request number of bytes of free disk space are available under the given
-     * path.
+     * Checks if the requested number of bytes of disk space are available under the given path.
      * @param path Directory to check
      * @param size Bytes of free space requested
-     * @return True iff the
-     * @throws IOException on failure to determine free space for given path's partition
+     * @return True iff the free space in the specified path meets or exceeds the requested space
      */
     public static boolean hasFreeSpace(final Path path, final long size)
-        throws IOException
     {
-        final Set<File> partitionRoots = new HashSet<>(Arrays.asList(File.listRoots()));
-
-        // crawl up file path until we find a root partition
-        File location = path.toFile().getCanonicalFile();
-        while (!partitionRoots.contains(location)) {
-            location = location.getParentFile();
-            if (location == null) {
-                throw new IllegalStateException(String.format("Unable to determine the partition that contains '%s'", path));
-            }
-        }
-
-        final long freeSpace = location.getFreeSpace();
+        final long freeSpace = path.toFile().getFreeSpace();
 
         if (freeSpace == 0L && IS_WINDOWS) {
             // On Windows, SUBST'ed drives report 0L from getFreeSpace().
@@ -50,10 +36,10 @@ public final class FsUtil {
             // There is no straightforward fix for this and it seems a fix is included in Java 9.
             // One alternative is to launch and parse a DIR command and look at the reported free space.
             // This is a temporary fix to get the CI tests going which relies on SUBST'ed drives to manage long paths.
-            logger.warn("Cannot retrieve free space on " +  location.toString() +  ". This is probably a SUBST'ed drive.");
+            logger.warn("Cannot retrieve free space on " +  path.toString() +  ". This is probably a SUBST'ed drive.");
             return true;
         }
 
-        return location.getFreeSpace() >= size;
+        return freeSpace >= size;
     }
 }
