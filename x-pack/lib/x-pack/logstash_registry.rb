245e4a8cb11dc3ea0152b574d5926ce03322b5e5
Jake Landis <jake.landis@elastic.co>
2018-07-02 10:53:44 -0500

    Introduction of the Azure module (experimental)
    This module leverages the Azure Event Hub to read events from the activity log and sql diagnostics.  Please note this module is considered experimental till otherwise noted.
    
    This change also introduces logstash-input-azure_event_hubs as a default plugin
    
    Fixes #9729

--- x-pack/lib/x-pack/logstash_registry.rb
+++ x-pack/lib/x-pack/logstash_registry.rb
@@ -1,13 +1,16 @@
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License;
 # you may not use this file except in compliance with the Elastic License.
-#
+
+require "logstash/runner" # needed for LogStash::XPACK_PATH
+$LOAD_PATH << File.join(LogStash::XPACK_PATH, "modules", "azure", "lib")
 require "logstash/plugins/registry"
 require "logstash/modules/util"
 require "monitoring/monitoring"
 require "monitoring/inputs/metrics"
 require "config_management/extension"
 require "modules/xpack_scaffold"
+require "filters/azure_event"
 
 LogStash::PLUGIN_REGISTRY.add(:input, "metrics", LogStash::Inputs::Metrics)
 LogStash::PLUGIN_REGISTRY.add(:universal, "monitoring", LogStash::MonitoringExtension)
@@ -17,3 +20,10 @@ LogStash::PLUGIN_REGISTRY.add(:modules, "arcsight",
                                                                    File.join(File.dirname(__FILE__), "..", "..", "modules", "arcsight", "configuration"),
                                                                    ["basic", "trial", "standard", "gold", "platinum"]
                               ))
+
+LogStash::PLUGIN_REGISTRY.add(:modules, "azure",
+                              LogStash::Modules::XpackScaffold.new("azure",
+                                                                   File.join(File.dirname(__FILE__), "..", "..", "modules", "azure", "configuration"),
+                                                                   ["basic", "trial", "standard", "gold", "platinum"]
+                              ))
+LogStash::PLUGIN_REGISTRY.add(:filter, "azure_event", LogStash::Filters::AzureEvent)
