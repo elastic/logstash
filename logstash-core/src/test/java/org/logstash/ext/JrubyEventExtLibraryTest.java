d214b252de8aa2835ffedd33b7b29b5e066bc5d5
Dan Hermann <dan.hermann@elastic.co>
2018-07-05 08:54:23 -0500

    back-port #9764
    Fixes #9821

---
+++ logstash-core/src/test/java/org/logstash/ext/JrubyEventExtLibraryTest.java
@@ -0,0 +1,27 @@
+package org.logstash.ext;
+
+import org.assertj.core.api.Assertions;
+import org.jruby.RubyString;
+import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.junit.Test;
+import org.logstash.RubyUtil;
+
+public class JrubyEventExtLibraryTest {
+
+    @Test
+    public void correctlyHandlesNonAsciiKeys() {
+        final RubyString key = rubyString("[テストフィールド]");
+        final RubyString value = rubyString("someValue");
+        final ThreadContext context = RubyUtil.RUBY.getCurrentContext();
+        final JrubyEventExtLibrary.RubyEvent event =
+            JrubyEventExtLibrary.RubyEvent.newRubyEvent(context.runtime);
+        event.ruby_set_field(context, key, value);
+        Assertions.assertThat(event.ruby_to_json(context, new IRubyObject[0]).asJavaString())
+            .contains("\"テストフィールド\":\"someValue\"");
+    }
+
+    private static RubyString rubyString(final String java) {
+        return RubyUtil.RUBY.newString(java);
+    }
+}
