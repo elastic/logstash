3c7d1e836bbdd0b760a0e05599a020f07ac15a6f
Guy Boertje <guy@elastic.co>
2018-10-19 20:17:02 +0100

    So this is the actual extent of changes (I thought it was to simple) Fixed the docs omission Fixed the load path issue Fixed the ERB binding context problem Added some basic happy path tests

--- x-pack/lib/x-pack/logstash_registry.rb
+++ x-pack/lib/x-pack/logstash_registry.rb
@@ -3,7 +3,10 @@
 # you may not use this file except in compliance with the Elastic License.
 
 require "logstash/runner" # needed for LogStash::XPACK_PATH
-$LOAD_PATH << File.join(LogStash::XPACK_PATH, "modules", "azure", "lib")
+xpack_modules = ["azure", "arcsight"]
+xpack_modules.each do |name|
+  $LOAD_PATH << File.join(LogStash::XPACK_PATH, "modules", name, "lib")
+end
 require "logstash/plugins/registry"
 require "logstash/modules/util"
 require "monitoring/monitoring"
@@ -15,15 +18,14 @@ require "filters/azure_event"
 LogStash::PLUGIN_REGISTRY.add(:input, "metrics", LogStash::Inputs::Metrics)
 LogStash::PLUGIN_REGISTRY.add(:universal, "monitoring", LogStash::MonitoringExtension)
 LogStash::PLUGIN_REGISTRY.add(:universal, "config_management", LogStash::ConfigManagement::Extension)
-LogStash::PLUGIN_REGISTRY.add(:modules, "arcsight",
-                              LogStash::Modules::XpackScaffold.new("arcsight",
-                                                                   File.join(File.dirname(__FILE__), "..", "..", "modules", "arcsight", "configuration"),
-                                                                   ["basic", "trial", "standard", "gold", "platinum"]
-                              ))
 
-LogStash::PLUGIN_REGISTRY.add(:modules, "azure",
-                              LogStash::Modules::XpackScaffold.new("azure",
-                                                                   File.join(File.dirname(__FILE__), "..", "..", "modules", "azure", "configuration"),
-                                                                   ["basic", "trial", "standard", "gold", "platinum"]
-                              ))
+license_levels = Hash.new
+license_levels.default = ["basic", "trial", "standard", "gold", "platinum"]
+
+xpack_modules.each do |name|
+  path = File.join(File.dirname(__FILE__), "..", "..", "modules", name, "configuration")
+  LogStash::PLUGIN_REGISTRY.add(:modules, name,
+    LogStash::Modules::XpackScaffold.new(name, path, license_levels[name]))
+end
+
 LogStash::PLUGIN_REGISTRY.add(:filter, "azure_event", LogStash::Filters::AzureEvent)
--- x-pack/modules/arcsight/lib/arcsight_module_config_helper.rb
+++ x-pack/modules/arcsight/lib/arcsight_module_config_helper.rb
@@ -6,56 +6,57 @@ require 'logstash/namespace'
 
 module LogStash
   module Arcsight
-    module Confighelper
+    module ConfigHelper
       extend self
-      def kafka_input_ssl_sasl_config
-        security_protocol = setting("var.input.kafka.security_protocol", "unset")
+      def kafka_input_ssl_sasl_config(bound_scope)
+        security_protocol = bound_scope.setting("var.input.kafka.security_protocol", "unset")
         return "" if security_protocol == "unset"
         lines = ["security_protocol => '#{security_protocol}'"]
-        lines.push("ssl_truststore_type => '#{setting("var.input.kafka.ssl_truststore_type", "")}'")
+        lines.push("ssl_truststore_type => '#{bound_scope.setting("var.input.kafka.ssl_truststore_type", "")}'")
 
-        ssl_truststore_location = setting("var.input.kafka.ssl_truststore_location", "")
+        ssl_truststore_location = bound_scope.setting("var.input.kafka.ssl_truststore_location","")
         lines.push("ssl_truststore_location => '#{ssl_truststore_location}'") unless ssl_truststore_location.empty?
 
-        lines.push("ssl_truststore_password => '#{setting("var.input.kafka.ssl_truststore_password", "")}'")
-        lines.push("ssl_keystore_type => '#{setting("var.input.kafka.ssl_keystore_type", "")}'")
+        ["ssl_truststore_password", "ssl_keystore_password", "ssl_key_password"].each do |name|
+          full_name = "var.input.kafka.".concat(name)
+          lines.push("#{name} => '#{bound_scope.raw_setting(full_name)}'") if bound_scope.has_setting?(full_name)
+        end
 
-        ssl_keystore_location = setting("var.input.kafka.ssl_keystore_location", "")
+        lines.push("ssl_keystore_type => '#{bound_scope.setting("var.input.kafka.ssl_keystore_type", "")}'")
+
+        ssl_keystore_location = bound_scope.setting("var.input.kafka.ssl_keystore_location","")
         lines.push("ssl_keystore_location => '#{ssl_keystore_location}'") unless ssl_keystore_location.empty?
 
-        lines.push("ssl_keystore_password => '#{setting("var.input.kafka.ssl_keystore_password", "")}'")
-        lines.push("ssl_key_password => '#{setting("var.input.kafka.ssl_key_password", "")}'")
+        lines.push("sasl_mechanism => '#{bound_scope.setting("var.input.kafka.sasl_mechanism", "")}'")
+        lines.push("sasl_kerberos_service_name => '#{bound_scope.setting("var.input.kafka.sasl_kerberos_service_name", "")}'")
 
-        lines.push("sasl_mechanism => '#{setting("var.input.kafka.sasl_mechanism", "")}'")
-        lines.push("sasl_kerberos_service_name => '#{setting("var.input.kafka.sasl_kerberos_service_name", "")}'")
-
-        jaas_path = setting("var.input.kafka.jaas_path", "")
+        jaas_path = bound_scope.setting("var.input.kafka.jaas_path","")
         lines.push("jaas_path => '#{jaas_path}'") unless jaas_path.empty?
 
-        kerberos_config = setting("var.input.kafka.kerberos_config", "")
+        kerberos_config = bound_scope.setting("var.input.kafka.kerberos_config","")
         lines.push("kerberos_config => '#{kerberos_config}'") unless kerberos_config.empty?
 
         lines.compact.join("\n    ")
       end
 
-      def tcp_input_ssl_config
-        ssl_enabled = setting("var.input.tcp.ssl_enable", false)
-        return "" if ssl_enabled == "false"
+      def tcp_input_ssl_config(bound_scope)
+        ssl_enabled = bound_scope.setting("var.input.tcp.ssl_enable", false)
+        return "" unless ssl_enabled
         lines = ["ssl_enable => true"]
 
-        verify_enabled = setting("var.input.tcp.ssl_verify", true)
+        verify_enabled = bound_scope.setting("var.input.tcp.ssl_verify", true)
         lines.push("ssl_verify => #{verify_enabled}")
 
-        ssl_cert = setting("var.input.tcp.ssl_cert", "")
+        ssl_cert = bound_scope.setting("var.input.tcp.ssl_cert","")
         lines.push("ssl_cert => '#{ssl_cert}'") unless ssl_cert.empty?
 
-        ssl_key = setting("var.input.tcp.ssl_key", "")
-        lines.push("ssl_key => '#{ssl_key}'") unless ssl_key.empty
+        ssl_key = bound_scope.setting("var.input.tcp.ssl_key","")
+        lines.push("ssl_key => '#{ssl_key}'") unless ssl_key.empty?
 
-        lines.push("ssl_key_passphrase => '#{ setting("var.input.tcp.ssl_key_passphrase", "")}'")
+        lines.push("ssl_key_passphrase => '#{ bound_scope.setting("var.input.tcp.ssl_key_passphrase", "")}'")
 
-        certs_array_as_string = array_to_string(
-          get_setting(LogStash::Setting::SplittableStringArray.new("var.input.tcp.ssl_extra_chain_certs", String, []))
+        certs_array_as_string = bound_scope.array_to_string(
+          bound_scope.get_setting(LogStash::Setting::SplittableStringArray.new("var.input.tcp.ssl_extra_chain_certs", String, []))
         )
         lines.push("ssl_extra_chain_certs => #{certs_array_as_string}")
 
