a88a904ad6ca985a7215e5eeb1b6fbfa338e9ae3
Dan Hermann <dan.hermann@elastic.co>
2018-12-16 14:41:53 -0600

    Remove obsolete plugin implementation examples

--- logstash-core/src/main/java/co/elastic/logstash/api/Filter.java
+++ logstash-core/src/main/java/co/elastic/logstash/api/Filter.java
@@ -1,9 +1,8 @@
 package co.elastic.logstash.api;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import org.logstash.Event;
+
+import java.util.Collection;
 
 /**
  * A Logstash Filter.
@@ -12,67 +11,4 @@ public interface Filter extends Plugin {
 
     Collection<Event> filter(Collection<Event> events);
 
-    @LogstashPlugin(name = "mutate")
-    final class Mutate implements Filter {
-
-        private static final PluginConfigSpec<String> FIELD_CONFIG =
-            Configuration.requiredStringSetting("field");
-
-        private static final PluginConfigSpec<String> VALUE_CONFIG =
-            Configuration.requiredStringSetting("value");
-
-        private final String field;
-
-        private final String value;
-
-        /**
-         * Required Constructor Signature only taking a {@link Configuration}.
-         * @param configuration Logstash Configuration
-         * @param context Logstash Context
-         */
-        public Mutate(final Configuration configuration, final Context context) {
-            this.field = (String)configuration.get(FIELD_CONFIG);
-            this.value = (String)configuration.get(VALUE_CONFIG);
-        }
-
-        @Override
-        public Collection<Event> filter(final Collection<Event> events) {
-            //TODO: Impl.
-            return events;
-        }
-
-        @Override
-        public Collection<PluginConfigSpec<?>> configSchema() {
-            return Arrays.asList(FIELD_CONFIG, VALUE_CONFIG);
-        }
-    }
-
-    @LogstashPlugin(name = "java-clone")
-    final class Clone implements Filter {
-
-        private Event clone;
-
-        private long lastSeq = -1L;
-
-        /**
-         * Required Constructor Signature only taking a {@link Configuration}.
-         * @param configuration Logstash Configuration
-         * @param context Logstash Context
-         */
-        public Clone(final Configuration configuration, final Context context) {
-        }
-
-        @Override
-        public Collection<Event> filter(final Collection<Event> events) {
-            for (Event e : events) {
-                e.setField("java-clone", "was here");
-            }
-            return events;
-        }
-
-        @Override
-        public Collection<PluginConfigSpec<?>> configSchema() {
-            return Collections.emptyList();
-        }
-    }
 }
--- logstash-core/src/main/java/co/elastic/logstash/api/Output.java
+++ logstash-core/src/main/java/co/elastic/logstash/api/Output.java
@@ -1,10 +1,8 @@
 package co.elastic.logstash.api;
 
-import java.io.PrintStream;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.concurrent.CountDownLatch;
 import org.logstash.Event;
+
+import java.util.Collection;
 
 /**
  * A Logstash Pipeline Output.
@@ -21,48 +19,4 @@ public interface Output extends Plugin {
 
     void awaitStop() throws InterruptedException;
 
-    @LogstashPlugin(name = "output")
-    final class StreamOutput implements Output {
-
-        private final PrintStream output;
-
-        private volatile boolean stopped;
-
-        private final CountDownLatch done = new CountDownLatch(1);
-
-        /**
-         * Required Constructor Signature only taking a {@link Configuration}.
-         * @param configuration Logstash Configuration
-         * @param context Logstash Context
-         */
-        public StreamOutput(final Configuration configuration, final Context context) {
-            this.output = new PrintStream(System.out);
-        }
-
-        @Override
-        public void output(final Collection<Event> events) {
-            try {
-
-            } finally {
-                stopped = true;
-                done.countDown();
-            }
-        }
-
-        @Override
-        public void stop() {
-            output.close();
-            stopped = true;
-        }
-
-        @Override
-        public void awaitStop() throws InterruptedException {
-            done.await();
-        }
-
-        @Override
-        public Collection<PluginConfigSpec<?>> configSchema() {
-            return Collections.emptyList();
-        }
-    }
 }
