19789bc06fb50054a23cefb2a0403c69ced8bab9
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2019-01-08 20:35:33 +0000

    plugin management: update internal bundler to 1.17.x APIs

--- lib/pluginmanager/bundler/logstash_injector.rb
+++ lib/pluginmanager/bundler/logstash_injector.rb
@@ -56,41 +56,36 @@ module Bundler
     # And managing the gemfile is down by using our own Gemfile parser, this allow us to
     # make it work with gems that are already defined in the gemfile.
     def inject(gemfile_path, lockfile_path, dependencies)
-      if Bundler.settings[:frozen]
-        # ensure the lock and Gemfile are synced
-        Bundler.definition.ensure_equivalent_gemfile_and_lockfile(true)
-        # temporarily remove frozen while we inject
-        frozen = Bundler.settings.delete(:frozen)
-      end
+      Bundler.definition.ensure_equivalent_gemfile_and_lockfile(true) if Bundler.settings[:frozen]
 
-      builder = Dsl.new
-      gemfile = LogStash::Gemfile.new(File.new(gemfile_path, "r+")).load
+      Bundler.settings.temporary(:frozen => false) do
+        builder = Dsl.new
+        gemfile = LogStash::Gemfile.new(File.new(gemfile_path, "r+")).load
 
-      begin
-        @deps.each do |dependency|
-          gemfile.update(dependency.name, dependency.requirement)
-        end
-
-        # If the dependency is defined in the gemfile, lets try to update the version with the one we have
-        # with the pack.
-        dependencies.each do |dependency|
-          if gemfile.defined_in_gemfile?(dependency.name)
+        begin
+          @deps.each do |dependency|
             gemfile.update(dependency.name, dependency.requirement)
           end
-        end
 
-        builder.eval_gemfile("bundler file", gemfile.generate())
-        definition = builder.to_definition(lockfile_path, {})
-        definition.lock(lockfile_path)
-        gemfile.save
-      rescue => e
-        # the error should be handled elsewhere but we need to get the original file if we dont
-        # do this logstash will be in an inconsistent state
-        gemfile.restore!
-        raise e
+          # If the dependency is defined in the gemfile, lets try to update the version with the one we have
+          # with the pack.
+          dependencies.each do |dependency|
+            if gemfile.defined_in_gemfile?(dependency.name)
+              gemfile.update(dependency.name, dependency.requirement)
+            end
+          end
+
+          builder.eval_gemfile("bundler file", gemfile.generate())
+          definition = builder.to_definition(lockfile_path, {})
+          definition.lock(lockfile_path)
+          gemfile.save
+        rescue => e
+          # the error should be handled elsewhere but we need to get the original file if we dont
+          # do this logstash will be in an inconsistent state
+          gemfile.restore!
+          raise e
+        end
       end
-    ensure
-      Bundler.settings[:frozen] = "1" if frozen
     end
   end
 end
