e9b2d7f75361e597c25359f464896f71922d2b1c
Dan Hermann <dan.hermann@elastic.co>
2019-01-21 17:30:55 -0600

    resolve compiler warnings

--- logstash-core/src/main/java/co/elastic/logstash/api/Codec.java
+++ logstash-core/src/main/java/co/elastic/logstash/api/Codec.java
@@ -76,6 +76,8 @@ public interface Codec extends Plugin {
 
     class EncodeException extends Exception {
 
+        private static final long serialVersionUID = 1L;
+
         public EncodeException(String message) {
             super(message);
         }
