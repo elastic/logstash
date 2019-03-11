f0006a051e21e0bc3827825caf628e13cb67b219
Rob Bavey <rob.bavey@elastic.co>
2019-03-11 16:43:56 -0400

    Fix issue setting 'enable_metric => false' on a plugin
    This commit fixes a ClassCastException which happens when
     a plugin has the `enable_metric` setting set to false - a
     NullMetricExt is assumed, but that is only created when
     'metric.collect' is set to 'false' in the Logstash configuration,
     not when an individual plugin disables its metrics.
    
    Fixes #10538

--- logstash-core/spec/logstash/filters/base_spec.rb
+++ logstash-core/spec/logstash/filters/base_spec.rb
@@ -322,4 +322,18 @@ describe LogStash::Filters::NOOP do
     end
 
   end
+
+  describe "when metrics are disabled" do
+    describe "An error should not be raised, and the event should be processed" do
+      config <<-CONFIG
+        filter {
+          noop { enable_metric => false }
+        }
+      CONFIG
+
+      sample_one("type" => "noop", "tags" => {"blackhole" => "go"}) do
+        expect(subject.get("[tags][blackhole]")).to eq("go")
+      end
+    end
+  end
 end
