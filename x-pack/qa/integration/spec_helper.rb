ebad88a358449df09dae97419c6802e6e5b967da
Joao Duarte <jsvduarte@gmail.com>
2019-03-13 12:00:38 +0000

    make monitoring specs resilient to internal api number changes
    Fixes #10552

--- x-pack/qa/integration/spec_helper.rb
+++ x-pack/qa/integration/spec_helper.rb
@@ -2,7 +2,7 @@
 # or more contributor license agreements. Licensed under the Elastic License;
 # you may not use this file except in compliance with the Elastic License.
 
-MONITORING_INDEXES = ".monitoring-logstash-2*,.monitoring-logstash-6*"
+MONITORING_INDEXES = ".monitoring-logstash-*"
 
 require_relative "support/helpers"
 require_relative "support/shared_examples"
