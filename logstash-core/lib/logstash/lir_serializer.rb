911f4c2b699fbd7aa29713fcda5aae0b7ee96dc9
Mike Place <mike.place@elastic.co>
2019-03-15 09:24:17 -0600

    Remove license and add encoding

--- logstash-core/lib/logstash/lir_serializer.rb
+++ logstash-core/lib/logstash/lir_serializer.rb
@@ -1,8 +1,4 @@
-# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License;
-# you may not use this file except in compliance with the Elastic License.
-#
-
+# encoding: utf-8
 require 'logstash-core'
 require 'logstash/compiler'
 
