7734be2a61eda32693cdc52df9f339ad15bf1ca8
Joao Duarte <jsvduarte@gmail.com>
2019-04-05 11:16:44 +0100

    work around jruby-5642

--- lib/systeminstall/pleasewrap.rb
+++ lib/systeminstall/pleasewrap.rb
@@ -8,5 +8,25 @@ Gem.use_paths(LogStash::Environment.logstash_gem_home)
 
 #libdir = File.expand_path("../lib", File.dirname(__FILE__))
 #$LOAD_PATH << libdir if File.exist?(File.join(libdir, "pleaserun", "cli.rb"))
+
+require 'open3'
+
+# Work around for https://github.com/elastic/logstash/issues/10593
+# Issue on JRUBY https://github.com/jruby/jruby/issues/5642
+# Workaround retrieved from https://github.com/jruby/jruby/issues/5642#issuecomment-479671017
+if java.lang.System.getProperty("java.version").start_with?("11")
+  class IO
+    def self.pipe
+      readwrite = Java::int[2].new
+      JRuby.runtime.posix.pipe(readwrite)
+      return readwrite.map do |fd|
+        io = IO.for_fd(fd)
+        io.close_on_exec = true
+        io
+      end
+    end
+  end
+end
+
 require "pleaserun/cli"
 exit(PleaseRun::CLI.run || 0)
