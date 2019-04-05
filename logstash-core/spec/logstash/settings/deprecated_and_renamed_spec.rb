6066790154a6b9a2d147c78b04914293d6f4ce83
Joao Duarte <jsvduarte@gmail.com>
2019-04-05 11:08:41 +0100

    use right exception for deprecated setting

--- logstash-core/spec/logstash/settings/deprecated_and_renamed_spec.rb
+++ logstash-core/spec/logstash/settings/deprecated_and_renamed_spec.rb
@@ -16,15 +16,4 @@ describe LogStash::Setting::DeprecatedAndRenamed do
       end
     end
   end
-
-  describe '#value' do
-    it 'fails with deprecation argument error' do
-      expect { setting.value }.to raise_exception do |exception|
-        expect(exception).to be_a_kind_of(ArgumentError)
-        expect(exception.message).to match(/deprecated and removed/)
-        expect(exception.message).to include("option.deprecated")
-      end
-    end
-  end
-
 end
