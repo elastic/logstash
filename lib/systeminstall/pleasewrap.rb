392b90ba90ea66153b8ca9f461bf28a73f42ac88
João Duarte <jsvd@users.noreply.github.com>
2019-04-05 15:33:06 +0100

    Update pleasewrap.rb

--- lib/systeminstall/pleasewrap.rb
+++ lib/systeminstall/pleasewrap.rb
@@ -14,10 +14,10 @@ require 'open3'
 # Work around for https://github.com/elastic/logstash/issues/10593
 # Issue on JRUBY https://github.com/jruby/jruby/issues/5642
 # Workaround retrieved from https://github.com/jruby/jruby/issues/5642#issuecomment-479671017
+if RUBY_ENGINE_VERSION != "9.2.6.0"
+  raise "A workaround is in place for JRUBY-5642 that should be applied only to JRuby 9.2.6.0, but found #{RUBY_ENGINE_VERSION}"
+end
 if java.lang.System.getProperty("java.version").start_with?("11")
-  if RUBY_ENGINE_VERSION != "9.2.6.0"
-    raise "A workaround is in place for JRUBY-5642 that should be applied only to JRuby 9.2.6.0, but found #{RUBY_ENGINE_VERSION}"
-  end
   class IO
     def self.pipe
       readwrite = Java::int[2].new
