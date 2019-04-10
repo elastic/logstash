96696718729939d50e81c6e0ad96e9a239806edf
Joao Duarte <jsvduarte@gmail.com>
2019-04-10 14:52:59 +0100

    bump jruby to 9.2.7.0
    Revert "work around jruby-5642 during package installation on jdk11 (#10658)"
    
    This reverts commit 033c8963301ab887037334d12b3d0968cf2ee5a8.
    
    skip the bundler-1.16.6 files when unpacking jruby
    
    Fixes #10674

--- lib/systeminstall/pleasewrap.rb
+++ lib/systeminstall/pleasewrap.rb
@@ -8,28 +8,5 @@ Gem.use_paths(LogStash::Environment.logstash_gem_home)
 
 #libdir = File.expand_path("../lib", File.dirname(__FILE__))
 #$LOAD_PATH << libdir if File.exist?(File.join(libdir, "pleaserun", "cli.rb"))
-
-require 'open3'
-
-# Work around for https://github.com/elastic/logstash/issues/10593
-# Issue on JRUBY https://github.com/jruby/jruby/issues/5642
-# Workaround retrieved from https://github.com/jruby/jruby/issues/5642#issuecomment-479671017
-if RUBY_ENGINE_VERSION != "9.2.6.0"
-  raise "A workaround is in place for JRUBY-5642 that should be applied only to JRuby 9.2.6.0, but found #{RUBY_ENGINE_VERSION}"
-end
-if java.lang.System.getProperty("java.version").start_with?("11")
-  class IO
-    def self.pipe
-      readwrite = Java::int[2].new
-      JRuby.runtime.posix.pipe(readwrite)
-      return readwrite.map do |fd|
-        io = IO.for_fd(fd)
-        io.close_on_exec = true
-        io
-      end
-    end
-  end
-end
-
 require "pleaserun/cli"
 exit(PleaseRun::CLI.run || 0)
