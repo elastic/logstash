9a78a6ce80880a0946956c384d8fc3f022d3b749
Dan Hermann <dan.hermann@elastic.co>
2019-05-21 07:49:38 -0500

    Document copy semantics of QueueWriter::push method
    Fixes #10808

--- logstash-core/src/main/java/org/logstash/execution/queue/QueueWriter.java
+++ logstash-core/src/main/java/org/logstash/execution/queue/QueueWriter.java
@@ -3,14 +3,16 @@ package org.logstash.execution.queue;
 import java.util.Map;
 
 /**
- * Writes to the Queue.
+ * Writes to the queue.
  */
 public interface QueueWriter {
 
     /**
      * Pushes a single event to the Queue, blocking indefinitely if the Queue is not ready for a
-     * write.
-     * @param event Logstash Event Data
+     * write. Implementations of this interface must produce events from a deep copy of the supplied
+     * map because upstream clients of this interface may reuse map instances between calls to push.
+     *
+     * @param event Logstash event data
      */
     void push(Map<String, Object> event);
 }
