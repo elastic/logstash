4e0ce6e416c2841a10e45de13c31f87bd5f68c01
Mike Place <mike.place@elastic.co>
2019-06-26 14:29:38 +0200

    Starting to audit tests

--- logstash-core/spec/logstash/api/commands/stats_spec.rb
+++ logstash-core/spec/logstash/api/commands/stats_spec.rb
@@ -17,7 +17,12 @@ describe LogStash::Api::Commands::Stats do
     let(:report_method) { :events }
 
     it "return events information" do
-      expect(report.keys).to include(:in, :filtered, :out)
+      expect(report.keys).to include(
+        :in,
+        :filtered,
+        :out,
+        :duration_in_millis,
+        :queue_push_duration_in_millis)
     end
   end
   
@@ -40,13 +45,26 @@ describe LogStash::Api::Commands::Stats do
       expect(report).not_to be_empty
     end
 
-    it "return heap information" do
-      expect(report.keys).to include(:heap_used_in_bytes)
+    it "return memory information" do
+      expect(report.keys).to include(
+        :heap_used_percent,
+        :heap_committed_in_bytes,
+        :heap_max_in_bytes,
+        :heap_used_in_bytes,
+        :non_heap_used_in_bytes,
+        :non_heap_committed_in_bytes,
+        :pools
+      )
     end
-
-    it "return non heap information" do
-      expect(report.keys).to include(:non_heap_used_in_bytes)
-    end
-
   end
+
+  describe "os stats" do
+    let(:report_method) { :os }
+
+    it "return os information" do
+      expect(report).not_to be_empty
+    end
+  end
+
+
 end
