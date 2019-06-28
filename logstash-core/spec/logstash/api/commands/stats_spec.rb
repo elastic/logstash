f57901d9dc32ea15ee86bd2a2eaa5da6ab35b314
Mike Place <mike.place@elastic.co>
2019-06-28 15:23:24 +0200

    Additional field checking in stats

--- logstash-core/spec/logstash/api/commands/stats_spec.rb
+++ logstash-core/spec/logstash/api/commands/stats_spec.rb
@@ -7,7 +7,7 @@ describe LogStash::Api::Commands::Stats do
   let(:report_method) { :run }
   subject(:report) do
     factory = ::LogStash::Api::CommandFactory.new(LogStash::Api::Service.new(@agent))
-   
+
     factory.build(:stats).send(report_method)
   end
 
@@ -25,10 +25,10 @@ describe LogStash::Api::Commands::Stats do
         :queue_push_duration_in_millis)
     end
   end
-  
+
   describe "#hot_threads" do
     let(:report_method) { :hot_threads }
-    
+
     it "should return hot threads information as a string" do
       expect(report.to_s).to be_a(String)
     end
@@ -40,7 +40,7 @@ describe LogStash::Api::Commands::Stats do
 
   describe "memory stats" do
     let(:report_method) { :memory }
-      
+
     it "return hot threads information" do
       expect(report).not_to be_empty
     end
@@ -58,13 +58,75 @@ describe LogStash::Api::Commands::Stats do
     end
   end
 
-  describe "os stats" do
-    let(:report_method) { :os }
+  describe "jvm stats" do
+    let(:report_method) { :jvm }
 
-    it "return os information" do
-      expect(report).not_to be_empty
+    it "return jvm information" do
+      expect(report.keys).to include(
+        :threads,
+        :mem,
+        :gc,
+        :uptime_in_millis
+      )
+      expect(report[:threads].keys).to include(
+        :count,
+        :peak_count
+      )
     end
   end
+
+  describe "reloads stats" do
+    let(:report_method) { :reloads }
+
+    it "return reloads information" do
+      expect(report.keys).to include(
+      :successes,
+      :failures,
+      )
+    end
+  end
+
+  describe "pipeline stats" do
+    let(:report_method) { :pipeline }
+
+    it "returns pipelines information" do
+      expect(report.keys).to include (
+        :main
+      )
+      expect(report[:main].keys).to include(
+        :events,
+        :plugins,
+        :reloads,
+        :queue,
+      )
+      expect(report[:main][:events].keys).to include(
+        :in,
+        :filtered,
+        :duration_in_millis,
+        :out,
+        :queue_push_duration_in_millis
+      )
+      expect(report[:main][:plugins].keys).to include(
+        :inputs,
+        :codecs,
+        :filters,
+        :outputs
+      )
+      for input in report[:main][:plugins][:inputs]
+        expect(input.keys).to include(
+          :id,
+          :events
+        )
+      end
+      for output in report[:main][:plugins][:outputs]
+        expect(output.keys).to include(
+          :id,
+          :events
+        )
+      end
+    end
+  end
+
 
 
 end
