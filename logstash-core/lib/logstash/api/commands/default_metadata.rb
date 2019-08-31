d2a094629c462594d42cb2590dd23d85dc466d3f
Mike Place <mike.place@elastic.co>
2019-08-31 20:32:47 +0200

    Add cluster_uuid setting to default config file, displaying it in Node stats HTTP API
    Fixes #11106

--- logstash-core/lib/logstash/api/commands/default_metadata.rb
+++ logstash-core/lib/logstash/api/commands/default_metadata.rb
@@ -7,20 +7,31 @@ module LogStash
     module Commands
       class DefaultMetadata < Commands::Base
         def all
-          {:host => host,
-           :version => version,
-           :http_address => http_address,
-           :id => service.agent.id,
-           :name => service.agent.name,
-           :ephemeral_id => service.agent.ephemeral_id,
-           :status => "green",  # This is hard-coded to mirror x-pack behavior
-           :snapshot => ::BUILD_INFO["build_snapshot"],
-           :pipeline => {
-             :workers => LogStash::SETTINGS.get("pipeline.workers"),
-             :batch_size => LogStash::SETTINGS.get("pipeline.batch.size"),
-             :batch_delay => LogStash::SETTINGS.get("pipeline.batch.delay"),
-           }
-           }
+          res = {:host => host,
+             :version => version,
+             :http_address => http_address,
+             :id => service.agent.id,
+             :name => service.agent.name,
+             :ephemeral_id => service.agent.ephemeral_id,
+             :status => "green",  # This is hard-coded to mirror x-pack behavior
+             :snapshot => ::BUILD_INFO["build_snapshot"],
+             :pipeline => {
+               :workers => LogStash::SETTINGS.get("pipeline.workers"),
+               :batch_size => LogStash::SETTINGS.get("pipeline.batch.size"),
+               :batch_delay => LogStash::SETTINGS.get("pipeline.batch.delay"),
+             },
+            }
+          monitoring = {}
+          if enabled_xpack_monitoring?
+            monitoring = monitoring.merge({
+                        :hosts => LogStash::SETTINGS.get("xpack.monitoring.elasticsearch.hosts"),
+                        :username => LogStash::SETTINGS.get("xpack.monitoring.elasticsearch.username")
+                        })
+          end
+          if LogStash::SETTINGS.set?("monitoring.cluster_uuid")
+            monitoring = monitoring.merge({:cluster_uuid => LogStash::SETTINGS.get("monitoring.cluster_uuid")})
+          end
+          res.merge(monitoring.empty? ? {} : {:monitoring => monitoring})
         end
 
         def host
@@ -35,6 +46,12 @@ module LogStash
           @http_address ||= service.get_shallow(:http_address).value
         rescue ::LogStash::Instrument::MetricStore::MetricNotFound, NoMethodError => e
           nil
+        end
+
+        private
+        def enabled_xpack_monitoring?
+          LogStash::SETTINGS.registered?("xpack.monitoring.enabled") &&
+          LogStash::SETTINGS.get("xpack.monitoring.enabled")
         end
       end
     end
