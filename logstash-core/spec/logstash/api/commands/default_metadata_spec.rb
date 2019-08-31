ec0c6e11541119324827db08ae068b939069707b
Mike Place <mike.place@elastic.co>
2019-08-31 20:32:47 +0200

    Add cluster_uuid setting to default config file, displaying it in Node stats HTTP API

--- logstash-core/spec/logstash/api/commands/default_metadata_spec.rb
+++ logstash-core/spec/logstash/api/commands/default_metadata_spec.rb
@@ -4,6 +4,10 @@ require "spec_helper"
 describe LogStash::Api::Commands::DefaultMetadata do
   include_context "api setup"
 
+  def registerIfNot(setting)
+    LogStash::SETTINGS.register(setting) unless LogStash::SETTINGS.registered?(setting.name)
+  end
+
   let(:report_method) { :all }
   subject(:report) do
     factory = ::LogStash::Api::CommandFactory.new(LogStash::Api::Service.new(@agent))
@@ -12,9 +16,43 @@ describe LogStash::Api::Commands::DefaultMetadata do
 
   let(:report_class) { described_class }
 
+  before :all do
+    registerIfNot(LogStash::Setting::Boolean.new("xpack.monitoring.enabled", false))
+    registerIfNot(LogStash::Setting::ArrayCoercible.new("xpack.monitoring.elasticsearch.hosts", String, [ "http://localhost:9200" ] ))
+    registerIfNot(LogStash::Setting::NullableString.new("xpack.monitoring.elasticsearch.username", "logstash_TEST system"))
+    registerIfNot(LogStash::Setting::NullableString.new("xpack.monitoring.elasticsearch.username", "logstash_TEST system"))
+  end
+
+  after :each do
+    LogStash::SETTINGS.set_value("xpack.monitoring.enabled", false)
+  end
+
   describe "#plugins_stats_report" do
     let(:report_method) { :all }
+
     # Enforce just the structure
+    it "check monitoring exist when cluster_uuid has been defined" do
+      LogStash::SETTINGS.set_value("monitoring.cluster_uuid", "cracking_cluster")
+      expect(report.keys).to include(
+        :monitoring
+        )
+    end
+
+    it "check monitoring exist when monitoring is enabled" do
+      LogStash::SETTINGS.set_value("xpack.monitoring.enabled", true)
+      expect(report.keys).to include(
+        :monitoring
+        )
+    end
+
+    it "check monitoring does not appear when not enabled and nor cluster_uuid is defined" do
+      LogStash::SETTINGS.set_value("xpack.monitoring.enabled", false)
+      LogStash::SETTINGS.get_setting("monitoring.cluster_uuid").reset
+      expect(report.keys).not_to include(
+        :monitoring
+        )
+    end
+
     it "check keys" do
       expect(report.keys).to include(
         :host,
