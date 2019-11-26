ff527a01cd8d1e70eaa8fa6eecc9e992b60154d3
João Duarte <jsvd@users.noreply.github.com>
2019-11-26 11:52:55 +0000

    Remove license header from pipelines_info.rb
    This code was moved to the apache 2.0 license part of the distribution and should not have the license header any more
    
    Fixes #11345

--- logstash-core/lib/logstash/config/pipelines_info.rb
+++ logstash-core/lib/logstash/config/pipelines_info.rb
@@ -1,7 +1,3 @@
-# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-# or more contributor license agreements. Licensed under the Elastic License;
-# you may not use this file except in compliance with the Elastic License.
-#
 module LogStash; module Config;
   class PipelinesInfo
     def self.format_pipelines_info(agent, metric_store, extended_performance_collection)
