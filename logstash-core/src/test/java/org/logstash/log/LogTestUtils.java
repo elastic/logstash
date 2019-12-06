71e702c16cbdd6de3d2338fedbaf09d241b5fde3
andsel <selva.andre@gmail.com>
2019-12-06 18:22:29 +0100

    Covered the read of logfile content with try-resource to avoid to keep a file descriptor open that later prohibited access to the process itself. Also added clean shutdown of LogManager before deleting log files used by log.
    Fixes #11399

--- logstash-core/src/test/java/org/logstash/log/LogTestUtils.java
+++ logstash-core/src/test/java/org/logstash/log/LogTestUtils.java
@@ -3,12 +3,12 @@ package org.logstash.log;
 import org.apache.logging.log4j.core.LoggerContext;
 
 import java.io.IOException;
-import java.nio.file.FileSystemException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertTrue;
 
@@ -19,7 +19,9 @@ class LogTestUtils {
                 .getPath(System.getProperty("user.dir"), System.getProperty("ls.logs"), logfileName);
 
         assertTrue("Log [" + path.toString() + "] file MUST exists", Files.exists(path));
-        return Files.lines(path).collect(Collectors.joining());
+        try (Stream<String> lines = Files.lines(path)) {
+            return lines.collect(Collectors.joining());
+        }
     }
 
     static void reloadLogConfiguration() {
@@ -30,31 +32,6 @@ class LogTestUtils {
     static void deleteLogFile(String logfileName) throws IOException {
         Path path = FileSystems.getDefault()
                 .getPath(System.getProperty("user.dir"), System.getProperty("ls.logs"), logfileName);
-        pollingDelete(path, 5, TimeUnit.SECONDS);
-    }
-
-    static void pollingDelete(Path path, int sleep, TimeUnit timeUnit) throws IOException {
-        final int maxRetries = 5;
-        int retries = 0;
-        do {
-            try {
-                Files.deleteIfExists(path);
-                break;
-            } catch (FileSystemException fsex) {
-                System.out.println("FS access error while deleting, " + fsex.getReason() + " deleting: " + fsex.getOtherFile());
-            }
-
-            try {
-                Thread.sleep(timeUnit.toMillis(sleep));
-            } catch (InterruptedException e) {
-                // follows up
-                Thread.currentThread().interrupt();
-                break;
-            }
-
-            retries++;
-        } while (retries < maxRetries);
-
-        assertTrue("Exhausted 5 retries to delete the file: " + path,retries < maxRetries);
+        Files.deleteIfExists(path);
     }
 }
