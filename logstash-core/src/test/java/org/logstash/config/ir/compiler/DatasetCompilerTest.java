af89d16b056e41fdfd5e0389a5dfc423aabca60f
Colin Surprenant <colin.surprenant@gmail.com>
2020-01-13 12:55:57 -0500

    [7.5 clean backport of #11482] cache compiled datasets (#11490)

--- logstash-core/src/test/java/org/logstash/config/ir/compiler/DatasetCompilerTest.java
+++ logstash-core/src/test/java/org/logstash/config/ir/compiler/DatasetCompilerTest.java
@@ -20,12 +20,13 @@ public final class DatasetCompilerTest {
      */
     @Test
     public void compilesOutputDataset() {
+        final ComputeStepSyntaxElement<Dataset> prepared = DatasetCompiler.outputDataset(
+            Collections.emptyList(),
+            PipelineTestUtil.buildOutput(events -> {}),
+            true
+        );
         assertThat(
-            DatasetCompiler.outputDataset(
-                Collections.emptyList(),
-                PipelineTestUtil.buildOutput(events -> {}),
-                true
-            ).instantiate().compute(RubyUtil.RUBY.newArray(), false, false),
+            prepared.instantiate(prepared.compile()).compute(RubyUtil.RUBY.newArray(), false, false),
             nullValue()
         );
     }
@@ -33,9 +34,10 @@ public final class DatasetCompilerTest {
     @Test
     public void compilesSplitDataset() {
         final FieldReference key = FieldReference.from("foo");
-        final SplitDataset left = DatasetCompiler.splitDataset(
+        final ComputeStepSyntaxElement<SplitDataset> prepared = DatasetCompiler.splitDataset(
             Collections.emptyList(), event -> event.getEvent().includes(key)
-        ).instantiate();
+        );
+        final SplitDataset left = prepared.instantiate(prepared.compile());
         final Event trueEvent = new Event();
         trueEvent.setField(key, "val");
         final JrubyEventExtLibrary.RubyEvent falseEvent =
