ba89b217e5c784711fab7c7eb0cd3d04529520ae
André Letterer <andre.letterer@web.de>
2020-04-05 00:11:21 +0200

    tools convert to batch files (#11753)
    Fixes #11754

---
+++ bin/ingest-convert.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd /d "%~dp0\.."
+for /f %%i in ('cd') do set RESULT=%%i
+
+java -cp "!!RESULT!!\tools\ingest-converter\build\libs\ingest-converter.jar;*" ^
+  org.logstash.ingest.Pipeline %*
+
+endlocal
