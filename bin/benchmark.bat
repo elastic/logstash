5e422bb82ce2ed98f311bd9f5c1d474088f41016
André Letterer <andre.letterer@web.de>
2020-04-05 00:11:21 +0200

    tools convert to batch files (#11753)
    Fixes #11754

---
+++ bin/benchmark.bat
@@ -0,0 +1,11 @@
+rem @echo off
+setlocal enabledelayedexpansion
+
+cd /d "%~dp0.."
+for /f %%i in ('cd') do set RESULT=%%i
+echo !!RESULT!!
+
+java -cp "!!RESULT!!\tools\benchmark-cli\build\libs\benchmark-cli.jar;*" ^
+  org.logstash.benchmark.cli.Main %*
+
+endlocal
---
+++ bin/pqcheck.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion 
+
+call "%~dp0setup.bat" || exit /b 1
+if errorlevel 1 (
+	if not defined nopauseonerror (
+		pause
+	)
+	exit /B %ERRORLEVEL%
+)
+
+
+set JAVA_OPTS=%LS_JAVA_OPTS%
+
+for %%i in ("%LS_HOME%\logstash-core\lib\jars\*.jar") do (
+	call :concat "%%i"
+)
+
+%JAVA% %JAVA_OPTS% -cp "%CLASSPATH%" org.logstash.ackedqueue.PqCheck %*
+
+:concat
+IF not defined CLASSPATH (
+  set CLASSPATH="%~1"
+) ELSE (
+  set CLASSPATH=%CLASSPATH%;"%~1"
+)
+goto :eof 
+
+endlocal
