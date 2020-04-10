6e48d966fa15874ad820eb6f5c685e580ee6bf07
andsel <selva.andre@gmail.com>
2020-04-10 17:43:24 +0200

    Backport of PR #11773 to branch 7.x
    Backports a PR that moved code out of LogStash::Compiler to org.logstash.config.ir.ConfigCompiler

--- x-pack/spec/monitoring/inputs/metrics/state_event/lir_serializer_spec.rb
+++ x-pack/spec/monitoring/inputs/metrics/state_event/lir_serializer_spec.rb
@@ -22,7 +22,9 @@ describe ::LogStash::Config::LIRSerializer do
   end
 
   let(:lir_pipeline) do
-    ::LogStash::Compiler.compile_sources(config_source_with_metadata, LogStash::SETTINGS)
+#     ::LogStash::Compiler.compile_sources(config_source_with_metadata, true)
+    java_import org.logstash.config.ir.ConfigCompiler
+    ConfigCompiler.compileSources(config_source_with_metadata, true)
   end
 
   describe "#serialize" do
