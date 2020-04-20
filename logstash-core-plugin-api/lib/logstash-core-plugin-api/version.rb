faf87699d468f743b2bda115ed388aa1efc3b475
Joao Duarte <jsvduarte@gmail.com>
2020-04-20 15:38:07 +0100

    dont use qualifier or snapshot in logstash core version
    Fixes #11813

--- logstash-core-plugin-api/lib/logstash-core-plugin-api/version.rb
+++ logstash-core-plugin-api/lib/logstash-core-plugin-api/version.rb
@@ -28,6 +28,5 @@ unless defined?(LOGSTASH_CORE_PLUGIN_API)
 end
 
 unless defined?(LOGSTASH_CORE_VERSION)
-  # PACKAGE_SUFFIX is declared in the artifact namespace from artifacts.rake
-  LOGSTASH_CORE_VERSION = defined?(PACKAGE_SUFFIX) ? "#{ALL_VERSIONS.fetch("logstash-core")}#{PACKAGE_SUFFIX}" : ALL_VERSIONS.fetch("logstash-core")
+  LOGSTASH_CORE_VERSION = ALL_VERSIONS.fetch("logstash-core")
 end
