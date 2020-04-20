55569c48d4c0ac8cb7888f010b603b6b5f46e72a
André Letterer <48132449+111andre111@users.noreply.github.com>
2020-04-20 16:45:54 +0200

    Update benchmark.bat

--- bin/benchmark.bat
+++ bin/benchmark.bat
@@ -3,7 +3,6 @@ setlocal enabledelayedexpansion
 
 cd /d "%~dp0.."
 for /f %%i in ('cd') do set RESULT=%%i
-echo !!RESULT!!
 
 java -cp "!!RESULT!!\tools\benchmark-cli\build\libs\benchmark-cli.jar;*" ^
   org.logstash.benchmark.cli.Main %*
