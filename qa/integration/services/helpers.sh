81faf8e1dd8f530f70e028d33f73c92f19035249
Rob Bavey <rob.bavey@elastic.co>
2020-05-29 12:46:24 -0400

    [Test] Enable fallback to sleep if nc not installed (#11942)
    Fixture test scripts use `nc` to wait for the port to determine
    whether a test fixture is up and running. This commit adds a fall
    back option to use ruby if `nc` is not available - it is not installed
    on Jenkins centos worker nodes.

--- qa/integration/services/helpers.sh
+++ qa/integration/services/helpers.sh
@@ -13,14 +13,29 @@ setup_install_dir() {
 
 wait_for_port() {
     count=$PORT_WAIT_COUNT
-    port=$1
-    while ! nc -z localhost $port && [[ $count -ne 0 ]]; do
+    while ! test_port "$1"  && [[ $count -ne 0 ]]; do
         count=$(( $count - 1 ))
         [[ $count -eq 0 ]] && return 1
         sleep 0.5
     done
     # just in case, one more time
-    nc -z localhost $port
+    test_port "$1"
+}
+
+test_port() {
+    if command -v nc 2>/dev/null; then
+      test_port_nc "$1"
+    else
+      test_port_ruby "$1"
+    fi
+}
+
+test_port_nc() {
+  nc -z localhost $1
+}
+
+test_port_ruby() {
+  ruby -rsocket -e "TCPSocket.new('localhost', $1) rescue exit(1)"
 }
 
 clean_install_dir() {
