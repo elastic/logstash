a614a5bb60d94b8131a6ec3e652103e26bddd420
Rob Bavey <rob.bavey@elastic.co>
2020-06-15 14:32:16 -0400

    Add es logging

--- qa/integration/services/elasticsearch_setup.sh
+++ qa/integration/services/elasticsearch_setup.sh
@@ -8,11 +8,13 @@ ES_HOME="$current_dir/../../../build/elasticsearch"
 
 start_es() {
   es_args=$@
-  $ES_HOME/bin/elasticsearch -Epath.data=/tmp/ls_integration/es-data -Epath.logs=/tmp/ls_integration/es-logs $es_args -p $ES_HOME/elasticsearch.pid > /tmp/elasticsearch.log 2>/dev/null &
+  $ES_HOME/bin/elasticsearch -Epath.data=/tmp/ls_integration/es-data -Epath.logs=/tmp/ls_integration/es-logs $es_args -p $ES_HOME/elasticsearch.pid > /tmp/elasticsearch.log &
   count=120
   echo "Waiting for elasticsearch to respond..."
   while ! curl --silent localhost:9200 && [[ $count -ne 0 ]]; do
       count=$(( $count - 1 ))
+      cat /tmp/elasticsearch.log
+      cat /tmp/
       [[ $count -eq 0 ]] && cat /tmp/elasticsearch.log && return 1
       sleep 1
   done
