b8c0cf2d441ffca964efbbe8ebc90e3405c71246
andsel <selva.andre@gmail.com>
2020-07-02 10:06:05 +0200

    Moved Base class for config sources subclasses

--- x-pack/lib/config_management/elasticsearch_source.rb
+++ x-pack/lib/config_management/elasticsearch_source.rb
@@ -43,7 +43,7 @@ module LogStash
       end
 
       def match?
-        @settings.get("xpack.management.enabled")
+        settings.get("xpack.management.enabled")
       end
 
       def config_conflict?
@@ -96,7 +96,7 @@ module LogStash
         config_part = org.logstash.common.SourceWithMetadata.new("x-pack-config-management", pipeline_id.to_s, config_string)
 
         # We don't support multiple pipelines, so use the global settings from the logstash.yml file
-        settings = @settings.clone
+        update_settings(settings.clone)
         settings.set("pipeline.id", pipeline_id)
 
         # override global settings with pipeline settings from ES, if any
@@ -186,7 +186,7 @@ module LogStash
 
       private
       def pipeline_ids
-        @settings.get("xpack.management.pipeline.id")
+        settings.get("xpack.management.pipeline.id")
       end
 
       def client
