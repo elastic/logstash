ca81a8f4a32457a7c100a4712c7c5a4c9b5c2faa
andsel <selva.andre@gmail.com>
2020-11-19 11:27:00 +0100

    Drop Password Ruby class to use only the Java version
    There is two Password classes that almost does the same thing. One in Ruby (LogStash::Util::Password) and one in Java (co.elastic.logstash.api.Password).
    This commit drop the the Ruby implementation to import the Java version in the LogStash::Util so that existing Ruby code haven't to be changed, works as it is.

--- logstash-core/src/main/java/co/elastic/logstash/api/Password.java
+++ logstash-core/src/main/java/co/elastic/logstash/api/Password.java
@@ -20,10 +20,14 @@
 
 package co.elastic.logstash.api;
 
+import java.io.Serializable;
+
 /**
  * Wraps a password string so that it is not inadvertently printed or logged.
  */
-public class Password {
+public class Password implements Serializable {
+
+    private static final long serialVersionUID = -8683271728417419530L;
 
     private String password;
 
@@ -40,7 +44,13 @@ public class Password {
         return "<password>";
     }
 
+    // Ruby code compatibility, value attribute
     public String getValue() {
         return getPassword();
+    }
+
+    // Ruby code compatibility, inspect method
+    public String inspect() {
+        return toString();
     }
 }
