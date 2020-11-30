dba408bafb1a6cfa64b27b817a7ce5a5b45168ab
Andres Rodriguez <andres.lazo@elastic.co>
2020-11-30 10:33:37 -0500

    Remove dependency on pleaserun

--- pkg/centos/before-install.sh
+++ pkg/centos/before-install.sh
@@ -9,7 +9,13 @@ if ! getent passwd logstash >/dev/null; then
     -s /sbin/nologin -c "logstash" logstash
 fi
 
-# Handle upgrade: Check if old service unit exists and remove it
+# Handle upgrade
+## Check if old service unit exists and remove it
 if [ -f /etc/systemd/system/logstash.service ]; then
   rm -rf /etc/systemd/system/logstash.service || true
+fi
+
+## Check if startup.options file exists and remote it
+if [ -f /etc/logstash/startup.options ]; then
+  rm -rf /etc/logstash/startup.options || true
 fi
