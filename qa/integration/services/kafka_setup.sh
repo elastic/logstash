15c13c759eb15bf068ed3eb9f0d862278e33d088
João Duarte <jsvd@users.noreply.github.com>
2021-01-29 17:13:21 +0000

    Update qa/integration/services/kafka_setup.sh
    Co-authored-by: Ry Biesemeyer <yaauie@users.noreply.github.com>

--- qa/integration/services/kafka_setup.sh
+++ qa/integration/services/kafka_setup.sh
@@ -23,7 +23,7 @@ setup_kafka() {
     local version=$1
     if [ ! -d $KAFKA_HOME ]; then
         echo "Downloading Kafka version $version"
-        curl -s -o $INSTALL_DIR/kafka.tgz "https://ftp.wayne.edu/apache/kafka/$version/kafka_2.13-$version.tgz"
+        curl -s -o $INSTALL_DIR/kafka.tgz "https://downloads.apache.org/kafka/$version/kafka_2.13-$version.tgz"
         mkdir $KAFKA_HOME && tar xzf $INSTALL_DIR/kafka.tgz -C $KAFKA_HOME --strip-components 1
         rm $INSTALL_DIR/kafka.tgz
         echo "dataDir=$ZOOKEEPER_DATA_DIR" >> $KAFKA_HOME/config/zookeeper.properties
