43ea2ce03642a41202f3d337044fff081f1b5365
Kaise Cheng <kaise.cheng@elastic.co>
2021-03-01 14:25:10 +0100

    Revert "cache metadata"
    This reverts commit c1d9332044fca464193df7e8ebe099e77f1ec2af.

--- x-pack/lib/filters/geoip/database_metadata.rb
+++ x-pack/lib/filters/geoip/database_metadata.rb
@@ -28,13 +28,11 @@ module LogStash module Filters module Geoip class DatabaseMetadata
       metadata.each { |row| csv << row }
     end
 
-    @cache = metadata
     logger.debug("metadata updated", :metadata => metadata)
   end
 
   def get_all
-    @cache ||= file_exist?(@metadata_path)?
-                 ::CSV.read(@metadata_path, headers: false) : Array.new
+    file_exist?(@metadata_path)? ::CSV.read(@metadata_path, headers: false) : Array.new
   end
 
   # Give rows of metadata in default database type, or empty array
