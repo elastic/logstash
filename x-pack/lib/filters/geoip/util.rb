ca76c0c484ff6a2c73ae173eb05c28b8e7e7cdcb
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2021-03-25 19:34:19 +0100

    GeoIP database add license files (#12756)
    GeoIP database service provides LICENSE.txt and COPYRIGHT.txt 
    along with the database in .tgz
    
    Fixed: #12560

--- x-pack/lib/filters/geoip/util.rb
+++ x-pack/lib/filters/geoip/util.rb
@@ -5,18 +5,29 @@
 require "digest"
 
 
-module LogStash module Filters module Geoip module Util
+module LogStash module Filters
+  module Geoip
+    GZ_EXT = 'tgz'.freeze
+    DB_EXT = 'mmdb'.freeze
+    DB_PREFIX = 'GeoLite2-'.freeze
 
-  def get_file_path(filename)
-    ::File.join(@vendor_path, filename)
+    module Util
+      def get_file_path(filename)
+        ::File.join(@vendor_path, filename)
+      end
+
+      def file_exist?(path)
+        !path.nil? && ::File.exist?(path) && !::File.empty?(path)
+      end
+
+      def md5(file_path)
+        file_exist?(file_path) ? Digest::MD5.hexdigest(::File.read(file_path)): ""
+      end
+
+      # replace *.mmdb to *.tgz
+      def get_gz_name(filename)
+        filename[0...-(DB_EXT.length)] + GZ_EXT
+      end
+    end
   end
-
-  def file_exist?(path)
-    !path.nil? && ::File.exist?(path) && !::File.empty?(path)
-  end
-
-  def md5(file_path)
-    file_exist?(file_path) ? Digest::MD5.hexdigest(::File.read(file_path)): ""
-  end
-
-end end end end
+end end
