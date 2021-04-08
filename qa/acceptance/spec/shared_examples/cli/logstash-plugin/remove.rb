b852daf2025f35f07205e874089b75c91718172f
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2021-04-08 18:25:58 +0200

    Allow plugin manager uninstall plugin regardless of working directory (#12786) (#12808)
    Fixed: #10781

--- qa/acceptance/spec/shared_examples/cli/logstash-plugin/remove.rb
+++ qa/acceptance/spec/shared_examples/cli/logstash-plugin/remove.rb
@@ -36,14 +36,12 @@ shared_examples "logstash remove" do |logstash|
       end
     end
 
-    # Disabled because of this bug https://github.com/elastic/logstash/issues/5286
-    xcontext "when the plugin is installed" do
+    context "when the plugin is installed" do
       it "successfully removes it" do
         result = logstash.run_command_in_path("bin/logstash-plugin install logstash-filter-qatest")
         expect(logstash).to have_installed?("logstash-filter-qatest")
 
         result = logstash.run_command_in_path("bin/logstash-plugin remove logstash-filter-qatest")
-        expect(result.stdout).to match(/^Removing logstash-filter-qatest/)
         expect(logstash).not_to have_installed?("logstash-filter-qatest")
       end
     end
