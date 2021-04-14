2b560285169fcf2b50fe37643707a6801b584667
andsel <selva.andre@gmail.com>
2021-04-14 11:48:57 +0200

    Added tests to verify that the PluginRegistry.lookup retrived the aliased plugin and that a pipeline is correcrtly instantiated when an aliased plugin is used

--- logstash-core/src/main/java/org/logstash/plugins/AliasRegistry.java
+++ logstash-core/src/main/java/org/logstash/plugins/AliasRegistry.java
@@ -4,44 +4,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AliasRegistry {
-    private final Map<String, String> ALIASES = new HashMap<>();
-    private final Map<String, String> REVERSE_ALIASES = new HashMap<>();
+    private final Map<String, String> aliases = new HashMap<>();
+    private final Map<String, String> reversedAliases = new HashMap<>();
 
     public AliasRegistry() {
-        configurePluginAliases();
+        Map<String, String> defaultDefinitions = new HashMap<>();
+        defaultDefinitions.put("elastic_agent", "beats");
+        configurePluginAliases(defaultDefinitions);
     }
 
-    private void configurePluginAliases() {
-        ALIASES.put("elastic_agent", "beats");
-        ALIASES.put("dna_java_generator", "java_generator");
-        for (Map.Entry<String, String> e : ALIASES.entrySet()) {
-            if (REVERSE_ALIASES.containsKey(e.getValue())) {
+    /**
+     * Constructor used in tests to customize the plugins renames
+     * */
+    public AliasRegistry(Map<String, String> aliasDefinitions) {
+        configurePluginAliases(aliasDefinitions);
+    }
+
+    private void configurePluginAliases(Map<String, String> aliases) {
+        this.aliases.putAll(aliases);
+        for (Map.Entry<String, String> e : this.aliases.entrySet()) {
+            if (reversedAliases.containsKey(e.getValue())) {
                 throw new IllegalStateException("Found plugin " + e.getValue() + " aliased more than one time");
             }
-            REVERSE_ALIASES.put(e.getValue(), e.getKey());
+            reversedAliases.put(e.getValue(), e.getKey());
         }
     }
 
     public boolean isAlias(String pluginName) {
-        return ALIASES.containsKey(pluginName);
+        return aliases.containsKey(pluginName);
     }
 
     public boolean isAliased(String realPluginName) {
-        return ALIASES.containsValue(realPluginName);
+        return aliases.containsValue(realPluginName);
     }
 
     public String originalFromAlias(String pluginAlias) {
-        return ALIASES.get(pluginAlias);
+        return aliases.get(pluginAlias);
     }
 
     public String aliasFromOriginal(String realPluginName) {
-        return REVERSE_ALIASES.get(realPluginName);
+        return reversedAliases.get(realPluginName);
     }
 
     /**
      * if pluginName is an alias then return the real plugin name else return it unchanged
      */
     public String resolveAlias(String pluginName) {
-        return ALIASES.getOrDefault(pluginName, pluginName);
+        return aliases.getOrDefault(pluginName, pluginName);
     }
 }
