d49c739d9f7677ebb18847a89999331f07cb90af
andsel <selva.andre@gmail.com>
2021-05-27 09:17:34 +0200

    [WIP] Remove the properties of routing instead of work on the configuration object directly

--- logstash-core/src/main/java/org/logstash/log/LogstashConfigurationFactory.java
+++ logstash-core/src/main/java/org/logstash/log/LogstashConfigurationFactory.java
@@ -21,17 +21,23 @@
 package org.logstash.log;
 
 import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.ConfigurationException;
 import org.apache.logging.log4j.core.config.ConfigurationFactory;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.Order;
+import org.apache.logging.log4j.core.config.builder.api.Component;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.properties.PropertiesConfiguration;
 import org.apache.logging.log4j.core.config.properties.PropertiesConfigurationBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * Logstash configuration factory customization used to remove the routing appender from log4j configuration
@@ -50,25 +56,90 @@ public class LogstashConfigurationFactory extends ConfigurationFactory {
     }
 
     @Override
-    public PropertiesConfiguration getConfiguration(final LoggerContext loggerContext, final ConfigurationSource source) {
+    public Configuration getConfiguration(final LoggerContext loggerContext, final ConfigurationSource source) {
         final Properties properties = new Properties();
         try (final InputStream configStream = source.getInputStream()) {
             properties.load(configStream);
         } catch (final IOException ioe) {
             throw new ConfigurationException("Unable to load " + source.toString(), ioe);
         }
+
+        if (System.getProperty(PIPELINE_SEPARATE_LOGS, "false").equals("false")) {
+            final String routingAppenderPrefix = findAppenderPrefixWithName(properties, PIPELINE_ROUTING_APPENDER_NAME);
+            if (!routingAppenderPrefix.isEmpty()) {
+                removeAllPropertiesWithPrefix(properties, routingAppenderPrefix);
+            }
+        }
+
         PropertiesConfiguration propertiesConfiguration = new PropertiesConfigurationBuilder()
                 .setConfigurationSource(source)
                 .setRootProperties(properties)
                 .setLoggerContext(loggerContext)
                 .build();
 
-        if (System.getProperty(PIPELINE_SEPARATE_LOGS, "false").equals("false")) {
-            // force init to avoid overwrite of appenders section
-            propertiesConfiguration.initialize();
-            propertiesConfiguration.removeAppender(PIPELINE_ROUTING_APPENDER_NAME);
-        }
+//        if (System.getProperty(PIPELINE_SEPARATE_LOGS, "false").equals("false")) {
+//            // force init to avoid overwrite of appenders section
+//            propertiesConfiguration.initialize();
+//            propertiesConfiguration.removeAppender(PIPELINE_ROUTING_APPENDER_NAME);
+//        }
 
         return propertiesConfiguration;
+
+//        final Component root = new Component();
+//        final List<Component> components = root.getComponents();
+//
+//        components.add(new Component("Properties"));
+//        components.add(new Component("Scripts"));
+//        components.add(new Component("CustomLevels"));
+//        components.add(new Component("Filters"));
+//        components.add(new Component("Appenders"));
+//        components.add(new Component("Loggers"));
+//
+//        return new LogstashPropertiesConfiguration(loggerContext, source,  root);
+    }
+
+    private void removeAllPropertiesWithPrefix(Properties properties, String prefix) {
+        Set<String> keysToRemove = new HashSet<>(12);
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            if (((String) entry.getKey()).startsWith(prefix)) {
+                keysToRemove.add((String) entry.getKey());
+            }
+        }
+
+        //remove the collected keys
+        for (String keyToDelete : keysToRemove) {
+            properties.remove(keyToDelete);
+        }
+    }
+
+    private String findAppenderPrefixWithName(Properties properties, String appenderName) {
+        if (!properties.containsValue(appenderName)) {
+            return "";
+        }
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            if (appenderName.equals(entry.getValue())) {
+                final String key = (String) entry.getKey();
+                if (key.endsWith(".name")) {
+                    return key.substring(0 ,key.lastIndexOf(".name"));
+                }
+            }
+        }
+        return "";
+    }
+
+    public class LogstashPropertiesConfiguration extends PropertiesConfiguration {
+
+        public LogstashPropertiesConfiguration(LoggerContext loggerContext, ConfigurationSource source, Component root) {
+            super(loggerContext, source, root);
+        }
+
+        @Override
+        protected void doConfigure() {
+            super.doConfigure();
+
+            if (System.getProperty(PIPELINE_SEPARATE_LOGS, "false").equals("false")) {
+                removeAppender(PIPELINE_ROUTING_APPENDER_NAME);
+            }
+        }
     }
 }
