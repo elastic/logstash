968b39fb3b168adcbc14422811fde4b572957ff9
andsel <selva.andre@gmail.com>
2021-07-05 14:44:46 +0200

    Minor, extracted common code

--- buildSrc/src/test/groovy/org/logstash/gradle/tooling/StackVersionSelectorTest.groovy
+++ buildSrc/src/test/groovy/org/logstash/gradle/tooling/StackVersionSelectorTest.groovy
@@ -1,6 +1,7 @@
 package org.logstash.gradle.tooling
 
 import org.junit.Test
+import org.junit.Before
 
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertFalse
@@ -9,22 +10,27 @@ import static org.junit.Assert.assertTrue
 
 class StackVersionSelectorTest {
 
+    def versionsFixture = [
+            "6.8.17-SNAPSHOT",
+            "6.8.17",
+            "7.13.2-SNAPSHOT",
+            "7.13.2",
+            "7.13.3-SNAPSHOT",
+            "7.13.3",
+            "7.14.0-SNAPSHOT",
+            "7.14.0",
+            "8.0.0-SNAPSHOT"
+    ]
+
+    def sut
+
+    @Before
+    void setUp() {
+        sut = new StackVersionSelector("")
+    }
+
     @Test
     void "selectClosestInList should return the exact match when present"() {
-        def sut = new StackVersionSelector("")
-
-        def result = sut.selectClosestInList("7.14.0", [
-                "6.8.17-SNAPSHOT",
-                "6.8.17",
-                "7.13.2-SNAPSHOT",
-                "7.13.2",
-                "7.13.3-SNAPSHOT",
-                "7.13.3",
-                "7.14.0-SNAPSHOT",
-                "7.14.0",
-                "8.0.0-SNAPSHOT"
-        ])
-
-        assert "7.14.0" == result
+        assert "7.14.0" == sut.selectClosestInList("7.14.0", versionsFixture)
     }
 }
