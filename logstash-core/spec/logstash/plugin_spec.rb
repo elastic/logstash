68f3cf3d90a952d01ccab2294fd155ebbc241a25
Ry Biesemeyer <yaauie@users.noreply.github.com>
2021-07-20 14:45:36 -0700

    ecs_compatibility: revert breaking change; keep `disabled` as default for 8.0.0 (#13080)
    As we close in on the availability of 8.0.0 alphas, we are reassessing which
    breaking changes are _necessary_, and which are merely _desired_. And while
    we would love to be in a world where ECS was on by default, and have put
    substantial effort into designing an upgrade path that would be as simple as
    possible, we have determined that the time may not be right to change the
    default value out of under our users.
    
    This change restores the default value for `pipeline.ecs_compatibility` to
    `disabled`, ensuring pipelines will continue running in Logstash 8 as they have
    in Logstash 7 without modification. We will still encourage our users to be
    explicit about which behaviour they desire, and will revisit making ECS on by
    default at a later date.

--- logstash-core/spec/logstash/plugin_spec.rb
+++ logstash-core/spec/logstash/plugin_spec.rb
@@ -461,9 +461,16 @@ describe LogStash::Plugin do
       end
 
       context 'and pipeline-level setting is not specified' do
-        it 'returns `v1`' do
+        it 'emits a deprecation warning about using the default which may change' do
+          instance.ecs_compatibility
+
+          expect(deprecation_logger_stub).to have_received(:deprecated) do |message|
+            expect(message).to include("Relying on default value of `pipeline.ecs_compatibility`")
+          end
+        end
+        it 'returns `disabled`' do
           # Default value of `pipeline.ecs_compatibility`
-          expect(instance.ecs_compatibility).to eq(:v1)
+          expect(instance.ecs_compatibility).to eq(:disabled)
         end
       end
     end
