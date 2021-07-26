31c2df893c84b9cd6220882577426dedb9d5ab35
Rob Bavey <rob.bavey@elastic.co>
2021-07-26 13:27:24 -0400

    Allow the possibility of multiple specific platforms to be removed
    Co-authored-by: Ry Biesemeyer <yaauie@users.noreply.github.com>

--- lib/pluginmanager/bundler/logstash_injector.rb
+++ lib/pluginmanager/bundler/logstash_injector.rb
@@ -94,8 +94,9 @@ module Bundler
           builder.eval_gemfile("bundler file", gemfile.generate())
           definition = builder.to_definition(lockfile_path, {})
           # Remove the specific platform, and leave the generic platform
-          specific_platform = LogStash::Bundler.specific_platform
-          definition.remove_platform(specific_platform) unless specific_platform.nil?
+          LogStash::Bundler.specific_platforms.each do |specific_platform|
+            definition.remove_platform(specific_platform)
+          end
           definition.lock(lockfile_path)
           gemfile.save
         rescue => e
