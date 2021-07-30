e249b5d6b446f23e76effb44f9a28d37de2b7aa4
André Letterer <48132449+111andre111@users.noreply.github.com>
2021-07-31 01:25:12 +0200

    valid variable expansion for the batch files (#12912)

--- bin/benchmark.bat
+++ bin/benchmark.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 cd /d "%~dp0.."
 for /f %%i in ('cd') do set RESULT=%%i
 
-java -cp "!!RESULT!!\tools\benchmark-cli\build\libs\benchmark-cli.jar;*" ^
+java -cp "!RESULT!\tools\benchmark-cli\build\libs\benchmark-cli.jar;*" ^
   org.logstash.benchmark.cli.Main %*
 
 endlocal
