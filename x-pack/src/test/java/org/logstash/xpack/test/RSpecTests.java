5b6c71f44dd57e564be0635db763eb0bcf363ec6
kares <self@kares.org>
2021-08-24 15:49:45 +0200

    re-use RSpec java test helper in x-pack

--- x-pack/src/test/java/org/logstash/xpack/test/RSpecTests.java
+++ x-pack/src/test/java/org/logstash/xpack/test/RSpecTests.java
@@ -7,11 +7,13 @@
 
 package org.logstash.xpack.test;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 import org.jruby.Ruby;
 import org.jruby.runtime.builtin.IRubyObject;
@@ -21,23 +23,29 @@ import org.logstash.Logstash;
 import org.logstash.RubyUtil;
 import org.logstash.Rubyfier;
 
-public final class RSpecTests {
+public class RSpecTests {
+
+    private static final String LS_HOME;
 
     static {
-        final String root = org.assertj.core.util.Files.currentFolder().getParent(); // CWD: x-pack
+        final File cwd = org.assertj.core.util.Files.currentFolder();
+        final String root = cwd.getParent(); // CWD: x-pack
         // initialize global (RubyUtil.RUBY) runtime :
-        Ruby.newInstance(Logstash.initRubyConfig(Paths.get(root)));
+        Ruby.newInstance(Logstash.initRubyConfig(Paths.get(root), cwd.getAbsolutePath(), new String[0]));
+        LS_HOME = root;
+    }
+
+    protected List<String> rspecArgs() {
+        return Arrays.asList("-fd", "--pattern", "spec/**/*_spec.rb");
     }
 
     @Test
     public void rspecTests() throws Exception {
         RubyUtil.RUBY.getENV().put("IS_JUNIT_RUN", "true");
         RubyUtil.RUBY.getGlobalVariables().set(
-            "$JUNIT_ARGV", Rubyfier.deep(RubyUtil.RUBY, Arrays.asList(
-                "-fd", "--pattern", "spec/**/*_spec.rb"
-            ))
+            "$JUNIT_ARGV", Rubyfier.deep(RubyUtil.RUBY, rspecArgs())
         );
-        final Path rspec = Paths.get(RubyUtil.RUBY.getCurrentDirectory(), "lib/bootstrap/rspec.rb");
+        final Path rspec = Paths.get(LS_HOME, "lib/bootstrap/rspec.rb");
         final IRubyObject result = RubyUtil.RUBY.executeScript(
             new String(Files.readAllBytes(rspec), StandardCharsets.UTF_8),
             rspec.toFile().getAbsolutePath()
