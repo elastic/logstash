2d831b85bd7e3d61d362c5ed8c9d5b0314b3d1a7
Rob Bavey <rob.bavey@elastic.co>
2021-12-16 10:35:51 -0500

    Backport #13442 to 7.17. Update logstash docker to use ubuntu 20.04 base image (#13529)
    Backport #13442 to 7.17 branch. Original message:
    
    * Update logstash docker to use ubuntu 20.04 base image
    * Correctly set locale for ubuntu docker image
    * tiny typo fix: ubunto -> ubuntu
    
    Co-authored-by: João Duarte <jsvd@users.noreply.github.com>
    
    Co-authored-by: João Duarte <jsvd@users.noreply.github.com>

--- qa/docker/spec/full/container_spec.rb
+++ qa/docker/spec/full/container_spec.rb
@@ -9,4 +9,19 @@ describe 'A container running the full image' do
   it_behaves_like 'it runs with different configurations', 'full'
   it_behaves_like 'it applies settings correctly', 'full'
   it_behaves_like 'a container with xpack features', 'full'
+
+  context 'The running container' do
+    before do
+      @image = find_image('full')
+      @container = start_container(@image, {})
+    end
+
+    after do
+      cleanup_container(@container)
+    end
+
+    it 'has an Ubuntu 20.04 base image' do
+      expect(exec_in_container(@container, 'cat /etc/os-release').chomp).to match /Ubuntu 20.04/
+    end
+  end
 end
