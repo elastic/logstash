36c5e2a097829e2fad55be5950d02e445e102372
Ry Biesemeyer <yaauie@users.noreply.github.com>
2022-01-18 10:10:39 -0800

    Backport PR #13525 to 7.17: fix: respect LS_JAVA_OPTS environment even when optionsfile missing (#13635)
    * fix: respect LS_JAVA_OPTS environment even when optionsfile missing (#13525)
    
    * fix: respect LS_JAVA_OPTS environment even when optionsfile missing
    
    * Fixed integration tests
    
    * Added unit test to cover the fix
    
    * Wipe commented code
    
    * Removed redundant log in a path that could never be reached
    
    * Moved jvm.options checks into only one place
    
    * javaopts: provide injection point for environment string
    
    Co-authored-by: andsel <selva.andre@gmail.com>
    (cherry picked from commit 2a248b2ea0c5837ff64c81c69ec5c5f2353bbe04)
    
    * backport: spec silencing noise

--- logstash-core/src/test/java/org/logstash/launchers/JvmOptionsParserTest.java
+++ logstash-core/src/test/java/org/logstash/launchers/JvmOptionsParserTest.java
@@ -1,14 +1,56 @@
 package org.logstash.launchers;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class JvmOptionsParserTest {
+
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void test_LS_JAVA_OPTS_isUsedWhenNoJvmOptionsIsAvailable() throws IOException, InterruptedException, ReflectiveOperationException {
+        JvmOptionsParser.handleJvmOptions(new String[] {temp.toString()}, "-Xblabla");
+
+        // Verify
+        final String output = outputStreamCaptor.toString();
+        assertEquals("Output MUST contains the options present in LS_JAVA_OPTS", "-Xblabla\n", output);
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public static void updateEnv(String name, String val) throws ReflectiveOperationException {
+        Map<String, String> env = System.getenv();
+        Field field = env.getClass().getDeclaredField("m");
+        field.setAccessible(true);
+        ((Map<String, String>) field.get(env)).put(name, val);
+    }
+
 
     @Test
     public void testParseCommentLine() throws IOException {
