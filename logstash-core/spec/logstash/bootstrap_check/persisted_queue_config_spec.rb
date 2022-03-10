df2c1a206cbff1f395c477a4be7630446a3a821b
Kaise Cheng <kaise.cheng@elastic.co>
2022-03-10 10:48:56 +0000

    add test

--- logstash-core/spec/logstash/bootstrap_check/persisted_queue_config_spec.rb
+++ logstash-core/spec/logstash/bootstrap_check/persisted_queue_config_spec.rb
@@ -18,22 +18,74 @@
 require "spec_helper"
 require "tmpdir"
 require "logstash/bootstrap_check/persisted_queue_config"
+require_relative '../../support/helpers'
 
 describe LogStash::BootstrapCheck::PersistedQueueConfig do
 
-  xcontext("when persisted queues are enabled") do
+  context("when persisted queues are enabled") do
+    let(:input_block) { "input { generator {} }" }
+    let(:config_path) { temporary_file(input_block) }
+    let(:queue_path) { Stud::Temporary.directory }
     let(:settings) do
-      settings = LogStash::SETTINGS.dup
-      settings.set_value("queue.type", "persisted")
-      settings.set_value("queue.page_capacity", 1024)
-      settings.set_value("path.queue", ::File.join(Dir.tmpdir, "some/path"))
-      settings
+      mock_settings(
+        "queue.type" => "persisted",
+        "queue.page_capacity" => 1024,
+        "path.queue" => queue_path,
+        "path.config" => config_path
+      )
     end
+    let(:pipeline_configs) { LogStash::Config::Source::Local.new(settings).pipeline_configs }
+    let(:successful_fetch) { LogStash::Config::SourceLoader::SuccessfulFetch.new(pipeline_configs) }
 
-    context("and 'queue.max_bytes' is set to a value less than the value of 'queue.page_capacity'") do
+    context("'queue.max_bytes' is less than 'queue.page_capacity'") do
       it "should throw" do
         settings.set_value("queue.max_bytes", 512)
-        expect { LogStash::BootstrapCheck::PersistedQueueConfig.check(settings) }.to raise_error
+        pq_config = LogStash::BootstrapCheck::PersistedQueueConfig.new(successful_fetch)
+        expect { pq_config.check }.to raise_error(LogStash::BootstrapCheckError, /'queue.page_capacity' must be less than or equal to 'queue.max_bytes'/)
+      end
+    end
+
+    context("'queue.max_bytes' = 0 which is less than 'queue.page_capacity'") do
+      it "should not throw" do
+        settings.set_value("queue.max_bytes", 0)
+        pq_config = LogStash::BootstrapCheck::PersistedQueueConfig.new(successful_fetch)
+        expect { pq_config.check }.not_to raise_error
+      end
+    end
+
+    context("queue size is greater than 'queue.max_bytes'") do
+      let(:pipeline_id) { "main" }
+      let(:page_file) do
+        FileUtils.mkdir_p(::File.join(queue_path, pipeline_id))
+        ::File.new(::File.join(queue_path, pipeline_id, "page.0"), "w")
+      end
+
+      before do
+        # create a 2MB file
+        page_file.truncate(2 ** 21)
+      end
+
+      it "should throw" do
+        settings.set_value("queue.max_bytes", "1mb")
+        pq_config = LogStash::BootstrapCheck::PersistedQueueConfig.new(successful_fetch)
+        expect { pq_config.check }.to raise_error(LogStash::BootstrapCheckError, /greater than 'queue.max_bytes'/)
+      end
+
+      after do
+        page_file.truncate(0)
+        page_file.close
+      end
+    end
+
+    context("disk does not have sufficient space") do
+      let(:pipeline_configs) { LogStash::Config::Source::Local.new(settings).pipeline_configs * 2 }
+
+      it "should throw" do
+        settings.set_value("queue.max_bytes", "1000pb")
+        pq_config = LogStash::BootstrapCheck::PersistedQueueConfig.new(successful_fetch)
+        expect { pq_config.check }.to raise_error(LogStash::BootstrapCheckError, /is unable to allocate/)
+        expect(pq_config.instance_variable_get(:@required_free_bytes).size).to eq(1)
+        expect(pq_config.instance_variable_get(:@required_free_bytes).values[0]).to eq(1024**5 * 1000 * 2) # require 2000pb
       end
     end
   end
