6e78441b228dfb2f4dd5387489a7a3065c440061
andsel <selva.andre@gmail.com>
2022-03-10 15:50:51 +0100

    Extracted output filename as configration property

--- buildSrc/src/main/groovy/org/logstash/gradle/tooling/ExtractBundledJdkVersion.groovy
+++ buildSrc/src/main/groovy/org/logstash/gradle/tooling/ExtractBundledJdkVersion.groovy
@@ -2,21 +2,32 @@ package org.logstash.gradle.tooling
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 abstract class ExtractBundledJdkVersion extends DefaultTask {
 
+    /**
+     * Defines the name of the output filename containing the JDK version.
+     * */
+    @Input
+    abstract Property<String> getOutputFilename()
+
     @InputFile
     File jdkReleaseFile = project.file("${project.projectDir}/jdk/release")
 
     @OutputFile
-    File jdkVersionFile = project.file("${project.projectDir}/JDK_VERSION")
+    File getJdkVersionFile() {
+        project.file("${project.projectDir}/${outputFilename.get()}")
+    }
 
     ExtractBundledJdkVersion() {
         description = "Extracts IMPLEMENTOR_VERSION from JDK's release file"
         group = "org.logstash.tooling"
+        outputFilename.convention("JDK_VERSION")
     }
 
     @TaskAction
