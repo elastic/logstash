25d36db5de35ed6f9957a9410caea8e6297f6ec3
Kaise Cheng <kaise.cheng@elastic.co>
2022-03-18 16:00:19 +0000

    add static and change to long

--- logstash-core/src/main/java/org/logstash/util/ExponentialBackoff.java
+++ logstash-core/src/main/java/org/logstash/util/ExponentialBackoff.java
@@ -7,8 +7,8 @@ import java.util.Random;
 
 public class ExponentialBackoff {
     private final Long maxRetry;
-    private final int[]  BACKOFF_SCHEDULE_MS = {100, 200, 400, 800, 1_600, 3_200, 6_400, 12_800, 25_600, 51_200};
-    private final int BACKOFF_MAX_MS = 60_000;
+    private static final int[] BACKOFF_SCHEDULE_MS = {100, 200, 400, 800, 1_600, 3_200, 6_400, 12_800, 25_600, 51_200};
+    private static final int BACKOFF_MAX_MS = 60_000;
 
     private static final Logger logger = LogManager.getLogger(ExponentialBackoff.class);
 
@@ -17,7 +17,7 @@ public class ExponentialBackoff {
     }
 
     public <T> T retry(CheckedSupplier<T> action) throws RetryException {
-        Long attempt = 0L;
+        long attempt = 0L;
 
         do {
             try {
@@ -27,7 +27,7 @@ public class ExponentialBackoff {
                 logger.error("Backoff retry exception", ex);
             }
 
-            if (isRetry(attempt)) {
+            if (hasRetry(attempt)) {
                 try {
                     int ms = backoffTime(attempt);
                     logger.info("Retry({}) will execute in {} second", attempt, ms/1000.0);
@@ -36,7 +36,7 @@ public class ExponentialBackoff {
                     throw new RetryException("Backoff retry aborted", e);
                 }
             }
-        } while (isRetry(attempt));
+        } while (hasRetry(attempt));
 
         throw new RetryException("Reach max retry");
     }
@@ -47,7 +47,7 @@ public class ExponentialBackoff {
                 BACKOFF_MAX_MS;
     }
 
-    private boolean isRetry(Long attempt) {
+    private boolean hasRetry(long attempt) {
         return attempt <= maxRetry;
     }
 
