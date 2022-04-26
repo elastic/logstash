0b599fe846c166a0702b42fcad3b4e2ca76c475d
Rob Bavey <rob.bavey@elastic.co>
2022-04-26 15:09:09 -0400

    Put polyglot patch require alongside treetop require

--- logstash-core/lib/logstash/compiler/lscl.rb
+++ logstash-core/lib/logstash/compiler/lscl.rb
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+require "logstash/patches/polyglot"
 require "treetop"
 require "logstash/compiler/treetop_monkeypatches"
 require "logstash/compiler/lscl/helpers"
--- logstash-core/lib/logstash/runner.rb
+++ logstash-core/lib/logstash/runner.rb
@@ -42,7 +42,6 @@ LogStash::Environment.load_locale!
 require "logstash/agent"
 require "logstash/config/defaults"
 require "logstash/patches/clamp"
-require "logstash/patches/polyglot"
 require "logstash/settings"
 require "logstash/version"
 require 'logstash/plugins'
