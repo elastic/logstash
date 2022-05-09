6558f0d149a23268718f7fd916ddfcd80640cb3e
Kaise Cheng <kaise.cheng@elastic.co>
2022-05-09 16:52:35 +0100

    remove inline ruby script

--- logstash-core/src/test/java/org/logstash/execution/ShutdownWatcherExtTest.java
+++ logstash-core/src/test/java/org/logstash/execution/ShutdownWatcherExtTest.java
@@ -69,39 +69,6 @@ public final class ShutdownWatcherExtTest extends RubyTestBase {
         appender = CTX.getListAppender("EventLogger").clear();
     }
 
-    public List<CharSequence> pipeline = Arrays.asList(
-            "pipeline = Object.new",
-            "reporter = Object.new",
-            "snapshot = Object.new",
-            "inflight_count = java.util.concurrent.atomic.AtomicInteger.new",
-            "snapshot.define_singleton_method(:inflight_count) do",
-            "inflight_count.increment_and_get + 1",
-            "end",
-            "threads = {}",
-            "snapshot.define_singleton_method(:stalling_threads) do",
-            "threads",
-            "end",
-            "snapshot.define_singleton_method(:to_s) do",
-            "\"inflight_count=>\" + inflight_count.get.to_s + \", stalling_threads_info=>{...}\"",
-            "end",
-            "reporter.define_singleton_method(:snapshot) do",
-            "snapshot",
-            "end",
-            "pipeline.define_singleton_method(:thread) do",
-            "Thread.current",
-            "end",
-            "pipeline.define_singleton_method(:finished_execution?) do",
-            "false",
-            "end",
-            "pipeline.define_singleton_method(:reporter) do",
-            "reporter",
-            "end",
-            "pipeline.define_singleton_method(:worker_threads_draining?) do",
-            "false",
-            "end",
-            "pipeline"
-    );
-
     @Test
     public void pipelineWithUnsafeShutdownShouldForceShutdown() throws InterruptedException, IOException {
         String pipeline = getPipeline(false);
