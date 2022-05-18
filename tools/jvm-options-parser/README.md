2c7f14d25aa736a25829464b336a7303250bc843
Rob Bavey <rob.bavey@elastic.co>
2022-05-18 14:58:53 -0400

    Move JvmOptionParser to separate project (#13657)
    This commit moves the JvmOptionParser into its own gradle project.
    
    This enables the JvmOptionParser to remain compatible with Java 1.8 to present a helpful error message to a user attempting to start Logstash using older versions of Java, while allowing the main Logstash code base to freely use idiomatic Java 11 features.

---
+++ tools/jvm-options-parser/README.md
@@ -0,0 +1,2 @@
+Standalone jar for JvmOptionsParser to enable it to run under Java 8 to provide
+helpful fail fast message when logstash is run older versions of Java.
--- tools/jvm-options-parser/src/main/java/org/logstash/launchers/JvmOptionsParser.java
+++ tools/jvm-options-parser/src/main/java/org/logstash/launchers/JvmOptionsParser.java
@@ -1,6 +1,23 @@
-package org.logstash.launchers;
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
 
-import org.logstash.util.JavaVersion;
+package org.logstash.launchers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -106,8 +123,6 @@ public class JvmOptionsParser {
             System.exit(1);
         }
     }
-
-
     static void handleJvmOptions(String[] args, String lsJavaOpts) {
         final JvmOptionsParser parser = new JvmOptionsParser(args[0]);
         final String jvmOpts = args.length == 2 ? args[1] : null;
