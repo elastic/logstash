6534386f21f3c5d029be21a8e14af45c54308a99
Andrea Selva <selva.andre@gmail.com>
2022-09-15 16:36:08 +0200

    Created DRA shell script to create artifacts and dependency report (#14522)
    * Created DRA shell script to create artifacts and dependency report
    * Added console listing of the generated artifacts

---
+++ ci/dra.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ie
+#Note - ensure that the -e flag is set to properly set the $? status if any command fails
+
+# Since we are using the system jruby, we need to make sure our jvm process
+# uses at least 1g of memory, If we don't do this we can get OOM issues when
+# installing gems. See https://github.com/elastic/logstash/issues/5179
+export JRUBY_OPTS="-J-Xmx1g"
+
+if [ -z "$VERSION_QUALIFIER_OPT" ]; then
+  RELEASE=1 rake artifact:all
+else
+  VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:all
+fi
+echo "GENERATED ARTIFACTS"
+for file in build/logstash-*; do shasum $file;done
+
+STACK_VERSION=`cat versions.yml | sed -n 's/^logstash\:\s\([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\)$/\1/p'`
+
+echo "Creating dependencies report for ${STACK_VERSION}"
+mkdir -p build/reports/dependencies-reports/
+bin/dependencies-report --csv=build/reports/dependencies-reports/logstash-${STACK_VERSION}.csv
+
+echo "GENERATED DEPENDENCIES REPORT"
+shasum build/reports/dependencies-reports/logstash-${STACK_VERSION}.csv
