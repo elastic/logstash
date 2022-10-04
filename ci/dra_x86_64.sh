fbf746b8a88770fb1e9eac9e60efb6489fb8201f
Andrea Selva <selva.andre@gmail.com>
2022-10-04 09:50:41 +0200

    Backport dra 7.17 (#14588)
    This is a **not clean backport** of the following PRs to `8.5` branch:
    
    - Introduces a bash script to build all the artifacts and dependencies report. #14522
    - Save docker images as tar.gz files move the CSV dependency report in the path that's expected by release-manager #14552 
    - Split ci scripts into ARM and x86 ones #14567
    - Uses the gsutil tool to upload all the generated artifacts into an intermediate collector bucket. #14568 
    - Collect all artifacts created and upload to GCP with release-manager #14584

---
+++ ci/dra_x86_64.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -ie
+#Note - ensure that the -e flag is set to properly set the $? status if any command fails
+
+# Since we are using the system jruby, we need to make sure our jvm process
+# uses at least 1g of memory, If we don't do this we can get OOM issues when
+# installing gems. See https://github.com/elastic/logstash/issues/5179
+export JRUBY_OPTS="-J-Xmx1g"
+
+if [ -z "$VERSION_QUALIFIER_OPT" ]; then
+  RELEASE=1 rake artifact:all
+else
+  VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:all
+fi
+
+STACK_VERSION=`cat versions.yml | sed -n 's/^logstash\:\s\([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\)$/\1/p'`
+
+echo "Saving tar.gz for docker images"
+docker save docker.elastic.co/logstash/logstash:${STACK_VERSION}-SNAPSHOT | gzip -c > build/logstash-${STACK_VERSION}-docker-image-x86_64.tar.gz
+docker save docker.elastic.co/logstash/logstash-oss:${STACK_VERSION}-SNAPSHOT | gzip -c > build/logstash-oss-${STACK_VERSION}-docker-image-x86_64.tar.gz
+docker save docker.elastic.co/logstash/logstash-ubi8:${STACK_VERSION}-SNAPSHOT | gzip -c > build/logstash-ubi8-${STACK_VERSION}-docker-image-x86_64.tar.gz
+
+echo "GENERATED ARTIFACTS"
+for file in build/logstash-*; do shasum $file;done
+
+echo "Creating dependencies report for ${STACK_VERSION}"
+mkdir -p build/distributions/dependencies-reports/
+bin/dependencies-report --csv=build/distributions/dependencies-reports/logstash-${STACK_VERSION}.csv
+
+echo "GENERATED DEPENDENCIES REPORT"
+shasum build/distributions/dependencies-reports/logstash-${STACK_VERSION}.csv
+
+echo "UPLOADING TO INTERMEDIATE BUCKET"
+for file in build/logstash-*; do
+  gsutil cp $file gs://logstash-ci-artifacts/dra/${STACK_VERSION}/
+done
+
+gsutil cp build/distributions/dependencies-reports/logstash-${STACK_VERSION}.csv gs://logstash-ci-artifacts/dra/${STACK_VERSION}/
+gsutil cp build/logstash-${STACK_VERSION}-docker-image-x86_64.tar.gz gs://logstash-ci-artifacts/dra/${STACK_VERSION}/
+gsutil cp build/logstash-oss-${STACK_VERSION}-docker-image-x86_64.tar.gz gs://logstash-ci-artifacts/dra/${STACK_VERSION}/
+gsutil cp build/logstash-ubi8-${STACK_VERSION}-docker-image-x86_64.tar.gz gs://logstash-ci-artifacts/dra/${STACK_VERSION}/
