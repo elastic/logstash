14a5268840b3b97d2906733b233a90aa051a2bab
Andres Rodriguez <andres.lazo@elastic.co>
2022-10-13 10:38:55 -0400

    DRA: Handle env variables better (#14644)
    * DRA: Handle env variables better
    * Moved the addition of SNAPSHOT suffix to the version after the VERSION_QUALIFIER
    * Fix badly assigned variable, version qualifier has to be appended also to PLAIN_STACK_VERSION and not RELEASE_VER
    
    Co-authored-by: andsel <selva.andre@gmail.com>
    (cherry picked from commit db6a7bc619c87fec4b85d60f84ab92fe9ca0694a)

--- ci/dra_aarch64.sh
+++ ci/dra_aarch64.sh
@@ -11,42 +11,45 @@ export JRUBY_OPTS="-J-Xmx1g"
 # The suffix part like alpha1 etc is managed by the optional VERSION_QUALIFIER_OPT environment variable
 STACK_VERSION=`cat versions.yml | sed -n 's/^logstash\:[[:space:]]\([[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\)$/\1/p'`
 
-# WORKFLOW_TYPE is a CI externally configured environment variable where, if not an empty string,
-# it's assumed to be snapshot
-if [ -n "$WORKFLOW_TYPE" ]; then
-  STACK_VERSION=${STACK_VERSION}-SNAPSHOT
-fi
+# WORKFLOW_TYPE is a CI externally configured environment variable that could assume "snapshot" or "staging" values
+case "$WORKFLOW_TYPE" in
+    snapshot)
 
-if [ -z "$WORKFLOW_TYPE" ]; then
-  if [ -z "$VERSION_QUALIFIER_OPT" ]; then
-    RELEASE=1 rake artifact:docker
-    RELEASE=1 rake artifact:docker_oss
-    rake artifact:dockerfiles
-  else
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:docker
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:docker_oss
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:dockerfiles
-    # Qualifier is passed from CI as optional field and specify the version postfix
-    # in case of alpha or beta releases:
-    # e.g: 8.0.0-alpha1
-    STACK_VERSION="${STACK_VERSION}-${VERSION_QUALIFIER_OPT}"
-  fi
-else
-  # WORKFLOW_TYPE is set, it has value "SNAPSHOT"
-  if [ -z "$VERSION_QUALIFIER_OPT" ]; then
-    rake artifact:docker
-    rake artifact:docker_oss
-    rake artifact:dockerfiles
-  else
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:docker
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:docker_oss
-    VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:dockerfiles
-    # Qualifier is passed from CI as optional field and specify the version postfix
-    # in case of alpha or beta releases:
-    # e.g: 8.0.0-alpha1
-    STACK_VERSION="${STACK_VERSION}-${VERSION_QUALIFIER_OPT}"
-  fi
-fi
+        if [ -z "$VERSION_QUALIFIER_OPT" ]; then
+            rake artifact:docker
+            rake artifact:docker_oss
+            rake artifact:dockerfiles
+        else
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:docker
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:docker_oss
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:dockerfiles
+            # Qualifier is passed from CI as optional field and specify the version postfix
+            # in case of alpha or beta releases:
+            # e.g: 8.0.0-alpha1
+            STACK_VERSION="${STACK_VERSION}-${VERSION_QUALIFIER_OPT}"
+        fi
+
+        STACK_VERSION=${STACK_VERSION}-SNAPSHOT
+	;;
+    staging)
+        if [ -z "$VERSION_QUALIFIER_OPT" ]; then
+            RELEASE=1 rake artifact:docker
+            RELEASE=1 rake artifact:docker_oss
+            rake artifact:dockerfiles
+        else
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:docker
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" RELEASE=1 rake artifact:docker_oss
+            VERSION_QUALIFIER="$VERSION_QUALIFIER_OPT" rake artifact:dockerfiles
+            # Qualifier is passed from CI as optional field and specify the version postfix
+            # in case of alpha or beta releases:
+            # e.g: 8.0.0-alpha1
+            STACK_VERSION="${STACK_VERSION}-${VERSION_QUALIFIER_OPT}"
+        fi
+	;;
+    *)
+        exit 1
+	;;
+esac
 
 echo "Saving tar.gz for docker images"
 docker save docker.elastic.co/logstash/logstash:${STACK_VERSION} | gzip -c > build/logstash-${STACK_VERSION}-docker-image-aarch64.tar.gz
