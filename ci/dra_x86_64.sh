7d0f27ec9e2a8598c6f60cc47e5ff7e955328b1d
Andres Rodriguez <andres.lazo@elastic.co>
2022-10-13 12:30:06 -0400

    Add -x

--- ci/dra_x86_64.sh
+++ ci/dra_x86_64.sh
@@ -4,7 +4,7 @@
 echo "####################################################################"
 echo "##################### Starting $0"
 echo "####################################################################"
-
+set -x
 # Since we are using the system jruby, we need to make sure our jvm process
 # uses at least 1g of memory, If we don't do this we can get OOM issues when
 # installing gems. See https://github.com/elastic/logstash/issues/5179
