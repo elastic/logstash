1c730258db6ddf5099fc4199b6fa58c2295b4239
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2022-11-18 07:40:34 +0000

    timer metric tests: extract util, refactor for reuse

--- logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricFactory.java
+++ logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricFactory.java
@@ -3,17 +3,17 @@ package org.logstash.instrument.metrics.timer;
 import org.logstash.instrument.metrics.TestClock;
 
 /**
- * This {@code TestTimerMetricFactory} provides factory methods for constructing implementations
+ * This {@code TimerMetricFactory} provides factory methods for constructing implementations
  * of {@link TimerMetric} for use in test that are connected to a {@link TestClock}
  * (typically {@link org.logstash.instrument.metrics.ManualAdvanceClock}).
  *
  * <p>The factory methods use the package-private constructors provided by the respective
  * implementations, but are <em>public</em>, which makes them available to other test packages.
  */
-public class TestTimerMetricFactory {
+public class TimerMetricFactory {
     private final TestClock testClock;
 
-    public TestTimerMetricFactory(TestClock testClock) {
+    public TimerMetricFactory(TestClock testClock) {
         this.testClock = testClock;
     }
 
--- logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricTest.java
+++ logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricTest.java
@@ -1,22 +1,19 @@
 package org.logstash.instrument.metrics.timer;
 
-import com.google.common.util.concurrent.Monitor;
 import org.junit.Test;
 import org.logstash.instrument.metrics.ManualAdvanceClock;
 
 import java.time.Duration;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
+import java.time.Instant;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * This {@code TimerMetricTest} is meant to be inherited by tests covering the implementations of {@link TimerMetric},
@@ -25,18 +22,22 @@ import static org.junit.Assert.assertThat;
  *
  * <p>At an <em>interface</em>-level, we make no guarantees that tracked executions will be committed into the
  * value <em>until</em> control is returned, which means that the value may or may not include "uncommitted" or
- * currently-executing tracked timings. This means that our assertions can only apply when there are no concurrent
- * executions happening.
+ * currently-executing tracked timings. As a result, our assertions can only place upper-bounds on tracked time
+ * when there are no concurrent executions happening.
  */
 public abstract class TimerMetricTest {
 
-    abstract ManualAdvanceClock getClock();
-    abstract TimerMetric getTimerMetric();
+    protected final ManualAdvanceClock manualAdvanceClock = new ManualAdvanceClock(Instant.now());
+    protected final TimerMetricFactory timerMetricFactory = new TimerMetricFactory(manualAdvanceClock);
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
+    private final BlockingTask.Factory blockingTaskFactory = new BlockingTask.Factory(executorService);
+
+    abstract TimerMetric initTimerMetric(String name);
 
     @Test
     public void testBaselineFunctionality() {
-        final ManualAdvanceClock manualAdvanceClock = getClock();
-        final TimerMetric timerMetric = getTimerMetric();
+        final TimerMetric timerMetric = initTimerMetric("duration_in_millis");
 
         assertThat(timerMetric.getValue(), is(equalTo(0L)));
 
@@ -61,9 +62,7 @@ public abstract class TimerMetricTest {
 
     @Test
     public void testValueAfterConcurrentTrackedExecutions() throws Exception {
-        final ManualAdvanceClock manualAdvanceClock = getClock();
-        final TimerMetric timerMetric = getTimerMetric();
-        final ExecutorService executor = Executors.newFixedThreadPool(5);
+        final TimerMetric timerMetric = initTimerMetric("duration_in_millis");
 
         // assert baseline timer is not incrementing when time is passing
         assertThat(timerMetric.getValue(), is(equalTo(0L)));
@@ -71,15 +70,15 @@ public abstract class TimerMetricTest {
         assertThat(timerMetric.getValue(), is(equalTo(0L)));
 
         // methodology note: each state-change affects a single column in a decimal-formatted long,
-        // which gives us a bread-crumb for identifying he cause of issues when our expectation
+        // which gives us a bread-crumb for identifying the cause of issues when our expectation
         // does not match
         long expectedAdvance = 0L;
 
-        final TimedBlockingTask taskOne = TimedBlockingTask.start(executor, timerMetric);
+        final BlockingTask<Void> taskOne = timedBlockingTask(timerMetric);
         manualAdvanceClock.advance(Duration.ofMillis(1L));
         expectedAdvance += Math.multiplyExact(1L, 1);
 
-        final TimedBlockingTask taskTwo = TimedBlockingTask.start(executor, timerMetric);
+        final BlockingTask<Void> taskTwo = timedBlockingTask(timerMetric);
         manualAdvanceClock.advance(Duration.ofMillis(10L));
         expectedAdvance += Math.multiplyExact(10L, 2);
 
@@ -87,11 +86,11 @@ public abstract class TimerMetricTest {
         manualAdvanceClock.advance(Duration.ofMillis(100L));
         expectedAdvance += Math.multiplyExact(100L, 1);
 
-        final TimedBlockingTask taskThree = TimedBlockingTask.start(executor, timerMetric);
+        final BlockingTask<Void> taskThree = timedBlockingTask(timerMetric);
         manualAdvanceClock.advance(Duration.ofMillis(1_000L));
         expectedAdvance += Math.multiplyExact(1_000L, 2);
 
-        final TimedBlockingTask taskFour = TimedBlockingTask.start(executor, timerMetric);
+        final BlockingTask<Void> taskFour = timedBlockingTask(timerMetric);
         manualAdvanceClock.advance(Duration.ofMillis(10_000L));
         expectedAdvance += Math.multiplyExact(10_000L, 3);
 
@@ -107,7 +106,7 @@ public abstract class TimerMetricTest {
         manualAdvanceClock.advance(Duration.ofMillis(10_000_000L));
         expectedAdvance += Math.multiplyExact(10_000_000L, 0);
 
-        final TimedBlockingTask taskFive = TimedBlockingTask.start(executor, timerMetric);
+        final BlockingTask<Void> taskFive = timedBlockingTask(timerMetric);
         manualAdvanceClock.advance(Duration.ofMillis(100_000_000L));
         expectedAdvance += Math.multiplyExact(100_000_000L, 1);
 
@@ -120,56 +119,42 @@ public abstract class TimerMetricTest {
         assertThat(timerMetric.getValue(), is(equalTo(expectedAdvance)));
     }
 
-    private static class TimedBlockingTask {
-        private volatile boolean shouldBlock = true;
-        private volatile boolean isBlocking = false;
-        private final Monitor monitor = new Monitor();
-        private final Monitor.Guard blockRelease = monitor.newGuard(() -> !shouldBlock);
-        private final Monitor.Guard blockActive = monitor.newGuard(() -> isBlocking);
+    @Test
+    public void testReturnValue() {
+        final TimerMetric timerMetric = initTimerMetric("ok");
 
-        protected volatile Future<?> future;
+        final Optional<String> original = Optional.of("hello");
 
-        private static final Duration SAFEGUARD = Duration.ofSeconds(10);
+        final Optional<String> result = timerMetric.time(() -> original);
 
-        static TimedBlockingTask start(final ExecutorService executorService, final TimerMetric timerMetric) {
-            final TimedBlockingTask timedBlockingTask = new TimedBlockingTask();
-            timedBlockingTask.future = executorService.submit(() -> timerMetric.time(timedBlockingTask::block));
-            timedBlockingTask.waitUntilBlocked();
-            return timedBlockingTask;
+        assertSame(original, result);
+    }
+
+    private static class ACheckedException extends Exception {
+        private static final long serialVersionUID = 1L;
+        public ACheckedException(String message) {
+            super(message);
         }
+    }
 
-        void block() {
-            try {
-                monitor.enterInterruptibly(10, TimeUnit.SECONDS);
-                this.isBlocking = true;
-                monitor.waitFor(blockRelease, SAFEGUARD);
-                this.isBlocking = false;
-                monitor.leave();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+    @Test
+    public void testThrowing() {
+        final TimerMetric timerMetric = initTimerMetric("oh no");
+        final ACheckedException checkedException = new ACheckedException("gotcha");
+        try {
+            timerMetric.time(() -> { throw checkedException; });
+        } catch (ACheckedException chk) {
+            assertSame(checkedException, chk);
+            return;
         }
+        fail("Checked exception not caught!");
+    }
 
-        void waitUntilBlocked() {
-            try {
-                monitor.enterWhen(blockActive, SAFEGUARD);
-                monitor.leave();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
-        void complete() throws ExecutionException, InterruptedException, TimeoutException {
-            Objects.requireNonNull(this.future);
-            try {
-                monitor.enterInterruptibly(SAFEGUARD);
-                this.shouldBlock = false;
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } finally {
-                monitor.leave();
-            }
-            this.future.get(SAFEGUARD.getSeconds(), TimeUnit.SECONDS);
-        }
+    private BlockingTask<Void> timedBlockingTask(final TimerMetric timerMetric) {
+        return blockingTaskFactory.wrapping((controlChannel) -> {
+            timerMetric.time(controlChannel::markReadyAndBlockUntilRelease);
+            return null;
+        });
     }
 }
