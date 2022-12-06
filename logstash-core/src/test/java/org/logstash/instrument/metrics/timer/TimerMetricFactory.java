3e308a29b1ab310e530cc96d2507bff89b77b315
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2022-12-06 19:12:12 +0000

    test helper: TimerMetricFactory prefers nanotime supplier

--- logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricFactory.java
+++ logstash-core/src/test/java/org/logstash/instrument/metrics/timer/TimerMetricFactory.java
@@ -2,26 +2,32 @@ package org.logstash.instrument.metrics.timer;
 
 import org.logstash.instrument.metrics.TestClock;
 
+import java.util.function.LongSupplier;
+
 /**
  * This {@code TimerMetricFactory} provides factory methods for constructing implementations
- * of {@link TimerMetric} for use in test that are connected to a {@link TestClock}
- * (typically {@link org.logstash.instrument.metrics.ManualAdvanceClock}).
+ * of {@link TimerMetric} for use in test that are connected to a nano-time supplier (typically
+ * {@link TestClock#nanoTime()} from {@link org.logstash.instrument.metrics.ManualAdvanceClock}).
  *
  * <p>The factory methods use the package-private constructors provided by the respective
  * implementations, but are <em>public</em>, which makes them available to other test packages.
  */
 public class TimerMetricFactory {
-    private final TestClock testClock;
+    private final LongSupplier nanoTimeSupplier;
 
     public TimerMetricFactory(TestClock testClock) {
-        this.testClock = testClock;
+        this(testClock::nanoTime);
+    }
+
+    public TimerMetricFactory(final LongSupplier nanoTimeSupplier) {
+        this.nanoTimeSupplier = nanoTimeSupplier;
     }
 
     public AfterCompletionTimerMetric newAfterCompletionTimerMetric(final String name) {
-        return new AfterCompletionTimerMetric(name, this.testClock::nanoTime);
+        return new AfterCompletionTimerMetric(name, this.nanoTimeSupplier);
     }
 
     public ConcurrentLiveTimerMetric newConcurrentLiveTimerMetric(final String name) {
-        return new ConcurrentLiveTimerMetric(name, this.testClock::nanoTime);
+        return new ConcurrentLiveTimerMetric(name, this.nanoTimeSupplier);
     }
 }
