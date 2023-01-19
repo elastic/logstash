4e322d13fed9fdd3a8cbaf79c1ecf82069673c35
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2023-01-19 13:53:36 +0000

    Update logstash-core/lib/logstash/runner.rb
    Co-authored-by: Ry Biesemeyer <yaauie@users.noreply.github.com>

--- logstash-core/lib/logstash/runner.rb
+++ logstash-core/lib/logstash/runner.rb
@@ -344,7 +344,7 @@ class LogStash::Runner < Clamp::StrictCommand
 
     tags_illegal_setting = settings.get_setting('event_api.tags.illegal').value
     if tags_illegal_setting == 'warn'
-      logger.warn(I18n.t("logstash.runner.tags-illegal-warning"))
+      deprecation_logger.deprecated(I18n.t("logstash.runner.tags-illegal-warning"))
       org.logstash.Event::set_illegal_tags_action(tags_illegal_setting)
     end
 
