06f32e1fafe37aea3f841077dc1a095c2735c2e3
andsel <selva.andre@gmail.com>
2023-04-05 16:29:10 +0200

    Simplified a little bit the code

--- bundler_error_simplified_reproducer.rb
+++ bundler_error_simplified_reproducer.rb
@@ -5,20 +5,28 @@
 # To run, copy script to Logstash folder and run:
 # 
 # bin/ruby bundler_error_simplified_reproducer.rb
+# Util JRuby flags
+# -J-Xmx2g
+# -J-Djruby.compile.mode=JIT | FORCE | OFF
+# -J-Djruby.compile.invokedynamic=false | true
+# -J-Djruby.reify.classes=true
 
-require_relative "lib/bootstrap/environment"
 require "bundler"
 
-lock_file = Pathname.new(::File.join(LogStash::Environment::LOGSTASH_HOME, "lockfiles", "Gemfile.lock"))
+cwd = ::File.expand_path(::File.join(__FILE__, ".."))
+puts "current working dir: #{cwd}"
+
+lock_file = Pathname.new(::File.join(cwd, "lockfiles", "Gemfile.lock"))
 puts "lock_file is: #{lock_file}"
 
-plugin = "logstash-input-cloudwatch"
-
-plugin_gemfile_path = ::File.join(LogStash::Environment::LOGSTASH_HOME, "lockfiles", plugin)  
-plugin_gemfile = ::File.new(plugin_gemfile_path, "r+").read
-puts "loaded content of #{plugin_gemfile_path}"
+gemfile_path = ::File.join(cwd, "lockfiles", "logstash-input-cloudwatch")
+gemfile = ::File.new(gemfile_path, "r+").read
+puts "loaded content of #{gemfile_path}"
 
 builder = Bundler::Dsl.new
-builder.eval_gemfile("bundler file", plugin_gemfile)
+builder.eval_gemfile("bundler file", gemfile)
 definition = builder.to_definition(lock_file, {})
+
+# the following is the incriminated
+puts "Before resolve_remotely!"
 definition.resolve_remotely!
