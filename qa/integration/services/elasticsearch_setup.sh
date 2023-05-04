811318e9a32dd2257c88a8ef3c4384e110ec6baa
edmocosta <11836452+edmocosta@users.noreply.github.com>
2023-05-04 13:08:21 +0200

    Revert "Changed the ES setup to use the cluster health API"
    This reverts commit 0d30b69b7bc8a281d4636ee6384ab5c6dc367985.

--- qa/integration/services/elasticsearch_setup.sh
+++ qa/integration/services/elasticsearch_setup.sh
@@ -11,7 +11,7 @@ start_es() {
   JAVA_HOME= $ES_HOME/bin/elasticsearch -Expack.security.enabled=false -Epath.data=/tmp/ls_integration/es-data -Ediscovery.type=single-node -Epath.logs=/tmp/ls_integration/es-logs $es_args -p $ES_HOME/elasticsearch.pid > /tmp/elasticsearch.log 2>/dev/null &
   count=120
   echo "Waiting for elasticsearch to respond..."
-  while ! curl --silent "localhost:9200/_cluster/health?wait_for_status=green&timeout=500ms" | grep \"status\":\"green\" && [[ $count -ne 0 ]]; do
+  while ! curl --silent localhost:9200 && [[ $count -ne 0 ]]; do
       count=$(( $count - 1 ))
       [[ $count -eq 0 ]] && cat /tmp/elasticsearch.log && return 1
       sleep 1
