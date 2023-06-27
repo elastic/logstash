914efe11c1eead3ff390e55902f72137c83d25f7
Kaise Cheng <kaise.cheng@elastic.co>
2023-06-27 17:12:23 +0200

    use vault

--- ci/serverless_tests.sh
+++ ci/serverless_tests.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
+vault_path=secret/ci/elastic-logstash/serverless-test
 # Since we are using the system jruby, we need to make sure our jvm process
 # uses at least 1g of memory, If we don't do this we can get OOM issues when
 # installing gems. See https://github.com/elastic/logstash/issues/5179
 export JRUBY_OPTS="-J-Xmx1g"
 export SERVERLESS=true
-export ES_ENDPOINT=
-export ES_USER=
-export ES_PW=
+export ES_ENDPOINT=$(vault read -field=host "${vault_path}")
+export ES_USER=$(vault read -field=super_user "${vault_path}")
+export ES_PW=$(vault read -field=super_user_pw "${vault_path}")
 
 ./gradlew clean bootstrap assemble installDefaultGems unpackTarDistribution :logstash-core:copyGemjar
 
