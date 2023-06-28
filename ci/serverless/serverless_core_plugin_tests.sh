8371fc9ba27a4243a4edc8dab250c783486fbfbd
Kaise Cheng <kaise.cheng@elastic.co>
2023-06-28 13:38:51 +0200

    bash script for core and plugin tests

---
+++ ci/serverless/serverless_core_plugin_tests.sh
@@ -0,0 +1,97 @@
+### require jq
+#!/usr/bin/env bash
+set -ex
+
+CURRENT_DIR="$(dirname "$0")"
+
+setup_vault() {
+  vault_path=secret/ci/elastic-logstash/serverless-test
+  export ES_ENDPOINT=$(vault read -field=host "${vault_path}")
+  export ES_USER=$(vault read -field=super_user "${vault_path}")
+  export ES_PW=$(vault read -field=super_user_pw "${vault_path}")
+}
+
+build_logstash() {
+    ./gradlew clean bootstrap assemble installDefaultGems
+}
+
+prepare_cpm_pipelines() {
+  # es-output
+  index_pipeline 'gen_es' '{
+    "pipeline": "input { generator { count => 100 } } output { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" } }",
+    "last_modified": "2022-02-22T22:22:22.222Z",
+    "pipeline_metadata": { "version": "1"},
+    "username": "log.stash",
+    "pipeline_settings": {"pipeline.batch.delay": "50"}
+  }'
+
+  # es-input
+  index_pipeline 'es_stdout' '{
+    "pipeline": "input { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" } } output { }",
+    "last_modified": "2022-02-22T22:22:22.222Z",
+    "pipeline_metadata": { "version": "1"},
+    "username": "log.stash",
+    "pipeline_settings": {"pipeline.batch.delay": "50"}
+  }'
+}
+
+index_pipeline() {
+  # index pipeline to serverless ES
+  curl -X PUT -u "$ES_USER:$ES_PW" "$ES_ENDPOINT/_logstash/pipeline/$1"  -H 'Content-Type: application/json' -d "$2"; 
+
+  #TODO check error
+}
+
+run_logstash() {
+  # create logstash.yml
+  cat <<EOF > "$CURRENT_DIR/logstash.yml"
+xpack.management.enabled: true
+xpack.management.pipeline.id: ["*"]
+xpack.management.elasticsearch.username: $ES_USER
+xpack.management.elasticsearch.password: $ES_PW
+xpack.management.elasticsearch.hosts: ["$ES_ENDPOINT"]
+EOF
+
+  $CURRENT_DIR/../../bin/logstash --path.settings $CURRENT_DIR --path.logs $CURRENT_DIR 2>/dev/null &
+
+  export LS_PID=$!
+}
+
+check_logstash_readiness() {
+  count=120
+  echo "Waiting Logstash to respond..."
+  while ! curl --silent localhost:9600 && [[ $count -ne -1 ]]; do
+      count=$(( $count - 1 ))
+      [[ $count -eq 0 ]] && cat "/$CURRENT_DIR/logstash-plain.log" && return 1
+      sleep 1
+  done
+  echo "Logstash is Up !"
+  return 0
+}
+
+check_logstash_api() {
+  count=30
+  echo "Checking Logstash API..."
+  while ! [[ `curl --silent localhost:9600/_node/stats | jq "$1"` -eq $2 ]] && [[ $count -ne -1 ]]; do
+      count=$(( $count - 1 ))
+      [[ $count -eq 0 ]] && cat "$CURRENT_DIR/logstash-plain.log" && clean_up && return 1
+      sleep 1
+  done
+  echo "Passed check"
+  return 0
+}
+
+clean_up() {
+  kill $LS_PID
+  echo "Clean up data"
+} 
+
+setup_vault
+prepare_cpm_pipelines
+run_logstash
+check_logstash_readiness
+
+# check es-output
+check_logstash_api '.pipelines.gen_es.plugins.outputs[0].documents.successes' '100'
+
+clean_up
