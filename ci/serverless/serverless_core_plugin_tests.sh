07b93cc2f1cb5a27881a2c55dbb9b117844f76bb
Kaise Cheng <kaise.cheng@elastic.co>
2023-06-29 14:44:26 +0200

    add stack monitoring, disable es-input

--- ci/serverless/serverless_core_plugin_tests.sh
+++ ci/serverless/serverless_core_plugin_tests.sh
@@ -18,27 +18,29 @@ build_logstash() {
 }
 
 prepare_cpm_pipelines() {
+  export INDEX_NAME="serverless_it_${BUILDKITE_BUILD_NUMBER:-`date +%s`}"
+
   # es-output
   index_pipeline 'gen_es' '{
-    "pipeline": "input { generator { count => 100 } } output { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" } }",
+    "pipeline": "input { generator { count => 100 } } output { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" index=> \"'$INDEX_NAME'\" } }",
     "last_modified": "2022-02-22T22:22:22.222Z",
     "pipeline_metadata": { "version": "1"},
     "username": "log.stash",
     "pipeline_settings": {"pipeline.batch.delay": "50"}
   }'
 
-  # es-input
-  index_pipeline 'es_stdout' '{
-    "pipeline": "input { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" index => \"logs-generic-default\" schedule => \"*/10 * * * * *\" size => 100 } } output { stdout { codec => dots } }",
-    "last_modified": "2022-02-22T22:22:22.222Z",
-    "pipeline_metadata": { "version": "1"},
-    "username": "log.stash",
-    "pipeline_settings": {"pipeline.batch.delay": "50"}
-  }'
+  # # es-input
+  # index_pipeline 'es_stdout' '{
+  #   "pipeline": "input { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" index => \"logs-generic-default\" schedule => \"*/10 * * * * *\" size => 100 } } output { stdout { codec => dots } }",
+  #   "last_modified": "2022-02-22T22:22:22.222Z",
+  #   "pipeline_metadata": { "version": "1"},
+  #   "username": "log.stash",
+  #   "pipeline_settings": {"pipeline.batch.delay": "50"}
+  # }'
 
   # es-filter
   index_pipeline 'gen_es_stdout' '{
-    "pipeline": "input { generator { count => 100 } } filter { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" index => \"logs-generic-default\" } } output { stdout { codec => dots } }",
+    "pipeline": "input { generator { count => 100 } } filter { elasticsearch { hosts => \"${ES_ENDPOINT}\" user => \"${ES_USER}\" password => \"${ES_PW}\" index => \"'$INDEX_NAME'\" query => \"*\" add_field => {\"passed\" => \"good\"} } if [passed] == \"good\" { mutate { add_tag => [\"yes\"] } }  } output { stdout { codec => dots } }",
     "last_modified": "2022-02-22T22:22:22.222Z",
     "pipeline_metadata": { "version": "1"},
     "username": "log.stash",
@@ -51,6 +53,10 @@ index_pipeline() {
   curl -X PUT -u "$ES_USER:$ES_PW" "$ES_ENDPOINT/_logstash/pipeline/$1"  -H 'Content-Type: application/json' -d "$2"; 
 
   #TODO check error
+}
+
+get_monitor_count() {
+  curl -s -u "$ES_USER:$ES_PW" "$ES_ENDPOINT/.monitoring-logstash-*/_count" | jq '.count'
 }
 
 run_logstash() {
@@ -82,7 +88,7 @@ check_logstash_readiness() {
   echo "Waiting Logstash to respond..."
   while ! curl --silent localhost:9600 && [[ $count -ne -1 ]]; do
       count=$(( $count - 1 ))
-      [[ $count -eq 0 ]] && cat "$CURRENT_DIR/logstash-plain.log" && return 1
+      [[ $count -eq 0 ]] && return 1
       sleep 1
   done
   echo "Logstash is Up !"
@@ -90,11 +96,11 @@ check_logstash_readiness() {
 }
 
 check_logstash_api() {
-  count=30
+  count=60
   echo "Checking Logstash API..."
   while ! [[ `curl --silent localhost:9600/_node/stats | jq "$1"` -ge $2 ]] && [[ $count -ne -1 ]]; do
       count=$(( $count - 1 ))
-      [[ $count -eq 0 ]] && cat "$CURRENT_DIR/logstash-plain.log" && clean_up && echo "1"
+      [[ $count -eq 0 ]] && echo "1" && return
       sleep 1
   done
   echo "Passed check"
@@ -103,12 +109,12 @@ check_logstash_api() {
 
 clean_up() {
   kill $LS_PID
-  curl -u "$ES_USER:$ES_PW" -X DELETE "$ES_ENDPOINT/_data_stream/logs-generic-default"
-  echo "Cleaned up data"
+  echo "Done"
 } 
 
 setup_vault
 build_logstash
+INITIAL_MONITOR_CNT=$(get_monitor_count)
 prepare_cpm_pipelines
 run_logstash
 check_logstash_readiness
@@ -122,12 +128,23 @@ PLUGIN_ES_INPUT=$(check_logstash_api '.pipelines.es_stdout.plugins.inputs[0].eve
 PLUGIN_ES_INPUT="${PLUGIN_ES_INPUT: -1}"
 
 # check es-filter, depend on es-output
-PLUGIN_ES_FILTER=$(check_logstash_api '.pipelines.gen_es_stdout.plugins.filters[0].events.out' '100')
+PLUGIN_ES_FILTER=$(check_logstash_api '.pipelines.gen_es_stdout.plugins.filters[1].events.out' '100')
 PLUGIN_ES_FILTER="${PLUGIN_ES_FILTER: -1}"
+
+# check monitor
+MONITOR_CNT=$(get_monitor_count)
+if [[ "$MONITOR_CNT" -gt "$INITIAL_MONITOR_CNT" ]]; then
+  MONITOR_CHECK=0
+else
+  MONITOR_CHECK=1
+fi
 
 clean_up
 
-if [[ ("$PLUGIN_ES_OUTPUT" -eq 0) && ("$PLUGIN_ES_INPUT" -eq 0) && ("$PLUGIN_ES_FILTER" -eq 0) ]]; then
+echo "es-output : es-input : es-filter : stack monitor"
+echo "$PLUGIN_ES_OUTPUT : $PLUGIN_ES_INPUT : $PLUGIN_ES_FILTER : $MONITOR_CHECK"
+
+if [[ ("$PLUGIN_ES_OUTPUT" -eq 0) && ("$PLUGIN_ES_INPUT" -eq 0) && ("$PLUGIN_ES_FILTER" -eq 0) && ("$MONITOR_CHECK" -eq 0) ]]; then
   exit 0
 else
   exit 1
