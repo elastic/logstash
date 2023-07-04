ae8a5e430ec8a51f5723a47fa6c37de811d59f30
Kaise Cheng <kaise.cheng@elastic.co>
2023-07-04 21:05:29 +0100

    update rspec test

--- ci/serverless/serverless_core_rspec_tests.sh
+++ ci/serverless/serverless_core_rspec_tests.sh
@@ -7,17 +7,18 @@ vault_path=secret/ci/elastic-logstash/serverless-test
 # installing gems. See https://github.com/elastic/logstash/issues/5179
 export JRUBY_OPTS="-J-Xmx1g"
 export SERVERLESS=true
+set +x
 export ES_ENDPOINT=$(vault read -field=host "${vault_path}")
 export ES_USER=$(vault read -field=super_user "${vault_path}")
 export ES_PW=$(vault read -field=super_user_pw "${vault_path}")
+set -x
 
 ./gradlew clean bootstrap assemble installDefaultGems unpackTarDistribution
 ./gradlew :logstash-core:copyGemjar
 
-export GEM_PATH=vendor/bundle/jruby/2.6.0                                                 
-export GEM_HOME=vendor/bundle/jruby/2.6.0 
+export GEM_PATH=vendor/bundle/jruby/2.6.0
+export GEM_HOME=vendor/bundle/jruby/2.6.0
 
 vendor/jruby/bin/jruby -S bundle install --with development
 
-vendor/jruby/bin/jruby -S bundle exec rspec -fd qa/integration/specs/es_output_how_spec.rb
-vendor/jruby/bin/jruby -S bundle exec rspec -fd qa/integration/specs/dlq_spec.rb
+vendor/jruby/bin/jruby -S bundle exec rspec -fd qa/integration/specs/dlq_spec.rb -e "using pipelines.yml"
