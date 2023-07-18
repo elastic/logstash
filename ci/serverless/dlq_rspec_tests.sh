456c8b713f654b60fc02667093bd55df1e6d0b3e
Kaise Cheng <kaise.cheng@elastic.co>
2023-07-18 16:24:53 +0100

    gem path is needed

--- ci/serverless/dlq_rspec_tests.sh
+++ ci/serverless/dlq_rspec_tests.sh
@@ -16,6 +16,9 @@ set -x
 ./gradlew clean bootstrap assemble installDefaultGems unpackTarDistribution
 ./gradlew :logstash-core:copyGemjar
 
+export GEM_PATH=vendor/bundle/jruby/3.1.0
+export GEM_HOME=vendor/bundle/jruby/3.1.0
+
 vendor/jruby/bin/jruby -S bundle install --with development
 
 vendor/jruby/bin/jruby -S bundle exec rspec -fd qa/integration/specs/dlq_spec.rb -e "using pipelines.yml"
