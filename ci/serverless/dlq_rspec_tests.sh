a49578339c0273763b31536509caef412d1cee9e
Kaise Cheng <kaise.cheng@elastic.co>
2023-07-19 17:19:16 +0100

    remove comment

--- ci/serverless/dlq_rspec_tests.sh
+++ ci/serverless/dlq_rspec_tests.sh
@@ -2,9 +2,7 @@
 set -ex
 
 vault_path=secret/ci/elastic-logstash/serverless-test
-# Since we are using the system jruby, we need to make sure our jvm process
-# uses at least 1g of memory, If we don't do this we can get OOM issues when
-# installing gems. See https://github.com/elastic/logstash/issues/5179
+
 export JRUBY_OPTS="-J-Xmx1g"
 export SERVERLESS=true
 set +x
