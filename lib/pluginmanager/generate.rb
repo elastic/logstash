3447e5c530c890c191f4a19c4feb271e25d994ae
Andres Rodriguez <andres.lazo@elastic.co>
2023-07-19 17:19:37 -0400

    Enable SpaceInsidePercentLiteralDelimiters

--- lib/pluginmanager/generate.rb
+++ lib/pluginmanager/generate.rb
@@ -98,8 +98,8 @@ class LogStash::PluginManager::Generate < LogStash::PluginManager::Command
 
   def get_git_info
     git = OpenStruct.new
-    git.author = %x{ git config --get user.name  }.strip rescue "your_username"
-    git.email  = %x{ git config --get user.email }.strip rescue "your_username@example.com"
+    git.author = %x{git config --get user.name}.strip rescue "your_username"
+    git.email  = %x{git config --get user.email}.strip rescue "your_username@example.com"
     git
   end
 
