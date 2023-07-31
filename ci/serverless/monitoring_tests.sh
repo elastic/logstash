0a096d2bdf8428b7301a84e0745001807bfe1764
Kaise Cheng <kaise.cheng@elastic.co>
2023-07-31 16:18:26 +0100

    comment out legacy montioring test

---
+++ ci/serverless/monitoring_tests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Legacy monitoring is disabled. Serverless does not support /_monitoring/bulk, hence this test always fails to ingest metrics.
+set -ex
+
+source ./$(dirname "$0")/common.sh
+
+get_monitor_count() {
+  curl -s -u "$ES_USER:$ES_PW" "$ES_ENDPOINT/.monitoring-logstash-7-*/_count" | jq '.count'
+}
+
+compare_monitor_count() {
+   [[ $(get_monitor_count) -gt "$INITIAL_MONITOR_CNT" ]] && echo "0"
+}
+
+check_monitor() {
+  count_down_check 20 compare_monitor_count
+}
+
+check() {
+  add_check check_monitor "Failed monitor check."
+}
+
+setup
+export INITIAL_MONITOR_CNT=$(get_monitor_count)
+run_cpm_logstash check
