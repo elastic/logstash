0a1ebd78b9f8b8431c39e89929febc45d6a99a84
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-09-28 11:43:51 +0300

    [buildkite, DRA] Replicate Jenkins umask
    Jenkins workers have a different default umask (0022 vs 0002 on
    Buildkite) which affects the default permissions of some artifacts
    (e.g. .deb)
    
    This commit explicitly sets the right umask so that Jenkins and
    Buildkite DRA artifacts are identical.

--- .buildkite/scripts/dra/common.sh
+++ .buildkite/scripts/dra/common.sh
@@ -45,3 +45,5 @@ if [[ ! -z $DRA_DRY_RUN && $BUILDKITE_STEP_KEY == "logstash_publish_dra" ]]; the
     info "Release manager will run in dry-run mode [$DRA_DRY_RUN]"
 fi
 
+# Replicate same default umask used on Jenkins workers
+umask 0022
