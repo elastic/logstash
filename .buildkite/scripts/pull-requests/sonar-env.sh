56a2eeff1ff6c4d4967fdd7bcc0ffb2db23f7227
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-10-12 13:16:10 +0300

    debug

--- .buildkite/scripts/pull-requests/sonar-env.sh
+++ .buildkite/scripts/pull-requests/sonar-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SONAR_TOKEN_PATH="kv/ci-shared/platform-ingest/elastic/logstash/sonar-creds"
-SONAR_TOKEN=$(retry -t 5 -- vault kv get -field=token ${SONAR_TOKEN_PATH})
+export SONAR_TOKEN=$(retry -t 5 -- vault kv get -field=token ${SONAR_TOKEN_PATH})
 
 export SOURCE_BRANCH=$GITHUB_PR_BRANCH
 export TARGET_BRANCH=$GITHUB_PR_TARGET_BRANCH
