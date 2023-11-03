731e6e576c2542aa6d1bfe49679cb5098687725c
Andres Rodriguez <andres.lazo@elastic.co>
2023-11-03 11:42:20 -0400

    Remove unnecessary file

--- ci/test_unsupported_plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ie
-export JRUBY_OPTS="-J-Xmx1g"
-export GRADLE_OPTS="-Xmx4g -Dorg.gradle.jvmargs=-Xmx4g -Dorg.gradle.daemon=false -Dorg.gradle.logging.level=info -Dfile.encoding=UTF-8"
-
-./gradlew assemble
-
-bin/ruby ci/test_plugins.rb $@
