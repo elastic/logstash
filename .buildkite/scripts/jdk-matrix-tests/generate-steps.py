a8c1fe24a26838eddcb6042339ba13bef8d65997
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-11-06 21:34:27 +0200

    use right command ...

--- .buildkite/scripts/jdk-matrix-tests/generate-steps.py
+++ .buildkite/scripts/jdk-matrix-tests/generate-steps.py
@@ -5,7 +5,7 @@ import typing
 import yaml
 
 def get_bk_metadata(key: str) -> typing.List[str]:
-    res = subprocess.run(["buildkite-metadata", "get", key], capture_output=True, shell=True)
+    res = subprocess.run(["buildkite-agent", "meta-data", "get", key], capture_output=True, shell=True)
     if res.returncode != 0:
         print(f"Failed to retrieve buildkite key [{key}]. Error:\n{res.stderr.decode('utf-8')}\nExiting.")
         exit(1)
