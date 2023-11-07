0bd57564472011863dbc55491b9104f8bebbbbf2
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-11-07 10:11:47 +0200

    Final improvements

--- .buildkite/scripts/jdk-matrix-tests/generate-steps.py
+++ .buildkite/scripts/jdk-matrix-tests/generate-steps.py
@@ -1,25 +1,28 @@
-from dataclasses import dataclass
-import subprocess
 import os
 import typing
 
 import yaml
 
 def get_bk_metadata(key: str) -> typing.List[str]:
-    return os.environ[key].split()
+    try:
+      return os.environ[key].split()
+    except KeyError:
+        print(f"Missing environment variable [{key}]. This should be set before calling this script using buildkite-agent meta-data get. Exiting.")
+        exit(1)
 
 def bk_annotate(job_name_human: str, job_name_slug: str, os: str, jdk: str, status: str) -> str:
   return f"""buildkite-agent annotate "{status} **{job_name_human}** / **{os}** / **{jdk}**" --context={job_name_slug}-{os}-{jdk}"""
 
-@dataclass
+
 class LinuxJobs:
     def __init__(self, os: str, jdk: str):
       self.running_emoji: str = ":bk-status-running:"
       self.success_emoji: str = ":bk-status-passed:"
+      self.failed_emoji: str = ":bk-status-failed:"
       self.os = os
       self.jdk = jdk
 
-    def all_jobs(self):
+    def all_jobs(self) -> list[typing.Callable[[], typing.Tuple[str, str]]]:
         return [
             self.java_unit_test,
             self.ruby_unit_test,
@@ -48,91 +51,87 @@ export PATH="/opt/buildkite-agent/.rbenv/bin:/opt/buildkite-agent/.pyenv/bin:$PA
 eval "$(rbenv init -)"
 """
 
-    def java_unit_test(self) -> str:
+    def emit_command(self, job_name_human, job_name_slug, test_command: str) -> str:
+      return f"""
+{self.prepare_shell()}
+{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+# temporarily disable immediately failure on error, so that we can update the BK annotation
+set +eo pipefail
+{test_command}
+if [[ $? -neq 0 ]]; then
+  {bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.failed_emoji)}
+  exit 1
+else
+  {bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
+fi
+      """        
+    def java_unit_test(self) -> typing.Tuple[str, str]:
         job_name_human = "Java Unit Test"
         job_name_slug = "java-unit-test"
-        shell_command = f"""
-{self.prepare_shell()}
+        test_command = '''
 export ENABLE_SONARQUBE="false"
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
 ci/unit_tests.sh java
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
-        """
+        '''
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
-    def ruby_unit_test(self) -> str:
+    def ruby_unit_test(self) -> typing.Tuple[str, str]:
         job_name_human = "Ruby Unit Test"
         job_name_slug = "ruby-unit-test"
-        shell_command = f"""
-{self.prepare_shell()}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+        test_command = """
 ci/unit_tests.sh ruby
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
         """
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
-    def integration_tests_part_1(self) -> str:
+    def integration_tests_part_1(self) -> typing.Tuple[str, str]:
         return self.integration_tests(part=1)
 
-    def integration_tests_part_2(self) -> str:
+    def integration_tests_part_2(self) -> typing.Tuple[str, str]:
         return self.integration_tests(part=2)
 
-    def integration_tests(self, part: str) -> str:
+    def integration_tests(self, part: int) -> typing.Tuple[str, str]:
         job_name_human = f"Integration Tests - {part}"
         job_name_slug = f"integration-tests-pt-{part}"
-        shell_command = f"""
-{self.prepare_shell()}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+        test_command = f"""
 ci/integration_tests.sh split {part-1}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
         """
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
-    def pq_integration_tests_part_1(self) -> str:
+    def pq_integration_tests_part_1(self) -> typing.Tuple[str, str]:
         return self.pq_integration_tests(part=1)
 
-    def pq_integration_tests_part_2(self) -> str:
+    def pq_integration_tests_part_2(self) -> typing.Tuple[str, str]:
         return self.pq_integration_tests(part=2)
 
-    def pq_integration_tests(self, part: str) -> str:
+    def pq_integration_tests(self, part: int) -> typing.Tuple[str, str]:
         job_name_human = f"IT Persistent Queues - {part}"
         job_name_slug = f"it-persistent-queues-pt-{part}"
-        shell_command = f"""
-{self.prepare_shell()}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+        test_command = f"""
 export FEATURE_FLAG=persistent_queues
 ci/integration_tests.sh split {part-1}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
         """
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
-    def x_pack_unit_tests(self) -> str:
-        job_name_human = f"x-pack unit tests"
+    def x_pack_unit_tests(self) -> typing.Tuple[str, str]:
+        job_name_human = "x-pack unit tests"
         job_name_slug = "x-pack-unit-tests"
-        shell_command = f"""
-{self.prepare_shell()}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+        test_command = """
 x-pack/ci/unit_tests.sh
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
         """
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
-    def x_pack_integration(self) -> str:
-        job_name_human = f"x-pack integration"
+    def x_pack_integration(self) -> typing.Tuple[str, str]:
+        job_name_human = "x-pack integration"
         job_name_slug = "x-pack-integration"
-        shell_command = f"""
-{self.prepare_shell()}
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.running_emoji)}
+        test_command = """
 x-pack/ci/integration_tests.sh
-{bk_annotate(job_name_human, job_name_slug, self.os, self.jdk, self.success_emoji)}
         """
 
-        return job_name_human, job_name_slug, shell_command
+        return job_name_human, self.emit_command(job_name_human, job_name_slug, test_command)
 
 
 if __name__ == "__main__":
@@ -147,7 +146,7 @@ if __name__ == "__main__":
           jobs = LinuxJobs(os=matrix_os, jdk=matrix_jdk)
 
           for job in jobs.all_jobs():
-            job_name_human, job_name_slug, shell_command = job()
+            job_name_human, shell_command = job()
             step = {
               "label": f"{matrix_os} / {matrix_jdk} / {job_name_human}",
               "agents": {
@@ -163,4 +162,4 @@ if __name__ == "__main__":
 
             structure["steps"].append(step)
 
-    print(yaml.dump(structure, Dumper=yaml.Dumper, sort_keys=False))
+    print(yaml.dump(data=structure, Dumper=yaml.Dumper, sort_keys=False))
