1e65f53d68444dae9d8f7dc2e3a330e5f0eed7cc
Mashhur <99575341+mashhurs@users.noreply.github.com>
2023-11-27 09:31:25 -0800

    Use proper BK agent and simplify some operations in Snyk report pipeline. (#15610)
    * Use Java installed BK agent and remove unnecessary git clone operation since repo is already cloned.
    
    * Switch back to normal VM since Logstash BK agent doesn't support docker operations.

--- .buildkite/scripts/snyk/report.sh
+++ .buildkite/scripts/snyk/report.sh
@@ -3,16 +3,16 @@
 set -e
 
 TARGET_BRANCHES=("main")
-cd .buildkite/scripts
 
 install_java() {
-  # TODO: let's think about regularly creating a custom image for Logstash which may align on version.yml definitions
+  # TODO: let's think about using BK agent which has Java installed
+  #   Current caveat is Logstash BK agent doesn't support docker operatioins in it
   sudo apt update && sudo apt install -y openjdk-17-jdk && sudo apt install -y openjdk-17-jre
 }
 
 # Resolves the branches we are going to track
 resolve_latest_branches() {
-  source snyk/resolve_stack_version.sh
+  source .buildkite/scripts/snyk/resolve_stack_version.sh
   for SNAPSHOT_VERSION in "${SNAPSHOT_VERSIONS[@]}"
   do
     IFS='.'
@@ -22,23 +22,15 @@ resolve_latest_branches() {
   done
 }
 
-# Clones the Logstash repo
-clone_logstash_repo() {
-  echo "Cloning logstash repo..."
-  git clone https://github.com/elastic/logstash.git
-}
-
 # Build Logstash specific branch to generate Gemlock file where Snyk scans
 build_logstash() {
-  cd logstash
   git reset --hard HEAD # reset if any generated files appeared
   git checkout "$1"
-  ./gradlew clean bootstrap assemble installDefaultGems && cd ..
+  ./gradlew clean bootstrap assemble installDefaultGems
 }
 
 # Downloads snyk distribution
 download_auth_snyk() {
-  cd logstash
   echo "Downloading snyk..."
   curl https://static.snyk.io/cli/latest/snyk-linux -o snyk
   chmod +x ./snyk
@@ -46,13 +38,11 @@ download_auth_snyk() {
   vault_path=secret/ci/elastic-logstash/snyk-creds
   SNYK_TOKEN=$(vault read -field=token "${vault_path}")
   ./snyk auth "$SNYK_TOKEN"
-  cd ..
 }
 
 # Reports vulnerabilities to the Snyk
 report() {
   echo "Reporting to Snyk..."
-  cd logstash
   REMOTE_REPO_URL=$1
   if [ "$REMOTE_REPO_URL" != "main" ]; then
     MAJOR_VERSION=$(echo "$REMOTE_REPO_URL"| cut -d'.' -f 1)
@@ -63,21 +53,27 @@ report() {
   # adding git commit hash to Snyk tag to improve visibility
   GIT_HEAD=$(git rev-parse --short HEAD 2> /dev/null)
   ./snyk monitor --all-projects --org=logstash --remote-repo-url="$REMOTE_REPO_URL" --target-reference="$REMOTE_REPO_URL" --detection-depth=6 --exclude=qa,tools,devtools,requirements.txt --project-tags=branch="$TARGET_BRANCH",git_head="$GIT_HEAD" && true
-  cd ..
 }
 
 install_java
 resolve_latest_branches
-clone_logstash_repo
 download_auth_snyk
 
 # clone Logstash repo, build and report
 for TARGET_BRANCH in "${TARGET_BRANCHES[@]}"
 do
-  echo "Using $TARGET_BRANCH branch."
-  build_logstash "$TARGET_BRANCH"
-  report "$TARGET_BRANCH"
+  # check if branch exists
+  if git show-ref --quiet refs/heads/"$TARGET_BRANCH"; then
+    echo "Using $TARGET_BRANCH branch."
+    build_logstash "$TARGET_BRANCH"
+    report "$TARGET_BRANCH"
+  else
+    echo "$TARGET_BRANCH branch doesn't exist."
+  fi
 done
+
+# Scan Logstash docker images and report
+REPOSITORY_BASE_URL="docker.elastic.co/logstash/"
 
 report_docker_image() {
   image=$1
@@ -122,7 +118,6 @@ report_docker_images() {
 }
 
 resolve_version_and_report_docker_images() {
-  cd logstash
   git reset --hard HEAD # reset if any generated files appeared
   git checkout "$1"
 
@@ -130,14 +125,15 @@ resolve_version_and_report_docker_images() {
   versions_file="$PWD/versions.yml"
   version=$(awk '/logstash:/ { print $2 }' "$versions_file")
   report_docker_images "$version"
-  cd ..
 }
-
-REPOSITORY_BASE_URL="docker.elastic.co/logstash/"
 
 # resolve docker artifact and report
 for TARGET_BRANCH in "${TARGET_BRANCHES[@]}"
 do
-  echo "Using $TARGET_BRANCH branch for docker images."
-  resolve_version_and_report_docker_images "$TARGET_BRANCH"
+  if git show-ref --quiet refs/heads/"$TARGET_BRANCH"; then
+    echo "Using $TARGET_BRANCH branch for docker images."
+    resolve_version_and_report_docker_images "$TARGET_BRANCH"
+  else
+    echo "$TARGET_BRANCH branch doesn't exist."
+  fi
 done
