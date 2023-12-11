241c03274c5084851c76baf145f3878bd3c9d39b
Andrea Selva <selva.andre@gmail.com>
2023-12-11 10:11:03 +0100

    Avoid to run integration tests while testing all supported plugin (#15511)
    Update the test_plugins pipeline script to execute only the unit tests.
    Use the vendored JRuby in every Ruby related duty, such as running `bundler` and `gem`.
    Temporarily comments plugins that has needs to be fixed and already fails on  their Travis CI.
    Executes the testing of input tier1 plugins in VM instead of Buildkite agent.

--- ci/test_plugins.sh
+++ ci/test_plugins.sh
@@ -1,7 +1,9 @@
-#!/bin/sh -ie
+#!/usr/bin/env bash
+set -euo pipefail
+
 export JRUBY_OPTS="-J-Xmx1g"
 export GRADLE_OPTS="-Xmx4g -Dorg.gradle.jvmargs=-Xmx4g -Dorg.gradle.daemon=false -Dorg.gradle.logging.level=info -Dfile.encoding=UTF-8"
 
 ./gradlew assemble
 
-bin/ruby ci/test_plugins.rb $@
+vendor/jruby/bin/jruby ci/test_plugins.rb $@
