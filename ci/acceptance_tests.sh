ec15dc0bdc330714274f87f0e6f7016a70443e6a
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-12-18 12:51:18 +0200

    Fix calling script

--- ci/acceptance_tests.sh
+++ ci/acceptance_tests.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
-set -e
-set -x
+set -eo pipefail
+
+PACKAGE_TYPE="all"
+
+function get_package_type {
+  source /etc/os-release
+
+  if [[ $ID == "ubuntu" || $ID == "debian" || $ID_LIKE == "debian" ]]; then
+    PACKAGE_TYPE="deb"
+  elif [[ $ID_LIKE == *"rhel"* || $ID_LIKE == *"fedora"* || $ID_LIKE == *"suse"* ]]; then
+    PACKAGE_TYPE="rpm"
+  else
+    echo "Unsupported Linux distribution [$ID]. Acceptance packaging tests only support deb or rpm based distributions. Exiting."
+    exit 1
+  fi
+}
 
 # Since we are using the system jruby, we need to make sure our jvm process
 # uses at least 1g of memory, If we don't do this we can get OOM issues when
@@ -9,82 +23,28 @@ export JRUBY_OPTS="-J-Xmx1g"
 export GRADLE_OPTS="-Xmx4g -Dorg.gradle.daemon=false -Dorg.gradle.logging.level=info -Dfile.encoding=UTF-8"
 export OSS=true
 
+# On Buildkite, artifacts will be prebuilt at a previous step and provided in $LS_ARTIFACTS_PATH
+export BUILD_ARTIFACTS=${BUILD_ARTIFACTS:-"true"}
+
 if [ -n "$BUILD_JAVA_HOME" ]; then
   GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.java.home=$BUILD_JAVA_HOME"
 fi
 
-
-SELECTED_TEST_SUITE=$1
-
-# The acceptance test in our CI infrastructure doesn't clear the workspace between run
-# this mean the lock of the Gemfile can be sticky from a previous run, before generating any package
-# we will clear them out to make sure we use the latest version of theses files
-# If we don't do this we will run into gem Conflict error.
-[ -f Gemfile ] && rm Gemfile
-[ -f Gemfile.lock ] && rm Gemfile.lock
-
-# When running these tests in a Jenkins matrix, in parallel, once one Vagrant job is done, the Jenkins ProcessTreeKiller will kill any other Vagrant processes with the same
-# BUILD_ID unless you set this magic flag:  https://wiki.jenkins.io/display/JENKINS/ProcessTreeKiller
-export BUILD_ID=dontKillMe
-
 LS_HOME="$PWD"
 QA_DIR="$PWD/qa"
 
-# Always run the halt, even if the test times out or an exit is sent
-cleanup() {
+cd $LS_HOME
 
-  cd $QA_DIR
-  bundle check || bundle install
-  bundle exec rake qa:vm:halt
-}
-trap cleanup EXIT
-
-# Cleanup any stale VMs from old jobs first
-cleanup
-
-if [[ $SELECTED_TEST_SUITE == $"redhat" ]]; then
-  echo "Generating the RPM, make sure you start with a clean environment before generating other packages."
-  cd $LS_HOME
+if [[ ! -z $BUILD_ARTIFACTS ]]; then
+  get_package_type
+  echo "Building Logstash artifacts for [$PACKAGE_TYPE]"
   ./gradlew clean bootstrap
-  rake artifact:rpm
-  echo "Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "Acceptance: Running the tests"
-  bundle exec rake qa:vm:setup["redhat"]
-  bundle exec rake qa:vm:ssh_config
-  bundle exec rake qa:acceptance:redhat
-  bundle exec rake qa:vm:halt["redhat"]
-elif [[ $SELECTED_TEST_SUITE == $"debian" ]]; then
-  echo "Generating the DEB, make sure you start with a clean environment before generating other packages."
-  cd $LS_HOME
-  ./gradlew clean bootstrap
-  rake artifact:deb
-  echo "Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "Acceptance: Running the tests"
-  bundle exec rake qa:vm:setup["debian"]
-  bundle exec rake qa:vm:ssh_config
-  bundle exec rake qa:acceptance:debian
-  bundle exec rake qa:vm:halt["debian"]
-elif [[ $SELECTED_TEST_SUITE == $"all" ]]; then
-  echo "Building Logstash artifacts"
-  cd $LS_HOME
-  ./gradlew clean bootstrap
-  rake artifact:all
-
-  echo "Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "Acceptance: Running the tests"
-  bundle exec rake qa:vm:setup
-  bundle exec rake qa:vm:ssh_config
-  bundle exec rake qa:acceptance:all
-  bundle exec rake qa:vm:halt
+  rake artifact:$PACKAGE_TYPE
 fi
 
+echo "Acceptance: Installing dependencies"
+cd $QA_DIR
+bundle install
 
+echo "Acceptance: Running the tests"
+rake qa:acceptance:all
