807230ef5194198df421c43ec681626de0bbe5d6
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-12-18 23:03:38 +0200

    debian 10 friendly yaml parse with awk ...

--- .buildkite/scripts/common/vm-agent-multi-jdk.sh
+++ .buildkite/scripts/common/vm-agent-multi-jdk.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 
 # the convoluted AWK script below grabs the major version from bundled_jdk:\nrevision in Logstash's `version.yml`
 # we don't use yq (or e.g. the yaml Python module) because both aren't guaranteed to be pre-installed on all VMs.
-_JAVA_MAJOR_VERSION=$(awk '/^bundled_jdk:/ {found_bundled_jdk=1; next} found_bundled_jdk && /^[[:space:]]*revision:/ {gsub(/^[[:space:]]*revision:[[:space:]]*/, ""); split($1, rev, "."); print rev[1]; found_bundled_jdk=0}' versions.yml)
+_JAVA_MAJOR_VERSION=$(awk '/^bundled_jdk:/ {found_bundled_jdk=1; next} found_bundled_jdk && /^[\ \t]*revision:/ {print $0}' versions.yml | awk -F ': ' '{print $2}' | cut -d '.' -f 1,1)
 
 export JAVA_HOME="/opt/buildkite-agent/.java/adoptiumjdk_${_JAVA_MAJOR_VERSION}"
 export PATH="/opt/buildkite-agent/.rbenv/bin:/opt/buildkite-agent/.pyenv/bin:${JAVA_HOME}/bin:$PATH"
