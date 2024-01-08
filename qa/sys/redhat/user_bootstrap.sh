cebe4a7537a2b94368a7c183c9d777ea86c3f264
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2024-01-08 09:40:58 +0200

    Refactor qa/acceptance tests to get away from vagrant (#15696)
    This commit modernizes the qa/acceptance (packaging) test framework by
    moving away from Vagrant and having the tests operate locally.
    
    As we are migrating to Buildkite, the expectation is that those tests
    will run on dedicated vms thus removing the necessity of vagrant.
    
    Relates: https://github.com/elastic/ingest-dev/issues/1722

--- qa/sys/redhat/user_bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=`cat /vagrant/config/platforms.json | grep  latest | cut -d":" -f2 | sed 's/["\|,| ]//g'`
-LOGSTASH_FILENAME="logstash-${VERSION}.rpm"
-wget -q https://download.elastic.co/logstash/logstash/packages/centos/$LOGSTASH_FILENAME
