85bb3d7a9c3c4100d469228d0ec95a9049467153
andsel <selva.andre@gmail.com>
2024-02-15 10:27:35 +0100

    Apply suggestion from https://github.com/jruby/jruby/issues/8061#issuecomment-1933009986

--- lib/bootstrap/rspec.rb
+++ lib/bootstrap/rspec.rb
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+java.util.LinkedHashSet.remove_method(:map) rescue nil
 
 require_relative "environment"
 LogStash::Bundler.setup!({:without => [:build]})
