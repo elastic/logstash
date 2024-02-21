a231375dc53a3ba25bf71ad067991294f502ba63
andsel <selva.andre@gmail.com>
2024-02-21 12:39:02 +0100

    Add shutdown step of DLQ flusher scheduled service

--- logstash-core/src/main/java/org/logstash/common/io/DeadLetterQueueWriter.java
+++ logstash-core/src/main/java/org/logstash/common/io/DeadLetterQueueWriter.java
@@ -134,6 +134,8 @@ public final class DeadLetterQueueWriter implements Closeable {
          * Register the callback action to invoke on every clock tick.
          * */
         void repeatedAction(Runnable action);
+
+        void shutdown();
     }
 
     private static class FixedRateScheduler implements SchedulerService {
@@ -155,11 +157,21 @@ public final class DeadLetterQueueWriter implements Closeable {
         public void repeatedAction(Runnable action) {
             scheduledExecutor.scheduleAtFixedRate(action, 1L, 1L, TimeUnit.SECONDS);
         }
+
+        @Override
+        public void shutdown() {
+            scheduledExecutor.shutdown();
+        }
     }
 
     private static class NoopScheduler implements SchedulerService {
         @Override
         public void repeatedAction(Runnable action) {
+            // Noop
+        }
+
+        @Override
+        public void shutdown() {
             // Noop
         }
     }
@@ -310,6 +322,8 @@ public final class DeadLetterQueueWriter implements Closeable {
             } catch (Exception e) {
                 logger.warn("Unable to release fileLock, ignoring", e);
             }
+
+            flusherService.shutdown();
 
             try {
                 // flushScheduler is null only if it's not explicitly started, which happens only in tests.
