5c5fd09a49c1894e865ebefe2b65683005c4af32
andsel <selva.andre@gmail.com>
2024-05-27 17:24:07 +0200

    Removed strict field from Ruby Setting class, using the one from the wrapped Java Setting class

--- logstash-core/lib/logstash/settings.rb
+++ logstash-core/lib/logstash/settings.rb
@@ -257,9 +257,8 @@ module LogStash
       @validator_proc = validator_proc
       @value = nil
       @value_is_set = false
-      @strict = strict
 
-      validate(default) if @strict
+      validate(default) if strict?
       @default = default
     end
 
@@ -289,7 +288,7 @@ module LogStash
     end
 
     def set(value)
-      validate(value) if @strict
+      validate(value) if strict?
       @wrapped_setting.set(value)
       @value
     end
