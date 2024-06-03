2c5c878976cbcb41cb1ba52ceb7d8256946167b6
Ry Biesemeyer <ry.biesemeyer@elastic.co>
2024-06-03 19:31:27 +0000

    p2p: extract pipeline push to abstract

--- logstash-core/src/main/java/org/logstash/plugins/pipeline/PipelineBusV1.java
+++ logstash-core/src/main/java/org/logstash/plugins/pipeline/PipelineBusV1.java
@@ -22,23 +22,20 @@ package org.logstash.plugins.pipeline;
 import com.google.common.annotations.VisibleForTesting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.logstash.RubyUtil;
 import org.logstash.ext.JrubyEventExtLibrary;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * This class is the communication bus for the `pipeline` inputs and outputs to talk to each other.
  * <p>
  * This class is threadsafe.
  */
-class PipelineBusV1 implements PipelineBus {
+class PipelineBusV1 extends AbstractPipelineBus implements PipelineBus {
 
     final ConcurrentHashMap<String, AddressState> addressStates = new ConcurrentHashMap<>();
     final ConcurrentHashMap<PipelineOutput, ConcurrentHashMap<String, AddressState>> outputsToAddressStates = new ConcurrentHashMap<>();
@@ -58,42 +55,7 @@ class PipelineBusV1 implements PipelineBus {
             JrubyEventExtLibrary.RubyEvent[] orderedEvents = events.toArray(new JrubyEventExtLibrary.RubyEvent[0]);
 
             addressesToInputs.forEach((address, addressState) -> {
-                boolean sendWasSuccess = false;
-                ReceiveResponse lastResponse = null;
-                boolean partialProcessing;
-                int lastFailedPosition = 0;
-                do {
-                    Stream<JrubyEventExtLibrary.RubyEvent> clones = Arrays.stream(orderedEvents)
-                            .skip(lastFailedPosition)
-                            .map(e -> e.rubyClone(RubyUtil.RUBY));
-
-                    PipelineInput input = addressState.getInput(); // Save on calls to getInput since it's volatile
-                    if (input != null) {
-                        lastResponse = input.internalReceive(clones);
-                        sendWasSuccess = lastResponse.wasSuccess();
-                    }
-                    partialProcessing = ensureDelivery && !sendWasSuccess;
-                    if (partialProcessing) {
-                        if (lastResponse != null && lastResponse.getStatus() == PipelineInput.ReceiveStatus.FAIL) {
-                            // when last call to internalReceive generated a fail for the subset of the orderedEvents
-                            // it is handling, restart from the cumulative last-failed position of the batch so that
-                            // the next attempt will operate on a subset that excludes those successfully received.
-                            lastFailedPosition += lastResponse.getSequencePosition();
-                            logger.warn("Attempted to send events to '{}' but that address reached error condition with {} events remaining. " +
-                                    "Will Retry. Root cause {}", address, orderedEvents.length - lastFailedPosition, lastResponse.getCauseMessage());
-                        } else {
-                            logger.warn("Attempted to send event to '{}' but that address was unavailable. " +
-                                    "Maybe the destination pipeline is down or stopping? Will Retry.", address);
-                        }
-
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            logger.error("Sleep unexpectedly interrupted in bus retry loop", e);
-                        }
-                    }
-                } while (partialProcessing);
+                doSendEvents(orderedEvents, addressState.getReadOnlyView(), ensureDelivery);
             });
         }
     }
