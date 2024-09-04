ac034a14ee422148483d42a51b68f07d1a38494c
Andrea Selva <selva.andre@gmail.com>
2024-09-04 11:10:29 +0200

    Generate Dataset code with meaningful fields names (#16386)
    This PR is intended to help Logstash developers or users that want to better understand the code that's autogenerated to model a pipeline, assigning more meaningful names to the Datasets subclasses' fields.
    
    Updates `FieldDefinition` to receive the name of the field from construction methods, so that it can be used during the code generation phase, instead of the existing incremental `field%n`.
    Updates `ClassFields` to propagate the explicit field name down to the `FieldDefinitions`.
    Update the `DatasetCompiler` that add fields to `ClassFields` to assign a proper name to generated Dataset's fields.

--- logstash-core/src/main/java/org/logstash/config/ir/compiler/DatasetCompiler.java
+++ logstash-core/src/main/java/org/logstash/config/ir/compiler/DatasetCompiler.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.jruby.RubyArray;
 import org.jruby.RubyHash;
@@ -58,8 +59,8 @@ public final class DatasetCompiler {
         final EventCondition condition)
     {
         final ClassFields fields = new ClassFields();
-        final ValueSyntaxElement ifData = fields.add(new ArrayList<>());
-        final ValueSyntaxElement elseData = fields.add(new ArrayList<>());
+        final ValueSyntaxElement ifData = fields.add("ifData", new ArrayList<>());
+        final ValueSyntaxElement elseData = fields.add("elseData", new ArrayList<>());
         final ValueSyntaxElement right = fields.add(DatasetCompiler.Complement.class);
         final VariableDefinition event =
             new VariableDefinition(JrubyEventExtLibrary.RubyEvent.class, "event");
@@ -75,7 +76,7 @@ public final class DatasetCompiler {
                 )
             )
         );
-        final ValueSyntaxElement conditionField = fields.add(condition);
+        final ValueSyntaxElement conditionField = fields.add("condition", condition);
         final DatasetCompiler.ComputeAndClear compute;
         if (parents.isEmpty()) {
             compute = withOutputBuffering(
@@ -83,9 +84,8 @@ public final class DatasetCompiler {
                 Closure.wrap(clear(elseData)), ifData, fields
             );
         } else {
-            final Collection<ValueSyntaxElement> parentFields =
-                parents.stream().map(fields::add).collect(Collectors.toList());
-            final ValueSyntaxElement inputBuffer = fields.add(new ArrayList<>());
+            final Collection<ValueSyntaxElement> parentFields = createParentStatementsFields(parents, fields);
+            final ValueSyntaxElement inputBuffer = fields.add("inputBuffer", new ArrayList<>());
             compute = withOutputBuffering(
                 withInputBuffering(
                     conditionalLoop(event, inputBuffer, conditionField, ifData, elseData),
@@ -111,26 +111,38 @@ public final class DatasetCompiler {
         final AbstractFilterDelegatorExt plugin)
     {
         final ClassFields fields = new ClassFields();
-        final ValueSyntaxElement outputBuffer = fields.add(new ArrayList<>());
+        final ValueSyntaxElement outputBuffer = fields.add("outputBuffer", new ArrayList<>());
         final Closure clear = Closure.wrap();
         final Closure compute;
         if (parents.isEmpty()) {
             compute = filterBody(outputBuffer, BATCH_ARG, fields, plugin);
         } else {
-            final Collection<ValueSyntaxElement> parentFields = parents
-                .stream()
-                .map(fields::add)
-                .collect(Collectors.toList()
-            );
+            final Collection<ValueSyntaxElement> parentFields = createParentStatementsFields(parents, fields);
+
             @SuppressWarnings("rawtypes") final RubyArray inputBuffer = RubyUtil.RUBY.newArray();
             clear.add(clearSyntax(parentFields));
-            final ValueSyntaxElement inputBufferField = fields.add(inputBuffer);
+            final ValueSyntaxElement inputBufferField = fields.add("inputBuffer", inputBuffer);
             compute = withInputBuffering(
                 filterBody(outputBuffer, inputBufferField, fields, plugin),
                 parentFields, inputBufferField
             );
         }
         return prepare(withOutputBuffering(compute, clear, outputBuffer, fields));
+    }
+
+    private static Collection<ValueSyntaxElement> createParentStatementsFields(Collection<Dataset> parents, ClassFields fields) {
+        if (parents.size() == 1) {
+            return List.of(fields.add("parentStatement", parents.iterator().next()));
+        }
+
+        final Collection<ValueSyntaxElement> parentFields = new ArrayList<>();
+        int i = 0;
+        for (Dataset parent : parents) {
+            ValueSyntaxElement add = fields.add("parentStatement" + i, parent);
+            parentFields.add(add);
+            i++;
+        }
+        return parentFields;
     }
 
     /**
@@ -155,10 +167,7 @@ public final class DatasetCompiler {
         }
 
         final ClassFields fields = new ClassFields();
-        final Collection<ValueSyntaxElement> parentFields = parents
-            .stream()
-            .map(fields::add)
-            .collect(Collectors.toList());
+        final Collection<ValueSyntaxElement> parentFields = createParentStatementsFields(parents, fields);
         @SuppressWarnings("rawtypes") final RubyArray inputBuffer = RubyUtil.RUBY.newArray();
         final ValueSyntaxElement inputBufferField = fields.add(inputBuffer);
         final ValueSyntaxElement outputBufferField = fields.add(new ArrayList<>());
@@ -199,10 +208,7 @@ public final class DatasetCompiler {
         }
 
         final ClassFields fields = new ClassFields();
-        final Collection<ValueSyntaxElement> parentFields = parents
-            .stream()
-            .map(fields::add)
-            .collect(Collectors.toList());
+        final Collection<ValueSyntaxElement> parentFields = createParentStatementsFields(parents, fields);
         final Closure compute =  Closure.wrap(parentFields
                 .stream()
                 .map(DatasetCompiler::computeDataset)
@@ -236,7 +242,7 @@ public final class DatasetCompiler {
         final ClassFields fields = new ClassFields();
         final Closure clearSyntax;
         final Closure computeSyntax;
-        final ValueSyntaxElement outputField = fields.add(output);
+        final ValueSyntaxElement outputField = fields.add("outputDelegator", output);
         if (parents.isEmpty()) {
             clearSyntax = Closure.EMPTY;
             computeSyntax = Closure.wrap(
@@ -244,8 +250,7 @@ public final class DatasetCompiler {
                 invokeOutput(outputField, BATCH_ARG),
                 unsetPluginIdForLog4j());
         } else {
-            final Collection<ValueSyntaxElement> parentFields =
-                parents.stream().map(fields::add).collect(Collectors.toList());
+            final Collection<ValueSyntaxElement> parentFields = createParentStatementsFields(parents, fields);
             @SuppressWarnings("rawtypes")
             final RubyArray buffer = RubyUtil.RUBY.newArray();
             final Closure inlineClear;
@@ -256,7 +261,7 @@ public final class DatasetCompiler {
                 inlineClear = Closure.EMPTY;
                 clearSyntax = clearSyntax(parentFields);
             }
-            final ValueSyntaxElement inputBuffer = fields.add(buffer);
+            final ValueSyntaxElement inputBuffer = fields.add("inputBuffer", buffer);
             computeSyntax = withInputBuffering(
                 Closure.wrap(
                     setPluginIdForLog4j(outputField),
@@ -283,7 +288,7 @@ public final class DatasetCompiler {
         final ClassFields fields,
         final AbstractFilterDelegatorExt plugin)
     {
-        final ValueSyntaxElement filterField = fields.add(plugin);
+        final ValueSyntaxElement filterField = fields.add("plugin", plugin);
         final Closure body = Closure.wrap(
             setPluginIdForLog4j(filterField),
             buffer(outputBuffer, filterField.call("multiFilter", inputBuffer))
@@ -378,14 +383,14 @@ public final class DatasetCompiler {
         final boolean shutdownOnly) {
         final MethodLevelSyntaxElement condition;
         final ValueSyntaxElement flushArgs;
-        final ValueSyntaxElement flushFinal = fields.add(flushOpts(true));
+        final ValueSyntaxElement flushFinal = fields.add("shutdownFlushOptions", flushOpts(true));
         if (shutdownOnly) {
             condition = SyntaxFactory.and(FLUSH_ARG, SHUTDOWN_ARG);
             flushArgs = flushFinal;
         } else {
             condition = FLUSH_ARG;
             flushArgs = SyntaxFactory.ternary(
-                SHUTDOWN_ARG, flushFinal, fields.add(flushOpts(false))
+                SHUTDOWN_ARG, flushFinal, fields.add("flushOptions", flushOpts(false))
             );
         }
         return SyntaxFactory.ifCondition(
