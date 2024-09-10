5195332bc6a758198cae70fea7d88dfddf0fa15a
Mashhur <99575341+mashhurs@users.noreply.github.com>
2024-09-10 11:14:14 -0700

    Logstash Health Report Tests Buildkite pipeline setup. (#16416)

---
+++ .buildkite/scripts/health-report-tests/main.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# TODO:
+#  if branch is specified with X.Y, pull branches from ACTIVE_BRANCHES_URL="https://raw.githubusercontent.com/elastic/logstash/main/ci/branches.json", parse and use
+#  build Logstash from specificed (ex: 8.x -> translates to 8.latest, 8.16) branch, defaults to main
+#  install requirements of the python package and run main.py
+
+
