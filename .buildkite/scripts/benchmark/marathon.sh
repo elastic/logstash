3e98cb16253b6135ed2cbd894ead3bed439f0185
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2024-09-17 22:29:02 +0100

    [CI] fix benchmark marathon (#16447)
    - split main.sh to core.sh and main.sh
    - rename all.sh to marathon.sh
    - fix vault expiry issue for long running task
    - fix unable to call main function
    - update save object runtime field `release` to return true when `version` contains "SNAPSHOT"

---
+++ .buildkite/scripts/benchmark/marathon.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# *******************************************************
+# Run benchmark for versions that have flow metrics
+# When the hardware changes, run the marathon task to establish a new baseline.
+# Usage:
+#   nohup bash -x all.sh > log.log 2>&1 &
+# Accept env vars:
+#   STACK_VERSIONS=8.15.0,8.15.1,8.16.0-SNAPSHOT # versions to test. It is comma separator string
+# *******************************************************
+
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_PATH/core.sh"
+
+parse_stack_versions() {
+  IFS=','
+  STACK_VERSIONS="${STACK_VERSIONS:-8.6.0,8.7.0,8.8.0,8.9.0,8.10.0,8.11.0,8.12.0,8.13.0,8.14.0,8.15.0}"
+  read -ra STACK_VERSIONS <<< "$STACK_VERSIONS"
+}
+
+main() {
+  parse_stack_versions
+  parse_args "$@"
+  get_secret
+  generate_logs
+  check_logs
+
+  for V in "${STACK_VERSIONS[@]}" ; do
+    LS_VERSION="$V"
+    pull_images
+    create_directory
+    if [[ $QTYPE == "all" ]]; then
+      queue
+    else
+      worker
+    fi
+  done
+}
+
+main "$@"
