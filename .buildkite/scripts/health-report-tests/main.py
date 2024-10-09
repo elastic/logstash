1bdab40a66730003cf6aeb184a7e73c04a642cb9
Mashhur <mashhur.sattorov@elastic.co>
2024-10-09 16:29:38 -0700

    Add CI step for 8.x branch

--- .buildkite/scripts/health-report-tests/main.py
+++ .buildkite/scripts/health-report-tests/main.py
@@ -1,10 +1,14 @@
 """
  Main entry point of the LS health report API integration test suites
 """
+import os
 
 
 def main():
     print(f"Hello World!")
+    logstash_branch = os.environ.get("LS_BRANCH")
+    if logstash_branch is None:
+        print(f"LS_BRANCH is not specified, using main branch.")
 
 
 if __name__ == "__main__":
