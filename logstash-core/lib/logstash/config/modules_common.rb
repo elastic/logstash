4201628f9e67524a194fc75f4ceee01123ba3682
Andrea Selva <selva.andre@gmail.com>
2024-11-06 12:30:45 +0100

    Update depreacation warning to provide the version the ArcSight module is removed. (#16648)

--- logstash-core/lib/logstash/config/modules_common.rb
+++ logstash-core/lib/logstash/config/modules_common.rb
@@ -68,12 +68,7 @@ module LogStash module Config
       end
 
       specified_and_available_names
-        .select { |mn| mn != "arcsight" }
         .each { |mn| deprecation_logger.deprecated("The #{mn} module has been deprecated and will be removed in version 9.") }
-
-      if specified_and_available_names.include?("arcsight")
-        deprecation_logger.deprecated("The ArcSight module has been deprecated and will be removed in future version.")
-      end
 
       specified_and_available_names.each do |module_name|
         connect_fail_args = {}
