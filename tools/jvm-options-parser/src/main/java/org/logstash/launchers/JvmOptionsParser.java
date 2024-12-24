45b6e2d0454448db3fcae9008a55795cb7becee4
Kaise Cheng <kaise.cheng@elastic.co>
2024-12-24 18:48:58 +0000

    remove wildcard import

--- tools/jvm-options-parser/src/main/java/org/logstash/launchers/JvmOptionsParser.java
+++ tools/jvm-options-parser/src/main/java/org/logstash/launchers/JvmOptionsParser.java
@@ -19,12 +19,27 @@
 
 package org.logstash.launchers;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
