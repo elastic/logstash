f0be60ab942de196382b4287da7b9234a4804dcc
donoghuc <cas.donoghue@gmail.com>
2025-02-07 12:34:05 -0800

    Move entrypoint for running integration tests inside docker

--- ci/run-fips-integration-tests.sh
+++ ci/run-fips-integration-tests.sh
@@ -4,9 +4,6 @@
 # half_number: 0 for first half, 1 for second half
 
 half_number=$1
-docker build -t test-runner-image -f x-pack/distributions/internal/observabilitySRE/docker/Dockerfile .
-docker run test-runner-image /bin/bash -c "
-    source ci/get-test-half.sh
-    specs=$(get_test_half $half_number)
-    ./gradlew --info --stacktrace -PrunTestsInFIPSMode=true runIntegrationTests -PrubyIntegrationSpecs=\"\$specs\"
-"
+source ci/get-test-half.sh
+specs=$(get_test_half "$half_number")
+./gradlew --info --stacktrace -PrunTestsInFIPSMode=true runIntegrationTests -PrubyIntegrationSpecs="$specs"
