78110b0a74de3ffafafb596bec83663b7af9f8c2
Cas Donoghue <cas.donoghue@gmail.com>
2025-02-07 13:57:33 -0800

    Split fips integration tests into two steps (#17038)
    * Split fips integration tests into two steps
    
    The integration tests suite takes about 40 minutes. This is far too slow for
    reasonable feedback on a PR. This commit follows the pattern for the non-fips
    integration tests whereby the tests are split into two sections that can run in
    parallel across two steps. This should halve the feedback time.
    
    The logic for getting a list of specs files to run has been extracted to a
    shared shell script for use here and in the integration tests shell script.
    
    * Use shared function for splitting integration tests
    
    The logic for getting a list of specs to run has been extracted so that it can
    be shared across fips and non fips integration test modes. This commit updates
    the non fips integration tests to use the shared function.
    
    * fix typo in helper name (kebab case, not snake)
    
    * Escape $ so buildkite upload does not try to interpolate
    
    * Wrap integration tests in shell script to avoid BK interpolation
    
    * Move entrypoint for running integration tests inside docker

---
+++ ci/run-fips-integration-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# get_test_half returns either the first or second half of integration tests
+# Usage: get_test_half <half_number>
+# half_number: 0 for first half, 1 for second half
+
+half_number=$1
+source ci/get-test-half.sh
+specs=$(get_test_half "$half_number")
+./gradlew --info --stacktrace -PrunTestsInFIPSMode=true runIntegrationTests -PrubyIntegrationSpecs="$specs"
