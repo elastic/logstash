974151771394720383fcca72c76d0e18617befad
andsel <selva.andre@gmail.com>
2025-04-10 17:31:38 +0200

    - inverted BufferedTokenizerExt§IterableAdapterWithEmptyCheck#isEmpty - specs improved (yaauie@b524a67) with a custom matcher that validates both `empty?` (which maps to isEmpty) and `entries` (which is provided by the jruby shim extending java-Iterator with RubyEnumerable)

--- logstash-core/src/main/java/org/logstash/common/BufferedTokenizerExt.java
+++ logstash-core/src/main/java/org/logstash/common/BufferedTokenizerExt.java
@@ -107,7 +107,7 @@ public class BufferedTokenizerExt extends RubyObject {
         }
 
         public boolean isEmpty() {
-            return origIterator.hasNext();
+            return !origIterator.hasNext();
         }
     }
 
