10e41a841a0f7b4200f82f757adc4e3c4ac12c16
Cas Donoghue <cas.donoghue@gmail.com>
2025-06-06 16:43:24 -0700

    Add a step to exhaustive tests for observabilitySRE accetpance testing (#17623)
    * Add a step to exhaustive tests for observabilitySRE accetpance testing
    
    This commit shows the proposed pattern for adding acceptance testing for the
    observability SRE image. This will run when exhaustive tests run. A new gradle
    task will hook in to rspec similar to how it is done for the smoke tests. The
    main difference is that instead of building a container, the latest is pulled
    from the container registry and run on a fips configured host VM.
    
    * WIP: Idea for how to handle multipe container configs for acceptance tests
    
    This commit shows the rough structure for how I am planning on handling docker
    compose networks for acceptance tests. The main idea is to use interpolation in
    the docker compose file to point to different configuration files for
    filebeat/logstash/elasticsearch. This is mainly due to the nature of these tests
    showing behavior when the system is and is not configured properly for FIPS. The
    breakdown in responsibility is:
    
    1. Gradle handles cert generation (similar to smoke test, this avoids checking
    in PKI)
    2. Rspec handles stopping/starting docker compose and managing environment vars
    for intperolation in docker compose manifests (different from smoke tests where
    a single static docker compose is started in gradle)
    3. Rspec handles deciding when containers are ready and querying state about
    data flowing through the system
    4. Gradle cleans up certs
    
    THis is just a rough sketch, there are still bugs to be worked out but before i
    get too far in to it I want to get the idea out there.
    
    * Add tests describing behavior of LS -> ES with non-fips config
    
    This commit adds a test to show that data will not flow from LS to ES
    when weak non fips config is used.
    
    * Use latest ES image
    
    This will be handled separately in a separate PR, but taking this
    commit for now on this branch.
    
    * Remove custom entrypoint from new container
    
    The latest ES images do not require this workaround.
    
    * Take up code review suggestions
    
    1. Remove rogue character from test file causing interpreter failure
    2. Split out helpers for docker compose orchestration
    3. Only send a single message instead of infinite through to ES
    
    * Add full prefix name for new image
    
    * Test filebeat -> LS -> ES using fips config
    
    As described in https://github.com/elastic/ingest-dev/issues/5471 this commit
    adds a test for filebeat sending data through logstash to elasticsearch using
    fips config.
    
    * Test LS wont accept input from non fips configured filebeat
    
    This test ensures logstash will not accept data from filebeat when using weak
    tls configuration.
    
    See https://github.com/elastic/ingest-dev/issues/5472
    
    * Fix a funny typo.
    
    Crytpo is actually kind of a funny.
    
    * Ensure we are using the purpose build ES image in testing
    
    Similar to https://github.com/elastic/logstash/pull/17627
    
    * Ensure JAVA_HOME is set etc
    
    Use the same buildkite agent script for setting up a vm based runner as other pipes

--- .buildkite/scripts/exhaustive-tests/generate-steps.py
+++ .buildkite/scripts/exhaustive-tests/generate-steps.py
@@ -168,6 +168,26 @@ ci/docker_acceptance_tests.sh {flavor}"""),
 
     return steps
 
+def fips_test_runner_step() -> dict[str, typing.Any]:
+    step = {
+        "label": "Observability SRE Acceptance Tests",
+        "key": "observabilitySRE-acceptance-tests",
+        "agents": {
+            "provider": "aws",
+            "instanceType": "m6i.xlarge",
+            "diskSizeGb": 60,
+            "instanceMaxAge": 1440,
+            "imagePrefix": "platform-ingest-logstash-ubuntu-2204-fips"
+        },
+        "retry": {"automatic": [{"limit": 1}]},
+        "command": LiteralScalarString("""#!/usr/bin/env bash
+set -euo pipefail
+source .buildkite/scripts/common/vm-agent.sh
+./gradlew observabilitySREacceptanceTests --stacktrace
+"""),
+    }
+    return step
+
 if __name__ == "__main__":
     LINUX_OS_ENV_VAR_OVERRIDE = os.getenv("LINUX_OS")
     WINDOWS_OS_ENV_VAR_OVERRIDE = os.getenv("WINDOWS_OS")
@@ -213,6 +233,13 @@ if __name__ == "__main__":
             "key": "acceptance-docker",
             "depends_on": ["testing-phase"],
             "steps": acceptance_docker_steps(),
+    })
+
+    structure["steps"].append({
+        "group": "Observability SRE Acceptance Tests",
+        "key": "acceptance-observability-sre",
+        "depends_on": ["testing-phase"],
+        "steps": [fips_test_runner_step()],
     })
 
     print('# yaml-language-server: $schema=https://raw.githubusercontent.com/buildkite/pipeline-schema/main/schema.json')
