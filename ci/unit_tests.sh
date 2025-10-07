953211a9e882cc2d94838971cca279f5654493a7
Victor Martinez <victormartinezrubio@gmail.com>
2025-10-07 08:59:44 +0200

    ci: remove sonarqube (#18273)
    this service will be decommissioned shortly
    
    (cherry picked from commit cbef0782e5210939c990d0b1cdde277cb3b8b066)

--- ci/unit_tests.sh
+++ ci/unit_tests.sh
@@ -10,8 +10,6 @@ export GRADLE_OPTS="-Xmx4g -Dorg.gradle.jvmargs=-Xmx4g -Dorg.gradle.daemon=false
 export SPEC_OPTS="--order rand --format documentation"
 export CI=true
 export TEST_DEBUG=true
-# don't rely on bash booleans for truth checks, since some CI platforms don't have a way to specify env vars as boolean
-export ENABLE_SONARQUBE=${ENABLE_SONARQUBE:-"true"}
 
 if [ -n "$BUILD_JAVA_HOME" ]; then
   GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.java.home=$BUILD_JAVA_HOME"
@@ -19,15 +17,9 @@ fi
 
 SELECTED_TEST_SUITE=$1
 
-SONAR_ARGS=()
-if [[ $(echo $ENABLE_SONARQUBE | tr '[:lower:]' '[:upper:]') == "TRUE" ]]; then
-  SONAR_ARGS=("jacocoTestReport")
-  export COVERAGE=true
-fi
-
 if [[ $SELECTED_TEST_SUITE == $"java" ]]; then
   echo "Running Java Tests"
-  ./gradlew javaTests "${SONAR_ARGS[@]}" --console=plain --warning-mode all
+  ./gradlew javaTests --console=plain --warning-mode all
 elif [[ $SELECTED_TEST_SUITE == $"ruby" ]]; then
   echo "Running Ruby unit tests"
   ./gradlew rubyTests --console=plain --warning-mode all
