b3c9cd2a37b3c9beef3f5314ebb2164ae5b37bfe
donoghuc <cas.donoghue@gmail.com>
2025-10-27 15:31:24 -0700

    Use logstash-versions.yml for observabilitySRE stack versions
    Previously the version of logstash checked in to the logstash repo was used to
    determine stack version for smoke and acceptance testing of the observabilitySRE
    artifact. Given the ES/Filebeat containers are subject to a unified release the
    delay between a logstash version bump and those artifacts being available was
    causing failures when the smoke/acceptance tests were run. In plugin testing the
    way we deal with this is to ensure the logstash-versions.yml file is updated
    when stack components are ready. This commit introduces the use of that pattern
    to the observabilitySRE artifact testing. The important step will be to set the
    STACK_VERSION to correspond to the desired stack version. For example the 9.2
    logstash branch should look up `9.current`.

--- ci/observabilitySREacceptance_tests.sh
+++ ci/observabilitySREacceptance_tests.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-QUALIFIED_VERSION="$(.buildkite/scripts/common/qualified-version.sh)"
-export OBSERVABILITY_SRE_IMAGE_VERSION="${OBSERVABILITY_SRE_IMAGE_VERSION:-$QUALIFIED_VERSION}"
-export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$QUALIFIED_VERSION}"
-export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$QUALIFIED_VERSION}"
+# Look up corresponding LOSTASH_STREAM to test against. MAKE SURE TO UDPATE this per branch for example the 9.2 branch in logstash would
+# correspond to the '9.current' key in the logstash-versions.yml file
+LOGSTASH_STREAM=main
+STACK_VERSION=$(ruby -ryaml -ropen-uri -e "puts YAML.load(URI.open('https://raw.githubusercontent.com/logstash-plugins/.ci/1.x/logstash-versions.yml'))['snapshots']['${LOGSTASH_STREAM}']")
+export OBSERVABILITY_SRE_IMAGE_VERSION="${OBSERVABILITY_SRE_IMAGE_VERSION:-$STACK_VERSION}"
+export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
+export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$STACK_VERSION}"
 
 ./gradlew observabilitySREacceptanceTests --stacktrace
--- ci/observabilitySREsmoke_tests.sh
+++ ci/observabilitySREsmoke_tests.sh
@@ -3,8 +3,12 @@
 set -e
 
 QUALIFIED_VERSION="$(.buildkite/scripts/common/qualified-version.sh)"
-export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$QUALIFIED_VERSION}"
-export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$QUALIFIED_VERSION}"
+# Look up corresponding LOSTASH_STREAM to test against. MAKE SURE TO UDPATE this per branch for example the 9.2 branch in logstash would
+# correspond to the '9.current' key in the logstash-versions.yml file
+LOGSTASH_STREAM=main
+STACK_VERSION=$(ruby -ryaml -ropen-uri -e "puts YAML.load(URI.open('https://raw.githubusercontent.com/logstash-plugins/.ci/1.x/logstash-versions.yml'))['snapshots']['${LOGSTASH_STREAM}']")
+export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
+export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$STACK_VERSION}"
 
 ./gradlew --stacktrace artifactDockerObservabilitySRE -PfedrampHighMode=true
 
