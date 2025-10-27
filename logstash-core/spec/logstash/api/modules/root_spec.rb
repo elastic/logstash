255bab7c13d68c4c39b2387c0b8dc203c54e391c
Emily Stolfo <emily.stolfo@elastic.co>
2025-10-27 11:24:17 +0100

    Support wait_for_status query param on root endpoint

--- logstash-core/spec/logstash/api/modules/root_spec.rb
+++ logstash-core/spec/logstash/api/modules/root_spec.rb
@@ -29,4 +29,113 @@ describe LogStash::Api::Modules::Root do
   end
 
   include_examples "not found"
+
+  describe 'wait_for_status query param' do
+
+    let(:margin_of_error) { 0.1 }
+
+    context 'no timeout is provided' do
+
+      let(:return_time) { 0.1 }
+
+      it 'returns immediately' do
+        start_time = Time.now
+        get "/?wait_for_status=red"
+        end_time = Time.now
+        expect(end_time - start_time).to be_within(margin_of_error).of(return_time)
+      end
+    end
+
+    context "timeout is provided" do
+
+      let(:timeout) { 1 }
+
+      context "the status doesn't change" do
+
+        let(:return_time) { timeout + 0.1 }
+
+        it 'checks the status until the timeout is reached' do
+          start_time = Time.now
+          get "/?wait_for_status=red&timeout=#{timeout}"
+          end_time = Time.now
+          expect(end_time - start_time).to be_within(margin_of_error).of(return_time)
+        end
+      end
+
+      context 'the status changes within the timeout' do
+
+        let(:timeout) { 2 }
+
+        before do
+          allow(@agent.health_observer.status).to receive(:to_s).and_return('GREEN', 'YELLOW')
+        end
+
+        it 'returns when the target status is reached' do
+          start_time = Time.now
+          get "/?wait_for_status=yellow&timeout=#{timeout}"
+          end_time = Time.now
+          expect(end_time - start_time).to be < timeout
+        end
+      end
+    end
+
+    described_class::HEALTH_STATUS.each do |status|
+
+      context "#{status} is provided" do
+
+        let(:timeout) do
+          # Two statuses are checked before the target is reached. The first wait time is 1 second,
+          # the second wait time is 2 seconds. So it takes at least 3 seconds to reach target status.
+          3.1
+        end
+
+        let(:return_statues) do
+          # Make the target status last in the returned values
+          statuses = described_class::HEALTH_STATUS.dup
+          statuses.delete(status)
+          statuses << status
+        end
+
+        it 'checks for the status until it changes' do
+          allow(@agent.health_observer.status).to receive(:to_s).and_return(*return_statues)
+          start_time = Time.now
+          get "/?wait_for_status=#{status}&timeout=#{timeout}"
+          end_time = Time.now
+          expect(end_time - start_time).to be < timeout
+        end
+      end
+    end
+
+    context 'status string is formatted differently' do
+
+      let(:timeout) { 2 }
+      let(:return_time) do
+        # We wait 1 second to check the status a second time. The target status
+        # is reached on the second check.
+        1.1
+      end
+
+      before { allow(@agent.health_observer.status).to receive(:to_s).and_return('GREEN', 'YELLOW') }
+
+      it 'normalizes the format and checks the status' do
+        start_time = Time.now
+        get "/?wait_for_status=yElLoW&timeout=#{timeout}"
+        end_time = Time.now
+        expect(end_time - start_time).to be_within(margin_of_error).of(return_time)
+      end
+    end
+
+    context "invalid status is provided" do
+
+      let(:timeout) { 2 }
+      let(:return_time) { 0.1 }
+
+      it 'returns immediately' do
+        start_time = Time.now
+        get "/?wait_for_status=invalid&timeout=#{timeout}"
+        end_time = Time.now
+        expect(end_time - start_time).to be_within(margin_of_error).of(return_time)
+      end
+    end
+  end
 end
