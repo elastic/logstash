fda69b952388a081cc2d4f09e1a3edddf93795cd
donoghuc <cas.donoghue@gmail.com>
2025-10-29 16:22:22 -0700

    track stream in versions.yml

--- ci/observabilitySREsmoke_tests.sh
+++ ci/observabilitySREsmoke_tests.sh
@@ -3,9 +3,9 @@
 set -e
 
 QUALIFIED_VERSION="$(.buildkite/scripts/common/qualified-version.sh)"
-# Look up corresponding LOSTASH_STREAM to test against. MAKE SURE TO UDPATE this per branch for example the 9.2 branch in logstash would
-# correspond to the '9.current' key in the logstash-versions.yml file
-LOGSTASH_STREAM=main
+# Look up corresponding LOGSTASH_STREAM from versions.yml
+LOGSTASH_STREAM=$(ruby -ryaml -e "puts YAML.load_file('versions.yml')['logstash-stream']")
+# Use logstash stream to find the corresponding stack verstion from logstash-versions.yml
 STACK_VERSION=$(ruby -ryaml -ropen-uri -e "puts YAML.load(URI.open('https://raw.githubusercontent.com/logstash-plugins/.ci/1.x/logstash-versions.yml'))['snapshots']['${LOGSTASH_STREAM}']")
 export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
 export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$STACK_VERSION}"
