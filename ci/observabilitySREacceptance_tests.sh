300124149bce8659b9aca5163135b2e6fa367e14
donoghuc <cas.donoghue@gmail.com>
2025-10-31 12:14:45 -0700

    Finalize name/location for versions.yml mapping

--- ci/observabilitySREacceptance_tests.sh
+++ ci/observabilitySREacceptance_tests.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# Look up corresponding LOGSTASH_STREAM from versions.yml
-LOGSTASH_STREAM=$(ruby -ryaml -e "puts YAML.load_file('versions.yml')['logstash-stream']")
+# Look up corresponding LOGSTASH_RELEASE_TRACK from versions.yml
+LOGSTASH_RELEASE_TRACK=$(ruby -ryaml -e "puts YAML.load_file('versions.yml')['logstash-release-track']")
 # Use logstash stream to find the corresponding stack verstion from logstash-versions.yml
-STACK_VERSION=$(ruby -ryaml -ropen-uri -e "puts YAML.load(URI.open('https://raw.githubusercontent.com/logstash-plugins/.ci/1.x/logstash-versions.yml'))['snapshots']['${LOGSTASH_STREAM}']")
+STACK_VERSION=$(ruby -ryaml -ropen-uri -e "puts YAML.load(URI.open('https://raw.githubusercontent.com/logstash-plugins/.ci/1.x/logstash-versions.yml'))['snapshots']['${LOGSTASH_RELEASE_TRACK}']")
 export OBSERVABILITY_SRE_IMAGE_VERSION="${OBSERVABILITY_SRE_IMAGE_VERSION:-$STACK_VERSION}"
 export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
 export FILEBEAT_IMAGE_VERSION="${FILEBEAT_IMAGE_VERSION:-$STACK_VERSION}"
