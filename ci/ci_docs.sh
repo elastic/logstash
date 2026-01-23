19cb9c71a29edd66ff21679add3a592612773703
donoghuc <cas.donoghue@gmail.com>
2026-01-23 11:21:01 -0800

    resolve merge conflicts

--- ci/ci_docs.sh
+++ ci/ci_docs.sh
@@ -4,19 +4,7 @@ set -e
 export JRUBY_OPTS="-J-Xmx2g"
 export GRADLE_OPTS="-Xmx2g -Dorg.gradle.daemon=false"
 
-<<<<<<< HEAD
-rake bootstrap
-# needed to workaround `group => :development`
-rake test:install-core
-rake plugin:install-default
-echo "Generate json with plugins version"
-# Since we generate the lock file and we try to resolve dependencies we will need
-# to use the bundle wrapper to correctly find the rake cli. If we don't do this we
-# will get an activation error,
-./bin/bundle exec rake generate_plugins_version
-=======
 # installCore is needed to workaround `group => :development`
 # generatePluginsVersion depends on bootstrap and installDefaultGems
 echo "Generate json with plugins version"
 ./gradlew installCore generatePluginsVersion
->>>>>>> 205d8ab78 (Consistent bundled jruby across all CI tasks (#18615))
