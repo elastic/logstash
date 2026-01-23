dfaa887c53fc0a5d0e7be78815c375ca424b925e
Cas Donoghue <cas.donoghue@gmail.com>
2026-01-23 11:07:02 -0800

    Consistent bundled jruby across all CI tasks (#18615)
    * extend patterns for using bundled jruby to all tasks
    
    * same rake
    
    * propogate BUNDLE_PATH
    
    (cherry picked from commit 205d8ab78453ef2abd4b8e9c5afacb8f010dcde5)
    
    # Conflicts:
    #	ci/ci_docs.sh
    #	ci/docker_acceptance_tests.sh

--- ci/docker_acceptance_tests.sh
+++ ci/docker_acceptance_tests.sh
@@ -16,11 +16,18 @@ if [ -n "$BUILD_JAVA_HOME" ]; then
 fi
 
 # Can run either a specific flavor, or all flavors -
+<<<<<<< HEAD
 # eg `ci/acceptance_tests.sh oss` will run tests for open source container
 #    `ci/acceptance_tests.sh full` will run tests for the default container
 #    `ci/acceptance_tests.sh ubi8` will run tests for the ubi8 based container
 #    `ci/acceptance_tests.sh wolfi` will run tests for the wolfi based container
 #    `ci/acceptance_tests.sh` will run tests for all containers
+=======
+# eg `ci/docker_acceptance_tests.sh oss` will run tests for open source container
+#    `ci/docker_acceptance_tests.sh full` will run tests for the default container
+#    `ci/docker_acceptance_tests.sh wolfi` will run tests for the wolfi based container
+#    `ci/docker_acceptance_tests.sh` will run tests for all containers
+>>>>>>> 205d8ab78 (Consistent bundled jruby across all CI tasks (#18615))
 SELECTED_TEST_SUITE=$1
 
 # The acceptance test in our CI infrastructure doesn't clear the workspace between run
@@ -30,26 +37,11 @@ SELECTED_TEST_SUITE=$1
 [ -f Gemfile ] && rm Gemfile
 [ -f Gemfile.lock ] && rm Gemfile.lock
 
-LS_HOME="$PWD"
-QA_DIR="$PWD/qa"
-
-cd $QA_DIR
-bundle check || bundle install
-
-echo "Building Logstash artifacts"
-cd $LS_HOME
-
 if [[ $SELECTED_TEST_SUITE == "oss" ]]; then
-  echo "--- Building $SELECTED_TEST_SUITE docker images"
-  cd $LS_HOME
-  ./gradlew artifactDockerOss
-  echo "--- Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "--- Acceptance: Running the tests"
-  bundle exec rspec docker/spec/oss/*_spec.rb
+  echo "--- Building and testing $SELECTED_TEST_SUITE docker images"
+  ./gradlew runDockerOssTests
 elif [[ $SELECTED_TEST_SUITE == "full" ]]; then
+<<<<<<< HEAD
   echo "--- Building $SELECTED_TEST_SUITE docker images"
   cd $LS_HOME
   ./gradlew artifactDocker
@@ -69,35 +61,17 @@ elif [[ $SELECTED_TEST_SUITE == "ubi8" ]]; then
 
   echo "--- Acceptance: Running the tests"
   bundle exec rspec docker/spec/ubi8/*_spec.rb
+=======
+  echo "--- Building and testing $SELECTED_TEST_SUITE docker images"
+  ./gradlew runDockerFullTests
+>>>>>>> 205d8ab78 (Consistent bundled jruby across all CI tasks (#18615))
 elif [[ $SELECTED_TEST_SUITE == "wolfi" ]]; then
-  echo "--- Building $SELECTED_TEST_SUITE docker images"
-  cd $LS_HOME
-  ./gradlew artifactDockerWolfi
-  echo "--- Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "--- Acceptance: Running the tests"
-  bundle exec rspec docker/spec/wolfi/*_spec.rb
+  echo "--- Building and testing $SELECTED_TEST_SUITE docker images"
+  ./gradlew runDockerWolfiTests
 elif [[ $SELECTED_TEST_SUITE == "ironbank" ]]; then
-  echo "--- Building $SELECTED_TEST_SUITE docker image with UBI base"
-  cd $LS_HOME
-  ./gradlew artifactDockerIronbank
-  echo "--- Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "--- Acceptance: Running the tests"
-  bundle exec rspec docker/spec/ironbank/*_spec.rb
+  echo "--- Building and testing $SELECTED_TEST_SUITE docker images"
+  ./gradlew runDockerIronbankTests
 else
-  echo "--- Building all docker images"
-  cd $LS_HOME
-  ./gradlew artifactDockerOnly
-
-  echo "--- Acceptance: Installing dependencies"
-  cd $QA_DIR
-  bundle install
-
-  echo "--- Acceptance: Running the tests"
-  bundle exec rspec docker/spec/**/*_spec.rb
+  echo "--- Building and testing all docker images"
+  ./gradlew runAllDockerTests
 fi
