0246722bfaeb953bf00349bbebe0c3b70d8fe012
donoghuc <cas.donoghue@gmail.com>
2026-02-04 16:25:07 -0800

    WIP: downlaod all JDK for a specific ARCH

--- buildSrc/src/main/groovy/org/logstash/gradle/tooling/ToolingUtils.groovy
+++ buildSrc/src/main/groovy/org/logstash/gradle/tooling/ToolingUtils.groovy
@@ -1,8 +1,25 @@
 package org.logstash.gradle.tooling
 
 class ToolingUtils {
+    static String jdkFolderName(String osName, String arch) {
+        String normalizedArch = (arch == "x86_64" || arch == "amd64") ? "x64" : "arm64"
+        return "jdk-${osName}-${normalizedArch}"
+    }
+
     static String jdkFolderName(String osName) {
-        return osName == "darwin" ? "jdk.app" : "jdk"
+        String envArch = System.getenv("ARCH")
+        String arch
+        if (envArch) {
+            arch = (envArch == "x86_64") ? "x64" : "arm64"
+        } else {
+            String hostArch = System.properties["os.arch"]
+            arch = (hostArch == "amd64" || hostArch == "x86_64") ? "x64" : "arm64"
+        }
+        return jdkFolderName(osName, arch)
+    }
+
+    static String jdkReleaseFilePath(String osName, String arch) {
+        jdkFolderName(osName, arch) + (osName == "darwin" ? "/Contents/Home/" : "")
     }
 
     static String jdkReleaseFilePath(String osName) {
