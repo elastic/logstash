60f6e70bc687dc4cb3e4298bf944f391b182459e
donoghuc <cas.donoghue@gmail.com>
2026-02-04 21:35:28 -0800

    fix container builds and remove dead code

--- buildSrc/src/main/groovy/org/logstash/gradle/tooling/ToolingUtils.groovy
+++ buildSrc/src/main/groovy/org/logstash/gradle/tooling/ToolingUtils.groovy
@@ -5,24 +5,4 @@ class ToolingUtils {
         String normalizedArch = (arch == "x86_64" || arch == "amd64") ? "x64" : "arm64"
         return "jdk-${osName}-${normalizedArch}"
     }
-
-    static String jdkFolderName(String osName) {
-        String envArch = System.getenv("ARCH")
-        String arch
-        if (envArch) {
-            arch = (envArch == "x86_64") ? "x64" : "arm64"
-        } else {
-            String hostArch = System.properties["os.arch"]
-            arch = (hostArch == "amd64" || hostArch == "x86_64") ? "x64" : "arm64"
-        }
-        return jdkFolderName(osName, arch)
-    }
-
-    static String jdkReleaseFilePath(String osName, String arch) {
-        jdkFolderName(osName, arch) + (osName == "darwin" ? "/Contents/Home/" : "")
-    }
-
-    static String jdkReleaseFilePath(String osName) {
-        jdkFolderName(osName) + (osName == "darwin" ? "/Contents/Home/" : "")
-    }
 }
