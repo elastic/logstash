0247874e7e49637291f18aa0e546d9992cf28769
Joao Duarte <jsvduarte@gmail.com>
2026-02-09 16:51:31 +0000

    minor experimental fix for integration tests

--- qa/integration/fixtures/plugins/generate-gems.sh
+++ qa/integration/fixtures/plugins/generate-gems.sh
@@ -4,5 +4,10 @@ SCRIPT_DIR="$( dirname "$0" )"
 PATH="$SCRIPT_DIR/../../../../vendor/jruby/bin:$PATH"
 export PATH
 
+# Use only vendored JRuby's environment; avoid RVM/rbenv GEM_PATH so RubyGems
+# does not load plugins (e.g. gem-wrappers) that require gems not in the test bundle.
+unset GEM_PATH
+unset GEM_HOME
+
 cd "$SCRIPT_DIR"
 find . -name '*.gemspec' | xargs -n1 gem build
