b05f0ddf0f27a4fdfea45341e3309dc965c181a8
Rye Biesemeyer <yaauie@users.noreply.github.com>
2026-02-10 08:38:02 -0800

    Genericise Flow Metrics' RetentionWindow (#18624)
    * Introduced common base class to group all kind of data that can be used to compute a flow metric.
    
    * Extracted flow metric's RetentionWindow in its own class
    
    * Moved selectNewerCapture to newly created DatapointCapture
    
    * Fixed method naming
    
    * Covered type cast with warn log (really unrealistic)
    
    * Added license header to source code file
    
    * Minor, added javadoc comment
    
    * Fixed bad method renaming
    
    * Tried to abstract the RetentionWindow to be just a container of subclasses od DatapointCapture.
    
    - Added type paramters with a constrant to be a subclass of DatapointCapture which is something that have a datapoint capture time
    - Peeled out baseline method into ExtendedFlowMetric, it's a wrapper on returnHead(nanos)
    
    * Minor, fixed synthax
    
    * checkpoint
    
    * refactor flow metric retention windows to be relative, provide lifetime impl
    
    * retention window cleanup
    
    * flow: make RetentionWindow responsible for calculating value
    
    * refactor for clarity, breaking lambda out into function
    
    * make RetentionWindow use own logger
    
    * Apply suggestion from @andsel
    
    Co-authored-by: Andrea Selva <selva.andre@gmail.com>
    
    * Apply suggestion from @andsel
    
    Co-authored-by: Andrea Selva <selva.andre@gmail.com>
    
    * use NodeStagingPair.Mutable correctly to fix build
    
    ---------
    
    Co-authored-by: andsel <selva.andre@gmail.com>

--- logstash-core/src/main/java/org/logstash/instrument/metrics/ExtendedFlowMetric.java
+++ logstash-core/src/main/java/org/logstash/instrument/metrics/ExtendedFlowMetric.java
@@ -23,24 +23,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.logstash.util.SetOnceReference;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.EnumSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalDouble;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.LongSupplier;
-import java.util.function.ToLongFunction;
+import java.util.function.ToLongBiFunction;
 import java.util.stream.Collectors;
-
-import static org.logstash.instrument.metrics.FlowMetricRetentionPolicy.BuiltInRetentionPolicy;
 
 /**
  * The {@link ExtendedFlowMetric} is an implementation of {@link FlowMetric} that produces
@@ -54,7 +47,7 @@ public class ExtendedFlowMetric extends BaseFlowMetric {
     private final Collection<? extends FlowMetricRetentionPolicy> retentionPolicies;
 
     // set-once atomic reference; see ExtendedFlowMetric#appendCapture(FlowCapture)
-    private final SetOnceReference<List<RetentionWindow>> retentionWindows = SetOnceReference.unset();
+    private final SetOnceReference<List<RetentionWindow<FlowCapture, Double>>> retentionWindows = SetOnceReference.unset();
 
     public ExtendedFlowMetric(final String name,
                               final Metric<? extends Number> numeratorMetric,
@@ -77,7 +70,7 @@ public class ExtendedFlowMetric extends BaseFlowMetric {
                               final String name,
                               final Metric<? extends Number> numeratorMetric,
                               final Metric<? extends Number> denominatorMetric) {
-        this(nanoTimeSupplier, name, numeratorMetric, denominatorMetric, EnumSet.allOf(BuiltInRetentionPolicy.class));
+        this(nanoTimeSupplier, name, numeratorMetric, denominatorMetric, BuiltInFlowMetricRetentionPolicies.all());
     }
 
     @Override
@@ -87,24 +80,15 @@ public class ExtendedFlowMetric extends BaseFlowMetric {
 
     @Override
     public Map<String, Double> getValue() {
+        capture();
+
         if (!lifetimeBaseline.isSet()) { return Map.of(); }
         if (!retentionWindows.isSet()) { return Map.of(); }
 
-        final Optional<FlowCapture> possibleCapture = doCapture();
-        if (possibleCapture.isEmpty()) { return Map.of(); }
-
-        final FlowCapture currentCapture = possibleCapture.get();
-
         final Map<String, Double> rates = new LinkedHashMap<>();
-
-        this.retentionWindows.get()
-                             .forEach(window -> window.baseline(currentCapture.nanoTime())
-                                                      .or(() -> windowDefaultBaseline(window))
-                                                      .map((baseline) -> calculateRate(currentCapture, baseline))
-                                                      .orElseGet(OptionalDouble::empty)
-                                                      .ifPresent((rate) -> rates.put(window.policy.policyName(), rate)));
-
-        injectLifetime(currentCapture, rates);
+        for (RetentionWindow<FlowCapture, Double> window : this.retentionWindows.get()) {
+            window.calculateValue().ifPresent(value -> rates.put(window.policy.policyName(), value));
+        }
 
         return Collections.unmodifiableMap(rates);
     }
@@ -121,36 +105,15 @@ public class ExtendedFlowMetric extends BaseFlowMetric {
         );
     }
 
-    private static List<RetentionWindow> initRetentionWindows(final Collection<? extends FlowMetricRetentionPolicy> retentionPolicies,
+    private static List<RetentionWindow<FlowCapture,Double>> initRetentionWindows(final Collection<? extends FlowMetricRetentionPolicy> retentionPolicies,
                                                               final FlowCapture capture) {
         return retentionPolicies.stream()
-                                .map((p) -> new RetentionWindow(p, capture))
+                                .map((p) -> new FlowRetentionWindow(p, capture))
                                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private static void injectIntoRetentionWindows(final List<RetentionWindow> retentionWindows, final FlowCapture capture) {
+    private static void injectIntoRetentionWindows(final List<RetentionWindow<FlowCapture, Double>> retentionWindows, final FlowCapture capture) {
         retentionWindows.forEach((rw) -> rw.append(capture));
-    }
-
-    /**
-     * Internal tooling to select the younger of two captures
-     */
-    private static FlowCapture selectNewerCapture(final FlowCapture existing, final FlowCapture proposed) {
-        if (existing == null) { return proposed; }
-        if (proposed == null) { return existing; }
-
-        return (existing.nanoTime() > proposed.nanoTime()) ? existing : proposed;
-    }
-
-    /**
-     * If a window's policy allows it to report before its retention has been reached,
-     * use our lifetime baseline as a default.
-     */
-    private Optional<FlowCapture> windowDefaultBaseline(final RetentionWindow window) {
-        if (window.policy.reportBeforeSatisfied()) {
-            return this.lifetimeBaseline.asOptional();
-        }
-        return Optional.empty();
     }
 
     /**
@@ -170,193 +133,46 @@ public class ExtendedFlowMetric extends BaseFlowMetric {
      * @param retentionWindowFunction given a policy, return a retention window, in nanos
      * @return the sum of over-retention durations.
      */
-    Duration estimateExcessRetained(final ToLongFunction<FlowMetricRetentionPolicy> retentionWindowFunction) {
+    Duration estimateExcessRetained(final ToLongBiFunction<FlowMetricRetentionPolicy, Long> retentionWindowFunction) {
         final long currentNanoTime = nanoTimeSupplier.getAsLong();
         final long cumulativeExcessRetained =
                 this.retentionWindows.orElse(Collections.emptyList())
-                                     .stream()
-                                     .map(s -> s.excessRetained(currentNanoTime, retentionWindowFunction))
-                                     .mapToLong(Long::longValue)
-                                     .sum();
+                        .stream()
+                        .map(s -> s.excessRetained(retentionWindowFunction.applyAsLong(s.policy, currentNanoTime)))
+                        .mapToLong(Long::longValue)
+                        .sum();
         return Duration.ofNanos(cumulativeExcessRetained);
     }
 
-    /**
-     * A {@link RetentionWindow} efficiently holds sufficient {@link FlowCapture}s to
-     * meet its {@link FlowMetricRetentionPolicy}, providing access to the youngest capture
-     * that is older than the policy's allowed retention (if any).
-     * The implementation is similar to a singly-linked list whose youngest captures are at
-     * the tail and oldest captures are at the head, with an additional pre-tail stage.
-     * Compaction is always done at read-time and occasionally at write-time.
-     * Both reads and writes are non-blocking and concurrency-safe.
-     */
-    private static class RetentionWindow {
-        private final AtomicReference<FlowCapture> stagedCapture = new AtomicReference<>();
-        private final AtomicReference<Node> tail;
-        private final AtomicReference<Node> head;
-        private final FlowMetricRetentionPolicy policy;
+    static class FlowRetentionWindow extends RetentionWindow<FlowCapture, Double> {
 
-        RetentionWindow(final FlowMetricRetentionPolicy policy, final FlowCapture zeroCapture) {
-            this.policy = policy;
-            final Node zeroNode = new Node(zeroCapture);
-            this.head = new AtomicReference<>(zeroNode);
-            this.tail = new AtomicReference<>(zeroNode);
-        }
-
-        /**
-         * Append the newest {@link FlowCapture} into this {@link RetentionWindow},
-         * while respecting our {@link FlowMetricRetentionPolicy}.
-         * We tolerate minor jitter in the provided {@link FlowCapture#nanoTime()}, but
-         * expect callers of this method to minimize lag between instantiating the capture
-         * and appending it.
-         *
-         * @param newestCapture the newest capture to stage
-         */
-        private void append(final FlowCapture newestCapture) {
-            final Node casTail = this.tail.getAcquire(); // for CAS
-            final long newestCaptureNanoTime = newestCapture.nanoTime();
-
-            // stage our newest capture unless it is older than the currently-staged capture
-            final FlowCapture previouslyStaged = stagedCapture.getAndAccumulate(newestCapture, ExtendedFlowMetric::selectNewerCapture);
-
-            // promote our previously-staged capture IFF our newest capture is too far
-            // ahead of the current tail to support policy's resolution.
-            if (previouslyStaged != null && Math.subtractExact(newestCaptureNanoTime, casTail.captureNanoTime()) > policy.resolutionNanos()) {
-                // attempt to set an _unlinked_ Node to our tail
-                final Node proposedNode = new Node(previouslyStaged);
-                if (this.tail.compareAndSet(casTail, proposedNode)) {
-                    // if we succeeded at setting an unlinked node, link to it from our old tail
-                    casTail.setNext(proposedNode);
-
-                    // perform a force-compaction of our head if necessary,
-                    // detected using plain memory access
-                    final Node currentHead = head.getPlain();
-                    final long headAgeNanos = Math.subtractExact(newestCaptureNanoTime, currentHead.captureNanoTime());
-                    if (LOGGER.isTraceEnabled()) {
-                        LOGGER.trace("{} post-append result (captures: `{}` span: `{}` }", this, estimateSize(currentHead), Duration.ofNanos(headAgeNanos));
-                    }
-                    if (headAgeNanos > policy.forceCompactionNanos()) {
-                        final Node compactHead = compactHead(Math.subtractExact(newestCaptureNanoTime, policy.retentionNanos()));
-                        if (LOGGER.isDebugEnabled()) {
-                            final long compactHeadAgeNanos = Math.subtractExact(newestCaptureNanoTime, compactHead.captureNanoTime());
-                            LOGGER.debug("{} forced-compaction result (captures: `{}` span: `{}`)", this, estimateSize(compactHead), Duration.ofNanos(compactHeadAgeNanos));
-                        }
-                    }
-                }
-            }
+        FlowRetentionWindow(FlowMetricRetentionPolicy policy, FlowCapture zeroCapture) {
+            super(policy, zeroCapture);
         }
 
         @Override
-        public String toString() {
-            return "RetentionWindow{" +
-                    "policy=" + policy.policyName() +
-                    " id=" + System.identityHashCode(this) +
-                    '}';
-        }
-
-        /**
-         * @param nanoTime the nanoTime of the capture for which we are retrieving a baseline.
-         * @return an {@link Optional} that contains the youngest {@link FlowCapture} that is older
-         *         than this window's {@link FlowMetricRetentionPolicy} allowed retention if one
-         *         exists, and is otherwise empty.
-         */
-        public Optional<FlowCapture> baseline(final long nanoTime) {
-            final long barrier = Math.subtractExact(nanoTime, policy.retentionNanos());
-            final Node head = compactHead(barrier);
-            if (head.captureNanoTime() <= barrier) {
-                return Optional.of(head.capture);
-            } else {
-                return Optional.empty();
+        FlowCapture mergeCaptures(FlowCapture oldCapture, FlowCapture newCapture) {
+            if (oldCapture == null) {
+                return newCapture;
             }
+            if (newCapture == null) {
+                return oldCapture;
+            }
+
+            return (oldCapture.nanoTime() > newCapture.nanoTime()) ? oldCapture : newCapture;
         }
 
-        /**
-         * @return a computationally-expensive estimate of the number of captures in this window,
-         *         using plain memory access. This should NOT be run in unguarded production code.
-         */
-        private static int estimateSize(final Node headNode) {
-            int i = 1; // assume we have one additional staged
-            // NOTE: we chase the provided headNode's tail with plain-gets,
-            //       which tolerates missed appends from other threads.
-            for (Node current = headNode; current != null; current = current.getNextPlain()) { i++; }
-            return i;
-        }
+        @Override
+        Optional<Double> calculateValue() {
+            return calculateFromBaseline((compareCapture, baselineCapture) -> {
+                if (compareCapture == null) { return Optional.empty(); }
+                if (baselineCapture == null) { return Optional.empty(); }
 
-        /**
-         * @see RetentionWindow#estimateSize(Node)
-         */
-        private int estimateSize() {
-            return estimateSize(this.head.getPlain());
-        }
+                final OptionalDouble rate = calculateRate(compareCapture, baselineCapture);
 
-        /**
-         * @param barrier a nanoTime that will NOT be crossed during compaction
-         * @return the head node after compaction up to the provided barrier.
-         */
-        private Node compactHead(final long barrier) {
-            return this.head.updateAndGet((existingHead) -> {
-                final Node proposedHead = existingHead.seekWithoutCrossing(barrier);
-                return Objects.requireNonNullElse(proposedHead, existingHead);
+                // convert from OptionalDouble to Optional<Double>
+                return rate.isPresent() ? Optional.of(rate.getAsDouble()) : Optional.empty();
             });
-        }
-
-        /**
-         * Internal testing support
-         */
-        private long excessRetained(final long currentNanoTime, final ToLongFunction<FlowMetricRetentionPolicy> retentionWindowFunction) {
-            final long barrier = Math.subtractExact(currentNanoTime, retentionWindowFunction.applyAsLong(this.policy));
-            return Math.max(0L, Math.subtractExact(barrier, this.head.getPlain().captureNanoTime()));
-        }
-
-        /**
-         * A {@link Node} holds a single {@link FlowCapture} and
-         * may link ahead to the next {@link Node}.
-         * It is an implementation detail of {@link RetentionWindow}.
-         */
-        private static class Node {
-            private static final VarHandle NEXT;
-            static {
-                try {
-                    MethodHandles.Lookup l = MethodHandles.lookup();
-                    NEXT = l.findVarHandle(Node.class, "next", Node.class);
-                } catch (ReflectiveOperationException e) {
-                    throw new ExceptionInInitializerError(e);
-                }
-            }
-
-            private final FlowCapture capture;
-            private volatile Node next;
-
-            Node(final FlowCapture capture) {
-                this.capture = capture;
-            }
-
-            Node seekWithoutCrossing(final long barrier) {
-                Node newestOlderThanThreshold = null;
-                Node candidate = this;
-
-                while(candidate != null && candidate.captureNanoTime() < barrier) {
-                    newestOlderThanThreshold = candidate;
-                    candidate = candidate.getNext();
-                }
-                return newestOlderThanThreshold;
-            }
-
-            long captureNanoTime() {
-                return this.capture.nanoTime();
-            }
-
-            void setNext(final Node nextNode) {
-                next = nextNode;
-            }
-
-            Node getNext() {
-                return next;
-            }
-
-            Node getNextPlain() {
-                return (Node)NEXT.get(this);
-            }
         }
     }
 }
