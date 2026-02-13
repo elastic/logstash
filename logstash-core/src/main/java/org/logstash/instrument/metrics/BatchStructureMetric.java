e4a3c52ad90b2ddbad45083757a6bc2bdd8612f4
andsel <selva.andre@gmail.com>
2026-02-13 17:25:31 +0100

    Updated the read side of the histpgram metric to create percentiles histograms per window

--- logstash-core/src/main/java/org/logstash/instrument/metrics/BatchStructureMetric.java
+++ logstash-core/src/main/java/org/logstash/instrument/metrics/BatchStructureMetric.java
@@ -1,18 +1,24 @@
 package org.logstash.instrument.metrics;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.HdrHistogram.Histogram;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.logstash.instrument.metrics.histogram.HistogramMetric;
 
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-public class BatchStructureMetric extends AbstractMetric<Map<String,Double>>{
+public class BatchStructureMetric extends AbstractMetric<Map<String, BatchStructureMetric.HistogramMetricData>>{
     private static final Logger LOG = LogManager.getLogger(BatchStructureMetric.class);
 
     private final HistogramMetric batchHistogram;
@@ -48,9 +54,60 @@ public class BatchStructureMetric extends AbstractMetric<Map<String,Double>>{
     }
 
     @Override
-    public Map<String, Double> getValue() {
-        // TODO collect these values from the retention window instead of hardcoding them
-        return Map.of("p75", 75.0, "p95", 95.0, "p99", 99.0);
+    public Map<String, HistogramMetricData> getValue() {
+        capture();
+
+        final Map<String, HistogramMetricData> rates = new LinkedHashMap<>();
+        histogramsWindows.forEach((retentionPolicy, retentionWindow) -> {
+            if (retentionWindow == null) {
+                return;
+            }
+
+            retentionWindow.calculateValue()
+                    .ifPresent(histogram -> rates.put(retentionPolicy.policyName(), new HistogramMetricData(histogram)));
+        });
+//        for (FlowMetricRetentionPolicy retentionPolicy : SUPPORTED_POLICIES) {
+//            HistogramRetentionWindow retentionWindow = histogramsWindows.get(retentionPolicy);
+//            if (retentionWindow != null) {
+//                retentionWindow.calculateValue()
+//                        .ifPresent(histogram -> rates.put(retentionPolicy.policyName(), new HistogramMetricData(histogram)));
+//            }
+//        }
+
+        return Collections.unmodifiableMap(rates);
+    }
+
+    /**
+     * Presentation class to render histogram data, leverage it's JSON serializability.
+     * */
+    public static class HistogramMetricData implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 4711735381843512566L;
+        private final long percentile50;
+        private final long percentile90;
+
+        public HistogramMetricData(Histogram hdrHistogram) {
+            percentile50 = hdrHistogram.getValueAtPercentile(50);
+            percentile90 = hdrHistogram.getValueAtPercentile(90);
+        }
+
+        @JsonProperty("p50")
+        public double get50Percentile() {
+            return percentile50;
+        }
+
+        @JsonProperty("p90")
+        public double get90Percentile() {
+            return percentile90;
+        }
+
+        @Override
+        public String toString() {
+            return "HistogramMetricData{" +
+                    "percentile50=" + percentile50 +
+                    ", percentile90=" + percentile90 +
+                    '}';
+        }
     }
 
     static class HistogramRetentionWindow extends RetentionWindow<HistogramCapture, Histogram> {
