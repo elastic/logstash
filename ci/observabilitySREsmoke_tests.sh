9efa2eb36136227bf50c85c2a2342f5eaa9d72a9
donoghuc <cas.donoghue@gmail.com>
2026-03-06 13:50:42 -0800

    only do docker override on exhaustive tests

--- ci/observabilitySREsmoke_tests.sh
+++ ci/observabilitySREsmoke_tests.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# TODO: Remove once the `platform-ingest-logstash-ubuntu-2204-fips` VM image is
-# republished with matching Docker CLI and daemon versions. Currently the CLI
-# speaks API v1.53 while the daemon only supports up to v1.43, causing every
-# docker/docker-compose command to fail.
-export DOCKER_API_VERSION=${DOCKER_API_VERSION:-1.43}
-
 QUALIFIED_VERSION="$(.buildkite/scripts/common/qualified-version.sh)"
 STACK_VERSION=$(./gradlew -q printStackVersion)
 export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
