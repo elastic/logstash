72c4439a2818080d1002a28f5b5c01ad74c006f0
Cas Donoghue <cas.donoghue@gmail.com>
2026-03-09 16:05:47 -0700

    Fix observabilitySRE tests (#18835)
    * Fix observabilitySRE tests
    
    This commit updates both the smoke and acceptance tests to run on the fips VM.
    Additionally it pins the docker api version to work around a bug in VM packaging
    where the CLI has a mismatch with the underlying docker api version.
    
    * build container on non-fips and test on fips
    
    * set arch for build step
    
    (cherry picked from commit 298d894b0d8fe281f19ec8256ecf47af08325a64)

--- ci/observabilitySREacceptance_tests.sh
+++ ci/observabilitySREacceptance_tests.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# TODO: Remove once the `platform-ingest-logstash-ubuntu-2204-fips` VM image is
+# republished with matching Docker CLI and daemon versions. Currently the CLI
+# speaks API v1.53 while the daemon only supports up to v1.43, causing every
+# docker/docker-compose command to fail.
+export DOCKER_API_VERSION=${DOCKER_API_VERSION:-1.43}
+
 STACK_VERSION=$(./gradlew -q printStackVersion)
 export OBSERVABILITY_SRE_IMAGE_VERSION="${OBSERVABILITY_SRE_IMAGE_VERSION:-$STACK_VERSION}"
 export ELASTICSEARCH_IMAGE_VERSION="${ELASTICSEARCH_IMAGE_VERSION:-$STACK_VERSION}"
