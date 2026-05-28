86890f5d1776ecf2aa9bc8bb176e72fc1f0ebc0f
Andrea Selva <selva.andre@gmail.com>
2026-05-28 16:28:34 +0200

    Add missed standard types during batch's size estimation (#19158)
    Fixed error during event size estimation on non recognised types.
    
    - Handle the missed `Timestamp` branch during the estimation.
    - Adds a couple of safe guards during estimation
      1.  if class is not belonging to the list of supported in Valufier (which should be a programming error) but it's serializable, use Java serialization
      2. if such class is neither serilizable log a warning error
    - Updates the BigDecimal estimation considering the inner BigInteger instance
    
    The safeguards log messages contains also the field path to the offending field.
    
    (cherry picked from commit 0b09a83ed946906f0eec407359f0b44576630ca1)
    
    # Conflicts:
    #	logstash-core/src/main/java/org/logstash/ConvertedMap.java

--- logstash-core/src/test/java/org/logstash/ConvertedMapTest.java
+++ logstash-core/src/test/java/org/logstash/ConvertedMapTest.java
@@ -20,8 +20,92 @@
 
 package org.logstash;
 
-import static org.junit.Assert.*;
+import org.apache.logging.log4j.core.test.appender.ListAppender;
+import org.apache.logging.log4j.core.test.junit.LoggerContextRule;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.NotSerializableException;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 public class ConvertedMapTest {
 
+    private static final String CONFIG = "log4j2-test1.xml";
+
+    @ClassRule
+    public static LoggerContextRule CTX = new LoggerContextRule(CONFIG);
+
+    private ListAppender appender;
+
+    @Before
+    public void setUp() {
+        appender = CTX.getListAppender("EventLogger").clear();
+    }
+    
+    private static class MySerializableClass implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 6867271638521724011L;
+
+        private final String message;
+
+        private MySerializableClass(String message) {
+            this.message = message;
+        }
+    }
+
+    private static class MyNonSerializableClass {
+
+        private final String message;
+
+        private MyNonSerializableClass(String message) {
+            this.message = message;
+        }
+    }
+
+    @Test
+    public void givenKnownClassInstancesWhenEstimateMemoryThenValueIsReturned() {
+        ConvertedMap sut = ConvertedMap.newFromMap(Map.of("name", "Pete", "surname", "Mitchell"));
+        assertThat("Estimate of know class must return without error", sut.estimateMemory(""), is(greaterThan(10L)));
+    }
+    
+    @Test
+    public void givenUnknownSerializableInstanceWhenEstimateSizeThenLogDebugAndReturnValue() {
+        ConvertedMap nested = ConvertedMap.newFromMap(Map.of("serializable", "to replace"));
+        nested.putInterned("serializable", new MySerializableClass("Mitchell"));
+        ConvertedMap sut = ConvertedMap.newFromMap(Map.of("known", "Pete", "nested", nested));
+        
+        long result = sut.estimateMemory("");
+
+        assertThat("Estimate of unknown serializable class must return without error", result, is(greaterThan(10L)));
+        assertEquals(1, appender.getMessages().size());
+        String debugMessage = appender.getMessages().getFirst();
+        assertThat("Expected log line", debugMessage, containsString("Used Java serialization to estimate ConvertedMap field <[nested][serializable]>"));
+        assertThat(debugMessage, containsString("MySerializableClass"));
+    }
+
+    @Test
+    public void givenUnknownNotSerializableInstanceWhenEstimateSizeThenThrowException() {
+        ConvertedMap nested = ConvertedMap.newFromMap(Map.of("notserializable", "to replace"));
+        nested.putInterned("notserializable", new MyNonSerializableClass("Mitchell"));
+        ConvertedMap sut = ConvertedMap.newFromMap(Map.of("known", "Pete", "nested", nested));
+
+        IllegalArgumentException e = Assert.assertThrows(
+                "Expected to throw an error on not serializable instance",
+                IllegalArgumentException.class,
+                () -> sut.estimateMemory("")
+        );
+        Throwable wrapped = e.getCause();
+        assertThat(wrapped, Matchers.instanceOf(NotSerializableException.class));
+        assertThat(e.getMessage(), containsString("Please ensure all objects passed to estimateMemory are of supported types"));
+        assertThat(e.getMessage(), containsString("on field <[nested][notserializable]>"));
+     }
 }
