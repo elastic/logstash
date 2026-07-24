d174388961fddd560668164175d63ecd91da14dd
andsel <selva.andre@gmail.com>
2026-07-24 12:44:55 +0200

    [Test] Create a test to verify correcness under concurrent access

--- logstash-core/src/test/java/org/logstash/common/BufferedTokenizerWithSizeLimitTest.java
+++ logstash-core/src/test/java/org/logstash/common/BufferedTokenizerWithSizeLimitTest.java
@@ -26,12 +26,12 @@ import org.junit.Test;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
@@ -188,5 +188,53 @@ public final class BufferedTokenizerWithSizeLimitTest {
 
     private static String generate(int length, String fillChar) {
         return fillChar.repeat(length);
+    }
+    
+    @Test
+    public void givenOneProducerAndOneConsumerWhenExecutedConcurrentlyThenNoRaceHappens() throws InterruptedException {
+        initSUTWithSizeLimit(50); // must contain the string "Token with index 1_000_000"
+        
+        AtomicInteger tokenCounter = new AtomicInteger(0);
+
+        int tokensToUse = 1_000_000;
+        Thread producer = new Thread(() -> fulfillBufferedTokenizer(sut, tokensToUse));
+        Thread consumer = new Thread(() -> consumerBufferedTokenizer(sut, tokensToUse, tokenCounter));
+        
+        producer.start();
+        consumer.start();
+        
+        producer.join(5_000);
+        consumer.join(5_000);
+        
+        // verify consumer status
+        assertEquals(tokensToUse, tokenCounter.get());
+    }
+
+    private static void consumerBufferedTokenizer(BufferedTokenizer tokenizer, int maxTokensToExpect, AtomicInteger counter) {
+        long startWait = Long.MAX_VALUE;
+        // invoke extract just to obtain the iterator
+        Iterator<String> tokens = tokenizer.extract("Pill data\n").iterator();
+        while (counter.get() < maxTokensToExpect) {
+            if (tokens.hasNext()) {
+                tokens.next();
+                counter.incrementAndGet();
+            } else {
+                if (startWait == Long.MAX_VALUE) {
+                    // start spinning
+                    startWait = System.currentTimeMillis();
+                } else {
+                    // spinning max for 5 seconds, then break the loop
+                    if (System.currentTimeMillis() - startWait > 5_000) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fulfillBufferedTokenizer(BufferedTokenizer tokenizer, int maxTokensToFill) {
+        for (int i = 0; i < maxTokensToFill; i++) {
+            tokenizer.extract("Token with index " + i + "\n");
+        }
     }
 }
