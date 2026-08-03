880eeeacc2a810ecd320bd6a33429d9b2d6f701d
donoghuc <cas.donoghue@gmail.com>
2026-08-03 14:16:46 -0700

    Revert "Avoid to run on JDK < 21 (#19311)"
    This reverts commit 1b9db53939a2e749e36befd2e0d4b8fdd69d94bf.

--- logstash-core/src/main/java/org/logstash/util/JavaVersion.java
+++ logstash-core/src/main/java/org/logstash/util/JavaVersion.java
@@ -22,7 +22,6 @@ package org.logstash.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Helper class to compare current version of JVM with a target version.
@@ -84,10 +83,5 @@ public class JavaVersion implements Comparable<JavaVersion> {
     @Override
     public int compareTo(JavaVersion other) {
         return compare(this, other);
-    }
-
-    @Override
-    public String toString() {
-        return version.stream().map(Object::toString).collect(Collectors.joining("."));
     }
 }
